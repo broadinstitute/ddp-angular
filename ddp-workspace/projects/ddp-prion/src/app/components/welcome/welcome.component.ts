2aeeb618d259d9794add906118aafb7b5774a248
cdye <39769705+charlottedye@users.noreply.github.com>
2019-12-13 13:32:42 -0500

    Initial work setting up prion frontend Initial version of main prion pages, consent survey, and beginning of medical questionnaire: DDP-4083DDP-4110, DDP-4118, DDP-4120, DDP-4091, DDP-4096, DDP-4097, DDP-4098, DDP-4099, DDP-4092, and some of DDP-4282

--- ddp-workspace/projects/ddp-prion/src/app/components/welcome/welcome.component.ts
+++ ddp-workspace/projects/ddp-prion/src/app/components/welcome/welcome.component.ts
@@ -1,40 +1,32 @@
 import { Component, OnInit } from '@angular/core';
-import {
-  AnalyticsEventActions,
-  AnalyticsEventCategories,
-  AnalyticsEventsService,
-  Auth0AdapterService,
-  BrowserContentService,
-  WindowRef
-} from 'ddp-sdk';
+import { AnalyticsEventCategories, AnalyticsEventActions, AnalyticsEventsService, Auth0AdapterService, BrowserContentService, GoogleAnalyticsEventService, WindowRef } from 'ddp-sdk';
 
 @Component({
     selector: 'welcome',
     template: `
-    <prion-header noBackground="true"></prion-header>
+    <toolkit-header noBackground="true"></toolkit-header>
     <div class="Container NoPadding">
-      <a id="topView"></a>
+        <a id="topView"></a>
         <div class="Landing-image">
-          <div class="Landing-content col-lg-6 col-md-6 col-sm-8 col-xs-12">
-            <h1 class="Title" translate>App.Welcome.WelcomeTitle</h1>
-            <p translate>App.Welcome.WelcomeText</p>
-            <div class="row">
-              <a (click)="clickJoinUs()" class="Button Button--primaryDarkYellow col-lg-4 col-lg-offset-4 col-md-6 col-md-offset-3 col-sm-8 col-sm-offset-2 col-xs-8 col-sm-offset-2" translate>App.Common.JoinUs</a>
+            <div class="Landing-content col-lg-6 col-md-6 col-sm-8 col-xs-12">
+                <h1 class="Title" translate>Toolkit.Welcome.WelcomeTitle</h1>
+                <p translate>Toolkit.Welcome.WelcomeText</p>
+                <div class="row">
+                    <a (click)="clickJoinUs()" class="Button Button--primaryDarkYellow col-lg-4 col-lg-offset-4 col-md-6 col-md-offset-3 col-sm-8 col-sm-offset-2 col-xs-8 col-sm-offset-2">Join Us</a>
+                </div>
             </div>
-          </div>
-          <div *ngIf="true" class="Intro-arrow">
-            <a (click)="scrollTo(secondView)"><img src="./assets/images/white-arrow.svg" [attr.alt]="'App.Welcome.ArrowAlt' | translate"></a>
-          </div>
+            <div *ngIf="showArrow" class="Intro-arrow">
+                <a (click)="scrollTo(secondView)"><img src="./assets/images/white-arrow.svg" alt="Arrow"></a>
+            </div>
         </div>
         
         <div class="FullWidth">
             <div class="row NoMargin">
-                <a #secondView></a>
                 <div class="col-lg-6 col-lg-offset-3 col-md-8 col-md-offset-2 col-sm-10 col-sm-offset-1 col-xs-12">
                     <div class="Message-content">
-                        <h1 class="Title Aligned--center" translate>App.Welcome.Intro</h1>
+                        <h1 class="Title Aligned--center" translate>Toolkit.Welcome.Intro</h1>
                         <div class="row Margin20">
-                            <a (click)="clickJoinUs()" class="Button Button--primaryYellow col-lg-4 col-lg-offset-4 col-md-4 col-md-offset-4 col-sm-8 col-sm-offset-1 col-xs-8 col-sm-offset-2" translate>App.Common.JoinUs</a>
+                            <a (click)="clickJoinUs()" class="Button Button--primaryYellow col-lg-4 col-lg-offset-4 col-md-4 col-md-offset-4 col-sm-8 col-sm-offset-1 col-xs-8 col-sm-offset-2" translate>Toolkit.Common.JoinUs</a>
                         </div>
                     </div>
                 </div>
@@ -42,34 +34,33 @@ import {
         </div>
 
         <div class="CenterDiv">
-            <a *ngIf="true" (click)="scrollToWithAdjust(thirdView, 100)" class="Arrow"><img src="/assets/images/yellow-arrow.svg" [attr.alt]="'App.Welcome.ArrowAlt' | translate"></a>
+            <img src="/assets/images/yellow-arrow.svg" alt="Arrow">
         </div>
     
         <div class="FullWidth">
             <div class="row NoMargin">
-              <a #thirdView></a>
                 <div class="col-lg-8 col-lg-offset-2 col-md-8 col-md-offset-2 col-sm-10 col-sm-offset-1 col-xs-12">
                     <div class="Message-content">
-                        <h1 class="Title col-lg-9 col-lg-offset-3 col-md-9 col-md-offset-3 col-sm-9 col-sm-offset-3 col-xs-12" translate>App.Welcome.Participate</h1>
+                        <h1 class="Title col-lg-9 col-lg-offset-3 col-md-9 col-md-offset-3 col-sm-9 col-sm-offset-3 col-xs-12" translate>Toolkit.Welcome.Participate</h1>
                         <div class="row Margin20">
                             <img src="/assets/images/step-1.svg" class="col-lg-3 col-md-3 col-sm-3 col-xs-3 HomeSteps" />
                             <div class="col-lg-9 col-md-9 col-sm-9 col-xs-9">
-                                <h2 class="Title Color--green" translate>App.Welcome.Steps.FirstStep.Title</h2>
-                                <p translate>App.Welcome.Steps.FirstStep.Text</p>
+                                <h2 class="Title Color--green" translate>Toolkit.Welcome.Steps.FirstStep.Title</h2>
+                                <p translate>Toolkit.Welcome.Steps.FirstStep.Text</p>
                             </div>
                         </div>
                         <div class="row Margin20">
                             <img src="/assets/images/step-2.svg" class="col-lg-3 col-md-3 col-sm-3 col-xs-3 HomeSteps" />
                             <div class="col-lg-9 col-md-9 col-sm-9 col-xs-9">
-                                <h2 class="Title Color--green" translate>App.Welcome.Steps.SecondStep.Title</h2>
-                                <p translate>App.Welcome.Steps.SecondStep.Text</p>
+                                <h2 class="Title Color--green" translate>Toolkit.Welcome.Steps.SecondStep.Title</h2>
+                                <p translate>Toolkit.Welcome.Steps.SecondStep.Text</p>
                             </div>
                         </div>
                         <div class="row Margin20">
                             <img src="/assets/images/step-3.svg" class="col-lg-3 col-md-3 col-sm-3 col-xs-3 HomeSteps" />
                             <div class="col-lg-9 col-md-9 col-sm-9 col-xs-9">
-                                <h2 class="Title Color--green" translate>App.Welcome.Steps.ThirdStep.Title</h2>
-                                <p translate>App.Welcome.Steps.ThirdStep.Text</p>
+                                <h2 class="Title Color--green" translate>Toolkit.Welcome.Steps.ThirdStep.Title</h2>
+                                <p translate>Toolkit.Welcome.Steps.ThirdStep.Text</p>
                             </div>
                         </div>
                     </div>
@@ -78,17 +69,16 @@ import {
         </div>
 
         <div class="CenterDiv">
-          <a *ngIf="true" (click)="scrollTo(fourthView)" class="Arrow"><img src="/assets/images/yellow-arrow.svg" [attr.alt]="'App.Welcome.ArrowAlt' | translate"></a>
+            <img src="/assets/images/yellow-arrow.svg" alt="Arrow">
         </div>
     
         <div class="FullWidth">
             <div class="row NoMargin">
-              <a #fourthView></a>
                 <div class="col-lg-6 col-lg-offset-3 col-md-8 col-md-offset-2 col-sm-10 col-sm-offset-1 col-xs-12">
                     <div class="Message-content">
-                        <h1 class="Title Aligned--center" translate>App.Welcome.Closing</h1>
+                        <h1 class="Title Aligned--center" translate>Toolkit.Welcome.Closing</h1>
                         <div class="row Margin20">
-                            <a (click)="clickJoinUs()" class="Button Button--primaryYellow col-lg-4 col-lg-offset-4 col-md-4 col-md-offset-4 col-sm-8 col-sm-offset-1 col-xs-8 col-sm-offset-2" translate>App.Common.JoinUs</a>
+                            <a (click)="clickJoinUs()" class="Button Button--primaryYellow col-lg-4 col-lg-offset-4 col-md-4 col-md-offset-4 col-sm-8 col-sm-offset-1 col-xs-8 col-sm-offset-2" translate>Toolkit.Common.JoinUs</a>
                         </div>
                     </div>
                 </div>
@@ -99,18 +89,19 @@ import {
 })
 export class WelcomeComponent implements OnInit {
     public unsupportedBrowser: boolean;
+    private readonly HEADER_HEIGHT: number = 70;
 
     constructor(
         private windowRef: WindowRef,
         private analytics: AnalyticsEventsService,
-        private browserContent: BrowserContentService, private auth0Adapter: Auth0AdapterService) { }
+        private browserContent: BrowserContentService) { }
 
     public ngOnInit(): void {
         this.unsupportedBrowser = this.browserContent.unsupportedBrowser();
     }
 
     public clickJoinUs(): void {
-      sessionStorage.setItem('nextUrl', 'start-study');
+      sessionStorage.setItem('nextUrl', 'account-verification');
       this.auth0Adapter.signup();
         this.doAnalytics();
         if (this.unsupportedBrowser) {
@@ -118,20 +109,16 @@ export class WelcomeComponent implements OnInit {
         }
     }
 
+    public get showArrow(): boolean {
+        return this.windowRef.nativeWindow.pageYOffset <= this.HEADER_HEIGHT;
+    }
+
     public scrollTo(target: any): void {
         this.isIE ? this.simpleScrolling(target) : this.smoothScrolling(target);
     }
 
-    public scrollToWithAdjust(target: any, offsetAdjust: number): void {
-      this.isIE ? this.simpleScrollingWithAdjust(target, offsetAdjust) : this.smoothScrollingWithAdjust(target, offsetAdjust);
-    }
-
     private simpleScrolling(target: any): void {
         this.windowRef.nativeWindow.scrollTo(0, target.offsetTop);
-    }
-
-    private simpleScrollingWithAdjust(target: any, offsetAdjust: number): void {
-      this.windowRef.nativeWindow.scrollTo(0, target.offsetTop + offsetAdjust);
     }
 
     private smoothScrolling(target: any): void {
@@ -139,13 +126,6 @@ export class WelcomeComponent implements OnInit {
             top: target.offsetTop,
             behavior: 'smooth'
         });
-    }
-
-    private smoothScrollingWithAdjust(target: any, offsetAdjust: number): void {
-      this.windowRef.nativeWindow.scrollTo({
-        top: target.offsetTop + offsetAdjust,
-        behavior: 'smooth'
-      });
     }
 
     private get isIE(): boolean {
