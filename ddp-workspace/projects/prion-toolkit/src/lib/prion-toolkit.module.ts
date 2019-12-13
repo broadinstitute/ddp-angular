3990e22fd494546b0ca775b3b3ed6954b17946d9
cdye <39769705+charlottedye@users.noreply.github.com>
2019-12-13 13:32:42 -0500

    Initial work setting up prion frontend Initial version of main prion pages, consent survey, and beginning of medical questionnaire: DDP-4083DDP-4110, DDP-4118, DDP-4120, DDP-4091, DDP-4096, DDP-4097, DDP-4098, DDP-4099, DDP-4092, and some of DDP-4282

---
+++ ddp-workspace/projects/prion-toolkit/src/lib/prion-toolkit.module.ts
@@ -0,0 +1,122 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { RouterModule } from '@angular/router';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+// Modules
+import { DdpModule } from 'ddp-sdk';
+
+// Services
+import { WorkflowMapperService } from './services/workflowMapper.service';
+import { WorkflowBuilderService } from './services/workflowBuilder.service';
+import { CommunicationService } from './services/communication.service';
+import { ToolkitConfigurationService } from './services/toolkitConfiguration.service';
+
+// Components
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { WorkflowStartActivityComponent } from './components/workflow-start-activity/workflowStartActivity.component';
+import { ActivityPageComponent } from './components/activity-page/activityPage.component';
+import { ActivityComponent } from './components/activity/activity.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { LoginLandingComponent } from './components/login-landing/loginLanding.component';
+import { ErrorComponent } from './components/error/error.component';
+import { DisclaimerComponent } from './components/dialogs/disclaimer.component';
+import { JoinMailingListComponent } from './components/dialogs/joinMailingList.component';
+import { ResendEmailComponent } from './components/dialogs/resendEmail.component';
+import { PasswordComponent } from './components/password/password.component';
+import { RedirectToLoginLandingComponent } from './components/redirect-to-login-landing/redirectToLoginLanding.component';
+import { CommonLandingComponent } from './components/common-landing/commonLanding.component';
+import { WarningComponent } from './components/dialogs/warning.component';
+import { WarningMessageComponent } from './components/warning-message/warning-message.component';
+import { AppComponent } from './components/app/app.component';
+import { ActivityLinkComponent } from './components/activity/activityLink.component';
+import { SessionExpiredComponent } from './components/session-expired/sessionExpired.component';
+import { RedirectToAuth0LoginComponent } from './components/redirect-to-auth0-login/redirectToAuth0Login.component';
+import { SessionWillExpireComponent } from './components/dialogs/sessionWillExpire.component';
+
+// Guards
+import { HeaderActionGuard } from './guards/headerAction.guard';
+
+// Angular Material
+import { MatCardModule } from '@angular/material/card';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatToolbarModule } from '@angular/material/toolbar';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    MatToolbarModule,
+    MatIconModule,
+    FlexLayoutModule,
+    MatFormFieldModule,
+    MatInputModule,
+    RouterModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatCardModule,
+    MatProgressSpinnerModule,
+    MatDialogModule,
+    DdpModule
+  ],
+  providers: [
+    CommunicationService,
+    ToolkitConfigurationService,
+    WorkflowBuilderService,
+    WorkflowMapperService,
+    HeaderActionGuard
+  ],
+  declarations: [
+    FooterComponent,
+    HeaderComponent,
+    WorkflowStartActivityComponent,
+    ActivityPageComponent,
+    ActivityComponent,
+    DashboardComponent,
+    LoginLandingComponent,
+    CommonLandingComponent,
+    RedirectToLoginLandingComponent,
+    ErrorComponent,
+    DisclaimerComponent,
+    JoinMailingListComponent,
+    ResendEmailComponent,
+    PasswordComponent,
+    WarningComponent,
+    WarningMessageComponent,
+    AppComponent,
+    ActivityLinkComponent,
+    SessionExpiredComponent,
+    RedirectToAuth0LoginComponent,
+    SessionWillExpireComponent
+  ],
+  exports: [
+    FooterComponent,
+    HeaderComponent,
+    WorkflowStartActivityComponent,
+    ActivityPageComponent,
+    ActivityComponent,
+    DashboardComponent,
+    LoginLandingComponent,
+    RedirectToLoginLandingComponent,
+    ErrorComponent,
+    PasswordComponent,
+    WarningMessageComponent,
+    AppComponent,
+    ActivityLinkComponent,
+    SessionExpiredComponent,
+    RedirectToAuth0LoginComponent
+  ],
+  entryComponents: [
+    DisclaimerComponent,
+    JoinMailingListComponent,
+    ResendEmailComponent,
+    WarningComponent,
+    SessionWillExpireComponent
+  ]
+})
+export class PrionToolkitModule { }
