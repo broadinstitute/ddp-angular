cd72ee9a11a1e67a3946385a027b98b8d97144a8
cdye <39769705+charlottedye@users.noreply.github.com>
2019-12-13 13:32:42 -0500

    Initial work setting up prion frontend Initial version of main prion pages, consent survey, and beginning of medical questionnaire: DDP-4083DDP-4110, DDP-4118, DDP-4120, DDP-4091, DDP-4096, DDP-4097, DDP-4098, DDP-4099, DDP-4092, and some of DDP-4282

---
+++ ddp-workspace/projects/prion-toolkit/src/lib/services/toolkitConfiguration.service.ts
@@ -0,0 +1,45 @@
+import { Injectable } from '@angular/core';
+
+@Injectable()
+export class ToolkitConfigurationService {
+    studyGuid: string;
+    prequalifierGuid: string;
+    consentGuid: string;
+    releaseGuid: string;
+    dashboardGuid: string;
+    thankYouGuid: string;
+    tissueConsentGuid: string;
+    tissueReleaseGuid: string;
+    bloodConsentGuid: string;
+    bloodReleaseGuid: string;
+    followupGuid: string;
+    aboutUsUrl: string;
+    prequalifierUrl: string;
+    lovedOneUrl: string;
+    internationalPatientsUrl: string;
+    consentUrl: string;
+    releaseUrl: string;
+    dashboardUrl: string;
+    activityUrl: string;
+    errorUrl: string;
+    stayInformedUrl: string;
+    thankYouUrl: string;
+    moreDetailsUrl: string;
+    tissueConsentUrl: string;
+    tissueReleaseUrl: string;
+    bloodConsentUrl: string;
+    bloodReleaseUrl: string;
+    followupUrl: string;
+    phone: string;
+    infoEmail: string;
+    dataEmail: string;
+    twitterAccountId: string;
+    facebookGroupId: string;
+    instagramId: string;
+    cBioPortalLink: string;
+    showDataRelease: boolean;
+    showInfoForPhysicians: boolean;
+    showBlog: boolean;
+    blogUrl: string;
+    enableRedesign = false;
+}
