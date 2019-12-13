71c5e01cb385a36301637872cd5fe8900efe7add
cdye <39769705+charlottedye@users.noreply.github.com>
2019-12-13 13:32:42 -0500

    Initial work setting up prion frontend Initial version of main prion pages, consent survey, and beginning of medical questionnaire: DDP-4083DDP-4110, DDP-4118, DDP-4120, DDP-4091, DDP-4096, DDP-4097, DDP-4098, DDP-4099, DDP-4092, and some of DDP-4282

---
+++ ddp-workspace/projects/prion-toolkit/src/lib/components/dialogs/sessionWillExpire.component.ts
@@ -0,0 +1,86 @@
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { MatDialogRef } from '@angular/material/dialog';
+import { SessionMementoService, Auth0AdapterService, RenewSessionNotifier } from 'ddp-sdk';
+import { interval, Subscription } from 'rxjs';
+
+@Component({
+    selector: 'ddp-session-will-expire',
+    template: `
+    <div class="Modal-title">
+        <h1 mat-dialog-title class="Modal-title">
+            <span translate>Toolkit.Dialogs.SessionWillExpire.Title</span>
+            <span>{{ timeLeft }}</span>
+        </h1>
+        <mat-icon (click)="closeDialog()" class="close">clear</mat-icon>
+    </div>
+    <mat-dialog-content>
+        <p class="Modal-text" translate>Toolkit.Dialogs.SessionWillExpire.Text</p>
+    </mat-dialog-content>
+    <mat-dialog-actions align="end" class="row NoMargin">
+        <button mat-button
+                color="primary"
+                class="ButtonFilled ButtonFilled--neutral ButtonFilled--neutral--margin Button--rect"
+                (click)="signOut()"
+                [innerHTML]="'Toolkit.Dialogs.SessionWillExpire.SignOut' | translate">
+        </button>
+        <button mat-button
+                color="primary"
+                class="ButtonFilled Button--rect"
+                (click)="renewSession()"
+                [innerHTML]="'Toolkit.Dialogs.SessionWillExpire.Continue' | translate">
+        </button>
+    </mat-dialog-actions>`
+})
+export class SessionWillExpireComponent implements OnInit, OnDestroy {
+    public timeLeft = '00:00';
+    private anchor: Subscription = new Subscription;
+
+    constructor(
+        private session: SessionMementoService,
+        private auth0: Auth0AdapterService,
+        private renewNotifier: RenewSessionNotifier,
+        private dialogRef: MatDialogRef<SessionWillExpireComponent>) { }
+
+    public ngOnInit(): void {
+        const expiresAt = this.session.expiresAt;
+        const timer = interval(1000).subscribe(() => {
+            const now = Date.now();
+            const remainingTime = expiresAt - now;
+            if (remainingTime) {
+                const totalSeconds = Math.floor(remainingTime / 1000);
+                const seconds = (totalSeconds) % 60;
+                const minutes = ((totalSeconds) - seconds) / 60;
+                const formattedMinutes = this.formatTime(minutes);
+                const formattedSeconds = this.formatTime(seconds);
+                this.timeLeft = `${formattedMinutes}:${formattedSeconds}`;
+            } else {
+                this.timeLeft = '00:00';
+            }
+        });
+        this.anchor.add(timer);
+    }
+
+    public ngOnDestroy(): void {
+        this.anchor.unsubscribe();
+    }
+
+    public signOut(): void {
+        this.renewNotifier.hideSessionExpirationNotifications();
+        this.session.disableTokenExpiredProcess();
+        this.auth0.logout();
+    }
+
+    public renewSession(): void {
+        this.renewNotifier.hideSessionExpirationNotifications();
+        this.session.disableTokenExpiredProcess();
+        this.auth0.auth0RenewToken();
+    }
+
+    public closeDialog(): void {
+        this.dialogRef.close();
+    }
+
+    private formatTime(value: number): string {
+        return value < 10 ? `0${value}` : `${value}`;
+    }
+}
