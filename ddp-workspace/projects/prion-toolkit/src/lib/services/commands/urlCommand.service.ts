5aeb1ce482e8ec5491893a821bf19e7071c5eaef
cdye <39769705+charlottedye@users.noreply.github.com>
2019-12-13 13:32:42 -0500

    Initial work setting up prion frontend Initial version of main prion pages, consent survey, and beginning of medical questionnaire: DDP-4083DDP-4110, DDP-4118, DDP-4120, DDP-4091, DDP-4096, DDP-4097, DDP-4098, DDP-4099, DDP-4092, and some of DDP-4282

---
+++ ddp-workspace/projects/prion-toolkit/src/lib/services/commands/urlCommand.service.ts
@@ -0,0 +1,15 @@
+import { Router } from '@angular/router';
+import { Observable } from 'rxjs';
+import { WorkflowCommand } from '../../models/workflowCommand';
+
+export class UrlCommand implements WorkflowCommand {
+    constructor(
+        private url: string,
+        private router: Router) { }
+
+    public execute(): Observable<void> {
+        this.router.navigateByUrl(this.url);
+        // We have navigated away so there's nothing emitted on this observable.
+        return new Observable(e => e);
+    }
+}
