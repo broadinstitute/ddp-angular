30b6a59a6cf53ed501963457bd2e8e844de780e2
Pavel Sidorenko <fat.cat.1703@gmail.com>
2019-12-18 12:17:57 +0300

    Horizontal orientation for composite questions

--- ddp-workspace/projects/ddp-sdk/src/lib/components/activityForm/activityCompositeAnswer.component.ts
+++ ddp-workspace/projects/ddp-sdk/src/lib/components/activityForm/activityCompositeAnswer.component.ts
@@ -50,13 +50,13 @@ import * as _ from 'underscore';
         }
 
         .vertical {
-            display: flex !important;
-            flex-direction: column !important;
+            display: flex;
+            flex-direction: column;
         }
 
         .horizontal {
-            display: flex !important;
-            flex-direction: row !important;
+            display: flex;
+            flex-direction: row;
         }`
     ]
 })
