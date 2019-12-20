db7bd48fa1e0a99ab55a9603e23224566b89adf6
Marco Ocana <mocana@broadinstitute.org>
2019-12-20 16:31:17 -0500

    Internationalize error messages in email validator rule

--- ddp-workspace/projects/ddp-sdk/src/lib/services/activity/validators/activityEmailValidatorRule.ts
+++ ddp-workspace/projects/ddp-sdk/src/lib/services/activity/validators/activityEmailValidatorRule.ts
@@ -1,14 +1,35 @@
 import { ActivityAbstractValidationRule } from './activityAbstractValidationRule';
 import { ActivityEmailQuestionBlock } from '../../../models/activity/activityEmailQuestionBlock';
+import { NGXTranslateService } from 'ddp-sdk';
+import { take } from 'rxjs/operators';
 
-const EMAIL_REGEXP =
-  /^(?=.{1,254}$)(?=.{1,64}@)[a-zA-Z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-zA-Z0-9!#$%&'*+/=?^_`{|}~-]+)*@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/;
+// Need to hold off on this expression for now until https://broadinstitute.atlassian.net/browse/DDP-4311 is fixed
+// const EMAIL_REGEXP =
+   // /^(?=.{1,254}$)(?=.{1,64}@)[a-zA-Z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-zA-Z0-9!#$%&'*+/=?^_`{|}~-]+)*@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/;
+
+const EMAIL_REGEXP = /^\S+@\S+\.\S+$/;
+const TRANSLATION_KEYS = ['SDK.EmailEntry.ConfirmationEmailMissingError',
+  'SDK.EmailEntry.ConfirmationEmailMismatchError',
+  'SDK.EmailEntry.EmailFormatError'];
 
 export class ActivityEmailValidatorRule extends ActivityAbstractValidationRule {
+  private emailFormatErrorMessage: string;
+  private confirmationEmailMismatchError: string;
+  private confirmationEmailMissingError: string;
+
   constructor(
-    question: ActivityEmailQuestionBlock){
+    question: ActivityEmailQuestionBlock, translate: NGXTranslateService) {
     super(question);
+    translate.getTranslation(TRANSLATION_KEYS
+    )
+      .pipe(take(1))
+      .subscribe((vals) => {
+        this.confirmationEmailMissingError = vals[TRANSLATION_KEYS[0]];
+        this.confirmationEmailMismatchError = vals[TRANSLATION_KEYS[1]];
+        this.emailFormatErrorMessage = vals[TRANSLATION_KEYS[2]];
+      })
   }
+
   recalculate(): boolean {
     this.result = null;
     const email = this.emailBlock().email;
@@ -17,18 +38,19 @@ export class ActivityEmailValidatorRule extends ActivityAbstractValidationRule {
       return true;
     } else if ((!email || !confirmation)) {
       // @todo internationalize messages
-      this.result = 'Please re-enter email address';
+      this.result = this.confirmationEmailMissingError;
       return false;
     } else if (email !== confirmation) {
-      this.result = 'Email addresses need to match';
+      this.result = this.confirmationEmailMismatchError;
       return false;
     } else if (!EMAIL_REGEXP.test(this.emailBlock().email) || !EMAIL_REGEXP.test(this.emailBlock().emailConfirmation)) {
-      this.result = 'The email addresses entered have an incorrect format';
+      this.result = this.emailFormatErrorMessage;
       return false;
     } else {
       return true;
     }
   }
+
   private emailBlock(): ActivityEmailQuestionBlock {
     return this.question as ActivityEmailQuestionBlock;
   }
