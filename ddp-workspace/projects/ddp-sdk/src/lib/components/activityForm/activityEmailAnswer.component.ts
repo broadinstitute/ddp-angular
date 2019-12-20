b165e324558da7c70b685153c372712d14806846
Marco Ocana <mocana@broadinstitute.org>
2019-12-20 16:37:48 -0500

    Fix initialization. And internationalization

--- ddp-workspace/projects/ddp-sdk/src/lib/components/activityForm/activityEmailAnswer.component.ts
+++ ddp-workspace/projects/ddp-sdk/src/lib/components/activityForm/activityEmailAnswer.component.ts
@@ -1,9 +1,10 @@
 import { Component, EventEmitter, Input, OnChanges, OnDestroy, OnInit, Output, SimpleChanges } from '@angular/core';
 import { FormControl, Validators } from '@angular/forms';
 import * as _ from 'underscore';
-import { combineLatest, Subscription } from 'rxjs';
+import { combineLatest, Observable, of, Subscription } from 'rxjs';
 import { ActivityEmailQuestionBlock } from '../../models/activity/activityEmailQuestionBlock';
 import { distinctUntilChanged, map, skip, startWith, tap } from 'rxjs/operators';
+import { NGXTranslateService } from 'ddp-sdk';
 
 @Component({
   selector: 'ddp-activity-email-answer',
@@ -25,7 +26,7 @@ import { distinctUntilChanged, map, skip, startWith, tap } from 'rxjs/operators'
                  [formControl]="formControls[1]"
                  [minlength]="block.minLength"
                  [maxlength]="block.maxLength"
-                 [placeholder]="confirmationPlaceHolder()"
+                 [placeholder]="confirmationPlaceholder | async"
                  [attr.data-ddp-test]="'answer:' + block.stableId">
       </mat-form-field>
   `,
@@ -37,9 +38,18 @@ export class ActivityEmailAnswerComponent implements OnInit, OnChanges, OnDestro
   @Input() readonly: boolean;
   @Output() valueChanged: EventEmitter<string | null> = new EventEmitter();
   public formControls: FormControl[];
+  public confirmationPlaceholder: Observable<string | null>;
   private subscription: Subscription;
 
+  constructor(private translate: NGXTranslateService) {
+  }
+
   ngOnInit(): void {
+    this.initFormControls();
+    this.initPlaceholders();
+  }
+
+  private initFormControls() {
     this.formControls = [0, 1].map(_ => new FormControl({
       value: this.block.answer,
       disabled: this.readonly
@@ -47,46 +57,48 @@ export class ActivityEmailAnswerComponent implements OnInit, OnChanges, OnDestro
     this.subscription = combineLatest(
       // need to emit some initial values; nothing emitted until all observable in combineLatest emit something
       this.formControls.map(ctrl => ctrl.valueChanges.pipe(startWith(ctrl.value as string)))).pipe(
-      // the first one is just part of the setup
-      skip(1),
-      map(([email, confirmVal]) => [email.trim(), confirmVal.trim()]),
+      tap(([email, confirmVal]) => console.debug('email: %o, confirmVal: %o', email, confirmVal)),
+      map((vals) => {
+        return vals.map(val => val ? val.trim() : null);
+      }),
       tap(([email, confirmVal]) => {
         this.block.email = email;
         this.block.emailConfirmation = confirmVal;
       }),
-      map(([email, _]) => this.block.validate() || !!email ? email : null),
+      // the first time through is just initial setup
+      skip(1),
+      map(([email, _]) => (this.block.validate() && !!email) ? email : null),
       distinctUntilChanged(),
       tap((val) => this.valueChanged.emit(val))
     ).subscribe();
   }
 
-  ngOnChanges(changes: SimpleChanges): void {
-    if (changes['block'] && !changes['block'].isFirstChange()) {
-      this.formControls.forEach(control => control.patchValue(this.block.answer, {onlySelf: true, emitEvent: false}));
-    } if (!_.isUndefined(changes['readonly']) && !changes['readonly'].isFirstChange()) {
-      this.formControls.forEach(control =>
-        this.readonly ?
-          control.disable({onlySelf: true, emitEvent: false}) :
-          control.enable({onlySelf: true, emitEvent: false}));
+  private initPlaceholders() {
+    const emailPlaceholder = this.emailPlaceHolder();
+    if (emailPlaceholder) {
+      this.confirmationPlaceholder = this.translate.getTranslation('SDK.EmailEntry.ConfirmationEmailPlaceholder',
+        { value: emailPlaceholder }) as Observable<string>;
+    } else {
+      this.confirmationPlaceholder = of(null);
     }
   }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    if (changes['block'] && !changes['block'].isFirstChange()) {
+      this.formControls.forEach(control => control.patchValue(this.block.answer, { onlySelf: true, emitEvent: false }));
+    }
+    if (!_.isUndefined(changes['readonly']) && !changes['readonly'].isFirstChange()) {
+      this.formControls.forEach(control =>
+        this.readonly ?
+          control.disable({ onlySelf: true, emitEvent: false }) :
+          control.enable({ onlySelf: true, emitEvent: false }));
+    }
+  }
+
   public emailPlaceHolder(): string | null {
     return this.placeholder ? this.placeholder : this.block.placeholder;
   }
-  public confirmationPlaceHolder(): string | null {
-    let uncapitalizedPlaceHolder;
-    if (this.emailPlaceHolder()) {
-      uncapitalizedPlaceHolder = this.emailPlaceHolder().charAt(0).toLowerCase() + this.emailPlaceHolder().slice(1);
-    } else {
-      uncapitalizedPlaceHolder = '';
-    }
-    return uncapitalizedPlaceHolder ? this.confirmationPlaceHolderPrefix() + uncapitalizedPlaceHolder : '';
-  }
 
-  public confirmationPlaceHolderPrefix(): string {
-    //todo internationalize
-    return 'Confirm ';
-  }
   ngOnDestroy(): void {
     this.subscription && this.subscription.unsubscribe();
   }
