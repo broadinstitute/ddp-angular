579c44e4a0af127b763acbce5bbda7809fa2fbaa
Marco Ocana <mocana@broadinstitute.org>
2020-01-03 12:21:49 -0500

    Modify component to ensure only valid data will be emitted. Uses form validators to show errors in fields but uses block validators to make sure data is valid.

--- ddp-workspace/projects/ddp-sdk/src/lib/components/activityForm/activityEmailInput.component.ts
+++ ddp-workspace/projects/ddp-sdk/src/lib/components/activityForm/activityEmailInput.component.ts
@@ -1,59 +1,66 @@
-import { Component, Input, Output, EventEmitter, OnChanges, SimpleChanges } from '@angular/core';
-import { FormGroup, FormControl, Validators, FormBuilder } from '@angular/forms';
+import { Component, EventEmitter, Input, OnChanges, OnDestroy, Output, SimpleChanges } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, ValidatorFn, Validators } from '@angular/forms';
 import { ActivityTextQuestionBlock } from '../../models/activity/activityTextQuestionBlock';
+import { EMAIL_REGEXP } from '../../services/activity/validators/activityEmailValidatorRule';
+import { distinctUntilChanged, filter, map, tap } from 'rxjs/operators';
+import { Subscription } from 'rxjs';
 
 @Component({
     selector: 'ddp-activity-email-input',
     template: `
-    <form [formGroup]="emailForm" class="email-input">
-        <mat-form-field>
-            <input matInput
-                   type="email"
-                   formControlName="email"
-                   class="email-input-field"
-                   [minlength]="block.minLength"
-                   [maxlength]="block.maxLength"
-                   [placeholder]="placeholder || block.placeholder"
-                   [attr.data-ddp-test]="'answer:' + block.stableId"
-                   (change)="onChange($event.target.value)">
-            <mat-error *ngIf="fieldHasError('email', 'email')" translate>SDK.EmailEntry.InvalidEmail</mat-error>
-        </mat-form-field>
-        <ng-container *ngIf="block.confirmEntry">
-            <p class="ddp-question-prompt" [ngClass]="{'ddp-required-question-prompt': this.block.isRequired}" [innerHTML]="block.confirmPrompt"></p>
+        <form [formGroup]="emailForm" class="email-input">
             <mat-form-field>
                 <input matInput
                        type="email"
-                       formControlName="confirmEmail"
-                       class="email-inpu-field"
+                       formControlName="email"
+                       class="email-input-field"
                        [minlength]="block.minLength"
                        [maxlength]="block.maxLength"
                        [placeholder]="placeholder || block.placeholder"
-                       [attr.data-ddp-test]="'answer:' + block.stableId"
-                       (change)="onChange($event.target.value)">
-                <mat-error *ngIf="fieldHasError('confirmEmail', 'email')" translate>SDK.EmailEntry.InvalidEmail</mat-error>
-                <mat-error *ngIf="fieldHasError('confirmEmail', 'mustMatch')">{{block.mismatchMessage}}</mat-error>
+                       [attr.data-ddp-test]="'answer:' + block.stableId">
+                <mat-error *ngIf="fieldHasError('email', 'pattern')" translate>SDK.EmailEntry.InvalidEmail</mat-error>
             </mat-form-field>
-        </ng-container>
-    </form>
+            <ng-container *ngIf="block.confirmEntry">
+                <p class="ddp-question-prompt"
+                   [ngClass]="{'ddp-required-question-prompt': this.block.isRequired}"
+                   [innerHTML]="block.confirmPrompt">
+                </p>
+                <mat-form-field>
+                    <input matInput
+                           type="email"
+                           formControlName="confirmEmail"
+                           class="email-input-field"
+                           [minlength]="block.minLength"
+                           [maxlength]="block.maxLength"
+                           [placeholder]="placeholder || block.placeholder"
+                           [attr.data-ddp-test]="'answer:' + block.stableId">
+                    <mat-error *ngIf="fieldHasError('confirmEmail', 'pattern')" translate>SDK.EmailEntry.InvalidEmail</mat-error>
+                    <mat-error *ngIf="fieldHasError('confirmEmail', 'mustMatch')">{{block.mismatchMessage}}</mat-error>
+                </mat-form-field>
+            </ng-container>
+        </form>
     `,
     styles: [`
         .email-input {
             display: flex;
             flex-direction: column;
         }
+
         .email-input-field {
             width: 100%;
         }
     `]
 })
-export class ActivityEmailInput implements OnChanges {
+export class ActivityEmailInput implements OnChanges, OnDestroy {
     @Input() block: ActivityTextQuestionBlock;
     @Input() readonly: boolean;
     @Input() placeholder: string;
     @Output() valueChanged: EventEmitter<string | null> = new EventEmitter();
     public emailForm: FormGroup;
+    private subscription: Subscription;
 
-    constructor(private formBuilder: FormBuilder) { }
+    constructor(private formBuilder: FormBuilder) {
+    }
 
     public ngOnChanges(changes: SimpleChanges): void {
         for (const propName in changes) {
@@ -66,63 +73,82 @@ export class ActivityEmailInput implements OnChanges {
         }
     }
 
-    public onChange(answer: string): void {
-        const allFieldsTouched = Object.values(this.emailForm.controls).every(control => control.touched);
-        if (this.emailForm.valid) {
-            this.saveAnswer(answer);
-        } else if (allFieldsTouched) {
-            this.saveAnswer(null);
-        }
-    }
-
     public fieldHasError(field: string, error: string): boolean {
-        return this.emailForm.touched && this.emailForm.controls[field].errors && this.emailForm.controls[field].errors[error];
+        return this.emailForm && this.emailForm.touched && this.emailForm.controls[field].errors
+            && this.emailForm.controls[field].errors[error];
     }
 
     private initEmailForm(): void {
+        interface EmailForm {
+            email: string;
+            confirmEmail?: string;
+        }
+        // can't use the Angular email validator as does not match Pepper back-end email validator
+        const EmailPatternValidator = Validators.pattern(EMAIL_REGEXP);
+
         if (this.block.confirmEntry) {
+
             this.emailForm = this.formBuilder.group({
                 email: new FormControl({
                     value: this.block.answer,
                     disabled: this.readonly
-                }, Validators.email),
+                }, EmailPatternValidator),
                 confirmEmail: new FormControl({
-                    value: this.block.answer,
+                    value: this.block.confirmationValue,
                     disabled: this.readonly
-                }, Validators.email)
+                }, EmailPatternValidator)
             }, {
-                validator: fieldsMatcher('email', 'confirmEmail')
+                validator: this.fieldsMatcher('email', 'confirmEmail')
             });
         } else {
             this.emailForm = new FormGroup({
                 email: new FormControl({
                     value: this.block.answer,
                     disabled: this.readonly
-                }, Validators.email)
+                }, EmailPatternValidator)
             });
         }
+
+        this.subscription = this.emailForm.valueChanges.pipe(
+            map((formData) => {
+                const cleanedFormData: EmailForm = { email: '' };
+                ['email', 'confirmEmail'].forEach(propName =>
+                    cleanedFormData[propName] = formData[propName] ? formData[propName].trim() : null
+                );
+                return cleanedFormData;
+            }),
+            tap((cleanedFormData) => {
+                this.block.setAnswer(cleanedFormData.email, false);
+                this.block.confirmEntry && (this.block.confirmationValue = cleanedFormData.confirmEmail);
+            }),
+            // we only emit data we know server will accept
+            filter((_) => this.block.canPatch()),
+            map((cleanedFormData) => !!cleanedFormData.email ? cleanedFormData.email : ''),
+            distinctUntilChanged(),
+            tap((val) => this.valueChanged.emit(val))
+        ).subscribe();
     }
 
-    private saveAnswer(answer: string | null): void {
-        this.block.answer = answer;
-        this.valueChanged.emit(answer);
+    private fieldsMatcher(controlName: string, matchingControlName: string): ValidatorFn {
+        return (formGroup: FormGroup) => {
+            const control = formGroup.controls[controlName];
+            const matchingControl = formGroup.controls[matchingControlName];
+
+            if (matchingControl.errors && !matchingControl.errors.mustMatch) {
+                // Return if another validator has already found an error on the matchingControl
+                return null;
+            }
+            // Set error on matchingControl if validation fails
+            if (control.value !== matchingControl.value) {
+                matchingControl.setErrors({ mustMatch: true });
+            } else {
+                matchingControl.setErrors(null);
+            }
+            return matchingControl.errors;
+        };
     }
-}
 
-function fieldsMatcher(controlName: string, matchingControlName: string) {
-    return (formGroup: FormGroup) => {
-        const control = formGroup.controls[controlName];
-        const matchingControl = formGroup.controls[matchingControlName];
-
-        if (matchingControl.errors && !matchingControl.errors.mustMatch) {
-            // Return if another validator has already found an error on the matchingControl
-            return;
-        }
-        // Set error on matchingControl if validation fails
-        if (control.value !== matchingControl.value) {
-            matchingControl.setErrors({ mustMatch: true });
-        } else {
-            matchingControl.setErrors(null);
-        }
+    ngOnDestroy(): void {
+        this.subscription && this.subscription.unsubscribe();
     }
 }
