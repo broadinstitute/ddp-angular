aaf12e542f420e5f350f72b8eefd64397f2cb727
Marco Ocana <mocana@broadinstitute.org>
2020-01-03 11:39:06 -0500

    Extend block to allow for holding confirmationValue if confirmEntry option is true. Needed to support validator

--- ddp-workspace/projects/ddp-sdk/src/lib/models/activity/activityTextQuestionBlock.ts
+++ ddp-workspace/projects/ddp-sdk/src/lib/models/activity/activityTextQuestionBlock.ts
@@ -11,12 +11,27 @@ export class ActivityTextQuestionBlock extends ActivityQuestionBlock<string> {
     public confirmEntry: boolean;
     public confirmPrompt: string | null = null;
     public mismatchMessage: string | null = null;
+    public confirmationValue: string | null;
 
     constructor() {
-        super();
+      super();
     }
 
     public get questionType(): QuestionType {
-        return QuestionType.Text;
+      return QuestionType.Text;
+    }
+
+    public get answer(): string | null {
+        return this._answer;
+    }
+
+    public set answer(value: string | null) {
+        this.setAnswer(value);
+    }
+
+    setAnswer(value: string | null, doValidation: boolean = true): void {
+      this._answer = value;
+      this.confirmEntry && (this.confirmationValue = value);
+      doValidation && this.validate();
     }
 }
