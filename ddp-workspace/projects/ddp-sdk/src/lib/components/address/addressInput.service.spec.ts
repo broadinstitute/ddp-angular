aa7e26893aeaa0020a761f88a94c7edd843999a7
Marco Ocana <mocana@broadinstitute.org>
2020-01-07 11:03:19 -0500

    Add test

--- ddp-workspace/projects/ddp-sdk/src/lib/components/address/addressInput.service.spec.ts
+++ ddp-workspace/projects/ddp-sdk/src/lib/components/address/addressInput.service.spec.ts
@@ -3,6 +3,8 @@ import { AddressInputService } from './addressInput.service';
 import { CountryService } from '../../services/addressCountry.service';
 import { AddressService } from '../../services/address.service';
 import { ChangeDetectorRef } from '@angular/core';
+import { of } from 'rxjs';
+import { bufferCount } from 'rxjs/operators';
 
 
 describe('AddressInputService', () => {
@@ -21,4 +23,23 @@ describe('AddressInputService', () => {
   it('should be created', () => {
     expect(ais).toBeTruthy();
   });
+  it('change in country updates countryInfo$ observable', done => {
+    expect(countryServiceSpy.findCountryInfoByCode).not.toHaveBeenCalled();
+    countryServiceSpy.findCountryInfoByCode.and.callFake((arg) => {
+      if (arg === 'CA') {
+        return of({name: 'Canada', code: 'CA', subnationalDivisionTypeName: 'blah', subnationalDivisions: [],
+          postalCodeLabel: 'Province', postalCodeRegex: 'x'});
+      } else {
+        return of(null);
+      }
+    });
+    ais.countryInfo$.pipe(bufferCount(2)).subscribe(countryArr => {
+      expect(countryArr.length).toBe(2);
+      expect(countryArr[0]).toBeNull();
+      expect(countryArr[1].name).toBe('Canada');
+      done();
+    });
+    ais.addressForm.patchValue({country : 'CA'});
+  });
+
 });
