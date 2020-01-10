7d17cde25036700b10c4132a631186a459f1164d
Marco Ocana <mocana@broadinstitute.org>
2020-01-10 09:54:23 -0500

    Add some docs

--- ddp-workspace/projects/ddp-sdk/src/lib/utility/ui/instantErrorStateMatcher.ts
+++ ddp-workspace/projects/ddp-sdk/src/lib/utility/ui/instantErrorStateMatcher.ts
@@ -1,6 +1,10 @@
 import { ErrorStateMatcher } from '@angular/material';
 import { FormControl, FormGroupDirective, NgForm } from '@angular/forms';
 
+/**
+ * Implementation of ErrorStateMatcher that allows for displaying form group errors
+ * by mat-error within a FormGroup right away/without waiting for field to be touched.
+ */
 export class InstantErrorStateMatcher implements ErrorStateMatcher {
     isErrorState(control: FormControl | null,
                  form: FormGroupDirective | NgForm | null): boolean {
