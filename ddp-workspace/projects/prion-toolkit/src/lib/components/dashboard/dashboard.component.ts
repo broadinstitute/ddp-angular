e6bb5318b2264fe24763a73ef210206f011b156e
cdye <39769705+charlottedye@users.noreply.github.com>
2020-01-22 10:55:47 -0500

    DDP-4127, set up dashboard: changed dashboard title and descriptive text and started working on adding study listing page to dashboard just below activities

--- ddp-workspace/projects/prion-toolkit/src/lib/components/dashboard/dashboard.component.ts
+++ ddp-workspace/projects/prion-toolkit/src/lib/components/dashboard/dashboard.component.ts
@@ -2,7 +2,8 @@ import { Component, Inject, OnInit, OnDestroy } from '@angular/core';
 import { Router } from '@angular/router';
 import { ToolkitConfigurationService } from './../../services/toolkitConfiguration.service';
 import { AnnouncementsServiceAgent } from 'ddp-sdk';
-import { Subscription } from 'rxjs';
+import { Observable, Subscription } from 'rxjs';
+import { TranslateService } from "@ngx-translate/core";
 
 @Component({
     selector: 'toolkit-dashboard',
@@ -14,7 +15,6 @@ import { Subscription } from 'rxjs';
             <div class="PageLayout PageLayout-dashboard">
                 <div class="row NoMargin">
                     <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
-                        <h1 translate>Toolkit.Dashboard.Title</h1>
                         <section *ngIf="announcementsText && showMessage" class="PageContent-section Dashboard-info-section">
                             <mat-icon (click)="closeMessage()" class="close">clear</mat-icon>
                             <div class="Announcements-section" [innerHTML]="announcementsText">
@@ -24,6 +24,33 @@ import { Subscription } from 'rxjs';
                             <ddp-dashboard [studyGuid]="studyGuid"
                                            (open)="navigate($event)">
                             </ddp-dashboard>
+                            <div>
+                              <mat-table #table [dataSource]="dataSource" data-ddp-test="staticActivitiesTable" class="ddp-dashboard ddp-dashboard-static">
+                                <!-- Activity Column -->
+                                <ng-container matColumnDef="name">
+                                  <mat-header-cell *matHeaderCellDef [innerHTML]="'SDK.UserActivities.ActivityName' | translate"></mat-header-cell>
+                                  <mat-cell *matCellDef="let element" class="padding-5">
+                                    <span class="dashboard-mobile-label" [innerHTML]="'SDK.UserActivities.ActivityName' | translate"></span>
+                                    <button class="dashboard-activity-button Link"
+                                            [attr.data-ddp-test]="'activityName::' + element.name"
+                                            (click)="navigate(element.url)">
+                                      {{ element.name }}
+                                    </button>
+                                  </mat-cell>
+                                </ng-container>
+                                <!-- Status Column -->
+                                <ng-container matColumnDef="summary">
+                                  <mat-header-cell *matHeaderCellDef [innerHTML]="'SDK.UserActivities.Summary' | translate">TODO</mat-header-cell>
+                                  <mat-cell *matCellDef="let element" class="padding-5"
+                                            [attr.data-ddp-test]="'activityName::' + element.status">
+                                    <span class="dashboard-mobile-label" [innerHTML]="'SDK.UserActivities.Summary' | translate"></span>
+                                    {{ element.status }}
+                                  </mat-cell>
+                                </ng-container>
+                                <mat-header-row *matHeaderRowDef="displayedColumns"></mat-header-row>
+                                <mat-row *matRowDef="let row; columns: displayedColumns;"></mat-row>
+                            </mat-table>
+                          </div>
                         </section>
                     </div>
                 </div>
@@ -37,11 +64,14 @@ export class DashboardComponent implements OnInit, OnDestroy {
     public announcementsText: string | null;
     public showMessage = true;
     private anchor: Subscription;
+    public dataSource: Observable<string>;
+    public displayedColumns = ['name', 'summary'];
 
     constructor(
         private router: Router,
         private announcements: AnnouncementsServiceAgent,
-        @Inject('toolkit.toolkitConfig') private toolkitConfiguration: ToolkitConfigurationService) {
+        @Inject('toolkit.toolkitConfig') private toolkitConfiguration: ToolkitConfigurationService,
+        public translator: TranslateService) {
         this.anchor = new Subscription();
     }
 
@@ -53,6 +83,8 @@ export class DashboardComponent implements OnInit, OnDestroy {
             }
         });
         this.anchor.add(anno);
+        this.dataSource = this.translator.get(['Toolkit.Dashboard.StudyListing.ActivityName',
+        'Toolkit.Dashboard.StudyListing.ActivityStatus', 'study-listing']);
     }
 
     public ngOnDestroy(): void {
@@ -63,7 +95,7 @@ export class DashboardComponent implements OnInit, OnDestroy {
         this.showMessage = false;
     }
 
-    public navigate(id: string): void {
-        this.router.navigate([this.toolkitConfiguration.activityUrl, id]);
+    public navigate(url: string): void {
+        this.router.navigateByUrl(url);
     }
 }
