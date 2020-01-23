79179a5b9b60d890ed3cb018d6e3da61bfeafc1b
cdye <39769705+charlottedye@users.noreply.github.com>
2020-01-23 10:25:12 -0500

    Prion: fixed errors from rebasing

--- ddp-workspace/projects/prion-toolkit/src/lib/components/dashboard/dashboard.component.ts
+++ ddp-workspace/projects/prion-toolkit/src/lib/components/dashboard/dashboard.component.ts
@@ -4,7 +4,9 @@ import { ToolkitConfigurationService } from './../../services/toolkitConfigurati
 import { AnnouncementsServiceAgent } from 'ddp-sdk';
 import { Subscription } from 'rxjs';
 import { TranslateService } from "@ngx-translate/core";
-import { StaticActivitiesDataSource } from "../../../../../toolkit/src/lib/components/dashboard/staticActivitiesDataSource";
+import { StaticActivitiesDataSource } from "./staticActivitiesDataSource";
+import { AnnouncementDashboardMessage } from "../../../../../toolkit/src/lib/models/announcementDashboardMessage";
+import { filter, map } from "rxjs/operators";
 
 @Component({
     selector: 'toolkit-dashboard',
@@ -16,11 +18,17 @@ import { StaticActivitiesDataSource } from "../../../../../toolkit/src/lib/compo
             <div class="PageLayout PageLayout-dashboard">
                 <div class="row NoMargin">
                     <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
-                        <section *ngIf="announcementsText && showMessage" class="PageContent-section Dashboard-info-section">
-                            <mat-icon (click)="closeMessage()" class="close">clear</mat-icon>
-                            <div class="Announcements-section" [innerHTML]="announcementsText">
-                            </div>
-                        </section>
+                        <ng-container *ngFor="let announcement of announcementMessages; let i = index">
+                          <ng-container *ngIf="announcement.shown">
+                            <section class="PageContent-section Dashboard-info-section">
+                              <button *ngIf="!announcement.permanent" mat-icon-button (click)="closeMessage(i)" class="close-button">
+                                <mat-icon class="close">clear</mat-icon>
+                              </button>
+                              <div class="Announcements-section" [innerHTML]="announcement.message">
+                              </div>
+                            </section>
+                          </ng-container>
+                        </ng-container>
                         <section class="PageContent-section">
                             <ddp-dashboard [studyGuid]="studyGuid"
                                            (open)="navigate($event)">
@@ -58,8 +66,7 @@ import { StaticActivitiesDataSource } from "../../../../../toolkit/src/lib/compo
 export class DashboardComponent implements OnInit, OnDestroy {
     public studyGuid: string;
     public welcomeText: string;
-    public announcementsText: string | null;
-    public showMessage = true;
+    public announcementMessages: Array<AnnouncementDashboardMessage>;
     private anchor: Subscription;
     public dataSource: StaticActivitiesDataSource;
     public displayedColumns = ['name', 'summary'];
@@ -76,21 +83,24 @@ export class DashboardComponent implements OnInit, OnDestroy {
     public ngOnInit(): void {
         this.dataSource = new StaticActivitiesDataSource(this.translator);
         this.studyGuid = this.toolkitConfiguration.studyGuid;
-        const anno = this.announcements.getMessage(this.studyGuid).subscribe(x => {
-            if (x !== null && x.length) {
-                this.announcementsText = x[0].message;
-            }
-        });
-        this.anchor.add(anno);
+      const anno = this.announcements.getMessages(this.studyGuid)
+        .pipe(
+          filter(messages => !!messages),
+          map(messages => messages.map(message => ({
+            ...message,
+            shown: true
+          })))
+        ).subscribe(messages => this.announcementMessages = messages);
+      this.anchor.add(anno);
     }
 
     public ngOnDestroy(): void {
         this.anchor.unsubscribe();
     }
 
-    public closeMessage(): void {
-        this.showMessage = false;
-    }
+  public closeMessage(index: number): void {
+    this.announcementMessages[index].shown = false;
+  }
 
 
     //TODO: Clean up this code so that it either is or is not flexible in terms of adding additional static activities
