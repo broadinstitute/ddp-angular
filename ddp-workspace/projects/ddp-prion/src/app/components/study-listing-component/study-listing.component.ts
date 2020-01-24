fac9fb8bf71a34ca5f72689183bdf2a03b3fdf2e
cdye <39769705+charlottedye@users.noreply.github.com>
2020-01-24 13:02:49 -0500

    Made all frontend text that will need to be translated come from en.json

--- ddp-workspace/projects/ddp-prion/src/app/components/study-listing-component/study-listing.component.ts
+++ ddp-workspace/projects/ddp-prion/src/app/components/study-listing-component/study-listing.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { TranslateService } from "@ngx-translate/core";
+import { LangChangeEvent, TranslateService } from "@ngx-translate/core";
 
 @Component({
   selector: 'study-listing',
@@ -13,17 +13,17 @@ import { TranslateService } from "@ngx-translate/core";
               <section class="PageContent-section">
                 <div class="row">
                   <div class="col-md-4">
-                    <input *ngIf="config.filtering" placeholder="Filter all columns"
-                           [ngTableFiltering]="config.filtering"
+                    <input *ngIf="translatedConfig.filtering" placeholder="{{'Toolkit.StudyListing.InputPlaceholder' | translate}}"
+                           [ngTableFiltering]="translatedConfig.filtering"
                            class="form-control"
-                           (tableChanged)="onChangeTable(config)"/>
+                           (tableChanged)="onChangeTable(translatedConfig)"/>
                   </div>
                 </div>
                 <br>
-                <ng-table [config]="config"
-                          (tableChanged)="onChangeTable(config)"
+                <ng-table [config]="translatedConfig"
+                          (tableChanged)="onChangeTable(translatedConfig)"
                           (cellClicked)="onCellClick($event)"
-                          [rows]="rows" [columns]="columns">
+                          [rows]="rows" [columns]="translatedColumns">
                 </ng-table>
               </section>
             </div>
@@ -37,49 +37,69 @@ import { TranslateService } from "@ngx-translate/core";
 export class StudyListingComponent implements OnInit {
 
   public rows:Array<any> = [];
-  public columns:Array<any> = [
-    {title: 'Study Name', name: 'name', filtering: {filterString: '', placeholder: 'Filter by name'}},
+  public untranslatedColumns:Array<any> = [
+    {title: 'Toolkit.StudyListing.Columns.0.title', name: 'name', filtering: {filterString: '', placeholder: 'Toolkit.StudyListing.Columns.0.placeholder'}},
     {
-      title: 'Study Description',
+      title: 'Toolkit.StudyListing.Columns.1.title',
       name: 'description',
       sort: false,
-      filtering: {filterString: '', placeholder: 'Filter by description'}
+      filtering: {filterString: '', placeholder: 'Toolkit.StudyListing.Columns.1.placeholder'}
     },
-    {title: 'Principal Investigator', name: 'nameOfPI', sort: '',filtering: {filterString: '', placeholder: 'Filter by PI'}},
-    {title: 'Study Site', name: 'studySite', sort: '', filtering: {filterString: '', placeholder: 'Filter by site'}},
-    {title: 'Eligibility Requirements', name: 'eligibilityRequirements', sort: '', filtering: {filterString: '', placeholder: 'Filter by eligibility'}},
-    {title: 'More Information', name: 'studyInfo', sort: false},
-
+    {title: 'Toolkit.StudyListing.Columns.2.title', name: 'nameOfPI', sort: '',filtering: {filterString: '', placeholder: 'Toolkit.StudyListing.Columns.2.placeholder'}},
+    {title: 'Toolkit.StudyListing.Columns.3.title', name: 'studySite', sort: '', filtering: {filterString: '', placeholder: 'Toolkit.StudyListing.Columns.3.placeholder'}},
+    {title: 'Toolkit.StudyListing.Columns.4.title', name: 'eligibilityRequirements', sort: '', filtering: {filterString: '', placeholder: 'Toolkit.StudyListing.Columns.4.placeholder'}},
+    {title: 'Toolkit.StudyListing.Columns.5.title', name: 'studyInfo', sort: false}
   ];
+  public translatedColumns: Array<any> = [
+    {title: null, name: this.untranslatedColumns[0].name, filtering: {filterString: '', placeholder: null}},
+    {title: null, name: this.untranslatedColumns[1].name, sort: false, filtering: {filterString: '', placeholder: null}},
+    {title: null, name: this.untranslatedColumns[2].name, sort: '', filtering: {filterString: '', placeholder: null}},
+    {title: null, name: this.untranslatedColumns[3].name, sort: '', filtering: {filterString: '', placeholder: null}},
+    {title: null, name: this.untranslatedColumns[4].name, sort: '', filtering: {filterString: '', placeholder: null}},
+    {title: null, name: this.untranslatedColumns[5].name, sort: false}
+  ];
+
   public page:number = 1;
   public itemsPerPage:number = 10;
   public maxSize:number = 5;
   public numPages:number = 1;
   public length:number = 0;
 
-  public config:any = {
-    sorting: {columns: this.columns},
-    filtering: {filterString: ''},
-    className: ['table-striped', 'table-bordered']
-  };
-
+  public translatedConfig: any = {sorting: null, filtering: {filterString: ''},
+    className: ['table-striped', 'table-bordered']};
   private data:Array<any>;
 
-  public constructor(translator:TranslateService) {
-    this.data = translator.instant('Toolkit.StudyListing.Rows');
+  public constructor(public translator: TranslateService) {
+    this.translateTable();
+    this.translator.onLangChange.subscribe((event: LangChangeEvent) => {
+      this.translator.use(event.lang);
+      this.translateTable();
+    });
+  }
+
+  private translateTable(){
+    this.data = this.translator.instant('Toolkit.StudyListing.Rows');
     this.length = this.data.length;
+    for (let i = 0; i < this.translatedColumns.length; i++) {
+      this.translatedColumns[i].title = this.translator.instant(this.untranslatedColumns[i].title);
+      if (i < 5) //Last column doesn't have filtering specified at all
+        this.translatedColumns[i].filtering.placeholder = this.translator.instant(this.untranslatedColumns[i].filtering.placeholder);
+    }
+    this.translatedConfig.sorting = {columns: this.translatedColumns};
+    //Reset filter string just in case
+    this.translatedConfig.filtering = {filterString: ''};
   }
 
   public ngOnInit():void {
-    this.onChangeTable(this.config);
+    this.onChangeTable(this.translatedConfig);
   }
 
-  public changeSort(data:any, config:any):any {
-    if (!config.sorting) {
+  public changeSort(data:any, translatedConfig:any):any {
+    if (!translatedConfig.sorting) {
       return data;
     }
 
-    let columns = this.config.sorting.columns || [];
+    let columns = this.translatedConfig.sorting.columns || [];
     let columnName:string = void 0;
     let sort:string = void 0;
 
@@ -105,9 +125,9 @@ export class StudyListingComponent implements OnInit {
     });
   }
 
-  public changeFilter(data:any, config:any):any {
+  public changeFilter(data:any, translatedConfig:any):any {
     let filteredData:Array<any> = data;
-    this.columns.forEach((column:any) => {
+    this.translatedColumns.forEach((column:any) => {
       if (column.filtering) {
         filteredData = filteredData.filter((item:any) => {
           return item[column.name].toLocaleLowerCase().match(column.filtering.filterString.toLocaleLowerCase());
@@ -115,21 +135,21 @@ export class StudyListingComponent implements OnInit {
       }
     });
 
-    if (!config.filtering) {
+    if (!translatedConfig.filtering) {
 
       return filteredData;
     }
 
-    if (config.filtering.columnName) {
+    if (translatedConfig.filtering.columnName) {
       return filteredData.filter((item:any) =>
-        item[config.filtering.columnName].toLocaleLowerCase().match(this.config.filtering.filterString.toLocaleLowerCase()));
+        item[translatedConfig.filtering.columnName].toLocaleLowerCase().match(this.translatedConfig.filtering.filterString.toLocaleLowerCase()));
     }
 
     let tempArray:Array<any> = [];
     filteredData.forEach((item:any) => {
       let flag = false;
-      this.columns.forEach((column:any) => {
-        if (item[column.name].toString().toLocaleLowerCase().match(this.config.filtering.filterString.toLocaleLowerCase())) {
+      this.translatedColumns.forEach((column:any) => {
+        if (item[column.name].toString().toLocaleLowerCase().match(this.translatedConfig.filtering.filterString.toLocaleLowerCase())) {
           flag = true;
         }
       });
@@ -141,17 +161,17 @@ export class StudyListingComponent implements OnInit {
     return filteredData;
   }
 
-  public onChangeTable(config:any, page:any = {page: this.page, itemsPerPage: this.itemsPerPage}):any {
-    if (config.filtering) {
-      Object.assign(this.config.filtering, config.filtering);
+  public onChangeTable(translatedConfig:any, page:any = {page: this.page, itemsPerPage: this.itemsPerPage}):any {
+    if (translatedConfig.filtering) {
+      Object.assign(this.translatedConfig.filtering, translatedConfig.filtering);
     }
 
-    if (config.sorting) {
-      Object.assign(this.config.sorting, config.sorting);
+    if (translatedConfig.sorting) {
+      Object.assign(this.translatedConfig.sorting, translatedConfig.sorting);
     }
 
-    let filteredData = this.changeFilter(this.data, this.config);
-    let sortedData = this.changeSort(filteredData, this.config);
+    let filteredData = this.changeFilter(this.data, this.translatedConfig);
+    let sortedData = this.changeSort(filteredData, this.translatedConfig);
     this.rows = sortedData;
     this.length = sortedData.length;
   }
