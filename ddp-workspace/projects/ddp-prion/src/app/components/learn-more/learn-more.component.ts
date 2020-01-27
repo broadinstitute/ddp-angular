0f23c4b421bbd5bb957ef3642288e4a46a2bfd2a
cdye <39769705+charlottedye@users.noreply.github.com>
2020-01-27 15:24:20 -0500

    Prion: DDP-4103: Learn more page: changed title, got rid of numbers, changed font and formatting of text, and fixed an issue where the header text wasn't changing color depending on the current page

--- ddp-workspace/projects/ddp-prion/src/app/components/learn-more/learn-more.component.ts
+++ ddp-workspace/projects/ddp-prion/src/app/components/learn-more/learn-more.component.ts
@@ -4,31 +4,33 @@ import { ToolkitConfigurationService } from 'toolkit';
 @Component({
     selector: 'learn-more',
     template: `
-    <toolkit-header></toolkit-header>
-        <article class="PageContent">
-            <div class="PageLayout row">
-                <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
-                    <section class="PageContent-section">
-                        <h1 class="PageContent-title" translate>Toolkit.LearnMore.LearnMore.Title</h1>
+    <toolkit-header currentRoute="/learn-more"></toolkit-header>
+    <div class="Container">
+      <article class="PageContent">
+        <div class="PageLayout row">
+          <div class="col-lg-8 col-md-10 col-sm-12 col-xs-12">
+            <section class="PageContent-section NoPadding">
+              <h1 class="PageContent-title" translate>Toolkit.LearnMore.LearnMore.Title</h1>
 
-                        <h2 class="PageContent-subtitle" translate>Toolkit.LearnMore.LearnMore.WhatIs.Question</h2>
-                        <p class="PageContent-text" translate>Toolkit.LearnMore.LearnMore.WhatIs.Text</p>
+              <h4 class="Subtitle Color--green" translate>Toolkit.LearnMore.LearnMore.WhatIs.Question</h4>
+              <p translate>Toolkit.LearnMore.LearnMore.WhatIs.Text</p>
 
-                        <h2 class="PageContent-subtitle" translate>Toolkit.LearnMore.LearnMore.For.Question</h2>
-                        <p class="PageContent-text" translate>Toolkit.LearnMore.LearnMore.For.Text</p>
+              <h4 class="Subtitle Color--green" translate>Toolkit.LearnMore.LearnMore.For.Question</h4>
+              <p translate>Toolkit.LearnMore.LearnMore.For.Text</p>
 
-                        <h2 class="PageContent-subtitle" translate>Toolkit.LearnMore.LearnMore.Participate.Question</h2>
-                        <p class="PageContent-text" translate>Toolkit.LearnMore.LearnMore.Participate.Text</p>
+              <h4 class="Subtitle Color--green" translate>Toolkit.LearnMore.LearnMore.Participate.Question</h4>
+              <p translate>Toolkit.LearnMore.LearnMore.Participate.Text</p>
 
-                        <h2 class="PageContent-subtitle" translate>Toolkit.LearnMore.LearnMore.Why.Question</h2>
-                        <p class="PageContent-text" translate>Toolkit.LearnMore.LearnMore.Why.Text</p>
+              <h4 class="Subtitle Color--green" translate>Toolkit.LearnMore.LearnMore.Why.Question</h4>
+              <p translate>Toolkit.LearnMore.LearnMore.Why.Text</p>
 
-                        <h2 class="PageContent-subtitle" translate>Toolkit.LearnMore.LearnMore.DoIHaveTo.Question</h2>
-                        <p class="PageContent-text" translate>Toolkit.LearnMore.LearnMore.DoIHaveTo.Text</p>
-                    </section>
-                </div>
-            </div>
-        </article>   
+              <h4 class="Subtitle Color--green" translate>Toolkit.LearnMore.LearnMore.DoIHaveTo.Question</h4>
+              <p translate>Toolkit.LearnMore.LearnMore.DoIHaveTo.Text</p>
+            </section>
+          </div>
+        </div>
+      </article>
+    </div>
     `
 })
 export class LearnMoreComponent implements OnInit {
