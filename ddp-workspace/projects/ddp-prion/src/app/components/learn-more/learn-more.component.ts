f1a243f2e073b9d636acbb43f99952a6ffe73f04
cdye <39769705+charlottedye@users.noreply.github.com>
2020-01-27 13:08:35 -0500

    Prion: DDP-4145: Footer: added missing links to footer, changed more details to learn more, refactored the translation file, fixed various display issues, got correct buttons to appear depending on user state, cleaned up CSS, fixed issue with FontAwesome icon, etc.

--- ddp-workspace/projects/ddp-prion/src/app/components/learn-more/learn-more.component.ts
+++ ddp-workspace/projects/ddp-prion/src/app/components/learn-more/learn-more.component.ts
@@ -1,43 +1,50 @@
 import { Component, Inject, OnInit } from '@angular/core';
-import { ToolkitConfigurationService } from "toolkit";
+import { ToolkitConfigurationService } from 'toolkit';
 
 @Component({
     selector: 'learn-more',
     template: `
-    <prion-header currentRoute="/learn-more"></prion-header>
-    <div class="Container">
-      <article class="PageContent">
-        <div class="PageLayout row">
-          <div class="col-lg-8 col-md-10 col-sm-12 col-xs-12">
-            <section class="PageContent-section NoPadding">
-              <h1 class="PageContent-title" translate>App.LearnMore.LearnMore.Title</h1>
+    <toolkit-header></toolkit-header>
+        <article class="PageContent">
+            <div class="PageLayout row">
+                <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+                    <section class="PageContent-section">
+                        <h1 class="PageContent-title" translate>Toolkit.LearnMore.LearnMore.Title</h1>
 
-              <h4 class="Subtitle Color--green" translate>App.LearnMore.LearnMore.WhatIs.Question</h4>
-              <p translate>App.LearnMore.LearnMore.WhatIs.Text</p>
+                        <h2 class="PageContent-subtitle" translate>Toolkit.LearnMore.LearnMore.WhatIs.Question</h2>
+                        <p class="PageContent-text" translate>Toolkit.LearnMore.LearnMore.WhatIs.Text</p>
 
-              <h4 class="Subtitle Color--green" translate>App.LearnMore.LearnMore.For.Question</h4>
-              <p translate>App.LearnMore.LearnMore.For.Text</p>
+                        <h2 class="PageContent-subtitle" translate>Toolkit.LearnMore.LearnMore.For.Question</h2>
+                        <p class="PageContent-text" translate>Toolkit.LearnMore.LearnMore.For.Text</p>
 
-              <h4 class="Subtitle Color--green" translate>App.LearnMore.LearnMore.Participate.Question</h4>
-              <p translate>App.LearnMore.LearnMore.Participate.Text</p>
+                        <h2 class="PageContent-subtitle" translate>Toolkit.LearnMore.LearnMore.Participate.Question</h2>
+                        <p class="PageContent-text" translate>Toolkit.LearnMore.LearnMore.Participate.Text</p>
 
-              <h4 class="Subtitle Color--green" translate>App.LearnMore.LearnMore.Why.Question</h4>
-              <p translate>App.LearnMore.LearnMore.Why.Text</p>
+                        <h2 class="PageContent-subtitle" translate>Toolkit.LearnMore.LearnMore.Why.Question</h2>
+                        <p class="PageContent-text" translate>Toolkit.LearnMore.LearnMore.Why.Text</p>
 
-              <h4 class="Subtitle Color--green" translate>App.LearnMore.LearnMore.DoIHaveTo.Question</h4>
-              <p translate>App.LearnMore.LearnMore.DoIHaveTo.Text</p>
-            </section>
-          </div>
-        </div>
-      </article>
-    </div>
+                        <h2 class="PageContent-subtitle" translate>Toolkit.LearnMore.LearnMore.DoIHaveTo.Question</h2>
+                        <p class="PageContent-text" translate>Toolkit.LearnMore.LearnMore.DoIHaveTo.Text</p>
+                    </section>
+                </div>
+            </div>
+        </article>   
     `
 })
 export class LearnMoreComponent implements OnInit {
+    public infoEmail: string;
+    public phone: string;
+    public infoEmailHref: string;
+    public phoneHref: string;
+    public countMeInUrl: string;
 
     constructor(@Inject('toolkit.toolkitConfig') private toolkitConfiguration: ToolkitConfigurationService) { }
 
     public ngOnInit(): void {
-
+        this.infoEmail = this.toolkitConfiguration.infoEmail;
+        this.infoEmailHref = `mailto:${this.toolkitConfiguration.infoEmail}`;
+        this.phone = this.toolkitConfiguration.phone;
+        this.phoneHref = `tel:${this.toolkitConfiguration.phone}`;
+        this.countMeInUrl = this.toolkitConfiguration.countMeInUrl;
     }
 }
