bd0e569e11b4a54ed60ede56863094eb22711b99
cdye <39769705+charlottedye@users.noreply.github.com>
2020-01-27 13:08:35 -0500

    Prion: DDP-4145: Footer: added missing links to footer, changed more details to learn more, refactored the translation file, fixed various display issues, got correct buttons to appear depending on user state, cleaned up CSS, fixed issue with FontAwesome icon, etc.

--- ddp-workspace/projects/prion-toolkit/src/lib/components/footer/footer.component.ts
+++ ddp-workspace/projects/prion-toolkit/src/lib/components/footer/footer.component.ts
@@ -1,28 +1,34 @@
 import { Component, Inject, OnInit } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
-import { NoopScrollStrategy } from '@angular/cdk/overlay';
 import { ToolkitConfigurationService } from './../../services/toolkitConfiguration.service';
-import { JoinMailingListComponent } from './../dialogs/joinMailingList.component';
-import { GoogleAnalyticsEventsService, GoogleAnalytics, WindowRef } from 'ddp-sdk';
+import {
+  GoogleAnalyticsEventsService,
+  GoogleAnalytics,
+  WindowRef,
+  Auth0AdapterService,
+  SessionMementoService
+} from 'ddp-sdk';
+import { Router } from "@angular/router";
 
 @Component({
     selector: 'toolkit-footer',
     template: `
-<footer class="Footer row">
-  <div class="col-lg-6 col-md-4 col-sm-4 col-xs-12 Footer-logo NoPadding">
-      <a href [routerLink]="['/home']">
-          <img src="/assets/images/project-logo-dark.svg" [attr.alt]="'Toolkit.Common.LogoAlt' | translate">
-      </a>
-  </div>
-  <div class="col-lg-6 col-md-4 col-sm-8 col-xs-12 NoPadding">
-    <p class="Footer-text Float--right">
-        <a [routerLink]="['/']" class="Footer-link" translate>Toolkit.Footer.Home</a>
-        <a [routerLink]="['/more-details']" class="Footer-link" translate>Toolkit.Footer.FAQ</a>
-        <!--TODO: Add the other links-->
-        <!--TODO: Figure out why footer arrow icon isn't showing up-->
-        <button (click)="goToTop()" class="Footer-link"><i class="fa fa-arrow-up Footer-arrow"></i><span translate>Toolkit.Footer.Top</span></button>
-    </p>
-  </div>
+      <footer class="Footer row">
+      <div class="col-lg-6 col-md-4 col-sm-4 col-xs-12 Footer-logo NoPadding">
+          <a href [routerLink]="['/home']">
+              <img lazy-resource src="/assets/images/project-logo-dark.svg" [attr.alt]="'Toolkit.Common.LogoAlt' | translate">
+          </a>
+      </div>
+      <div class="col-lg-6 col-md-4 col-sm-8 col-xs-12 NoPadding">
+        <p class="Footer-text Float--right">
+            <a [routerLink]="['/']" class="Footer-link" translate>Toolkit.Common.Home</a>
+            <span  (click)="clickLearnMore()" class="Footer-link" translate>Toolkit.Common.LearnMore</span>
+            <span *ngIf="!isLoggedIn()"  (click)="clickJoinUs()" class="Footer-link" translate>Toolkit.Common.JoinUs</span>
+            <span *ngIf="isLoggedIn()"  (click)="clickDashboard()" class="Footer-link" translate>Toolkit.Common.Dashboard</span>
+            <ddp-sign-in-out class="Footer-link"></ddp-sign-in-out>
+            <button (click)="goToTop()" class="Footer-link"><i class="fas fa-arrow-up Footer-arrow"></i><span translate>Toolkit.Footer.BackToTop</span></button>
+        </p>
+      </div>
 
               
 
@@ -31,10 +37,13 @@ import { GoogleAnalyticsEventsService, GoogleAnalytics, WindowRef } from 'ddp-sd
 })
 export class FooterComponent implements OnInit {
     constructor(
+        private router: Router,
         private dialog: MatDialog,
         private analytics: GoogleAnalyticsEventsService,
         private windowRef: WindowRef,
-        @Inject('toolkit.toolkitConfig') private toolkitConfiguration: ToolkitConfigurationService) { }
+        @Inject('toolkit.toolkitConfig') private toolkitConfiguration: ToolkitConfigurationService,
+        private auth0Adapter: Auth0AdapterService,
+        private session: SessionMementoService) { }
 
     public ngOnInit(): void {
     }
@@ -46,14 +55,25 @@ export class FooterComponent implements OnInit {
         });
     }
 
-    public openJoinDialog(): void {
-        this.dialog.open(JoinMailingListComponent, {
-            width: '740px',
-            position: { top: '30px' },
-            data: {},
-            autoFocus: false,
-            scrollStrategy: new NoopScrollStrategy()
-        });
+    public isLoggedIn(): boolean {
+      return this.session.isAuthenticatedSession();
+    }
+
+    public clickDashboard(): void {
+      if (this.isLoggedIn()){
+        this.router.navigateByUrl('dashboard');
+      }
+    }
+
+    public clickJoinUs(): void {
+      if (!this.isLoggedIn()){
+        sessionStorage.setItem('nextUrl', 'start-study');
+        this.auth0Adapter.signup();
+      }
+    }
+
+    public clickLearnMore(): void {
+      this.router.navigateByUrl('/learn-more');
     }
 
     public doAnalytics(action: string): void {
