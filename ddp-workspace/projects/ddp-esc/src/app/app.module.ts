2d993d0049dceb4c3914713453001ae5e66f7133
Pavel Sidorenko <fat.cat.1703@gmail.com>
2020-01-30 16:12:57 +0300

    Setup app module and config

--- ddp-workspace/projects/ddp-esc/src/app/app.module.ts
+++ ddp-workspace/projects/ddp-esc/src/app/app.module.ts
@@ -1,18 +1,129 @@
+import { NgModule, Injector, APP_INITIALIZER } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-
+import { LOCATION_INITIALIZED } from '@angular/common';
+import { NavigationEnd, Router } from '@angular/router';
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+
+import { TranslateService } from '@ngx-translate/core';
+
+import {
+  DdpModule,
+  LogLevel,
+  ConfigurationService,
+  GoogleAnalyticsEventsService
+} from 'ddp-sdk';
+
+import {
+  ToolkitModule,
+  ToolkitConfigurationService,
+  AppComponent
+} from 'toolkit';
+
+const baseElt = document.getElementsByTagName('base');
+
+let base = '';
+if (baseElt) {
+  base = baseElt[0].getAttribute('href');
+}
+
+declare const DDP_ENV: any;
+
+export const tkCfg = new ToolkitConfigurationService();
+tkCfg.studyGuid = DDP_ENV.studyGuid;
+tkCfg.aboutYouGuid = ''; // to be defined later
+tkCfg.consentGuid = ''; // to be defined later
+tkCfg.releaseGuid = ''; // to be defined later
+tkCfg.followupGuid = ''; // to be defined later
+tkCfg.dashboardGuid = 'DASHBOARD';
+tkCfg.aboutYouUrl = 'about-you';
+tkCfg.lovedOneUrl = 'loved-one';
+tkCfg.consentUrl = 'consent';
+tkCfg.releaseUrl = 'release-survey';
+tkCfg.followupUrl = 'followup-consent';
+tkCfg.dashboardUrl = 'dashboard';
+tkCfg.activityUrl = 'activity';
+tkCfg.errorUrl = 'error';
+tkCfg.stayInformedUrl = 'stay-informed';
+tkCfg.internationalPatientsUrl = 'international-patients';
+tkCfg.phone = '651-229-6991';
+tkCfg.infoEmail = 'info@escproject.org';
+tkCfg.twitterAccountId = 'ecscproject';
+tkCfg.facebookGroupId = 'ecscproject';
+tkCfg.countMeInUrl = 'https://joincountmein.org/';
+tkCfg.showDataRelease = false;
+tkCfg.showInfoForPhysicians = true;
+tkCfg.showBlog = false;
+
+export let config = new ConfigurationService();
+config.backendUrl = DDP_ENV.basePepperUrl;
+config.auth0Domain = DDP_ENV.auth0Domain;
+config.auth0ClientId = DDP_ENV.auth0ClientId;
+config.studyGuid = DDP_ENV.studyGuid;
+config.logLevel = LogLevel.Info;
+config.baseUrl = location.origin + base;
+config.auth0SilentRenewUrl = DDP_ENV.auth0SilentRenewUrl;
+config.loginLandingUrl = DDP_ENV.loginLandingUrl;
+config.auth0CodeRedirect = location.origin + base + 'auth';
+config.localRegistrationUrl = config.backendUrl + '/pepper/v1/register';
+config.doLocalRegistration = DDP_ENV.doLocalRegistration;
+config.mapsApiKey = DDP_ENV.mapsApiKey;
+config.auth0Audience = DDP_ENV.auth0Audience;
+config.projectGAToken = DDP_ENV.projectGAToken;
+
+export function translateFactory(translate: TranslateService, injector: Injector) {
+  return () => new Promise<any>((resolve: any) => {
+    const locationInitialized = injector.get(LOCATION_INITIALIZED, Promise.resolve(null));
+    locationInitialized.then(() => {
+      const locale = 'en';
+      translate.setDefaultLang(locale);
+      translate.use(locale).subscribe(() => {
+        console.log(`Successfully initialized '${locale}' language as default.`);
+      }, err => {
+        console.error(`Problem with '${locale}' language initialization.`);
+      }, () => {
+        resolve(null);
+      });
+    });
+  });
+}
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    DdpModule,
+    ToolkitModule
   ],
-  providers: [],
+  providers: [
+    {
+      provide: 'ddp.config',
+      useValue: config
+    },
+    {
+      provide: 'toolkit.toolkitConfig',
+      useValue: tkCfg
+    },
+    {
+      provide: APP_INITIALIZER,
+      useFactory: translateFactory,
+      deps: [
+        TranslateService,
+        Injector
+      ],
+      multi: true
+    }
+  ],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+  constructor(
+    private router: Router,
+    private analytics: GoogleAnalyticsEventsService) {
+    this.router.events.subscribe(event => {
+      if (event instanceof NavigationEnd) {
+        this.analytics.emitNavigationEvent();
+      }
+    });
+  }
+}
