86e51394c82ec2f622a9a3228a4f092fe90bd6b7
Pavel Sidorenko <fat.cat.1703@gmail.com>
2020-02-06 14:33:33 +0300

    Example row

--- ddp-workspace/projects/ddp-esc/src/app/components/about-us/about-us.component.ts
+++ ddp-workspace/projects/ddp-esc/src/app/components/about-us/about-us.component.ts
@@ -57,49 +57,21 @@ import { ToolkitConfigurationService } from 'toolkit';
           </section>
           <img lazy-resource src="./assets/images/about-page-broad-building.png" class="PageContent-image" alt="Broad Building">
         </div>
-        <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 row--moreTopMargin">
-          <section class="Message">
-            <h1 class="Message-title" translate>AboutUs.Partners</h1>
-          </section>
-        </div>
-        <div class="row row--moreTopMargin">
-          <section class="Message">
+      </div>
+      <div class="row council-row">
+        <div class="col-lg-10 col-lg-offset-1 col-md-10 col-md-offset-1 col-sm-12 col-xs-12 no-padding">
+          <section class="PageContent-section council">
+            <h1 class="PageContent-title council">Leadership Team</h1>
             <div class="row">
-              <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12 Message-partners">
-                <a href="http://www.sarctrials.org" target="_blank">
-                  <img lazy-resource src="./assets/images/SARC-logo.svg" alt="SARC Logo">
-                </a>
+              <div class="col-lg-2 col-md-3 col-sm-2 col-xs-8 col-xs-offset-2">
+                <img class="PageContent-image" alt="Adam Bass photo" src="/assets/images/adam_bass.jpg">
               </div>
-              <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12 Message-partners">
-                <a href="http://www.sarcomaalliance.org" target="_blank">
-                  <img lazy-resource src="./assets/images/SarcomaAlliance-logo.svg" alt="Sarcoma Alliance Logo">
-                </a>
+              <div class="col-lg-10 col-md-9 col-sm-10 col-xs-12">
+                <h2 class="PageContent-subtitle council">Adam Bass, MD</h2>
+                <p class="PageContent-text">Adam Bass is an Associate Professor of medicine at Harvard Medical School, a physician/scientist at the Dana-Farber Cancer Institute and Brigham and Women’s Hospital and also is an associate member of the Broad Institute. Dr. Bass obtained his undergraduate degree from Amherst College and his MD degree at Duke University School of Medicine then pursued clinical training in internal medicine at the Massachusetts General Hospital and medical oncology at the Dana-Farber/Partners Cancer Center. At the completion of his clinical training, he was a post-doctoral fellow with Dr. Matthew Meyerson at both the Dana-Farber Cancer Institute and Broad Institute</p>
               </div>
             </div>
-            <div class="row">
-              <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12 Message-partners">
-                <a href="http://www.curesarcoma.org" target="_blank">
-                  <img lazy-resource src="./assets/images/SarcomaFoundationOfAmerica-logo.svg" alt="Sarcoma Foundation of America Logo">
-                </a>
-              </div>
-              <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12 Message-partners">
-                <a href="http://www.targetcancerfoundation.org" target="_blank">
-                  <img lazy-resource src="./assets/images/TargetCancerFoundation-logo.svg" alt="Target Cancer Foundation Logo">
-                </a>
-              </div>
-            </div>
-            <div class="row">
-              <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12 Message-partners">
-                <a href="https://www.paulatakacsfoundation.org" target="_blank">
-                  <img lazy-resource src="./assets/images/PaulaTakacs-logo.svg" alt="The Paula Takacs Foundation Logo">
-                </a>
-              </div>
-              <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12 Message-partners">
-                <a href="http://www.cureasc.org" target="_blank">
-                  <img lazy-resource src="./assets/images/AngiosarcomaAwareness-logo.svg" alt="Angiosarcoma Awareness Logo">
-                </a>
-              </div>
-            </div>
+            <hr class="HorizontalLine">
           </section>
         </div>
       </div>
