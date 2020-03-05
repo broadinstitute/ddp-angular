e564d1fffd55ced763826ea40f771534259031ca
Marco Ocana <mocana@broadinstitute.org>
2020-03-05 08:20:38 -0500

    Modify sheet append to accept multiple value arguments instead of one JSON array

--- build-utils/sheetappend/index.js
+++ build-utils/sheetappend/index.js
@@ -7,28 +7,14 @@ function readCredentialsFromStdIn() {
     const keyDataString = fs.readFileSync(0, "utf-8");
     return JSON.parse(keyDataString);
 }
-function readRowValues(rawArrayString) {
-    try {
-        rowValues = JSON.parse(rawArrayString);
-    } catch(error) {
-        console.error('Unable to JSON.parse second argument with values for row: %o', error);
-        process.exit(1);
-    }
-
-    if (!Array.isArray(rowValues)) {
-        console.error('Was expecting an array as row input value');
-        process.exit(1);
-    }
-    return rowValues;
-}
-
 
 async function main() {
     const args = process.argv.slice(2);
-    if (args.length < 2) {
-        console.error('USAGE: sheetappend spreadsheet_id data_row');
-        console.log('where:\n    spreadsheet_id is the Google Sheet Id for spreadsheet to update')
-        console.log('    data_row is a JSON string (e.g. ["foo", "bar"] ) that resolves to an array that contains values to add to last row of spreadsheet')
+    if (args < 2) {
+        console.error('USAGE: sheetappend spreadsheet_id val1 [val2 val3 ...]');
+        console.log('where:\n    spreadsheet_id is the Google Sheet Id for spreadsheet to update');
+        console.log('    val1, val2 are values to be included in a row that will be appended to spreadsheet');
+        console.log('Google account credentials in JSON format are expected to be piped in via stdin');
         process.exit(1);
     }
 
@@ -42,7 +28,7 @@ async function main() {
     }
 
     const spreadsheetId = args[0];
-    let rowValues = readRowValues(args[1]);
+    let rowValues = args.slice(1);
 
     const client = auth.fromJSON(keys);
     client.scopes = ['https://www.googleapis.com/auth/spreadsheets'];
@@ -53,15 +39,15 @@ async function main() {
         "values": [rowValues]
     };
 
-    const url = `https://sheets.googleapis.com/v4/spreadsheets/${spreadsheetId}/values/Sheet1!A1:B1:append?valueInputOption=USER_ENTERED`
+    const url = `https://sheets.googleapis.com/v4/spreadsheets/${spreadsheetId}/values/Sheet1!A1:B1:append?valueInputOption=USER_ENTERED`;
     const callback = (error, response) => {
         if (error) {
             console.error('The following error was reported:\n %o', error);
             if (response) {
-                if(response.status == 404) {
+                if(response.status === 404) {
                     console.error('Could not find Google sheet with id: %s', spreadsheetId);
                 }
-                if(response.status == 403) {
+                if(response.status === 403) {
                     console.error('Access was not allowed to Google Sheet %s. Ensure permission granted to access the specify sheet to client_email specified in credentials', spreadsheetId);
                 } else {
                     console.error('The response from the server:\n %o', response);
