f2b03859882f57a3957fb01f91daba9d18b411e4
Pavel Sidorenko <fat.cat.1703@gmail.com>
2020-03-20 17:59:41 +0300

    Basic module config

--- ddp-workspace/projects/ddp-rgp/src/app/app-routing.module.ts
+++ ddp-workspace/projects/ddp-rgp/src/app/app-routing.module.ts
@@ -12,7 +12,7 @@ const routes: Routes = [
     path: '',
     component: HomeComponent,
     pathMatch: 'full',
-    //canActivate: [IrbGuard]
+    canActivate: [IrbGuard]
   },
   {
     path: '**',
--- ddp-workspace/projects/ddp-rgp/src/app/app.module.ts
+++ ddp-workspace/projects/ddp-rgp/src/app/app.module.ts
@@ -1,11 +1,63 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, Injector, APP_INITIALIZER } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
+
+import {
+  DdpModule,
+  ConfigurationService,
+  LogLevel,
+  GoogleAnalyticsEventsService
+} from 'ddp-sdk';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
 // import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
+import { Router, NavigationEnd } from '@angular/router';
+import { LOCATION_INITIALIZED } from '@angular/common';
+import { TranslateService } from '@ngx-translate/core';
+
+const baseElt = document.getElementsByTagName('base');
+
+let base = '';
+if (baseElt) {
+  base = baseElt[0].getAttribute('href');
+}
+
+declare let DDP_ENV: any;
+
+export let config = new ConfigurationService();
+config.backendUrl = DDP_ENV.basePepperUrl;
+config.auth0Domain = DDP_ENV.auth0Domain;
+config.auth0ClientId = DDP_ENV.auth0ClientId;
+config.studyGuid = DDP_ENV.studyGuid;
+config.logLevel = LogLevel.Info;
+config.baseUrl = location.origin + base;
+config.auth0SilentRenewUrl = DDP_ENV.auth0SilentRenewUrl;
+config.loginLandingUrl = DDP_ENV.loginLandingUrl;
+config.auth0CodeRedirect = location.origin + base + 'auth';
+config.localRegistrationUrl = config.backendUrl + '/pepper/v1/register';
+config.doLocalRegistration = DDP_ENV.doLocalRegistration;
+config.mapsApiKey = DDP_ENV.mapsApiKey;
+config.auth0Audience = DDP_ENV.auth0Audience;
+config.projectGAToken = DDP_ENV.projectGAToken;
+
+export function translateFactory(translate: TranslateService, injector: Injector) {
+  return () => new Promise<any>((resolve: any) => {
+    const locationInitialized = injector.get(LOCATION_INITIALIZED, Promise.resolve(null));
+    locationInitialized.then(() => {
+      const locale = 'en';
+      translate.setDefaultLang(locale);
+      translate.use(locale).subscribe(() => {
+        console.log(`Successfully initialized '${locale}' language as default.`);
+      }, err => {
+        console.error(`Problem with '${locale}' language initialization.`);
+      }, () => {
+        resolve(null);
+      });
+    });
+  });
+}
 
 @NgModule({
   declarations: [
@@ -16,9 +68,34 @@ import { FooterComponent } from './components/footer/footer.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    DdpModule
   ],
-  providers: [],
+  providers: [
+    {
+      provide: 'ddp.config',
+      useValue: config
+    },
+    {
+      provide: APP_INITIALIZER,
+      useFactory: translateFactory,
+      deps: [
+        TranslateService,
+        Injector
+      ],
+      multi: true
+    }
+  ],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+  constructor(
+    private router: Router,
+    private analytics: GoogleAnalyticsEventsService) {
+    this.router.events.subscribe(event => {
+      if (event instanceof NavigationEnd) {
+        this.analytics.emitNavigationEvent();
+      }
+    });
+  }
+}
