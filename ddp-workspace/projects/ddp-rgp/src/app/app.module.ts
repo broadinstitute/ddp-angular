996277635b0657681cb042da69eaea5ff2637b89
Pavel Sidorenko <fat.cat.1703@gmail.com>
2020-03-20 17:45:42 +0300

    Migrate footer

--- ddp-workspace/projects/ddp-rgp/src/app/app.module.ts
+++ ddp-workspace/projects/ddp-rgp/src/app/app.module.ts
@@ -5,14 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
 // import { HeaderComponent } from './components/header/header.component';
-// import { FooterComponent } from './components/footer/footer.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     // HeaderComponent,
-    // FooterComponent
+    FooterComponent
   ],
   imports: [
     BrowserModule,
