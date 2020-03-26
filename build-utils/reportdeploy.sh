e8cfc62555356ca24bb362715d0bafef6bd99783
Marco Ocana <mocana@broadinstitute.org>
2020-03-26 12:53:10 -0400

    Add some debug

--- build-utils/reportdeploy.sh
+++ build-utils/reportdeploy.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+set +x
 current_branch() {
   echo $(git branch --contains HEAD | grep -E '^\*' | grep -Eo '\b\w.*' );
 }
@@ -17,33 +18,36 @@ other_branches_same_commit() {
   grep $CURRENT_COMMIT |  #find current commit sha
   grep -Eo "$REMOTE_BRANCH_PREFIX.*" |  #keep only remote branches
   cut -c "$((${#REMOTE_BRANCH_PREFIX} + 1))-" | #remove prefixes
-  grep -Ev "^${CURRENT_BRANCH}"  #exclude the current working branch
-  EXIT_CODE=$?
-  # turns out grep if no matching lines found returns exit code 1. We don't want that for our script!
-  if [ $EXIT_CODE == 1 ] || [ $EXIT_CODE == 0 ]
-  then
-    exit 0
-  else
-    exit $EXIT_CODE
-  fi
+  grep -Ev "^(${CURRENT_BRANCH})|(HEAD)"  #exclude the current working branch
 }
+
+tags_same_commit() {
+  echo $(git tag --points-at HEAD)
+}
+
+commit_author() {
+  echo $(git show -s --format='%ae' HEAD)
+}
+if [ "$#" -lt 5 ]; then
+  echo "usage: GOOGLE_JSON_KEY | $(basename $0) RELEASE_SHEET_ID COMPONENT ENVIRONMENT CIRCLE_BUILD_NUM CIRCLE_BUILD_URL"
+  exit 1
+fi
+RELEASE_SHEET_ID=$1
+COMPONENT=$2
+ENVIRONMENT=$3
+CIRCLE_BUILD_NUM=$4
+CIRCLE_BUILD_URL=$5
+
 TZ=America/New_York
 DATE=`TZ=$TZ date +%F`
 TIME=`TZ=$TZ date +%T`
-RELEASE_SHEET_ID=$1
-COMPONENT=$2
 BRANCH=$(current_branch)
 SHORT_GIT_SHA=$(short_git_sha)
 # Picking up any tags attached to the current commit
-TAG=`git tag --points-at HEAD | xargs`
+TAG=$(tags_same_commit | xargs)
 # List of other branches that have same commit
-#OTHER_BRANCHES=$(./scripts/ci/otherbranchessamecommit.sh | xargs)
-OTHER_BRANCHER=other_branches_same_commit
+OTHER_BRANCHES=$(other_branches_same_commit | xargs)
 # Extract email from current commit
-AUTHOR=`git show -s --format='%ae' HEAD`
-for argument in $RELEASE_SHEET_ID $DATE $TIME "$COMPONENT" $ENVIRONMENT $BRANCH "$TAG" $SHORT_GIT_SHA "$AUTHOR" $CIRCLE_BUILD_NUM "$CIRCLE_BUILD_URL" "$OTHER_BRANCHES"
-do
-  echo "-${argument}-"
-done
-#vault read -format=json secret/pepper/${ENVIRONMENT}/v1/conf  | jq -r .data.gcp.serviceKey | \
-#sheetappend $RELEASE_SHEET_ID $DATE $TIME "$COMPONENT" $ENVIRONMENT $BRANCH "$TAG" $SHORT_GIT_SHA "$AUTHOR" $CIRCLE_BUILD_NUM "$CIRCLE_BUILD_URL" "$OTHER_BRANCHES"
+AUTHOR=$(commit_author)
+cat /dev/stdin |
+sheetappend $RELEASE_SHEET_ID $DATE $TIME "$COMPONENT" $ENVIRONMENT $BRANCH "$TAG" $SHORT_GIT_SHA "$AUTHOR" $CIRCLE_BUILD_NUM "$CIRCLE_BUILD_URL" "$OTHER_BRANCHES"
