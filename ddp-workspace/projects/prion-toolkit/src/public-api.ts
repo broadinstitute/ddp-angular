f8a7f009c5d3adafadc6aa2e7e56151c451b7555
cdye <39769705+charlottedye@users.noreply.github.com>
2020-04-06 13:03:50 -0400

    Prion: fixed issue caused by rebasing on latest version of develop

--- ddp-workspace/projects/prion-toolkit/src/public-api.ts
+++ ddp-workspace/projects/prion-toolkit/src/public-api.ts
@@ -16,7 +16,6 @@ export * from './lib/components/common-landing/commonLanding.component';
 export * from './lib/components/workflow-start-activity/workflowStartActivity.component';
 export * from './lib/components/dashboard/dashboard.component';
 export * from './lib/components/dialogs/disclaimer.component';
-export * from './lib/components/dialogs/joinMailingList.component';
 export * from './lib/components/dialogs/resendEmail.component';
 export * from './lib/components/dialogs/sessionWillExpire.component';
 export * from './lib/components/dialogs/warning.component';
