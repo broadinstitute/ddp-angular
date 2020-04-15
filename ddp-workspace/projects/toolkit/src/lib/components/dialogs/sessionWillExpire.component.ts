0a9ceca8c902be34893f9a37b2b5c440bc98c0df
Pavel Sidorenko <fat.cat.1703@gmail.com>
2020-04-15 21:29:24 +0300

    New renewing process

--- ddp-workspace/projects/toolkit/src/lib/components/dialogs/sessionWillExpire.component.ts
+++ ddp-workspace/projects/toolkit/src/lib/components/dialogs/sessionWillExpire.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, OnDestroy } from '@angular/core';
 import { MatDialogRef } from '@angular/material/dialog';
 import { SessionMementoService, Auth0AdapterService, RenewSessionNotifier } from 'ddp-sdk';
 import { interval, Subscription } from 'rxjs';
+import { skip } from 'rxjs/operators';
 
 @Component({
     selector: 'ddp-session-will-expire',
@@ -13,7 +14,7 @@ import { interval, Subscription } from 'rxjs';
                 <span>{{ timeLeft }}</span>
             </span>
         </h1>
-        <button mat-icon-button (click)="closeDialog()">
+        <button mat-icon-button (click)="closeDialog()" [disabled]="isRenewing">
             <mat-icon class="ddp-close-button">clear</mat-icon>
         </button>
     </div>
@@ -23,16 +24,19 @@ import { interval, Subscription } from 'rxjs';
     <mat-dialog-actions align="end" class="row NoMargin">
         <button class="ButtonFilled ButtonFilled--neutral ButtonFilled--neutral--margin Button--rect button button_small button_secondary"
                 (click)="signOut()"
+                [disabled]="isRenewing"
                 [innerHTML]="'Toolkit.Dialogs.SessionWillExpire.SignOut' | translate">
         </button>
         <button class="ButtonFilled Button--rect button button_small button_primary"
                 (click)="renewSession()"
+                [disabled]="isRenewing"
                 [innerHTML]="'Toolkit.Dialogs.SessionWillExpire.Continue' | translate">
         </button>
     </mat-dialog-actions>`
 })
 export class SessionWillExpireComponent implements OnInit, OnDestroy {
     public timeLeft = '00:00';
+    public isRenewing = false;
     private anchor: Subscription = new Subscription();
 
     constructor(
@@ -72,7 +76,24 @@ export class SessionWillExpireComponent implements OnInit, OnDestroy {
     }
 
     public renewSession(): void {
-        this.renewNotifier.hideSessionExpirationNotifications();
+        // Blocks UI to prevent interference in the session renewing process
+        this.isRenewing = true;
+        this.dialogRef.disableClose = true;
+        const participantGuid = this.session.session.participantGuid;
+        const session = this.session.sessionObservable.pipe(
+            // Because the session is BehaviorSubject, we should skip a current session,
+            // we are only interested in the renewed session
+            skip(1)
+        ).subscribe((session) => {
+            // If participantGuid is in place or we didn't have participantGuid at all, return to the app
+            if ((participantGuid && session.participantGuid) || !participantGuid) {
+                this.renewNotifier.hideSessionExpirationNotifications();
+                // If we had participantGuid, but new session lost it, restore it
+            } else if (participantGuid && !session.participantGuid) {
+                this.session.setParticipant(participantGuid);
+            }
+        });
+        this.anchor.add(session);
         this.auth0.auth0RenewToken();
     }
 
