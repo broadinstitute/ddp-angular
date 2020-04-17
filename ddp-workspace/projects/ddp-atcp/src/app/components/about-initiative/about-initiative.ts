a840ccd4f5b1ea3d4e6f443832e1f67098aadf71
Marat Mamytov <mmarat@broadinstitute.org>
2020-04-18 01:24:37 +0800

    added translate for about-initiative, statistics, removed languages choice. fixew warning scss

--- ddp-workspace/projects/ddp-atcp/src/app/components/about-initiative/about-initiative.ts
+++ ddp-workspace/projects/ddp-atcp/src/app/components/about-initiative/about-initiative.ts
@@ -1,9 +1,23 @@
 import { Component, OnInit } from '@angular/core';
+import { NGXTranslateService } from 'ddp-sdk';
 
 @Component({
   selector: 'app-about-initiative',
   templateUrl: './about-initiative.html',
   styleUrls: ['./about-initiative.scss']
 })
-export class AboutInitiativeComponent {
+export class AboutInitiativeComponent implements OnInit {
+  public AboutInitiative;
+  public steps;
+
+  constructor(private ngxTranslate: NGXTranslateService) {
+  }
+
+  public ngOnInit(): void {
+    this.ngxTranslate.getTranslation('AboutInitiative')
+      .subscribe((data: any) => {
+        this.AboutInitiative = data;
+        this.steps = this.AboutInitiative.ThirdParagraph.Steps;
+      });
+  }
 }
--- ddp-workspace/projects/ddp-atcp/src/app/components/statistics/statistics.ts
+++ ddp-workspace/projects/ddp-atcp/src/app/components/statistics/statistics.ts
@@ -1,9 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { NGXTranslateService } from 'ddp-sdk';
 
 @Component({
   selector: 'app-statistics',
   templateUrl: './statistics.html',
   styleUrls: ['./statistics.scss']
 })
-export class StatisticsComponent {
+export class StatisticsComponent implements OnInit {
+  public Statistics;
+
+  constructor(private ngxTranslate: NGXTranslateService) {
+  }
+
+  public ngOnInit(): void {
+    this.ngxTranslate.getTranslation('Statistics')
+      .subscribe((data: any) => this.Statistics = data);
+  }
 }
