31fee17d97c80f40da0b9366ff75acc3c6e06fec
Marat Mamytov <mmarat@broadinstitute.org>
2020-04-20 18:35:19 +0800

    fixed work for activity

--- ddp-workspace/projects/ddp-atcp/src/app/components/activity/activity.ts
+++ ddp-workspace/projects/ddp-atcp/src/app/components/activity/activity.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, Inject, OnInit } from '@angular/core';
+import { ToolkitConfigurationService } from 'toolkit';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: `app-activity`,
@@ -7,11 +9,24 @@ import { Component } from '@angular/core';
     <div class="activity">
       <app-header></app-header>
       <div class="page-padding content">
-        <ddp-activity-redesigned></ddp-activity-redesigned>
+        <ddp-activity-redesigned [studyGuid]="studyGuid" [activityGuid]="id"></ddp-activity-redesigned>
       </div>
       <app-footer></app-footer>
     </div>
   `
 })
-export class ActivityComponent {
+export class ActivityComponent implements OnInit {
+  public studyGuid: string;
+  public id: string;
+
+  constructor(@Inject('toolkit.toolkitConfig') private toolkitConfiguration: ToolkitConfigurationService,
+              private activatedRoute: ActivatedRoute) {
+  }
+
+  ngOnInit() {
+    this.studyGuid = this.toolkitConfiguration.studyGuid;
+    this.activatedRoute.params.subscribe(x => {
+      this.id = x.id;
+    });
+  }
 }
