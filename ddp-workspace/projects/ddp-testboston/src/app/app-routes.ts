3663c2c2dc107ea7ce1e0d980d9c262a146af7ef
Pavel Sidorenko <fat.cat.1703@gmail.com>
2020-05-07 14:19:33 +0300

    Add guids and routes as consts

---
+++ ddp-workspace/projects/ddp-testboston/src/app/app-routes.ts
@@ -0,0 +1,12 @@
+export const AppRoutes = {
+    Activity: 'activity',
+    ActivityId: 'activity/:id',
+    ActivityLinkId: 'activity-link/:id',
+    Error: 'error',
+    Prequalifier: 'prequalifier',
+    LoginLanding: 'login-landing',
+    Consent: 'consent',
+    CovidSurvey: 'covid-survey',
+    Dashboard: 'dashboard',
+    Password: 'password'
+};
