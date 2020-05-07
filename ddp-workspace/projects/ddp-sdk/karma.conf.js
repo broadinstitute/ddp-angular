ffbae8b569486d655bdce54bb40491167c0eb932
Marco Ocana <mocana@broadinstitute.org>
2020-05-07 14:45:17 -0400

    Tweak karma headless configuration so actually can run

--- ddp-workspace/projects/ddp-sdk/karma.conf.js
+++ ddp-workspace/projects/ddp-sdk/karma.conf.js
@@ -25,7 +25,13 @@ module.exports = function (config) {
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ['Chrome'],
+    browsers: ['Chrome','ChromeHeadlessNoSandbox'],
+    customLaunchers: {
+      ChromeHeadlessNoSandbox: {
+        base: 'ChromeHeadless',
+        flags: ['--no-sandbox','--headless', '--disable-gpu','--disable-dev-shm-usage']
+      }
+    },
     singleRun: false,
     restartOnFileChange: true
   });
