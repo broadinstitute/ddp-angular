ab375490cc9f6c1b0173de7923de4c9bacb97cce
MocanaAtBroad <mocana@broadinstitute.org>
2020-05-20 08:17:13 -0600

    DDP-4292: Refactor mail address component (#1)
    * Refactor mail address component
    * Add tests for it.
    * Fix all tests. They should all run now.
    * Add CircleCi test to run.
    * Allow for temp empty address to be saved/clobber old one
    * Updates to .gitgnore to prevent config files from being added to repo.
    * Tweak .gitignore to prevent generated files from being checked in
    
    Co-authored-by: Andrew Zimmer <andrew@broadinstitute.org>
    Co-authored-by: fat23cat <fat.cat.1703@gmail.com>

--- ddp-workspace/projects/ddp-sdk/karma.conf.js
+++ ddp-workspace/projects/ddp-sdk/karma.conf.js
@@ -10,6 +10,7 @@ module.exports = function (config) {
       require('karma-chrome-launcher'),
       require('karma-jasmine-html-reporter'),
       require('karma-coverage-istanbul-reporter'),
+      require('karma-junit-reporter'),
       require('@angular-devkit/build-angular/plugins/karma')
     ],
     client: {
@@ -20,12 +21,23 @@ module.exports = function (config) {
       reports: ['html', 'lcovonly'],
       fixWebpackSourcePaths: true
     },
-    reporters: ['progress', 'kjhtml'],
+    reporters: ['junit'],
+    junitReporter: {
+      outputDir: process.env.JUNIT_REPORT_PATH,
+      outputFile: process.env.JUNIT_REPORT_NAME,
+      useBrowserName: false
+    },
     port: 9876,
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ['Chrome'],
+    browsers: ['Chrome','ChromeHeadlessNoSandbox'],
+    customLaunchers: {
+      ChromeHeadlessNoSandbox: {
+        base: 'ChromeHeadless',
+        flags: ['--no-sandbox','--headless', '--disable-gpu','--disable-dev-shm-usage']
+      }
+    },
     singleRun: false,
     restartOnFileChange: true
   });
