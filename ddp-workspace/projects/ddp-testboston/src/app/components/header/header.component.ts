be1adbc5dda826070c1cbcdfee2f0226cf882f05
Pavel Sidorenko <fat.cat.1703@gmail.com>
2020-05-26 17:27:36 +0300

    DDP-4588, DDP-4587, DDP-4570: Testboston homepage, footer, header (#117)
    * Too small text
    
    * First menu impl
    
    * Fix
    
    * Update header and menu
    
    * Add responsivness
    
    * Update styles
    
    * Update styles and buttons
    
    * Trying to fix tooltip
    
    * Tooltip update
    
    * Cleanup colors
    
    * Rename method
    
    * Enable second scroll
    
    * Fix

--- ddp-workspace/projects/ddp-testboston/src/app/components/header/header.component.ts
+++ ddp-workspace/projects/ddp-testboston/src/app/components/header/header.component.ts
@@ -1,7 +1,8 @@
-import { Component, HostListener, Inject } from '@angular/core';
+import { Component, HostListener, Inject, ViewChild, ElementRef, Renderer2 } from '@angular/core';
 import { DOCUMENT } from '@angular/common';
 import { SessionMementoService, WindowRef } from 'ddp-sdk';
-import { AppRoutes } from './../../app-routes';
+import { AppRoutes } from '../../app-routes';
+import { ScrollerService } from '../../services/scroller.service';
 
 @Component({
   selector: 'app-header',
@@ -10,15 +11,46 @@ import { AppRoutes } from './../../app-routes';
 })
 export class HeaderComponent {
   public isPageScrolled = false;
+  public isMenuOpened = false;
   public appRoutes = AppRoutes;
+  @ViewChild('overlay', { static: false }) private overlay: ElementRef;
+  @ViewChild('menu', { static: false }) private menu: ElementRef;
 
   constructor(
+    private renderer: Renderer2,
     private session: SessionMementoService,
     private window: WindowRef,
+    private scrollerService: ScrollerService,
     @Inject(DOCUMENT) private document: Document) { }
 
   public get isAuthenticated(): boolean {
     return this.session.isAuthenticatedSession();
+  }
+
+  public openMenu(): void {
+    this.isMenuOpened = true;
+    this.window.nativeWindow.requestAnimationFrame(() => {
+      this.renderer.addClass(this.menu.nativeElement, 'menu_visible');
+    });
+    this.window.nativeWindow.requestAnimationFrame(() => {
+      this.renderer.addClass(this.overlay.nativeElement, 'overlay_visible');
+    });
+  }
+
+  public closeMenu(): void {
+    if (this.isMenuOpened) {
+      this.isMenuOpened = false;
+      this.window.nativeWindow.requestAnimationFrame(() => {
+        this.renderer.removeClass(this.overlay.nativeElement, 'overlay_visible');
+      });
+      this.window.nativeWindow.requestAnimationFrame(() => {
+        this.renderer.removeClass(this.menu.nativeElement, 'menu_visible');
+      });
+    }
+  }
+
+  public scrollToAnchor(anchor: string): void {
+    this.scrollerService.scrollToAnchor(anchor);
   }
 
   @HostListener('window: scroll') public onWindowScroll(): void {
@@ -26,5 +58,9 @@ export class HeaderComponent {
       || this.document.documentElement.scrollTop
       || this.document.body.scrollTop || 0;
     this.isPageScrolled = !!scrolledPixels;
+  }
+
+  @HostListener('window: resize') public onWindowResize(): void {
+    this.closeMenu();
   }
 }
