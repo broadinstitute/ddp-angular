92293ac1f84cdbe0a2867fbe0df5a22dd0f0587d
Yufeng Wang <yufeng@broadinstitute.org>
2020-05-29 19:28:05 -0400

    DDP-4600 improve date validations

--- ddp-workspace/projects/ddp-sdk/src/lib/services/activity/validators/dateValidators/activityYearRequiredDateValidationRule.ts
+++ ddp-workspace/projects/ddp-sdk/src/lib/services/activity/validators/dateValidators/activityYearRequiredDateValidationRule.ts
@@ -11,12 +11,16 @@ export class ActivityYearRequiredDateValidationRule extends ActivityAbstractVali
     public recalculate(): boolean {
         if (this.question.answer != null) {
             const value = this.question.answer;
-            if (value.year == null) {
+            if (!this.isBlank(value) && value.year == null) {
                 this.result = this.message;
                 return false;
             }
         }
         this.result = null;
         return true;
+    }
+
+    private isBlank(value: DatePickerValue): boolean {
+        return value.year == null && value.month == null && value.day == null;
     }
 }
