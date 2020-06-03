d71b8f400dd6b863993c2168cf73bf7bd311575c
Pavel Sidorenko <fat.cat.1703@gmail.com>
2020-06-03 15:25:58 +0300

    Header impl

--- ddp-workspace/projects/ddp-testboston/src/app/components/welcome/welcome.component.ts
+++ ddp-workspace/projects/ddp-testboston/src/app/components/welcome/welcome.component.ts
@@ -1,5 +1,5 @@
 import { Component, Inject, OnInit } from '@angular/core';
-import { ToolkitConfigurationService } from 'toolkit';
+import { ToolkitConfigurationService, HeaderConfigurationService } from 'toolkit';
 import { AppRoutes } from '../../app-routes';
 
 @Component({
@@ -14,12 +14,15 @@ export class WelcomeComponent implements OnInit {
   public emailHref: string;
   public appRoutes = AppRoutes;
 
-  constructor(@Inject('toolkit.toolkitConfig') private config: ToolkitConfigurationService) { }
+  constructor(
+    private headerConfig: HeaderConfigurationService,
+    @Inject('toolkit.toolkitConfig') private config: ToolkitConfigurationService) { }
 
   public ngOnInit(): void {
     this.phone = this.config.phone;
     this.email = this.config.infoEmail;
     this.phoneHref = `tel:${this.phone}`;
     this.emailHref = `mailto:${this.email}`;
+    this.headerConfig.setupDefaultHeader();
   }
 }
