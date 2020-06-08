0401b074697d1a19b5651dfe00322b388882159f
MocanaAtBroad <mocana@broadinstitute.org>
2020-06-08 17:34:55 -0400

    DDP-4723 Internationalize Mail Address, refactor, and performance boot (#149)
    * Add i18n translation to all mail address UI strings, including labels and error messages coming indirectly from EasyPost.
    
    * Use OnPush change detection for better performance
    
    * Implementation of custom rxjs extract operator
    
    * Refactor a bit using new operator
    
    * use imported operator in test

--- ddp-workspace/projects/ddp-sdk/src/lib/components/address/addressInput.component.spec.ts
+++ ddp-workspace/projects/ddp-sdk/src/lib/components/address/addressInput.component.spec.ts
@@ -7,6 +7,9 @@ import { AddressInputService } from './addressInput.service';
 import { Directive, Input, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { AddressService } from '../../services/address.service';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { of } from 'rxjs';
+import { NGXTranslateService } from '../../services/internationalization/ngxTranslate.service';
+import { TranslateTestingModule } from '../../testsupport/translateTestingModule';
 
 
 @Directive({
@@ -22,20 +25,28 @@ describe('AddressInputComponent', () => {
   let countryServiceSpy: jasmine.SpyObj<CountryService>;
   let aisSpy: jasmine.SpyObj<AddressInputService>;
   let addressServiceSpy: jasmine.SpyObj<AddressService>;
+  const translateServiceSpy: jasmine.SpyObj<NGXTranslateService> = jasmine.createSpyObj('NGXTranslateService', ['getTranslation']);
 
   beforeEach(async(() => {
     countryServiceSpy = jasmine.createSpyObj('CountryService', ['findAllCountryInfoSummaries']);
     aisSpy = jasmine.createSpyObj('AddressInputService', ['createForm']);
 
     addressServiceSpy = jasmine.createSpyObj('AddressService', ['verifyAddress']);
+    // @ts-ignore
+    translateServiceSpy.getTranslation.and.callFake((word: string | Array<string>, keyToValue?: object) => {
+      return of(Array.isArray(word) ?
+          word.map((each, i) => ({ each: 'label' + i })).reduce((prev, current) => ({ ...prev, ...current }), {}) as object :
+          'label1');
+    });
 
     TestBed.configureTestingModule({
       declarations: [ AddressInputComponent, FakeAddressGoogleAutocomplete ],
       providers: [
         {provide: CountryService, useValue: countryServiceSpy},
         {provide: AddressService, useValue: addressServiceSpy},
+        {provide: NGXTranslateService, useValue: translateServiceSpy},
         AddressInputService],
-      imports: [MatFormFieldModule, MatSelectModule, ReactiveFormsModule, MatInputModule, NoopAnimationsModule],
+      imports: [MatFormFieldModule, MatSelectModule, ReactiveFormsModule, MatInputModule, NoopAnimationsModule, TranslateTestingModule],
       // schemas needed to avoid problems with google autocomplete directive attributes in input element
       schemas: [CUSTOM_ELEMENTS_SCHEMA]
     })
