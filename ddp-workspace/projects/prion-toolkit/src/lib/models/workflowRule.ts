43c8653c0688593ac89059f0976754b98ee8934d
cdye <39769705+charlottedye@users.noreply.github.com>
2020-06-22 15:12:41 -0400

    Prion: Refactored prion-toolkit to only contain things that should be different from toolkit

--- ddp-workspace/projects/prion-toolkit/src/lib/models/workflowRule.ts
+++
@@ -1,7 +0,0 @@
-import { WorkflowCommand } from './workflowCommand';
-import { WorkflowAction } from './../models/actions/workflowAction';
-
-export interface WorkflowRule {
-    type: string;
-    func: (action: WorkflowAction) => WorkflowCommand;
-}
