f97ed70cd7ff39168f401601b62568c15bbb3488
cdye <39769705+charlottedye@users.noreply.github.com>
2020-06-22 15:12:41 -0400

    Prion: Refactored prion-toolkit to only contain things that should be different from toolkit

---
+++ ddp-workspace/projects/toolkit-prion/src/public-api.ts
@@ -0,0 +1,12 @@
+/*
+ * Public API Surface of toolkit
+ */
+
+export * from './lib/toolkit.module';
+
+export * from './lib/services/toolkitConfiguration.service';
+
+
+export * from './lib/components/dashboard/dashboard.component';
+export * from './lib/components/footer/footer.component';
+export * from './lib/components/header/header.component';
