02e0379123b7b3979ada4b3c2243f3f94410f440
cdye <39769705+charlottedye@users.noreply.github.com>
2020-06-22 15:12:41 -0400

    Prion: Refactored prion-toolkit to only contain things that should be different from toolkit

--- ddp-workspace/projects/prion-toolkit/src/lib/models/actions/mailingListWorkflowAction.ts
+++
@@ -1,6 +0,0 @@
-import { WorkflowAction } from './workflowAction';
-import { WorkflowActionType } from './../workflowActionType';
-
-export class MailingListWorkflowAction implements WorkflowAction {
-    public actionType = WorkflowActionType.MAILING_LIST;
-}
