75344f904af3d4d03e58ff1a4ff679d579976e6e
cdye <39769705+charlottedye@users.noreply.github.com>
2020-06-22 15:12:41 -0400

    Prion: Refactored prion-toolkit to only contain things that should be different from toolkit

--- ddp-workspace/projects/prion-toolkit/src/lib/models/actions/registrationWorkflowAction.ts
+++
@@ -1,6 +0,0 @@
-import { WorkflowAction } from './workflowAction';
-import { WorkflowActionType } from '../workflowActionType';
-
-export class RegistrationWorkflowAction implements WorkflowAction {
-    public actionType = WorkflowActionType.REGISTRATION;
-}
