134bde14d665e57c4e55cf8a29c9a90df06c8a69
cdye <39769705+charlottedye@users.noreply.github.com>
2020-06-22 15:12:41 -0400

    Prion: Refactored prion-toolkit to only contain things that should be different from toolkit

--- ddp-workspace/projects/prion-toolkit/src/lib/models/workflowCommand.ts
+++
@@ -1,8 +0,0 @@
-import { Observable } from 'rxjs';
-
-export interface WorkflowCommand {
-    // The observable should emit once if the command has finished executing
-    // and the app has not progressed to a different "stage" of the workflow,
-    // i.e. we're still on the same page.
-    execute(): Observable<void>;
-}
