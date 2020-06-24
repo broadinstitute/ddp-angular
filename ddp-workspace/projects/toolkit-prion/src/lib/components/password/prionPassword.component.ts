51feb16cc044627e5b00c2d61b4196a7685a91e6
cdye <39769705+charlottedye@users.noreply.github.com>
2020-06-24 11:44:07 -0400

    Prion: DDP-4434: Support language selection for Prion: worked on getting language selection to work with Prion, including starting to reimplement the study-listing component to translate more cleanly, and did more refactoring.

---
+++ ddp-workspace/projects/toolkit-prion/src/lib/components/password/prionPassword.component.ts
@@ -0,0 +1,41 @@
+import { Component, OnInit } from "@angular/core";
+import { PasswordComponent } from "toolkit";
+import { FormBuilder } from "@angular/forms";
+import { IrbPasswordService } from "ddp-sdk";
+import { Router } from "@angular/router";
+
+@Component({
+  selector: 'prion-password',
+  template: `<prion-header></prion-header>
+  <div class="Container row">
+    <div class="col-lg-6 col-lg-offset-3 col-md-8 col-md-offset-2 col-sm-12 col-xs-12">
+      <h1 class="PageContent-title">Please enter your password:</h1>
+      <form [formGroup]="passwordForm" (ngSubmit)="submitForm()">
+        <mat-form-field>
+          <input matInput
+                 type="password"
+                 formControlName="password"
+                 maxLength="200">
+          <mat-error *ngIf="isValid('password')" translate>Toolkit.Password.PasswordField.Error</mat-error>
+        </mat-form-field>
+        <button class="ButtonFilled"
+                type="submit"
+                [innerHTML]="'Toolkit.Password.SubmitButton' | translate">
+        </button>
+        <div *ngIf="isPasswordWrong" class="ErrorMessage">
+          <span translate>Toolkit.Password.PasswordWrongError</span>
+        </div>
+      </form>
+    </div>
+  </div>
+  `
+})
+export class PrionPasswordComponent extends PasswordComponent implements OnInit {
+
+  constructor(
+    private _formBuilder: FormBuilder,
+    private _irbPassword: IrbPasswordService,
+    private _router: Router) {
+    super(_formBuilder, _irbPassword, _router);
+  }
+}
