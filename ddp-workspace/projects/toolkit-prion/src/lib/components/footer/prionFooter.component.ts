690ba677139407ce793b40263b357f99a00a123b
cdye <39769705+charlottedye@users.noreply.github.com>
2020-06-24 11:44:07 -0400

    Prion: DDP-4434: Support language selection for Prion: worked on getting language selection to work with Prion, including starting to reimplement the study-listing component to translate more cleanly, and did more refactoring.

--- ddp-workspace/projects/toolkit-prion/src/lib/components/footer/prionFooter.component.ts
+++ ddp-workspace/projects/toolkit-prion/src/lib/components/footer/prionFooter.component.ts
@@ -13,7 +13,7 @@ import { FooterComponent, ToolkitConfigurationService } from "toolkit";
   selector: 'prion-footer',
   template: `
     <footer class="Footer row">
-      <div class="col-lg-4 col-md-4 col-sm-4 col-xs-12 Footer-left">
+      <div class="col-lg-6 col-md-4 col-sm-4 col-xs-12 Footer-left">
         <a href [routerLink]="['/home']">
           <img lazy-resource src="/assets/images/project-logo-dark.svg" [attr.alt]="'Toolkit.Common.LogoAlt' | translate">
         </a>
@@ -22,20 +22,19 @@ import { FooterComponent, ToolkitConfigurationService } from "toolkit";
           <a [href]="'Toolkit.Footer.ContactMailTo' | translate" class="Footer-link NoPadding" translate>Toolkit.Footer.ContactEmail</a>
         </p>
       </div>
-      <div class="col-lg-8 col-md-8 col-sm-8 col-xs-12 Footer-links">
+      <div class="col-lg-6 col-md-4 col-sm-8 col-xs-12 Footer-links">
         <p class="Footer-text Float--right">
           <a [routerLink]="['/']" class="Footer-link" translate>Toolkit.Common.Home</a>
           <span  (click)="clickLearnMore()" class="Footer-link" translate>Toolkit.Common.LearnMore</span>
           <span *ngIf="!isLoggedIn()"  (click)="clickJoinUs()" class="Footer-link" translate>Toolkit.Common.JoinUs</span>
           <span *ngIf="isLoggedIn()"  (click)="clickDashboard()" class="Footer-link" translate>Toolkit.Common.Dashboard</span>
           <ddp-sign-in-out class="Footer-link"></ddp-sign-in-out>
-          <a [routerLink]="['/privacy-policy']" class="Footer-link" translate>Toolkit.Footer.PrivacyPolicy</a>
-          <button (click)="goToTop()" class="Footer-link">
-            <i class="fas fa-arrow-up Footer-arrow"></i>
-            <span translate>Toolkit.Footer.BackToTop</span>
-          </button>
+          <button (click)="goToTop()" class="Footer-link"><i class="fas fa-arrow-up Footer-arrow"></i><span translate>Toolkit.Footer.BackToTop</span></button>
         </p>
       </div>
+
+
+
     </footer>
   `
 })
