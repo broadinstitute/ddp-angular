060f669c4b0cbb4e3858eb58bd0c58ae56a2443a
cdye <39769705+charlottedye@users.noreply.github.com>
2020-06-24 11:44:07 -0400

    Prion: DDP-4434: Support language selection for Prion: worked on getting language selection to work with Prion, including starting to reimplement the study-listing component to translate more cleanly, and did more refactoring.

--- ddp-workspace/projects/ddp-prion/src/app/components/study-listing-component/study-listing-data-source.ts
+++ ddp-workspace/projects/ddp-prion/src/app/components/study-listing-component/study-listing-data-source.ts
@@ -1,93 +1,17 @@
-import { StudyInfo } from "../../models/study-listing/study-info";
+import { StudyInfo } from "./study-info";
 import { DataSource } from "@angular/cdk/table";
-import { BehaviorSubject } from "rxjs";
+import { Observable } from "rxjs";
 import { TranslateService } from "@ngx-translate/core";
-import { FilterInfo } from "../../models/study-listing/filter-info";
+import { map } from "rxjs/operators";
 
 export class StudyListingDataSource extends DataSource<StudyInfo> {
-  private baseData: StudyInfo[] = null;
-  private filterBy: string = null;
-  shouldSort: boolean = false;
-  sortIndex: number = -1;
-  sortDir: string = null;
-  dataSubj: BehaviorSubject<StudyInfo[]>;
-
-  constructor(private translator: TranslateService, private bucketUrl: string, private columnFilters: FilterInfo[]) {
+  constructor(private translator: TranslateService, private bucketUrl: string) {
     super();
-    this.baseData = (this.translator.instant('App.StudyListing.Rows') as StudyInfo[])
-      .map(x => new StudyInfo(x.studyName, x.description, x.nameOfPI, x.site, x.eligibilityRequirements,
-        x.moreInfo.replace('{{bucketUrl}}', this.bucketUrl)));
-    this.dataSubj = new BehaviorSubject<StudyInfo[]>(this.baseData);
-    this.updateDataProcessing();
-    this.translator.onLangChange.subscribe(() => this.updateTranslations());
   }
 
-  connect(): BehaviorSubject<StudyInfo[]> {
-    return this.dataSubj;
+  public connect(): Observable<Array<StudyInfo>> {
+    return this.translator.stream('Toolkit.StudyListing.Rows', {bucketUrl: this.bucketUrl}).pipe(map(x => x as Array<StudyInfo>));
   }
 
-  disconnect(): void { }
-
-  updateDataProcessing() {
-    let data = this.baseData.filter(x => this.rowMatchesFilter(x));
-    data = data.filter(x => this.rowMeetsColumnFilters(x));
-    data = this.sortData(data);
-    this.dataSubj.next(data);
-  }
-
-  updateTranslations() {
-    this.baseData = (this.translator.instant('App.StudyListing.Rows') as StudyInfo[])
-      .map(x => new StudyInfo(x.studyName, x.description, x.nameOfPI, x.site, x.eligibilityRequirements,
-        x.moreInfo.replace('{{bucketUrl}}', this.bucketUrl)));
-    this.updateDataProcessing();
-  }
-
-  public addSort(shouldSort: boolean = false, sortIndex: number = -1, sortDir: string = null): void {
-    this.shouldSort = shouldSort;
-    this.sortIndex = sortIndex;
-    this.sortDir = sortDir;
-    this.updateDataProcessing();
-  }
-
-  public addFilter(filterBy: string) {
-    if (filterBy !== null && filterBy !== undefined && filterBy.length > 0) {
-      this.filterBy = filterBy.toLowerCase();
-    }
-    else {
-      this.filterBy = null;
-    }
-    this.updateDataProcessing();
-  }
-
-  public addColumnFilters(columnFilters: FilterInfo[]) {
-    this.columnFilters = columnFilters;
-    this.updateDataProcessing();
-  }
-
-  private isFiltered(): boolean {
-    return this.filterBy !== undefined && this.filterBy !== null;
-  }
-
-  private rowMatchesFilter(row: StudyInfo): boolean {
-    return !this.isFiltered() || row.getStringValues().some(element => element.toLowerCase().includes(this.filterBy.toLowerCase()));
-  }
-
-  private rowMeetsColumnFilters(row: StudyInfo): boolean {
-    let validFilters: FilterInfo[] = this.columnFilters.filter(x => x.canFilter && x.isFiltered);
-    return validFilters.length == 0 || validFilters.every(x => x.studyInfoIsMatch(row));
-  }
-
-  private sortData(rows: StudyInfo[]): StudyInfo[] {
-    if (this.shouldSort && this.sortIndex > -1 && this.sortDir === 'asc' || this.sortDir === 'desc') {
-      return rows.sort((a, b) => {
-        const isAsc = this.sortDir === 'asc';
-        return StudyListingDataSource.compare(a.getStringValues()[this.sortIndex], b.getStringValues()[this.sortIndex], isAsc);
-      });
-    }
-    return rows;
-  }
-
-  private static compare(a: string, b: string, isAsc: boolean) {
-    return (a < b ? -1 : 1) * (isAsc ? 1 : -1);
-  }
+  public disconnect(): void { }
 }
--- ddp-workspace/projects/toolkit-prion/src/lib/services/toolkitConfiguration.service.ts
+++
@@ -1,7 +0,0 @@
-import { Injectable } from '@angular/core';
-import { ToolkitConfigurationService } from "toolkit";
-
-@Injectable()
-export class PrionToolkitConfigurationService extends ToolkitConfigurationService {
-  assetsBucketUrl: string;
-}
