54e1e7aafca214db783f27c15bc10f0159a6cc23
cdye <39769705+charlottedye@users.noreply.github.com>
2020-06-25 16:41:31 -0400

    Prion: DDP-4434, language selection for Prion: Study listing page: added ability to filter by a particular column

--- ddp-workspace/projects/ddp-prion/src/app/components/study-listing-component/study-listing.component.ts
+++ ddp-workspace/projects/ddp-prion/src/app/components/study-listing-component/study-listing.component.ts
@@ -3,6 +3,7 @@ import { MatTable } from "@angular/material";
 import { TranslateService } from "@ngx-translate/core";
 import { PrionToolkitConfigurationService } from "toolkit-prion";
 import { StudyListingDataSource } from "./study-listing-data-source";
+import { Column } from "../../models/study-listing/column";
 
 @Component({
   selector: 'study-listing',
@@ -16,18 +17,26 @@ import { StudyListingDataSource } from "./study-listing-data-source";
               <section class="PageContent-section">
                 <div class="row">
                   <div class="col-md-4">
-                    <input class="form-control study-listing-filter-all" matInput (keyup)="applyFilter($event)" [placeholder]="'App.StudyListing.InputPlaceholder' | translate">
+                    <input class="form-control study-listing-filter study-listing-filter-all" matInput 
+                           (keyup)="applyFilter($event)" [placeholder]="'App.StudyListing.InputPlaceholder' | translate">
                   </div>
                 </div>
                 <br/>
-                <table mat-table [dataSource]="dataSource" class="table dataTable table-striped table-bordered study-listing-table">
+                <table mat-table [dataSource]="dataSource" class="table dataTable table-bordered study-listing-table">
                   <ng-container [matColumnDef]="column" *ngFor="let column of displayedColumns; index as i">
-                    <th mat-header-cell *matHeaderCellDef translate [innerHTML]="getTitle(i)"></th>
+                    <th mat-header-cell *matHeaderCellDef translate [innerHTML]="columns[i].columnTitleKey">
+                    </th>
                     <td mat-cell *matCellDef="let element">{{element[column]}}</td>
                   </ng-container>
+                  <ng-container [matColumnDef]="column" *ngFor="let column of filterColumns; index as i">
+                    <th mat-header-cell *matHeaderCellDef class="filter-cell">
+                      <input *ngIf="columns[i].filterInfo.canFilter" class="form-control study-listing-filter study-listing-filter-col" 
+                             matInput (keyup)="applyColumnFilter(i, $event)" [placeholder]="columns[i].filterPlaceholder | translate">
+                    </th>
+                  </ng-container>
                   <tr mat-header-row *matHeaderRowDef="displayedColumns"></tr>
-                  <!-- TODO: Add second header row with column filters -->
-                  <tr mat-row *matRowDef="let row; columns: displayedColumns;"></tr>
+                  <tr mat-header-row *matHeaderRowDef="filterColumns" class="filter-row"></tr>
+                  <tr mat-row *matRowDef="let row; columns: displayedColumns"></tr>
                 </table>
               </section>
             </div>
@@ -41,11 +50,15 @@ import { StudyListingDataSource } from "./study-listing-data-source";
 export class StudyListingComponent implements OnInit {
   //TODO: Fix literal rendering of HTML in data
   //TODO: Fix display of table
-  //TODO: Add filtering of specific columns
+  //TODO: Add ablity to sort by column
 
   public dataSource: StudyListingDataSource;
-  public displayedColumns: string[] = ['studyName', 'description', 'nameOfPI', 'site', 'eligibilityRequirements', 'moreInfo'];
-  public columnTitles: string[] = ['App.StudyListing.Columns.0.title', 'App.StudyListing.Columns.1.title', 'App.StudyListing.Columns.2.title', 'App.StudyListing.Columns.3.title', 'App.StudyListing.Columns.4.title', 'App.StudyListing.Columns.5.title'];
+  public displayedColumns: string[] = ['studyName', 'description', 'nameOfPI',
+    'site', 'eligibilityRequirements', 'moreInfo'];
+  public filterColumns: string[] = ['studyNameFilter', 'descriptionFilter', 'nameOfPIFilter',
+    'siteFilter', 'eligibilityRequirementsFilter', 'moreInfoFilter'];
+  public columns: Column[];
+  private generalFilterString: string = null;
 
   @ViewChild(MatTable, {static:false})
   private table: MatTable<any>;
@@ -54,20 +67,33 @@ export class StudyListingComponent implements OnInit {
                      @Inject('toolkit.toolkitConfig') private toolkitConfiguration:PrionToolkitConfigurationService) {
   }
 
-  public getTitle(i: number) {
-    return this.columnTitles[i];
-  }
-
   public ngOnInit(): void {
-    this.dataSource = new StudyListingDataSource(this.translator, this.toolkitConfiguration.assetsBucketUrl, null);
+    this.columns = [
+      new Column(0, true),
+      new Column(1, true),
+      new Column(2, true),
+      new Column(3, true),
+      new Column(4, true),
+      new Column(5, false)
+    ];
+
+    this.updateDataSource();
   }
 
   public applyFilter(event): void {
-    let filterString: string = (event.target as HTMLInputElement).value;
-    this.dataSource = new StudyListingDataSource(this.translator, this.toolkitConfiguration.assetsBucketUrl,filterString);
+    this.generalFilterString = (event.target as HTMLInputElement).value;
+    this.updateDataSource();
   }
 
-  public applyColumnFilter(column, event): void {
-    //TODO: Implement applyColumnFilter and use
+  public applyColumnFilter(column: number, event: Event): void {
+    if (this.columns[column].filterInfo.canFilter) {
+      let filterString: string = (event.target as HTMLInputElement).value;
+      this.columns[column].filterInfo.addFilter(filterString);
+      this.updateDataSource();
+    }
+  }
+
+  private updateDataSource() {
+    this.dataSource = new StudyListingDataSource(this.translator, this.toolkitConfiguration.assetsBucketUrl, this.generalFilterString, this.columns.map(x => x.filterInfo));
   }
 }
