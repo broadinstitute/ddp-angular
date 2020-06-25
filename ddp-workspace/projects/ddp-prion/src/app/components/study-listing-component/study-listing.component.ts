4ae5b146c1ce2c28e7d7dfb7996d51e488879ab2
cdye <39769705+charlottedye@users.noreply.github.com>
2020-06-25 11:39:25 -0400

    Prion: DDP-4434, language selection for Prion: Study listing page: re-implemented ability to filter full table, fixed appearance of table header, and miscellaneous UI fixes and refactoring

--- ddp-workspace/projects/ddp-prion/src/app/components/study-listing-component/study-listing.component.ts
+++ ddp-workspace/projects/ddp-prion/src/app/components/study-listing-component/study-listing.component.ts
@@ -1,8 +1,7 @@
-import { Component, Inject, OnInit } from '@angular/core';
+import { Component, Inject, OnInit, ViewChild } from "@angular/core";
+import { MatTable } from "@angular/material";
 import { TranslateService } from "@ngx-translate/core";
 import { PrionToolkitConfigurationService } from "toolkit-prion";
-import { DataSource } from "@angular/cdk/table";
-import { StudyInfo } from "./study-info";
 import { StudyListingDataSource } from "./study-listing-data-source";
 
 @Component({
@@ -15,12 +14,19 @@ import { StudyListingDataSource } from "./study-listing-data-source";
           <div class="row NoMargin">
             <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
               <section class="PageContent-section">
-                <table mat-table [dataSource]="dataSource" class="dataTable table-striped table-bordered study-listing-table">
-                  <ng-container [matColumnDef]="column" *ngFor="let column of displayedColumns">
-                    <th mat-header-cell *matHeaderCellDef translate>{{column}}</th>
+                <div class="row">
+                  <div class="col-md-4">
+                    <input class="form-control study-listing-filter-all" matInput (keyup)="applyFilter($event)" [placeholder]="'App.StudyListing.InputPlaceholder' | translate">
+                  </div>
+                </div>
+                <br/>
+                <table mat-table [dataSource]="dataSource" class="table dataTable table-striped table-bordered study-listing-table">
+                  <ng-container [matColumnDef]="column" *ngFor="let column of displayedColumns; index as i">
+                    <th mat-header-cell *matHeaderCellDef translate [innerHTML]="getTitle(i)"></th>
                     <td mat-cell *matCellDef="let element">{{element[column]}}</td>
                   </ng-container>
                   <tr mat-header-row *matHeaderRowDef="displayedColumns"></tr>
+                  <!-- TODO: Add second header row with column filters -->
                   <tr mat-row *matRowDef="let row; columns: displayedColumns;"></tr>
                 </table>
               </section>
@@ -33,16 +39,35 @@ import { StudyListingDataSource } from "./study-listing-data-source";
   `
 })
 export class StudyListingComponent implements OnInit {
-  public dataSource: DataSource<StudyInfo>;
+  //TODO: Fix literal rendering of HTML in data
+  //TODO: Fix display of table
+  //TODO: Add filtering of specific columns
 
-  //TODO: Make columns translated
+  public dataSource: StudyListingDataSource;
   public displayedColumns: string[] = ['studyName', 'description', 'nameOfPI', 'site', 'eligibilityRequirements', 'moreInfo'];
+  public columnTitles: string[] = ['App.StudyListing.Columns.0.title', 'App.StudyListing.Columns.1.title', 'App.StudyListing.Columns.2.title', 'App.StudyListing.Columns.3.title', 'App.StudyListing.Columns.4.title', 'App.StudyListing.Columns.5.title'];
+
+  @ViewChild(MatTable, {static:false})
+  private table: MatTable<any>;
 
   public constructor(private translator: TranslateService,
                      @Inject('toolkit.toolkitConfig') private toolkitConfiguration:PrionToolkitConfigurationService) {
   }
 
+  public getTitle(i: number) {
+    return this.columnTitles[i];
+  }
+
   public ngOnInit(): void {
-    this.dataSource = new StudyListingDataSource(this.translator, this.toolkitConfiguration.assetsBucketUrl);
+    this.dataSource = new StudyListingDataSource(this.translator, this.toolkitConfiguration.assetsBucketUrl, null);
+  }
+
+  public applyFilter(event): void {
+    let filterString: string = (event.target as HTMLInputElement).value;
+    this.dataSource = new StudyListingDataSource(this.translator, this.toolkitConfiguration.assetsBucketUrl,filterString);
+  }
+
+  public applyColumnFilter(column, event): void {
+    //TODO: Implement applyColumnFilter and use
   }
 }
