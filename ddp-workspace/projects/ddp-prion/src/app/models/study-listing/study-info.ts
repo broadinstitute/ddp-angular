d9413e37314abe652c177eab8e6aea7e3d8158d6
cdye <39769705+charlottedye@users.noreply.github.com>
2020-06-25 11:39:25 -0400

    Prion: DDP-4434, language selection for Prion: Study listing page: re-implemented ability to filter full table, fixed appearance of table header, and miscellaneous UI fixes and refactoring

--- ddp-workspace/projects/ddp-prion/src/app/models/study-listing/study-info.ts
+++ ddp-workspace/projects/ddp-prion/src/app/models/study-listing/study-info.ts
@@ -5,19 +5,4 @@ export class StudyInfo {
   site: string;
   eligibilityRequirements: string;
   moreInfo: string;
-  colValues: string[];
-
-  public constructor(studyName, description, nameOfPI, site, eligibilityRequirements, moreInfo) {
-    this.studyName = studyName;
-    this.description = description;
-    this.nameOfPI = nameOfPI;
-    this.site = site;
-    this.eligibilityRequirements = eligibilityRequirements;
-    this.moreInfo = moreInfo;
-    this.colValues = [studyName, description, nameOfPI, site, eligibilityRequirements, moreInfo];
-  }
-
-  public getStringValues(): string[] {
-    return [this.studyName, this.description, this.nameOfPI, this.site, this.eligibilityRequirements, this.moreInfo];
-  }
 }
