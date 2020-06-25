cc15009d36d251f08e0544de9a2f167c4e955030
cdye <39769705+charlottedye@users.noreply.github.com>
2020-06-25 16:41:31 -0400

    Prion: DDP-4434, language selection for Prion: Study listing page: added ability to filter by a particular column

--- ddp-workspace/projects/ddp-prion/src/app/components/study-listing-component/study-listing-data-source.ts
+++ ddp-workspace/projects/ddp-prion/src/app/components/study-listing-component/study-listing-data-source.ts
@@ -3,28 +3,37 @@ import { DataSource } from "@angular/cdk/table";
 import { Observable } from "rxjs";
 import { TranslateService } from "@ngx-translate/core";
 import { map } from "rxjs/operators";
+import { FilterInfo } from "../../models/study-listing/filter-info";
 
 export class StudyListingDataSource extends DataSource<StudyInfo> {
-  constructor(private translator: TranslateService, private bucketUrl: string, private filterBy: string) {
+  private filterBy: string = null;
+  constructor(private translator: TranslateService, private bucketUrl: string, filterBy: string, private columnFilters: FilterInfo[]) {
     super();
+    if (filterBy !== null && filterBy !== undefined) {
+      this.filterBy = filterBy.toLowerCase();
+    }
   }
 
   public connect(): Observable<Array<StudyInfo>> {
     return this.translator.stream('App.StudyListing.Rows', {bucketUrl: this.bucketUrl})
-      .pipe(map(x => (x as Array<StudyInfo>).filter(x => this.rowMatchesFilter(x))));
+      .pipe(map(x => (x as Array<StudyInfo>)
+        .map(x => new StudyInfo(x.studyName, x.description, x.nameOfPI, x.site, x.eligibilityRequirements, x.moreInfo))
+        .filter(x => this.rowMatchesFilter(x))
+        .filter(x => this.rowMeetsColumnFilters(x))));
   }
 
-  private rowMatchesFilter(row: StudyInfo): boolean {
-    return !this.isFiltered() || Object.values(row).some(element => this.elementMatchesFilter(element));
-  }
+  public disconnect(): void { }
 
   private isFiltered(): boolean {
     return this.filterBy !== undefined && this.filterBy !== null;
   }
 
-  private elementMatchesFilter(elem: string): boolean {
-    return elem.toLowerCase().includes(this.filterBy.toLowerCase());
+  private rowMatchesFilter(row: StudyInfo): boolean {
+    return !this.isFiltered() || Object.values(row).some(element => element.toLowerCase().includes(this.filterBy.toLowerCase()));
   }
 
-  public disconnect(): void { }
+  private rowMeetsColumnFilters(row: StudyInfo): boolean {
+    let validFilters: FilterInfo[] = this.columnFilters.filter(x => x.canFilter && x.isFiltered);
+    return validFilters.length == 0 || validFilters.every(x => x.studyInfoIsMatch(row));
+  }
 }
