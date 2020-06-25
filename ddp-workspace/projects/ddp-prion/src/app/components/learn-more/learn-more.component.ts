837e0a620978c323a36ebe401c296469701f6ea3
cdye <39769705+charlottedye@users.noreply.github.com>
2020-06-25 10:51:33 -0400

    Prion: Re-added SessionWillExpireComponent and reorganized language files

--- ddp-workspace/projects/ddp-prion/src/app/components/learn-more/learn-more.component.ts
+++ ddp-workspace/projects/ddp-prion/src/app/components/learn-more/learn-more.component.ts
@@ -10,22 +10,22 @@ import { ToolkitConfigurationService } from "toolkit";
         <div class="PageLayout row">
           <div class="col-lg-8 col-md-10 col-sm-12 col-xs-12">
             <section class="PageContent-section NoPadding">
-              <h1 class="PageContent-title" translate>Toolkit.LearnMore.LearnMore.Title</h1>
+              <h1 class="PageContent-title" translate>App.LearnMore.LearnMore.Title</h1>
 
-              <h4 class="Subtitle Color--green" translate>Toolkit.LearnMore.LearnMore.WhatIs.Question</h4>
-              <p translate>Toolkit.LearnMore.LearnMore.WhatIs.Text</p>
+              <h4 class="Subtitle Color--green" translate>App.LearnMore.LearnMore.WhatIs.Question</h4>
+              <p translate>App.LearnMore.LearnMore.WhatIs.Text</p>
 
-              <h4 class="Subtitle Color--green" translate>Toolkit.LearnMore.LearnMore.For.Question</h4>
-              <p translate>Toolkit.LearnMore.LearnMore.For.Text</p>
+              <h4 class="Subtitle Color--green" translate>App.LearnMore.LearnMore.For.Question</h4>
+              <p translate>App.LearnMore.LearnMore.For.Text</p>
 
-              <h4 class="Subtitle Color--green" translate>Toolkit.LearnMore.LearnMore.Participate.Question</h4>
-              <p translate>Toolkit.LearnMore.LearnMore.Participate.Text</p>
+              <h4 class="Subtitle Color--green" translate>App.LearnMore.LearnMore.Participate.Question</h4>
+              <p translate>App.LearnMore.LearnMore.Participate.Text</p>
 
-              <h4 class="Subtitle Color--green" translate>Toolkit.LearnMore.LearnMore.Why.Question</h4>
-              <p translate>Toolkit.LearnMore.LearnMore.Why.Text</p>
+              <h4 class="Subtitle Color--green" translate>App.LearnMore.LearnMore.Why.Question</h4>
+              <p translate>App.LearnMore.LearnMore.Why.Text</p>
 
-              <h4 class="Subtitle Color--green" translate>Toolkit.LearnMore.LearnMore.DoIHaveTo.Question</h4>
-              <p translate>Toolkit.LearnMore.LearnMore.DoIHaveTo.Text</p>
+              <h4 class="Subtitle Color--green" translate>App.LearnMore.LearnMore.DoIHaveTo.Question</h4>
+              <p translate>App.LearnMore.LearnMore.DoIHaveTo.Text</p>
             </section>
           </div>
         </div>
