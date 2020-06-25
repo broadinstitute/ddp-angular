15e9d81f0bfdf09b3f288ed63a787e44c2e9909b
cdye <39769705+charlottedye@users.noreply.github.com>
2020-06-25 11:39:25 -0400

    Prion: DDP-4434, language selection for Prion: Study listing page: re-implemented ability to filter full table, fixed appearance of table header, and miscellaneous UI fixes and refactoring

--- ddp-workspace/projects/ddp-prion/src/app/components/study-listing-component/study-listing-data-source.ts
+++ ddp-workspace/projects/ddp-prion/src/app/components/study-listing-component/study-listing-data-source.ts
@@ -1,16 +1,29 @@
-import { StudyInfo } from "./study-info";
+import { StudyInfo } from "../../models/study-listing/study-info";
 import { DataSource } from "@angular/cdk/table";
 import { Observable } from "rxjs";
 import { TranslateService } from "@ngx-translate/core";
 import { map } from "rxjs/operators";
 
 export class StudyListingDataSource extends DataSource<StudyInfo> {
-  constructor(private translator: TranslateService, private bucketUrl: string) {
+  constructor(private translator: TranslateService, private bucketUrl: string, private filterBy: string) {
     super();
   }
 
   public connect(): Observable<Array<StudyInfo>> {
-    return this.translator.stream('Toolkit.StudyListing.Rows', {bucketUrl: this.bucketUrl}).pipe(map(x => x as Array<StudyInfo>));
+    return this.translator.stream('App.StudyListing.Rows', {bucketUrl: this.bucketUrl})
+      .pipe(map(x => (x as Array<StudyInfo>).filter(x => this.rowMatchesFilter(x))));
+  }
+
+  private rowMatchesFilter(row: StudyInfo): boolean {
+    return !this.isFiltered() || Object.values(row).some(element => this.elementMatchesFilter(element));
+  }
+
+  private isFiltered(): boolean {
+    return this.filterBy !== undefined && this.filterBy !== null;
+  }
+
+  private elementMatchesFilter(elem: string): boolean {
+    return elem.toLowerCase().includes(this.filterBy.toLowerCase());
   }
 
   public disconnect(): void { }
