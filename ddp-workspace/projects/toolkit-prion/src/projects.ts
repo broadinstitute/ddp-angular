c213513f7e41bd9c8919a83890cb8fd4958d98e3
cdye <39769705+charlottedye@users.noreply.github.com>
2020-06-29 13:29:41 -0400

    Prion: Fixes for some refactoring issues

--- ddp-workspace/projects/toolkit-prion/src/projects.ts
+++ ddp-workspace/projects/toolkit-prion/src/projects.ts
@@ -6,11 +6,14 @@ export * from './lib/toolkit.module';
 export * from './lib/components/activity/prionActivity.component';
 export * from './lib/components/activity-page/prionActivityPage.component';
 export * from './lib/components/app/prionApp.component';
+export * from './lib/components/common-landing/prionCommonLanding.component';
 export * from './lib/components/dashboard/prionDashboard.component';
 export * from './lib/components/error/prionError.component';
 export * from './lib/components/footer/prionFooter.component';
 export * from './lib/components/header/prionHeader.component';
+export * from './lib/components/login-landing/prionLoginLanding.component'
 export * from './lib/components/password/prionPassword.component';
+export * from './lib/components/redirect-to-login-landing/prionRedirectToLoginLanding.component'
 export * from './lib/components/session-expired/prionSessionExpired.component';
 export * from './lib/components/workflow-start-activity/prionWorkflowStartActivity.component';
 export * from './lib/services/prionToolkitConfiguration.service';
