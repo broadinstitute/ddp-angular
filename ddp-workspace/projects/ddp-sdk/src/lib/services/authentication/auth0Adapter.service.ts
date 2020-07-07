009095744e341187cb1686efead422d5666e991f
Pavel Sidorenko <fat.cat.1703@gmail.com>
2020-07-07 21:00:37 +0300

    DDP-4786 : Admin session expire page (#182)
    * Admin session and url improvements
    
    * Session expired improvement
    
    * Rename and fix
    
    * Improvement
    
    * Refactor

--- ddp-workspace/projects/ddp-sdk/src/lib/services/authentication/auth0Adapter.service.ts
+++ ddp-workspace/projects/ddp-sdk/src/lib/services/authentication/auth0Adapter.service.ts
@@ -191,7 +191,7 @@ export class Auth0AdapterService implements OnDestroy {
             ...(additionalParams && additionalParams)
         };
         if (this.configuration.doLocalRegistration) {
-            sessionStorage.setItem("localAdminAuth", 'true');
+            sessionStorage.setItem('localAdminAuth', 'true');
             sessionStorage.setItem('localAuthParams', JSON.stringify(auth0Params));
         }
         this.adminWebAuth.authorize(
@@ -323,15 +323,19 @@ export class Auth0AdapterService implements OnDestroy {
     }
 
     public handleExpiredAuthenticatedSession(): void {
+        const returnToUrl = this.getSessionExpiredUrl();
         this.renewNotifier.hideSessionExpirationNotifications();
-        if (!this.configuration.doLocalRegistration) {
-            sessionStorage.setItem('nextUrl', this.router.url);
-            this.logout('session-expired');
-        }
+        sessionStorage.setItem('nextUrl', this.router.url);
+        this.logout(returnToUrl);
     }
 
     private handleExpiredTemporarySession(): void {
         this.session.clear();
         window.location.reload();
+    }
+
+    private getSessionExpiredUrl(): string {
+        return this.session.session.isAdmin ?
+            this.configuration.adminSessionExpiredUrl : this.configuration.sessionExpiredUrl;
     }
 }
