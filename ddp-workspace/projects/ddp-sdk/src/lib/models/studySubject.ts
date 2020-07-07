96943f03a2103dfc779c6c6653acb5f4c57c7c18
Pavel Sidorenko <fat.cat.1703@gmail.com>
2020-07-07 20:59:58 +0300

    API changes

--- ddp-workspace/projects/ddp-sdk/src/lib/models/studySubject.ts
+++ ddp-workspace/projects/ddp-sdk/src/lib/models/studySubject.ts
@@ -2,7 +2,6 @@ import { Invitation } from './invitation';
 
 export interface StudySubject extends Invitation {
     userGuid: string | null;
-    userHruid: string | null;
     userLoginEmail: string | null;
     notes: string | null;
 }
