2c018475374ccbb2ce0396d832cf8ca4d9a824e8
Pavel Sidorenko <fat.cat.1703@gmail.com>
2020-07-14 10:17:59 +0300

    Cleanup language service

--- ddp-workspace/projects/ddp-sdk/src/lib/services/languageService.service.ts
+++ ddp-workspace/projects/ddp-sdk/src/lib/services/languageService.service.ts
@@ -3,49 +3,49 @@ import { TranslateService } from '@ngx-translate/core';
 
 @Injectable()
 export class LanguageService {
-    constructor(private translate: TranslateService) { }
+  constructor(private translate: TranslateService) { }
 
-    public getCurrentLanguage(): string {
-        return this.translate.currentLang;
-    }
+  public getCurrentLanguage(): string {
+    return this.translate.currentLang;
+  }
 
-    public canUseLanguage(languageCode: string): boolean {
-      if (!!languageCode) {
-        return false;
-      }
-      return this.translate.getLangs().includes(languageCode);
-    }
-
-    public addLanguages(languageCodes: Array<string>): void {
-      this.translate.addLangs(languageCodes);
-    }
-
-    public useStoredLanguage(): string {
-      let loadedCode: string = localStorage.getItem('studyLanguage');
-      if (this.changeLanguage(loadedCode)) {
-        return loadedCode;
-      }
-      return null;
-    }
-
-    public changeLanguagePromise(languageCode: string): Promise<any> {
-      if (this.canUseLanguage(languageCode)) {
-        localStorage.setItem('studyLanguage', languageCode);
-        return this.translate.use(languageCode).toPromise();
-      }
-      else {
-        console.log(`Error: cannot use language ${languageCode}`);
-        return null;
-      }
-    }
-
-    public changeLanguage(languageCode: string): boolean {
-      if (this.canUseLanguage(languageCode)) {
-        this.translate.use(languageCode);
-        localStorage.setItem('studyLanguage', languageCode);
-        return true;
-      }
-
+  public canUseLanguage(languageCode: string): boolean {
+    if (!languageCode) {
       return false;
     }
+    return this.translate.getLangs().includes(languageCode);
+  }
+
+  public addLanguages(languageCodes: Array<string>): void {
+    this.translate.addLangs(languageCodes);
+  }
+
+  public useStoredLanguage(): string {
+    let loadedCode: string = localStorage.getItem('studyLanguage');
+    if (this.changeLanguage(loadedCode)) {
+      return loadedCode;
+    }
+    return null;
+  }
+
+  public changeLanguagePromise(languageCode: string): Promise<any> {
+    if (this.canUseLanguage(languageCode)) {
+      localStorage.setItem('studyLanguage', languageCode);
+      return this.translate.use(languageCode).toPromise();
+    }
+    else {
+      console.log(`Error: cannot use language ${languageCode}`);
+      return null;
+    }
+  }
+
+  public changeLanguage(languageCode: string): boolean {
+    if (this.canUseLanguage(languageCode)) {
+      this.translate.use(languageCode);
+      localStorage.setItem('studyLanguage', languageCode);
+      return true;
+    }
+
+    return false;
+  }
 }
