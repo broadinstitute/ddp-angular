fd5b90970b77bf3353cfc2bf8423297b25650209
Pavel Sidorenko <fat.cat.1703@gmail.com>
2020-07-14 13:32:39 +0300

    Remove real backend call

--- ddp-workspace/projects/ddp-sdk/src/lib/services/irbPassword.service.spec.ts
+++ ddp-workspace/projects/ddp-sdk/src/lib/services/irbPassword.service.spec.ts
@@ -1,3 +1,4 @@
+import { HttpTestingController, HttpClientTestingModule } from '@angular/common/http/testing';
 import { TestBed } from '@angular/core/testing';
 import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { CookieService } from 'ngx-cookie';
@@ -8,29 +9,41 @@ import { LoggingService } from './logging.service';
 describe('IrbPasswordService', () => {
     let service: IrbPasswordService;
     let httpClient: HttpClient;
+    let httpTestingController: HttpTestingController;
     const config = new ConfigurationService();
-    config.backendUrl = 'https://pepper-dev.datadonationplatform.org';
-    // config.backendUrl = 'http://localhost:5555';
-    config.studyGuid = 'ANGIO';
+    config.backendUrl = 'https://pepper-mock-backend.org';
+    config.studyGuid = 'study';
+    const mockRequestUrl = `${config.backendUrl}/pepper/v1/studies/${config.studyGuid}/irb-password-check`;
     // Faking out the cookieservice
-    const cookieServiceSpy: jasmine.SpyObj<CookieService> = jasmine.createSpyObj('CookieService', ['put']);
+    const cookieServiceSpy = jasmine.createSpyObj('CookieService', ['checkPassword']);
     // called within the parent class
-    const loggingServiceSpy: jasmine.SpyObj<LoggingService> = jasmine.createSpyObj('LoggingService', ['logException']);
+    const loggingServiceSpy = jasmine.createSpyObj('LoggingService', ['logException']);
+
     beforeEach(() => {
         TestBed.configureTestingModule({
             // this the import to get a real httpclient
-            imports: [HttpClientModule],
-            providers: [IrbPasswordService, { provide: CookieService, useValue: cookieServiceSpy },
-                { provide: 'ddp.config', useValue: config }, LoggingService]
+            imports: [
+                HttpClientModule,
+                HttpClientTestingModule
+            ],
+            providers: [
+                IrbPasswordService,
+                CookieService,
+                { provide: 'ddp.config', useValue: config },
+                LoggingService
+            ]
         });
         // For some reason TestBed is willing to inject Configuration into service, but not httpclient
         // So we need to instantiate ourselves
         httpClient = TestBed.inject(HttpClient);
+        httpTestingController = TestBed.inject(HttpTestingController);
         service = new IrbPasswordService(config, cookieServiceSpy, httpClient, loggingServiceSpy);
+    });
 
-    });
     afterEach(() => {
+        httpTestingController.verify();
     });
+
     it('Check http password call', (done) => {
         // spy on handle error. Checking that we can make call cleanly
         // not the callThrough!!!!
@@ -40,10 +53,12 @@ describe('IrbPasswordService', () => {
             expect(result).toBe(false, 'We were expecting a false from server!');
             expect(handleErrorSpy).not.toHaveBeenCalled();
             done();
-        }, (error) => {
-            fail(error);
-            done();
-
+        });
+        // mock backend request
+        const req = httpTestingController
+            .expectOne(mockRequestUrl);
+        req.flush({
+            result: false
         });
     });
 });
