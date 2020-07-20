cd02f509a20b9d5941e9edabec800dca50983b78
Pavel Sidorenko <fat.cat.1703@gmail.com>
2020-07-20 11:51:42 +0300

    Fix sandbox app

--- ddp-workspace/projects/sandbox-app/src/polyfills.ts
+++ ddp-workspace/projects/sandbox-app/src/polyfills.ts
@@ -23,9 +23,9 @@
 import 'classlist.js';  // Run `npm install --save classlist.js`.
 
 /** Evergreen browsers require these. **/
-import 'core-js/es6/reflect';
+/** import 'core-js/es6/reflect';
 import 'core-js/es7/reflect';
-import 'core-js/es7/array';
+import 'core-js/es7/array'; **/
 
 
 /**
