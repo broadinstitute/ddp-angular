b12e386ebd8dd274632fb0cdb8e7a63ccb62bcd7
Pavel Sidorenko <fat.cat.1703@gmail.com>
2020-07-21 18:10:32 +0300

    Intro block

--- ddp-workspace/projects/ddp-brain-v2/src/app/components/welcome/welcome.component.ts
+++ ddp-workspace/projects/ddp-brain-v2/src/app/components/welcome/welcome.component.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import { AppRoutes } from '../../app-routes';
 
 @Component({
   selector: 'app-welcome',
   templateUrl: './welcome.component.html',
   styleUrls: ['./welcome.component.scss']
 })
-export class WelcomeComponent { }
+export class WelcomeComponent {
+  public appRoutes = AppRoutes;
+}
