b08f396ca2d7a76a9611d05cd69e7c8a804929bc
Pavel Sidorenko <fat.cat.1703@gmail.com>
2020-07-22 14:48:37 +0300

    About section

--- ddp-workspace/projects/ddp-brain-v2/src/app/components/welcome/welcome.component.ts
+++ ddp-workspace/projects/ddp-brain-v2/src/app/components/welcome/welcome.component.ts
@@ -1,11 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, Inject, OnInit } from '@angular/core';
 import { AppRoutes } from '../../app-routes';
+import { ToolkitConfigurationService } from 'toolkit';
 
 @Component({
   selector: 'app-welcome',
   templateUrl: './welcome.component.html',
   styleUrls: ['./welcome.component.scss']
 })
-export class WelcomeComponent {
+export class WelcomeComponent implements OnInit {
   public appRoutes = AppRoutes;
+  public cmiUrl: string;
+
+  constructor(@Inject('toolkit.toolkitConfig') private config: ToolkitConfigurationService) { }
+
+  public ngOnInit(): void {
+    this.cmiUrl = this.config.countMeInUrl;
+  }
 }
