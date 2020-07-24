07ab658d9deaab42e7d001b4dba883973d46d13a
Pavel Sidorenko <fat.cat.1703@gmail.com>
2020-07-24 11:25:54 +0300

    Migrate MPC to Angular 10

--- ddp-workspace/projects/ddp-mpc/src/polyfills.ts
+++ ddp-workspace/projects/ddp-mpc/src/polyfills.ts
@@ -22,9 +22,9 @@
 import 'classlist.js';  // Run `npm install --save classlist.js`.
 
 /** Evergreen browsers require these. **/
-import 'core-js/es6/reflect';
+/** import 'core-js/es6/reflect';
 import 'core-js/es7/reflect';
-import 'core-js/es7/array';
+import 'core-js/es7/array'; **/
 
 
 /**
