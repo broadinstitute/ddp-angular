95517a84b2448885ed8f2e1d3bdfacc4efb2c900
Marat Mamytov <mmarat@broadinstitute.org>
2020-07-28 15:43:27 +0600

    4560 | AC8 - added privacy policy static page linked at footer & banner (with AC1, AC2, AC3, AC5 from 4519)

--- ddp-workspace/projects/ddp-sdk/src/public-api.ts
+++ ddp-workspace/projects/ddp-sdk/src/public-api.ts
@@ -82,6 +82,8 @@ export * from './lib/components/address/addressEmbedded.component';
 export * from './lib/components/activityForm/activity.component';
 export * from './lib/components/changeLanguageRedirect.component';
 export * from './lib/components/cookies/cookiesBanner/cookiesBanner.component';
+export * from './lib/components/cookies/privacyPolicy/privacyPolicyModal.component';
+export * from './lib/components/cookies/privacyPolicy/privacyPolicyStatic.component';
 
 export * from './lib/guards/auth.guard';
 export * from './lib/guards/adminAuth.guard';
