86b2cf8ef67aa48e0ce12fe6a4ed3e7fb74b5b5d
Marat Mamytov <mmarat@broadinstitute.org>
2020-07-28 12:32:36 +0600

    4560 | AC1 - add cookies list draft to config

--- ddp-workspace/projects/ddp-sdk/src/lib/services/configuration.service.ts
+++ ddp-workspace/projects/ddp-sdk/src/lib/services/configuration.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { LogLevel } from '../models/logLevel';
 import { QuestionType } from './../models/activity/questionType';
+import { Cookies } from '../models/cookies';
 
 @Injectable()
 export class ConfigurationService {
@@ -48,4 +49,5 @@ export class ConfigurationService {
     compositeRequiredFieldExceptions: QuestionType[] = [];
     // this property reflects offset from the top of the page when we scroll to invalid question
     scrollToErrorOffset = 100;
+    cookies: Cookies | null;
 }
