98c94408e4aeecaaf50d4a912b7d5ea9da875d26
Marat Mamytov <mmarat@broadinstitute.org>
2020-07-30 15:57:56 +0600

    4560 | revert footer change

--- ddp-workspace/projects/toolkit-prion/src/lib/components/footer/prionFooter.component.ts
+++ ddp-workspace/projects/toolkit-prion/src/lib/components/footer/prionFooter.component.ts
@@ -13,7 +13,7 @@ import { FooterComponent, ToolkitConfigurationService } from "toolkit";
   selector: 'prion-footer',
   template: `
     <footer class="Footer row">
-      <div class="col-lg-4 col-md-4 col-sm-4 col-xs-12 Footer-left">
+      <div class="col-lg-6 col-md-4 col-sm-4 col-xs-12 Footer-left">
         <a href [routerLink]="['/home']">
           <img lazy-resource src="/assets/images/project-logo-dark.svg" [attr.alt]="'Toolkit.Common.LogoAlt' | translate">
         </a>
@@ -29,12 +29,12 @@ import { FooterComponent, ToolkitConfigurationService } from "toolkit";
           <span *ngIf="!isLoggedIn()"  (click)="clickJoinUs()" class="Footer-link" translate>Toolkit.Common.JoinUs</span>
           <span *ngIf="isLoggedIn()"  (click)="clickDashboard()" class="Footer-link" translate>Toolkit.Common.Dashboard</span>
           <ddp-sign-in-out class="Footer-link"></ddp-sign-in-out>
-          <button (click)="goToTop()" class="Footer-link">
-            <i class="fas fa-arrow-up Footer-arrow"></i>
-            <span translate>Toolkit.Footer.BackToTop</span>
-          </button>
+          <button (click)="goToTop()" class="Footer-link"><i class="fas fa-arrow-up Footer-arrow"></i><span translate>Toolkit.Footer.BackToTop</span></button>
         </p>
       </div>
+
+
+
     </footer>
   `
 })
