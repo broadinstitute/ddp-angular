84ed1ef9a9b502043a245141109499466e832eea
cdye <39769705+charlottedye@users.noreply.github.com>
2020-08-11 13:19:53 -0400

    DDP-4796: Prion, privacy policy page: Finished privacy policy and added stub for third party page.

--- ddp-workspace/projects/ddp-prion/src/app/app-routing.module.ts
+++ ddp-workspace/projects/ddp-prion/src/app/app-routing.module.ts
@@ -28,6 +28,7 @@ import { LearnMoreComponent } from './components/learn-more/learn-more.component
 import { StudyListingComponent } from './components/study-listing-component/study-listing.component';
 import { RedirectJoinComponent } from './components/redirect-join/redirect-join.component';
 import { PrivacyPolicyFullComponent } from "./components/privacy-policy/privacy-policy-full.component";
+import { ThirdPartyComponent } from "./components/third-party/third-party.component";
 
 const routes: Routes = [
   {
@@ -53,6 +54,11 @@ const routes: Routes = [
     data: {
       activityGuid: 'PRIONMEDICAL'
     }
+  },
+  {
+    path: '3rd-party',
+    component: ThirdPartyComponent,
+    canActivate: [IrbGuard]
   },
   {
     path: 'study-listing',
