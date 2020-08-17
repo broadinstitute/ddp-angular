9e06eb94a23bdaf382c9186b68f24d898c166cac
Mikhail Aleksandrov <aleksand@broadinstitute.org>
2020-08-17 18:30:27 +0300

    add AccountActivationRequired, EmailConfirmed messages

---
+++ ddp-workspace/projects/ddp-atcp/src/app/components/account-activation/accountActivated.ts
@@ -0,0 +1,41 @@
+import {Component, Inject, OnDestroy, OnInit} from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { of } from 'rxjs';
+import { Auth0AdapterService, CompositeDisposable, NGXTranslateService } from 'ddp-sdk';
+import { CommunicationService } from 'toolkit';
+import { ServerMessage } from '../../../../../toolkit/src/lib/models/serverMessage';
+import { delay } from 'rxjs/operators';
+import { ToolkitConfigurationService } from 'toolkit';
+@Component({
+    selector: 'app-account-activation',
+    template: `<p></p>`
+})
+export class AccountActivatedComponent implements OnInit, OnDestroy {
+
+  private anchor = new CompositeDisposable();
+
+  constructor(
+        private route: ActivatedRoute,
+        private auth0: Auth0AdapterService,
+        private communicationService: CommunicationService,
+        private ngxTranslate: NGXTranslateService,
+        private router: Router,
+        @Inject('toolkit.toolkitConfig') private toolkitConfiguration: ToolkitConfigurationService) { }
+
+    public ngOnInit(): void {
+      this.anchor.addNew(this.ngxTranslate.getTranslation('AccountActivation.EmailConfirmed')
+        .subscribe(translationResult => {
+        this.communicationService
+          .showMessageFromServer(new ServerMessage(translationResult,
+            false));
+          of('').pipe(delay(4000)).toPromise().then(() => {
+            this.communicationService.closeMessageFromServer();
+            this.router.navigateByUrl(this.toolkitConfiguration.dashboardUrl);
+          });
+      }));
+    }
+
+  ngOnDestroy(): void {
+    this.anchor.removeAll();
+  }
+}
--- ddp-workspace/projects/ddp-sdk/src/lib/components/login/auth0-code-callback.component.ts
+++ ddp-workspace/projects/ddp-sdk/src/lib/components/login/auth0-code-callback.component.ts
@@ -1,12 +1,13 @@
 import { Component, Inject, OnInit, OnDestroy } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { ConfigurationService } from '../../services/configuration.service';
 import { Auth0AdapterService } from '../../services/authentication/auth0Adapter.service';
 import { LoggingService } from '../../services/logging.service';
 import { SessionMementoService } from '../../services/sessionMemento.service';
 import { WindowRef } from '../../services/windowRef';
 import { Subscription } from 'rxjs';
+import { ToolkitConfigurationService } from 'toolkit';
 
 @Component({
     selector: 'ddp-auth0-code-callback',
@@ -14,7 +15,7 @@ import { Subscription } from 'rxjs';
 })
 export class Auth0CodeCallbackComponent implements OnInit, OnDestroy {
     private LOG_SOURCE = 'Auth0CodeCallback';
-    private anchor: Subscription;
+    private anchor: Subscription = null;
 
     constructor(
         @Inject('ddp.config') private configuration: ConfigurationService,
@@ -23,15 +24,23 @@ export class Auth0CodeCallbackComponent implements OnInit, OnDestroy {
         private log: LoggingService,
         private httpClient: HttpClient,
         private route: ActivatedRoute,
-        private windowRef: WindowRef) { }
+        private windowRef: WindowRef,
+        private router: Router) { }
 
     public ngOnInit(): void {
         this.log.logEvent(this.LOG_SOURCE, 'post-auth0 callback code for ' + this.windowRef.nativeWindow.location + ':' + location.hash);
+        const authError = this.route.snapshot.queryParams['error'];
+       if (authError) {
+         const authErrorDescription = this.route.snapshot.queryParams['error_description'];
+         if (JSON.parse(authErrorDescription).code === 'unauthorized') {
+           this.router.navigateByUrl('account-activation-required');
+           return;
+         }
+       }
         const authCode = this.route.snapshot.queryParams['code'];
 
         if (authCode) {
             this.log.logEvent(this.LOG_SOURCE, 'Will login to ' + this.configuration.localRegistrationUrl + ' using code ' + authCode);
-
             const params = this.consumeLocalAuthParams();
             const isAdmin = this.consumeLocalAdminAuth();
             const registrationPayload = {
@@ -71,7 +80,9 @@ export class Auth0CodeCallbackComponent implements OnInit, OnDestroy {
     }
 
     public ngOnDestroy(): void {
+      if (!!this.anchor) {
         this.anchor.unsubscribe();
+      }
     }
 
     private consumeLocalAuthParams(): object {
