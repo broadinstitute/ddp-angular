13fa5632b4baec4b85f7bd18b1af100febc5dcbd
Marat Mamytov <mmarat@broadinstitute.org>
2020-08-20 18:15:26 +0600

    4799 | small fixes

--- ddp-workspace/projects/ddp-prion/src/app/components/third-party/third-party.component.ts
+++ ddp-workspace/projects/ddp-prion/src/app/components/third-party/third-party.component.ts
@@ -9,23 +9,23 @@ import { Component } from '@angular/core';
       <p translate>App.ThirdParty.Description</p>
       <ul class="list">
         <li>
-          <a href="https://sendgrid.com/" translate>App.ThirdParty.Tools.Sendgrid</a>
+          <a href="https://sendgrid.com/" target="_blank" rel="noopener" translate>App.ThirdParty.Tools.Sendgrid</a>
           <span translate>App.ThirdParty.Usage.email</span>
         </li>
         <li>
-          <a href="https://auth0.com/" translate>App.ThirdParty.Tools.Auth0</a>
+          <a href="https://auth0.com/" target="_blank" rel="noopener" translate>App.ThirdParty.Tools.Auth0</a>
           <span translate>App.ThirdParty.Usage.authentication</span>
         </li>
         <li>
-          <a href="https://earlyaccess.rapid7.com/tcell/" translate>App.ThirdParty.Tools.Tcell</a>
+          <a href="https://earlyaccess.rapid7.com/tcell/" target="_blank" rel="noopener" translate>App.ThirdParty.Tools.Tcell</a>
           <span translate>App.ThirdParty.Usage.security</span>
         </li>
         <li>
-          <a href="https://cloud.google.com/" translate>App.ThirdParty.Tools.Google_Cloud</a>
+          <a href="https://cloud.google.com/" target="_blank" rel="noopener" translate>App.ThirdParty.Tools.Google_Cloud</a>
           <span translate>App.ThirdParty.Usage.host</span>
         </li>
         <li>
-          <a href="https://analytics.google.com/" translate>App.ThirdParty.Tools.Google_Analytics</a>
+          <a href="https://analytics.google.com/" target="_blank" rel="noopener" translate>App.ThirdParty.Tools.Google_Analytics</a>
           <span translate>App.ThirdParty.Usage.analytics</span>
         </li>
       </ul>
