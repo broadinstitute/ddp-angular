3ea29395c941217490f36df495e6f7fbb97315d1
Marat Mamytov <mmarat@broadinstitute.org>
2020-08-21 07:33:50 +0600

    4799 | add third-party vendor list page

--- ddp-workspace/projects/ddp-prion/src/app/components/third-party/third-party.component.ts
+++ ddp-workspace/projects/ddp-prion/src/app/components/third-party/third-party.component.ts
@@ -1,13 +1,40 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'third-party',
+  selector: 'app-third-party',
   template: `
     <prion-header currentRoute="/3rd-party"></prion-header>
     <div class="Container">
+      <h2 class="PageContent-title" translate>App.ThirdParty.Title</h2>
       <p translate>App.ThirdParty.Description</p>
+      <ul class="list">
+        <li>
+          <a href="https://sendgrid.com/" target="_blank" rel="noopener" translate>App.ThirdParty.Tools.Sendgrid</a>
+          <span translate>App.ThirdParty.Usage.email</span>
+        </li>
+        <li>
+          <a href="https://auth0.com/" target="_blank" rel="noopener" translate>App.ThirdParty.Tools.Auth0</a>
+          <span translate>App.ThirdParty.Usage.authentication</span>
+        </li>
+        <li>
+          <a href="https://earlyaccess.rapid7.com/tcell/" target="_blank" rel="noopener" translate>App.ThirdParty.Tools.Tcell</a>
+          <span translate>App.ThirdParty.Usage.security</span>
+        </li>
+        <li>
+          <a href="https://cloud.google.com/" target="_blank" rel="noopener" translate>App.ThirdParty.Tools.Google_Cloud</a>
+          <span translate>App.ThirdParty.Usage.host</span>
+        </li>
+        <li>
+          <a href="https://analytics.google.com/" target="_blank" rel="noopener" translate>App.ThirdParty.Tools.Google_Analytics</a>
+          <span translate>App.ThirdParty.Usage.analytics</span>
+        </li>
+      </ul>
+      <p class="third-party--date">
+        <span translate>App.ThirdParty.Last_updated</span>
+        <span>8/14/2020</span>
+      </p>
     </div>
-    `
+  `
 })
 export class ThirdPartyComponent  {
   constructor() { }
