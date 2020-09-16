f5f70fbd8093b6bb61ab64586f3dad37c7152a6f
cdye <39769705+charlottedye@users.noreply.github.com>
2020-09-16 11:07:59 -0400

    Prion: rebase fixes

--- ddp-workspace/projects/ddp-sdk/src/public-api.ts
+++ ddp-workspace/projects/ddp-sdk/src/public-api.ts
@@ -87,11 +87,6 @@ export * from './lib/components/address/addressEmbedded.component';
 export * from './lib/components/activityForm/activity.component';
 export * from './lib/components/changeLanguageRedirect.component';
 export * from './lib/components/user/activities/userActivitiesDataSource';
-export * from './lib/components/cookies/cookiesBanner/cookiesBanner.component';
-export * from './lib/components/privacy-policy/privacyPolicyButton.component';
-export * from './lib/components/cookies/cookiesPreferencesModal/cookiesPreferencesButton.component';
-export * from './lib/components/privacy-policy/privacyPolicyModal.component';
-export * from './lib/components/privacy-policy/prionPrivacyPolicy.component';
 
 export * from './lib/guards/auth.guard';
 export * from './lib/guards/adminAuth.guard';
