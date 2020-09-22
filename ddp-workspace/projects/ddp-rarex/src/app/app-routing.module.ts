80cecfdda6640005e8d17c635105f64b60420601
Marat Mamytov <mmarat@broadinstitute.org>
2020-09-22 12:34:25 +0600

    rarex | add components for dif pages

--- ddp-workspace/projects/ddp-rarex/src/app/app-routing.module.ts
+++ ddp-workspace/projects/ddp-rarex/src/app/app-routing.module.ts
@@ -1,13 +1,73 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-import { BrowserGuard, IrbGuard } from 'ddp-sdk';
+import {Auth0CodeCallbackComponent, AuthGuard, BrowserGuard, IrbGuard} from 'ddp-sdk';
 
 import { WelcomeComponent } from './components/welcome/welcome.component';
 import { RoutePaths } from './router-resources';
-import { PasswordRedesignedComponent, WorkflowStartActivityRedesignedComponent } from 'toolkit';
+import {
+  ActivityPageRedesignedComponent, DashboardRedesignedComponent,
+  LoginLandingRedesignedComponent,
+  PasswordRedesignedComponent,
+  WorkflowStartActivityRedesignedComponent
+} from 'toolkit';
 
 const routes: Routes = [
+    {
+      path: RoutePaths.Dashboard,
+      component: DashboardRedesignedComponent,
+      canActivate: [
+        IrbGuard,
+        BrowserGuard,
+        AuthGuard
+      ]
+    },
+    {
+      path: RoutePaths.LoginLanding,
+      component: LoginLandingRedesignedComponent,
+      canActivate: [IrbGuard]
+    },
+    {
+      path: RoutePaths.Consent,
+      component: ActivityPageRedesignedComponent,
+      canActivate: [
+        IrbGuard,
+        BrowserGuard,
+        AuthGuard
+      ],
+      data: {
+        activityGuid: 'CONSENT'
+      }
+    },
+    {
+      path: RoutePaths.Demographics,
+      component: ActivityPageRedesignedComponent,
+      canActivate: [
+        IrbGuard,
+        BrowserGuard,
+        AuthGuard
+      ],
+      data: {
+        activityGuid: 'DEMOGRAPHICS'
+      }
+    },
+    {
+      path: RoutePaths.MedicalBackground,
+      component: ActivityPageRedesignedComponent,
+      canActivate: [
+        IrbGuard,
+        BrowserGuard,
+        AuthGuard
+      ],
+      data: {
+        activityGuid: 'GENERAL_MEDICAL_BACKGROUND_SURVEY'
+      }
+    },
+    {
+      path: RoutePaths.Auth,
+      component: Auth0CodeCallbackComponent,
+      canActivate: [IrbGuard]
+    },
     {
       path: RoutePaths.ShareMyData,
       component: WorkflowStartActivityRedesignedComponent,
--- ddp-workspace/projects/ddp-rarex/src/app/router-resources.ts
+++ ddp-workspace/projects/ddp-rarex/src/app/router-resources.ts
@@ -1,5 +1,11 @@
 export enum RoutePaths {
   Welcome = '',
   Password = 'password',
-  ShareMyData = 'share-my-data'
+  ShareMyData = 'share-my-data',
+  Dashboard = 'dashboard',
+  LoginLanding = 'login-landing',
+  Consent = 'consent',
+  Demographics = 'demographics',
+  MedicalBackground = 'medical-background',
+  Auth = 'auth'
 }
