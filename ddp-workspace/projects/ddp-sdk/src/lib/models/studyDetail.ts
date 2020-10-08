df87b0eeee8c01d81d37bf79b9c0922e1f1bf790
cdye <39769705+charlottedye@users.noreply.github.com>
2020-10-08 10:11:49 -0400

    Prion: DDP-4912, language popup infrastructure: code review fix

--- ddp-workspace/projects/ddp-sdk/src/lib/models/studyDetail.ts
+++ ddp-workspace/projects/ddp-sdk/src/lib/models/studyDetail.ts
@@ -2,7 +2,7 @@ export class StudyDetail {
     public studyGuid: string | null;
     public name: string | null;
     public summary: number | null;
-    public registrationCount: number | null;
+    public registeredCount: number | null;
     public participantCount: number | null;
     public restricted: boolean | null;
     public studyEmail: string | null;
