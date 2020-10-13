21ff744aa7986f3e91613881ffbb345c8455c843
Pavel Sidorenko <fat.cat.1703@gmail.com>
2020-10-13 11:25:26 +0300

    Fix

--- ddp-workspace/projects/ddp-sdk/src/lib/services/internationalization/languageService.service.ts
+++ ddp-workspace/projects/ddp-sdk/src/lib/services/internationalization/languageService.service.ts
@@ -3,68 +3,68 @@ import { TranslateService, LangChangeEvent } from '@ngx-translate/core';
 import { isNullOrUndefined } from 'util';
 import { Observable, Subject } from 'rxjs';
 import { startWith } from 'rxjs/operators';
-import { LoggingService } from './logging.service';
+import { LoggingService } from '../logging.service';
 
 @Injectable()
 export class LanguageService {
-    private profileLanguageUpdateNotifier: Subject<void>;
-    private readonly LOG_SOURCE = 'LanguageService';
+  private profileLanguageUpdateNotifier: Subject<void>;
+  private readonly LOG_SOURCE = 'LanguageService';
 
-    constructor(
-      private logger: LoggingService,
-      private translate: TranslateService) {
-      this.profileLanguageUpdateNotifier = new Subject<void>();
-      this.profileLanguageUpdateNotifier.next();
+  constructor(
+    private logger: LoggingService,
+    private translate: TranslateService) {
+    this.profileLanguageUpdateNotifier = new Subject<void>();
+    this.profileLanguageUpdateNotifier.next();
+  }
+
+  public getCurrentLanguage(): string {
+    return this.translate.currentLang;
+  }
+
+  public onLanguageChange(): Observable<LangChangeEvent> {
+    return this.translate.onLangChange;
+  }
+
+  public canUseLanguage(languageCode: string): boolean {
+    if (isNullOrUndefined(languageCode)) {
+      return false;
     }
+    return this.translate.getLangs().includes(languageCode);
+  }
 
-    public getCurrentLanguage(): string {
-        return this.translate.currentLang;
+  public addLanguages(languageCodes: Array<string>): void {
+    this.translate.addLangs(languageCodes);
+  }
+
+  public useStoredLanguage(): string {
+    const loadedCode: string = localStorage.getItem('studyLanguage');
+    if (this.changeLanguage(loadedCode)) {
+      return loadedCode;
     }
+    return null;
+  }
 
-    public onLanguageChange(): Observable<LangChangeEvent> {
-      return this.translate.onLangChange;
-    }
+  public getProfileLanguageUpdateNotifier(): Observable<void> {
+    return this.profileLanguageUpdateNotifier.asObservable().pipe(
+      startWith(null as void)); // Make sure anything that updates when the language updates gets an initial value!
+  }
 
-    public canUseLanguage(languageCode: string): boolean {
-      if (isNullOrUndefined(languageCode)) {
-        return false;
-      }
-      return this.translate.getLangs().includes(languageCode);
-    }
+  public notifyOfProfileLanguageUpdate(): void {
+    this.profileLanguageUpdateNotifier.next();
+  }
 
-    public addLanguages(languageCodes: Array<string>): void {
-      this.translate.addLangs(languageCodes);
-    }
-
-    public useStoredLanguage(): string {
-      const loadedCode: string = localStorage.getItem('studyLanguage');
-      if (this.changeLanguage(loadedCode)) {
-        return loadedCode;
-      }
+  public changeLanguageObservable(languageCode: string): Observable<any> {
+    if (this.canUseLanguage(languageCode)) {
+      localStorage.setItem('studyLanguage', languageCode);
+      return this.translate.use(languageCode);
+    } else {
+      this.logger.logError(this.LOG_SOURCE, `Error: cannot use language ${languageCode}`);
       return null;
     }
+  }
 
-    public getProfileLanguageUpdateNotifier(): Observable<void> {
-      return this.profileLanguageUpdateNotifier.asObservable().pipe(
-        startWith(null as void)); // Make sure anything that updates when the language updates gets an initial value!
-    }
-
-    public notifyOfProfileLanguageUpdate(): void {
-      this.profileLanguageUpdateNotifier.next();
-    }
-
-    public changeLanguageObservable(languageCode: string): Observable<any> {
-      if (this.canUseLanguage(languageCode)) {
-        localStorage.setItem('studyLanguage', languageCode);
-        return this.translate.use(languageCode);
-      } else {
-        this.logger.logError(this.LOG_SOURCE, `Error: cannot use language ${languageCode}`);
-        return null;
-      }
-    }
-
-    public changeLanguage(languageCode: string): boolean {
-      const obs = this.changeLanguageObservable(languageCode);
-      return obs !== null;
-    }
+  public changeLanguage(languageCode: string): boolean {
+    const obs = this.changeLanguageObservable(languageCode);
+    return obs !== null;
+  }
 }
