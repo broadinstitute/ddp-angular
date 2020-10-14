9e323ed2a2dd291d295140bf8be68a6dad5523c0
cdye <39769705+charlottedye@users.noreply.github.com>
2020-10-14 13:15:11 -0400

    Prion: DDP-5072, add study listing to header: tweaked CSS and cleaned up

--- ddp-workspace/projects/ddp-prion/src/app/components/study-listing-component/study-listing.component.ts
+++ ddp-workspace/projects/ddp-prion/src/app/components/study-listing-component/study-listing.component.ts
@@ -8,21 +8,21 @@ import { StudyListingDataSource } from './study-listing-data-source';
 @Component({
   selector: 'app-study-listing',
   template: `<prion-header></prion-header>
-    <div class="Container table-container">
+    <div class="Container">
       <article class="PageContent">
         <div class="PageLayout">
           <div class="row NoMargin">
             <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
-              <section class="PageContent-section ">
+              <section class="PageContent-section">
                 <div class="scroll-table-container">
-                  <div class="row">
+                  <div class="row study-listing-filter-all-container">
                     <div class="col-md-4">
                       <input class="form-control study-listing-filter study-listing-filter-all" matInput
                              (keyup)="applyFilter($event)" [placeholder]="'App.StudyListing.InputPlaceholder' | translate">
                     </div>
                   </div>
                   <br/>
-                  <table mat-table [dataSource]="dataSource"  class="table dataTable table-bordered study-listing-table">
+                  <table mat-table [dataSource]="dataSource"  class="table dataTable table-bordered table--studyListing">
                     <ng-container [matColumnDef]="column" *ngFor="let column of displayedColumns; index as i">
                       <th mat-header-cell *matHeaderCellDef (click)="sortByCol(i)">
                         <span translate [innerHTML]="columns[i].columnTitleKey"></span>
@@ -37,7 +37,7 @@ import { StudyListingDataSource } from './study-listing-data-source';
                                matInput (keyup)="applyColumnFilter(i, $event)" [placeholder]="columns[i].filterInfo.filterPlaceholder | translate">
                       </th>
                     </ng-container>
-                    <tr mat-header-row *matHeaderRowDef="displayedColumns"></tr>
+                    <tr mat-header-row *matHeaderRowDef="displayedColumns" class="header-row"></tr>
                     <tr mat-header-row *matHeaderRowDef="filterColumns" class="filter-row"></tr>
                     <tr mat-row *matRowDef="let row; columns: displayedColumns"></tr>
                   </table>
--- ddp-workspace/projects/toolkit-prion/src/lib/components/app/prionApp.component.ts
+++ ddp-workspace/projects/toolkit-prion/src/lib/components/app/prionApp.component.ts
@@ -1,22 +1,23 @@
 import { Component, Inject, OnDestroy, OnInit } from '@angular/core';
 import { BrowserContentService, RenewSessionNotifier, UserProfileServiceAgent, WindowRef } from 'ddp-sdk';
-import {
-  CommunicationService, CookiesManagementService,
-  SessionWillExpireComponent,
-  ToolkitConfigurationService,
-  WarningComponent
-} from 'toolkit';
-import { ActivatedRoute, Router } from '@angular/router';
 import { MatDialog } from '@angular/material';
 import { Subscription } from 'rxjs';
 import { TranslateService } from '@ngx-translate/core';
 import { NoopScrollStrategy } from '@angular/cdk/overlay';
+import {
+  CommunicationService,
+  SessionWillExpireComponent,
+  ToolkitConfigurationService,
+  WarningComponent,
+  CookiesManagementService
+} from 'toolkit';
+
 
 @Component({
   selector: 'app-root',
   template: `
       <toolkit-warning-message *ngIf="unsupportedBrowser" class="warning-message"></toolkit-warning-message>
-      <div class="MainContainer">  <!--[ngClass]="scrollType"-->
+      <div class="MainContainer">
         <router-outlet></router-outlet>
       </div>
       <prion-footer></prion-footer>
@@ -24,11 +25,9 @@ import { NoopScrollStrategy } from '@angular/cdk/overlay';
     `
 })
 export class PrionAppComponent implements OnInit, OnDestroy {
-  // private scrollableRoutes: string[] = ['/study-listing'];
   public unsupportedBrowser: boolean;
   public showCookiesBanner: boolean;
   private anchor: Subscription = new Subscription();
-  // public scrollType: string;
   private readonly dialogBaseSettings = {
     width: '740px',
     position: { top: '30px' },
@@ -46,11 +45,7 @@ export class PrionAppComponent implements OnInit, OnDestroy {
     private windowRef: WindowRef,
     private userProfile: UserProfileServiceAgent,
     private cookiesManagementService: CookiesManagementService,
-    @Inject('toolkit.toolkitConfig') private toolkitConfiguration: ToolkitConfigurationService,
-    private activatedRoute: ActivatedRoute,
-    private router: Router) {
-    // this.scrollType = 'NoScrollContainer';
-  }
+    @Inject('toolkit.toolkitConfig') private toolkitConfiguration: ToolkitConfigurationService) { }
 
   public ngOnInit(): void {
     this.initBrowserWarningListener();
@@ -58,8 +53,6 @@ export class PrionAppComponent implements OnInit, OnDestroy {
     this.initHasToSetCookiesPreferencesListener();
     this.initTranslate();
     this.unsupportedBrowser = this.browserContent.unsupportedBrowser();
-    // const sub = this.router.events.subscribe(result => this.updateScrollType(result));
-    // this.anchor.add(sub);
   }
 
   public ngOnDestroy(): void {
@@ -77,18 +70,6 @@ export class PrionAppComponent implements OnInit, OnDestroy {
   public closeSessionWillExpireDialog(): void {
     this.dialog.closeAll();
   }
-
-  // private updateScrollType(event: any): void {
-  //   if (event instanceof RouterEvent) {
-  //     const routerEvent = event as RouterEvent;
-  //     if (this.scrollableRoutes.includes(routerEvent.url)) {
-  //      // this.scrollType = 'ScrollContainer';
-  //       this.scrollType = '';
-  //     } else {
-  //       //this.scrollType = 'NoScrollContainer';
-  //     }
-  //   }
-  // }
 
   private initBrowserWarningListener(): void {
     const modal = this.browserContent.events.subscribe(() => {
