4735b2ba4d224b0f7bc71600a29c98f747ae12c9
Pavel Sidorenko <fat.cat.1703@gmail.com>
2020-11-03 22:39:10 +0300

    Replace brain project (#323)
    Co-authored-by: Marco Ocana <mocana@broadinstitute.org>

--- ddp-workspace/projects/ddp-brain/src/app/app.module.ts
+++ ddp-workspace/projects/ddp-brain/src/app/app.module.ts
@@ -1,9 +1,11 @@
-import { NgModule, Injector, APP_INITIALIZER } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { NgModule, Injector, APP_INITIALIZER } from '@angular/core';
 import { LOCATION_INITIALIZED } from '@angular/common';
-import { AppRoutingModule } from './app-routing.module';
 
 import { TranslateService } from '@ngx-translate/core';
+
+import { AppRoutes } from './app-routes';
+import { ActivityGuids } from './aсtivity-guids';
 
 import {
   DdpModule,
@@ -15,13 +17,22 @@ import {
 
 import {
   ToolkitModule,
-  ToolkitConfigurationService,
-  AppComponent
+  ToolkitConfigurationService
 } from 'toolkit';
 
-import { AboutUsComponent } from './components/about-us/about-us.component';
-import { MoreDetailsComponent } from './components/more-details/more-details.component';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './components/app/app.component';
 import { WelcomeComponent } from './components/welcome/welcome.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { FaqComponent } from './components/faq/faq.component';
+import { DataComponent } from './components/data/data.component';
+import { AboutUsComponent } from './components/about-us/about-us.component';
+import { LearnMoreSectionComponent } from './components/learn-more-section/learn-more-section.component';
+
+import { MatIconModule } from '@angular/material/icon';
+import { MatExpansionModule } from '@angular/material/expansion';
+import { ModalImageComponent } from './components/modal-image/modal-image.component';
 
 const baseElt = document.getElementsByTagName('base');
 
@@ -32,51 +43,44 @@ if (baseElt) {
 
 declare const DDP_ENV: any;
 
-declare const ga: Function;
+declare const ga: (...args: any[]) => void;
 
-export const tkCfg = new ToolkitConfigurationService();
-tkCfg.studyGuid = DDP_ENV.studyGuid;
-tkCfg.aboutYouGuid = 'ABOUTYOU';
-tkCfg.consentGuid = 'CONSENT';
-tkCfg.releaseGuid = 'RELEASE';
-tkCfg.dashboardGuid = 'DASHBOARD';
-tkCfg.aboutYouUrl = 'about-you';
-tkCfg.aboutUsUrl = 'about-us';
-tkCfg.consentUrl = 'consent';
-tkCfg.releaseUrl = 'release-survey';
-tkCfg.dashboardUrl = 'dashboard';
-tkCfg.activityUrl = 'activity';
-tkCfg.errorUrl = 'error';
-tkCfg.stayInformedUrl = 'stay-informed';
-tkCfg.moreDetailsUrl = 'more-details';
-tkCfg.internationalPatientsUrl = 'international-patients';
-tkCfg.mailingListDialogUrl = 'updates';
-tkCfg.phone = '651-229-3480';
-tkCfg.infoEmail = 'info@braincancerproject.org';
-tkCfg.dataEmail = 'data@braincancerproject.org';
-tkCfg.twitterAccountId = 'BrainCancerProj';
-tkCfg.facebookGroupId = 'braincancerproject';
-tkCfg.countMeInUrl = 'https://joincountmein.org/';
-tkCfg.showDataRelease = false;
-tkCfg.showInfoForPhysicians = true;
-tkCfg.showBlog = false;
-tkCfg.blogUrl = '';
+export const toolkitConfig = new ToolkitConfigurationService();
+toolkitConfig.studyGuid = DDP_ENV.studyGuid;
+toolkitConfig.aboutYouGuid = ActivityGuids.AboutYou;
+toolkitConfig.consentGuid = ActivityGuids.Consent;
+toolkitConfig.releaseGuid = ActivityGuids.Release;
+toolkitConfig.dashboardGuid = ActivityGuids.Dashboard;
+toolkitConfig.aboutYouUrl = AppRoutes.AboutYou;
+toolkitConfig.consentUrl = AppRoutes.Consent;
+toolkitConfig.releaseUrl = AppRoutes.Release;
+toolkitConfig.dashboardUrl = AppRoutes.Dashboard;
+toolkitConfig.activityUrl = AppRoutes.Activity;
+toolkitConfig.errorUrl = AppRoutes.Error;
+toolkitConfig.stayInformedUrl = AppRoutes.StayInformed;
+toolkitConfig.mailingListDialogUrl = AppRoutes.MailingList;
+toolkitConfig.phone = '651-229-3480';
+toolkitConfig.infoEmail = 'info@braincancerproject.org';
+toolkitConfig.dataEmail = 'data@braincancerproject.org';
+toolkitConfig.twitterAccountId = 'BrainCancerProj';
+toolkitConfig.facebookGroupId = 'braincancerproject';
+toolkitConfig.countMeInUrl = 'https://joincountmein.org/';
 
-export const config = new ConfigurationService();
-config.backendUrl = DDP_ENV.basePepperUrl;
-config.auth0Domain = DDP_ENV.auth0Domain;
-config.auth0ClientId = DDP_ENV.auth0ClientId;
-config.studyGuid = DDP_ENV.studyGuid;
-config.logLevel = LogLevel.Info;
-config.baseUrl = location.origin + base;
-config.auth0SilentRenewUrl = DDP_ENV.auth0SilentRenewUrl;
-config.loginLandingUrl = DDP_ENV.loginLandingUrl;
-config.auth0CodeRedirect = location.origin + base + 'auth';
-config.localRegistrationUrl = config.backendUrl + '/pepper/v1/register';
-config.doLocalRegistration = DDP_ENV.doLocalRegistration;
-config.mapsApiKey = DDP_ENV.mapsApiKey;
-config.auth0Audience = DDP_ENV.auth0Audience;
-config.projectGAToken = DDP_ENV.projectGAToken;
+export const sdkConfig = new ConfigurationService();
+sdkConfig.backendUrl = DDP_ENV.basePepperUrl;
+sdkConfig.auth0Domain = DDP_ENV.auth0Domain;
+sdkConfig.auth0ClientId = DDP_ENV.auth0ClientId;
+sdkConfig.studyGuid = DDP_ENV.studyGuid;
+sdkConfig.logLevel = LogLevel.Info;
+sdkConfig.baseUrl = location.origin + base;
+sdkConfig.auth0SilentRenewUrl = DDP_ENV.auth0SilentRenewUrl;
+sdkConfig.loginLandingUrl = DDP_ENV.loginLandingUrl;
+sdkConfig.auth0CodeRedirect = location.origin + base + 'auth';
+sdkConfig.localRegistrationUrl = sdkConfig.backendUrl + '/pepper/v1/register';
+sdkConfig.doLocalRegistration = DDP_ENV.doLocalRegistration;
+sdkConfig.mapsApiKey = DDP_ENV.mapsApiKey;
+sdkConfig.auth0Audience = DDP_ENV.auth0Audience;
+sdkConfig.projectGAToken = DDP_ENV.projectGAToken;
 
 export function translateFactory(translate: TranslateService, injector: Injector) {
   return () => new Promise<any>((resolve: any) => {
@@ -96,25 +100,33 @@ export function translateFactory(translate: TranslateService, injector: Injector
 }
 
 @NgModule({
+  declarations: [
+    AppComponent,
+    WelcomeComponent,
+    HeaderComponent,
+    FooterComponent,
+    FaqComponent,
+    DataComponent,
+    AboutUsComponent,
+    ModalImageComponent,
+    LearnMoreSectionComponent
+  ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     DdpModule,
-    ToolkitModule
-  ],
-  declarations: [
-    AboutUsComponent,
-    MoreDetailsComponent,
-    WelcomeComponent
+    ToolkitModule,
+    MatIconModule,
+    MatExpansionModule
   ],
   providers: [
     {
       provide: 'ddp.config',
-      useValue: config
+      useValue: sdkConfig
     },
     {
       provide: 'toolkit.toolkitConfig',
-      useValue: tkCfg
+      useValue: toolkitConfig
     },
     {
       provide: APP_INITIALIZER,
@@ -124,7 +136,7 @@ export function translateFactory(translate: TranslateService, injector: Injector
         Injector
       ],
       multi: true
-    }
+    },
   ],
   bootstrap: [AppComponent]
 })
