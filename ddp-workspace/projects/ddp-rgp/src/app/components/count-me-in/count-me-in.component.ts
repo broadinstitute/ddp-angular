989361fa818c4c7f72703ff7742a87052c48427e
cdye <39769705+charlottedye@users.noreply.github.com>
2020-11-09 10:50:13 -0500

    RGP: DDP-5223, initial setup: Added workflow start activity to tell us your story page and various refactoring

--- ddp-workspace/projects/ddp-rgp/src/app/components/count-me-in/count-me-in.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-count-me-in',
-  templateUrl: './count-me-in.component.html',
-  styleUrls: ['./count-me-in.component.scss']
-})
-export class CountMeInComponent { }
