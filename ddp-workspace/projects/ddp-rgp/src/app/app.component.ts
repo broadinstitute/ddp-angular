2b65b8840a964a302857074923957f6abc403e61
Pavel Sidorenko <fat.cat.1703@gmail.com>
2020-11-18 10:10:47 +0300

    RGP angular 10

--- ddp-workspace/projects/ddp-rgp/src/app/app.component.ts
+++ ddp-workspace/projects/ddp-rgp/src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
-import { MatDialog } from '@angular/material';
+import { MatDialog } from '@angular/material/dialog';
 import { NoopScrollStrategy } from '@angular/cdk/overlay';
 import { SessionWillExpireComponent } from 'toolkit';
 import { CompositeDisposable, RenewSessionNotifier } from 'ddp-sdk';
