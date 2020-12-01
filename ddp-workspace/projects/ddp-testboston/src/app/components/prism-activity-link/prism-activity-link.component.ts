24efcff711582e63494aae594eb545ddfaaefb65
Yufeng Wang <yufeng@broadinstitute.org>
2020-12-01 17:11:50 -0500

    Update ddp-workspace/projects/ddp-testboston/src/app/components/prism-activity-link/prism-activity-link.component.ts
    Co-authored-by: Pavel Sidorenko <fat.cat.1703@gmail.com>

--- ddp-workspace/projects/ddp-testboston/src/app/components/prism-activity-link/prism-activity-link.component.ts
+++ ddp-workspace/projects/ddp-testboston/src/app/components/prism-activity-link/prism-activity-link.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Inject } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { take } from 'rxjs/operators';
 import {
