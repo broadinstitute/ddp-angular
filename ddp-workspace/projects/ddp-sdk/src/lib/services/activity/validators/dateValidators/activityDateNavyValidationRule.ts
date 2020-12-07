13d2ca70105e91c15110adff8865a6f7cb896042
Pavel Sidorenko <fat.cat.1703@gmail.com>
2020-12-07 18:11:25 +0300

    SDK Validators tests (#373)
    * Validators fixes
    
    * Tests

--- ddp-workspace/projects/ddp-sdk/src/lib/services/activity/validators/dateValidators/activityDateNavyValidationRule.ts
+++ ddp-workspace/projects/ddp-sdk/src/lib/services/activity/validators/dateValidators/activityDateNavyValidationRule.ts
@@ -24,7 +24,7 @@ export class ActivityDateNavyValidationRule extends ActivityAbstractValidationRu
         return true;
     }
 
-    public isDateReadyForSave(value: DatePickerValue): boolean {
+    private isDateReadyForSave(value: DatePickerValue): boolean {
         if (value.year === 0 || value.month === 0 || value.day === 0) {
             return false;
         }
