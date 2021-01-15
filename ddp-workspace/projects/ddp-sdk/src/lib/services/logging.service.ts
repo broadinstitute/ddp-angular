4a00655dbb227a27ab2992cd63d446dd209b4463
alextanlan <alextanlan@gmail.com>
2021-01-15 13:37:33 +0300

    Wire the GCP error reporting into our error logging
      e.g. if we call LoggingService.logError,
      the error is sent to GCP error report as well

--- ddp-workspace/projects/ddp-sdk/src/lib/services/logging.service.ts
+++ ddp-workspace/projects/ddp-sdk/src/lib/services/logging.service.ts
@@ -1,10 +1,19 @@
 import { Injectable, Inject } from '@angular/core';
 import { ConfigurationService } from './configuration.service';
 import { LogLevel } from '../models/logLevel';
+import {StackdriverErrorReporterService} from './stackdriverErrorReporter.service';
 
 @Injectable()
 export class LoggingService {
-    constructor(@Inject('ddp.config') private config: ConfigurationService) { }
+    constructor(
+        @Inject('ddp.config') private config: ConfigurationService,
+        private stackdriverErrorReporterService: StackdriverErrorReporterService
+    ) { }
+
+    private logErrorHandler = (...args) => {
+        this.stackdriverErrorReporterService.handleError(args.join(' '));
+        console.error.apply(window.console, args);
+    };
 
     public logDebug = this.showEvent(LogLevel.Debug) ? console.debug.bind(window.console) : () => { };
 
@@ -12,7 +21,7 @@ export class LoggingService {
 
     public logWarning = this.showEvent(LogLevel.Warning) ? console.warn.bind(window.console) : () => { };
 
-    public logError = this.showEvent(LogLevel.Error) ? console.error.bind(window.console) : () => { };
+    public logError = this.showEvent(LogLevel.Error) ? this.logErrorHandler : () => { };
 
     private showEvent(level: LogLevel): boolean {
         return this.config.logLevel <= level;
