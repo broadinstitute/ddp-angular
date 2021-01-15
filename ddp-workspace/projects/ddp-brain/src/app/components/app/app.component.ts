3c702be4d5d7d3b1eb60fdb067c3a8851e6d270b
alextanlan <alextanlan@gmail.com>
2021-01-15 16:22:20 +0300

    Test purpose commit

--- ddp-workspace/projects/ddp-brain/src/app/components/app/app.component.ts
+++ ddp-workspace/projects/ddp-brain/src/app/components/app/app.component.ts
@@ -1,6 +1,10 @@
 import { Component, Inject } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
-import { RenewSessionNotifier } from 'ddp-sdk';
+import {
+  RenewSessionNotifier,
+  LoggingService,
+  StackdriverErrorReporterService
+} from 'ddp-sdk';
 import { AppRedesignedBaseComponent, CommunicationService, ToolkitConfigurationService } from 'toolkit';
 import { Router } from '@angular/router';
 
@@ -15,7 +19,21 @@ export class AppComponent extends AppRedesignedBaseComponent {
     private _dialog: MatDialog,
     private _renewNotifier: RenewSessionNotifier,
     private _router: Router,
-    @Inject('toolkit.toolkitConfig') private _config: ToolkitConfigurationService) {
+    @Inject('toolkit.toolkitConfig') private _config: ToolkitConfigurationService,
+    private logger: LoggingService,
+    private stackdriverErrorReporterService: StackdriverErrorReporterService) {
     super(_communicationService, _dialog, _renewNotifier, _router, _config);
+
+    this.testIt();
+  }
+
+  private testIt() {
+    this.logger.logError('test error 1 via logger.logError');
+
+    try {
+      throw new Error('Test error');
+    } catch(e) {
+      this.stackdriverErrorReporterService.handleError('test error 2 via stackdriverErrorReporterService.handleError: ' + e);
+    }
   }
 }
