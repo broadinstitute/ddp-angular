f41d777ac0b00b78b7ad1b3f10048962fcb2d60f
alextanlan <alextanlan@gmail.com>
2021-01-18 17:13:37 +0300

    Init doGcpErrorReporting flag in app modules
      get it from pepperConfig
      for angio, brain, mbc, mpc, osteo, testboston

--- ddp-workspace/projects/ddp-angio/src/app/app.module.ts
+++ ddp-workspace/projects/ddp-angio/src/app/app.module.ts
@@ -84,6 +84,7 @@ config.auth0Audience = DDP_ENV.auth0Audience;
 config.projectGAToken = DDP_ENV.projectGAToken;
 config.errorReportingApiKey = DDP_ENV.errorReportingApiKey;
 config.projectGcpId = DDP_ENV.projectGcpId;
+config.doGcpErrorReporting = DDP_ENV.doGcpErrorReporting;
 
 export function translateFactory(translate: TranslateService, injector: Injector, logger: LoggingService) {
   return () => new Promise<any>((resolve: any) => {
