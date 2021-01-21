f416b02959c188278746635c03c5b6af252ee9de
alextanlan <alextanlan@gmail.com>
2021-01-21 16:30:44 +0300

    Fix code analyzer warnings
      - remove redundant "px" measure
      - shortened an import

--- ddp-workspace/projects/ddp-sdk/src/lib/components/activityForm/groupBlockList.component.ts
+++ ddp-workspace/projects/ddp-sdk/src/lib/components/activityForm/groupBlockList.component.ts
@@ -1,6 +1,6 @@
 import { Component, Input, Output, EventEmitter } from '@angular/core';
 import { ActivityGroupBlock } from '../../models/activity/activityGroupBlock';
-import { AnswerValue } from './../../models/activity/answerValue';
+import { AnswerValue } from '../../models/activity/answerValue';
 import { ListStyleHint } from '../../models/activity/listStyleHint';
 
 @Component({
