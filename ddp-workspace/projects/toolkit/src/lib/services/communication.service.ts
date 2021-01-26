0897ae16088e14b6db59b06796253666342093f4
alextanlan <alextanlan@gmail.com>
2021-01-26 14:41:04 +0300

    Fix linter errors/warnings for toolkit

--- ddp-workspace/projects/toolkit/src/lib/services/communication.service.ts
+++ ddp-workspace/projects/toolkit/src/lib/services/communication.service.ts
@@ -3,12 +3,12 @@ import { Observable, Subject } from 'rxjs';
 
 @Injectable()
 export class CommunicationService {
-  private openSidePanelSubject: Subject<void> = new Subject<void>();
-  private closeSidePanelSubject: Subject<void> = new Subject<void>();
-  private openJoinDialogSubject: Subject<void> = new Subject<void>();
   public openSidePanel$: Observable<void> = this.openSidePanelSubject.asObservable();
   public closeSidePanel$: Observable<void> = this.closeSidePanelSubject.asObservable();
   public openJoinDialog$: Observable<void> = this.openJoinDialogSubject.asObservable();
+  private openSidePanelSubject: Subject<void> = new Subject<void>();
+  private closeSidePanelSubject: Subject<void> = new Subject<void>();
+  private openJoinDialogSubject: Subject<void> = new Subject<void>();
 
   public openSidePanel(): void {
     this.openSidePanelSubject.next();
