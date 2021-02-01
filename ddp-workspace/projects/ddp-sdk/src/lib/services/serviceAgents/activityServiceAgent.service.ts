f15d8a5cea1c17940810979238b48eb746e73c5d
alextanlan <alextanlan@gmail.com>
2021-02-01 11:06:34 +0300

    Fix another linter deprecation warning

--- ddp-workspace/projects/ddp-sdk/src/lib/services/serviceAgents/activityServiceAgent.service.ts
+++ ddp-workspace/projects/ddp-sdk/src/lib/services/serviceAgents/activityServiceAgent.service.ts
@@ -8,11 +8,16 @@ import { SessionMementoService } from '../sessionMemento.service';
 import { LanguageService } from '../internationalization/languageService.service';
 import { AnswerValue } from '../../models/activity/answerValue';
 import { ActivityInstanceGuid } from '../../models/activityInstanceGuid';
-import { Observable, of, throwError } from 'rxjs';
-import { combineLatest, flatMap, catchError, map, switchMap } from 'rxjs/operators';
+import { combineLatest, Observable, of, throwError } from 'rxjs';
+import { mergeMap, catchError, map, switchMap } from 'rxjs/operators';
 import { AnswerSubmission } from '../../models/activity/answerSubmission';
 import { PatchAnswerResponse } from '../../models/activity/patchAnswerResponse';
 import { ActivityForm } from '../../models/activity/activityForm';
+
+interface GuidsObject {
+    study: string;
+    activity: string;
+}
 
 @Injectable()
 export class ActivityServiceAgent extends UserServiceAgent<any> {
@@ -26,34 +31,42 @@ export class ActivityServiceAgent extends UserServiceAgent<any> {
         super(session, configuration, http, logger, null);
     }
 
-    public getActivity(studyGuid: Observable<string | null>,
-        activityGuid: Observable<string | null>): Observable<ActivityForm> {
-        return this.__language.getProfileLanguageUpdateNotifier().pipe(
-          switchMap(() => studyGuid)).pipe(
-          combineLatest(activityGuid, (x, y) => {
-            return { study: x, activity: y };
-          }),
-          flatMap(x => {
+    public getActivity(studyGuid$: Observable<string | null>,
+                       activityGuid$: Observable<string | null>): Observable<ActivityForm> {
+
+        const studyGuidEmitted$: Observable<string | null> = this.__language.getProfileLanguageUpdateNotifier()
+            .pipe(
+                switchMap(() => studyGuid$)
+            );
+
+        let getActivity$: (x) => Observable<any>;
+        getActivity$ = (x: GuidsObject) => {
             if (x.study == null || x.study === '' ||
-              x.activity == null || x.activity === '') {
-              return of(null);
+                x.activity == null || x.activity === '') {
+                return of(null);
             }
             const baseUrl = this.getBaseUrl(x.study, x.activity);
             return this.getObservable(baseUrl, {}, [404]);
-          }, (x, y) => y),
-          catchError(e => {
-            if (e.error && e.error.code && e.error.code === 'ACTIVITY_NOT_FOUND') {
-              return throwError('ACTIVITY_NOT_FOUND');
-            }
-            return throwError(e);
-          }),
-          map(x => {
-            if (x == null) {
-              return null;
-            }
-            return this.converter.convertActivity(x);
-          })
-        );
+        };
+
+        return combineLatest([studyGuidEmitted$, activityGuid$]).pipe(
+            map((guids: Array<string>) => {
+                return {study: guids[0], activity: guids[1]} as GuidsObject;
+            }),
+            mergeMap((guidsObj: GuidsObject) => getActivity$(guidsObj)),
+            catchError(e => {
+                if (e.error && e.error.code && e.error.code === 'ACTIVITY_NOT_FOUND') {
+                    return throwError('ACTIVITY_NOT_FOUND');
+                }
+                return throwError(e);
+            }),
+            map(x => {
+                if (x == null) {
+                    return null;
+                }
+                return this.converter.convertActivity(x);
+            })
+          );
     }
 
     public saveAnswerSubmission(studyGuid: string,
