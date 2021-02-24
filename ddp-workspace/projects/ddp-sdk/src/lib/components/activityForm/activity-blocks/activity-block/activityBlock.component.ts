093f58965d69560ba28f9ba6c9ea63d92541617c
alextanlan <alextanlan@gmail.com>
2021-02-24 21:10:07 +0300

    Improve "add-siblings" button

--- ddp-workspace/projects/ddp-sdk/src/lib/components/activityForm/activity-blocks/activity-block/activityBlock.component.ts
+++ ddp-workspace/projects/ddp-sdk/src/lib/components/activityForm/activity-blocks/activity-block/activityBlock.component.ts
@@ -1,18 +1,20 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { ActivityActivityBlock } from '../../../../models/activity/activityActivityBlock';
 
+const DEBUG_DATA = {
+    title: `<span>Title 123</span>`,
+    instances: [1, 2, 3],
+    addButtonText: 'Add sibling or half-sibling',
+    allowMultiple: true
+} as any;
+
 @Component({
     selector: 'ddp-activity-block',
     templateUrl: './activity-block.component.html',
     styleUrls: ['./activity-block.component.scss']
 })
 export class ActivityBlockComponent implements OnInit {
-    @Input() block: ActivityActivityBlock = {
-        title: `<span>Title 123</span>`,
-        instances: [1, 2, 3],
-        addButtonText: 'My btn; ',
-        allowMultiple: true
-    } as any;
+    @Input() block: ActivityActivityBlock = DEBUG_DATA;
     @Input() readonly: boolean;
     @Input() validationRequested: boolean;
     @Input() studyGuid: string;
