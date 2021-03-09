ee28f3c28c8edd024f678b9d9cc8aea7aaa1a997
alextanlan <alextanlan@gmail.com>
2021-03-09 22:19:52 +0300

    Fix tests

--- ddp-workspace/projects/ddp-sdk/src/lib/components/activityForm/activity-blocks/activityBlock/activityBlock.component.spec.ts
+++ ddp-workspace/projects/ddp-sdk/src/lib/components/activityForm/activity-blocks/activityBlock/activityBlock.component.spec.ts
@@ -3,21 +3,24 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { ActivityBlockComponent } from './activityBlock.component';
 import { ActivityServiceAgent } from '../../../../services/serviceAgents/activityServiceAgent.service';
 import { ActivityRenderHintType } from '../../../../models/activity/activityRenderHintType';
+import { LoggingService } from 'ddp-sdk';
 
 describe('ActivityBlockComponent', () => {
     let component: ActivityBlockComponent;
     let fixture: ComponentFixture<ActivityBlockComponent>;
     let activityServiceAgentSpy: jasmine.SpyObj<ActivityServiceAgent>;
+    let loggingServiceSpy: jasmine.SpyObj<LoggingService>;
 
     beforeEach(async() => {
         activityServiceAgentSpy = jasmine.createSpyObj('ActivityServiceAgent', ['createInstance', 'getActivitySummary']);
+        loggingServiceSpy = jasmine.createSpyObj('LoggingService', ['logError']);
 
         await TestBed.configureTestingModule({
                 declarations: [ActivityBlockComponent],
-                providers: [{
-                    provide: ActivityServiceAgent,
-                    useValue: activityServiceAgentSpy
-                }]
+                providers: [
+                    {provide: ActivityServiceAgent, useValue: activityServiceAgentSpy},
+                    {provide: LoggingService, useValue: loggingServiceSpy}
+                ]
             })
             .compileComponents();
     });
