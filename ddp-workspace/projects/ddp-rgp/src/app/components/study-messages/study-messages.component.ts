7250cf6f632e7f9a6acad6c76dda0b6c5858db08
Aleksey Kuznetsov <akuznets@broadinstitute.org>
2021-03-26 16:28:17 +0300

    RGP: Dashboard refactoring

--- ddp-workspace/projects/ddp-rgp/src/app/components/study-messages/study-messages.component.ts
+++ ddp-workspace/projects/ddp-rgp/src/app/components/study-messages/study-messages.component.ts
@@ -1,6 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
-import { StudyMessagesService } from '../../services/study-messages.service';
 import { StudyMessage } from '../../models/StudyMessage';
 
 @Component({
@@ -8,28 +7,14 @@ import { StudyMessage } from '../../models/StudyMessage';
   templateUrl: './study-messages.component.html',
   styleUrls: ['./study-messages.component.scss'],
 })
-export class StudyMessagesComponent implements OnInit {
-  messages: StudyMessage[] = [];
+export class StudyMessagesComponent {
+  @Input() messages: StudyMessage[] = [];
   displayedColumns = ['date', 'title', 'description'];
   showMoreMap: Record<string, boolean> = {};
-
-  constructor(private studyMessagesService: StudyMessagesService) {}
-
-  ngOnInit(): void {
-    this.loadMessages();
-  }
 
   toggleShowMore({ description }: StudyMessage): void {
     const shown = !!this.showMoreMap[description];
 
     this.showMoreMap[description] = !shown;
-  }
-
-  private loadMessages(): void {
-    this.studyMessagesService.getMessages().subscribe(messages => {
-      console.log(messages);
-
-      this.messages = messages;
-    });
   }
 }
--- ddp-workspace/projects/ddp-rgp/src/app/components/user-activities/user-activities.component.ts
+++ ddp-workspace/projects/ddp-rgp/src/app/components/user-activities/user-activities.component.ts
@@ -1,13 +1,7 @@
-import { Component, Inject, OnDestroy, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { Router } from '@angular/router';
-import { of } from 'rxjs';
 
-import {
-  ActivityInstance,
-  CompositeDisposable,
-  ConfigurationService,
-  UserActivityServiceAgent,
-} from 'ddp-sdk';
+import { ActivityInstance } from 'ddp-sdk';
 
 import { ActivityStatusCode } from '../../constants/activity-status-code';
 import { Routes } from '../../routes';
@@ -23,44 +17,17 @@ enum StatusIcon {
   templateUrl: './user-activities.component.html',
   styleUrls: ['./user-activities.component.scss'],
 })
-export class UserActivitiesComponent implements OnInit, OnDestroy {
-  loading = false;
-  activities: ActivityInstance[] = [];
+export class UserActivitiesComponent {
+  @Input() activities: ActivityInstance[] = [];
   displayedColumns = ['name', 'summary', 'status', 'actions'];
   StatusIcon = StatusIcon;
   ActivityStatusCode = ActivityStatusCode;
-  private subs = new CompositeDisposable();
 
-  constructor(
-    private router: Router,
-    private userActivitiesService: UserActivityServiceAgent,
-    @Inject('ddp.config') private config: ConfigurationService,
-  ) {}
-
-  ngOnInit(): void {
-    this.loadActivities();
-  }
-
-  ngOnDestroy(): void {
-    this.subs.removeAll();
-  }
+  constructor(private router: Router) {}
 
   redirectToActivity(activity: ActivityInstance): void {
     const path = Routes.ActivityId.replace(':id', activity.instanceGuid);
 
     this.router.navigateByUrl(path);
-  }
-
-  private loadActivities(): void {
-    this.loading = true;
-
-    const sub = this.userActivitiesService
-      .getActivities(of(this.config.studyGuid))
-      .subscribe(activities => {
-        this.activities = activities;
-        this.loading = false;
-      });
-
-    this.subs.addNew(sub);
   }
 }
