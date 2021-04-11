ca60e556386098aa892aba2f739e4098554c5432
Marco Ocana <mocana@broadinstitute.org>
2021-04-11 19:13:24 -0400

    Remove hide/show functionality out of conditional block. Remove unused visibilityChanged attribute

--- ddp-workspace/projects/ddp-sdk/src/lib/components/activityForm/activity-blocks/conditionalBlock.component.ts
+++ ddp-workspace/projects/ddp-sdk/src/lib/components/activityForm/activity-blocks/conditionalBlock.component.ts
@@ -1,4 +1,4 @@
-import { Input, OnDestroy, OnInit, Component, Output, EventEmitter } from '@angular/core';
+import { Input, OnDestroy, OnInit, Component, Output, EventEmitter, ChangeDetectorRef } from '@angular/core';
 import { AnswerValue } from '../../../models/activity/answerValue';
 import { ConditionalBlock } from '../../../models/activity/conditionalBlock';
 import { BlockVisibility } from '../../../models/activity/blockVisibility';
@@ -13,8 +13,7 @@ import { CompositeDisposable } from '../../../compositeDisposable';
                             [validationRequested]="validationRequested"
                             [studyGuid]="studyGuid"
                             [activityGuid]="activityGuid"
-                            (valueChanged)="handleChange($event)"
-                            (visibilityChanged)="updateVisibility($event)">
+                            (valueChanged)="handleChange($event)">
     </ddp-activity-question>
     <ddp-group-block [block]="block.nestedGroupBlock"
                         [readonly]="readonly"
@@ -24,39 +23,15 @@ import { CompositeDisposable } from '../../../compositeDisposable';
                         (valueChanged)="handleChange($event)">
     </ddp-group-block>`
 })
-export class ConditionalBlockComponent implements OnInit, OnDestroy {
+export class ConditionalBlockComponent {
     @Input() block: ConditionalBlock;
     @Input() readonly: boolean;
     @Input() validationRequested: boolean;
     @Input() studyGuid: string;
     @Input() activityGuid: string;
     @Output() valueChanged: EventEmitter<AnswerValue> = new EventEmitter();
-    private anchor: CompositeDisposable;
-
-    constructor(private submissionManager: SubmissionManager) { }
-
-    public ngOnInit(): void {
-        const sub = this.submissionManager.answerSubmissionResponse$.subscribe((response) => {
-            this.updateVisibility(response.blockVisibility);
-        });
-        this.anchor = new CompositeDisposable(sub);
-    }
-
-    public ngOnDestroy(): void {
-        this.anchor.removeAll();
-    }
 
     public handleChange(value: AnswerValue): void {
         this.valueChanged.emit(value);
-    }
-
-    public updateVisibility(visibility: BlockVisibility[]): void {
-        visibility.forEach(shownBlockStatus => {
-            const matchingBlock = this.block.nestedGroupBlock
-                .nestedBlocks.find(nestedBlock => nestedBlock.id === shownBlockStatus.blockGuid);
-            if (matchingBlock) {
-                matchingBlock.shown = shownBlockStatus.shown;
-            }
-        });
     }
 }
