e853e9709edd9aa13aab750842dc4f5020b3201e
aglushko-broad <82094572+aglushko-broad@users.noreply.github.com>
2021-04-20 19:22:53 +0300

    DDP-5848 translate Data Release page + Sign Out + "Visit the Count Me In website" in Spanish (#514)
    Co-authored-by: Anastasiia_Glushkova <anastasiia.glushkova@clarivate.com>

--- ddp-workspace/projects/ddp-mbc/src/app/components/data-release/data-release.component.ts
+++ ddp-workspace/projects/ddp-mbc/src/app/components/data-release/data-release.component.ts
@@ -181,11 +181,11 @@ import { DisclaimerComponent, ToolkitConfigurationService } from 'toolkit';
                                 <p class="PageContent-text" translate>
                                     Toolkit.DataRelease.Glossary.Text
                                 </p>
-                                <div id="NCITermDictionaryWidgetEnglish">
+                                <div [id]="'Toolkit.DataRelease.Glossary.WidgetWrapperId' | translate">
                                     <iframe lazy-resource
                                             frameborder="0"
-                                            src="https://www.cancer.gov/widgets/TermDictionaryWidgetEnglish"
-                                            id="NCITermDictionaryWidgetContainerEnglish"
+                                            [src]="'Toolkit.DataRelease.Glossary.WidgetLink' | translate"
+                                            [id]="'Toolkit.DataRelease.Glossary.WidgetId' | translate"
                                             title="https://www.mbcproject.org/data-release"
                                             name="https://www.mbcproject.org/data-release"
                                             style="width: 100%; height: 300px;">
