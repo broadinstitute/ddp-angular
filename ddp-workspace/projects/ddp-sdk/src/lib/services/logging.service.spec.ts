d28fd183f2ebb59a3a0f411e3a6d98ba99d6cce7
alextanlan <alextanlan@gmail.com>
2021-05-04 22:09:31 +0300

    Revert debugging comments

--- ddp-workspace/projects/ddp-sdk/src/lib/services/logging.service.spec.ts
+++ ddp-workspace/projects/ddp-sdk/src/lib/services/logging.service.spec.ts
@@ -5,72 +5,72 @@ import { LogLevel } from '../models/logLevel';
 import { StackdriverErrorReporterService } from './stackdriverErrorReporter.service';
 
 describe('LoggingService', () => {
-    // let service: LoggingService;
-    // let config: ConfigurationService;
-    // const stackdriverErrorReporterServiceSpy = jasmine.createSpyObj('StackdriverErrorReporterService', ['handleError']);
+    let service: LoggingService;
+    let config: ConfigurationService;
+    const stackdriverErrorReporterServiceSpy = jasmine.createSpyObj('StackdriverErrorReporterService', ['handleError']);
 
-    // beforeEach(() => {
-    //     TestBed.configureTestingModule({
-    //         providers: [
-    //             LoggingService,
-    //             { provide: 'ddp.config', useValue: new ConfigurationService() },
-    //             { provide: StackdriverErrorReporterService, useValue: stackdriverErrorReporterServiceSpy }
-    //         ]
-    //     });
-    //
-    //   config = new ConfigurationService();
-    // });
-    //
-    // it('should create service', () => {
-    //     service = new LoggingService(config, stackdriverErrorReporterServiceSpy);
-    //     expect(service).toBeTruthy();
-    // });
-    //
-    // it('should create all types of loggers', () => {
-    //     config.logLevel = LogLevel.Debug;
-    //     service = new LoggingService(config, stackdriverErrorReporterServiceSpy);
-    //
-    //     expect(service.logDebug).not.toEqual(() => { });
-    //     expect(service.logEvent).not.toEqual(() => { });
-    //     expect(service.logWarning).not.toEqual(() => { });
-    //     expect(service.logError).not.toEqual(() => { });
-    // });
-    //
-    // it('should create only Info, Warning and Error loggers', () => {
-    //     config.logLevel = LogLevel.Info;
-    //     service = new LoggingService(config, stackdriverErrorReporterServiceSpy);
-    //
-    //     expect(JSON.stringify(service.logDebug)).toEqual(JSON.stringify(() => { }));
-    //     expect(service.logEvent).not.toEqual(() => { });
-    //     expect(service.logWarning).not.toEqual(() => { });
-    //     expect(service.logError).not.toEqual(() => { });
-    // });
-    //
-    // it('should create only Warning and Error loggers', () => {
-    //     config.logLevel = LogLevel.Warning;
-    //     service = new LoggingService(config, stackdriverErrorReporterServiceSpy);
-    //
-    //     expect(JSON.stringify(service.logDebug)).toEqual(JSON.stringify(() => { }));
-    //     expect(JSON.stringify(service.logEvent)).toEqual(JSON.stringify(() => { }));
-    //     expect(service.logWarning).not.toEqual(() => { });
-    //     expect(service.logError).not.toEqual(() => { });
-    // });
-    //
-    // it('should create only Error loggers', () => {
-    //     config.logLevel = LogLevel.Error;
-    //     service = new LoggingService(config, stackdriverErrorReporterServiceSpy);
-    //
-    //     expect(JSON.stringify(service.logDebug)).toEqual(JSON.stringify(() => { }));
-    //     expect(JSON.stringify(service.logEvent)).toEqual(JSON.stringify(() => { }));
-    //     expect(JSON.stringify(service.logWarning)).toEqual(JSON.stringify(() => { }));
-    //     expect(service.logError).not.toEqual(() => { });
-    // });
-    //
-    // it('should call StackdriverErrorReporterService.handleReport if logLevel is Error', () => {
-    //   config.logLevel = LogLevel.Error;
-    //   service = new LoggingService(config, stackdriverErrorReporterServiceSpy);
-    //
-    //   service.logError('an error');
-    //   expect(stackdriverErrorReporterServiceSpy.handleError).toHaveBeenCalledWith('an error');
-    // });
+    beforeEach(() => {
+        TestBed.configureTestingModule({
+            providers: [
+                LoggingService,
+                { provide: 'ddp.config', useValue: new ConfigurationService() },
+                { provide: StackdriverErrorReporterService, useValue: stackdriverErrorReporterServiceSpy }
+            ]
+        });
+
+      config = new ConfigurationService();
+    });
+
+    it('should create service', () => {
+        service = new LoggingService(config, stackdriverErrorReporterServiceSpy);
+        expect(service).toBeTruthy();
+    });
+
+    it('should create all types of loggers', () => {
+        config.logLevel = LogLevel.Debug;
+        service = new LoggingService(config, stackdriverErrorReporterServiceSpy);
+
+        expect(service.logDebug).not.toEqual(() => { });
+        expect(service.logEvent).not.toEqual(() => { });
+        expect(service.logWarning).not.toEqual(() => { });
+        expect(service.logError).not.toEqual(() => { });
+    });
+
+    it('should create only Info, Warning and Error loggers', () => {
+        config.logLevel = LogLevel.Info;
+        service = new LoggingService(config, stackdriverErrorReporterServiceSpy);
+
+        expect(JSON.stringify(service.logDebug)).toEqual(JSON.stringify(() => { }));
+        expect(service.logEvent).not.toEqual(() => { });
+        expect(service.logWarning).not.toEqual(() => { });
+        expect(service.logError).not.toEqual(() => { });
+    });
+
+    it('should create only Warning and Error loggers', () => {
+        config.logLevel = LogLevel.Warning;
+        service = new LoggingService(config, stackdriverErrorReporterServiceSpy);
+
+        expect(JSON.stringify(service.logDebug)).toEqual(JSON.stringify(() => { }));
+        expect(JSON.stringify(service.logEvent)).toEqual(JSON.stringify(() => { }));
+        expect(service.logWarning).not.toEqual(() => { });
+        expect(service.logError).not.toEqual(() => { });
+    });
+
+    it('should create only Error loggers', () => {
+        config.logLevel = LogLevel.Error;
+        service = new LoggingService(config, stackdriverErrorReporterServiceSpy);
+
+        expect(JSON.stringify(service.logDebug)).toEqual(JSON.stringify(() => { }));
+        expect(JSON.stringify(service.logEvent)).toEqual(JSON.stringify(() => { }));
+        expect(JSON.stringify(service.logWarning)).toEqual(JSON.stringify(() => { }));
+        expect(service.logError).not.toEqual(() => { });
+    });
+
+    it('should call StackdriverErrorReporterService.handleReport if logLevel is Error', () => {
+      config.logLevel = LogLevel.Error;
+      service = new LoggingService(config, stackdriverErrorReporterServiceSpy);
+
+      service.logError('an error');
+      expect(stackdriverErrorReporterServiceSpy.handleError).toHaveBeenCalledWith('an error');
+    });
 });
