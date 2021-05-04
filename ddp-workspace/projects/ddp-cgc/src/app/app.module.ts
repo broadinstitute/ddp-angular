b30e4c46143b8695cae94938c5429086395ec149
Aleksey Kuznetsov <akuznets@broadinstitute.org>
2021-05-04 18:20:38 +0300

    CGC: Fix home page markup & extract translate factory to separate file

--- ddp-workspace/projects/ddp-cgc/src/app/app.module.ts
+++ ddp-workspace/projects/ddp-cgc/src/app/app.module.ts
@@ -1,5 +1,4 @@
 import { NgModule, Injector, APP_INITIALIZER } from '@angular/core';
-import { CommonModule, LOCATION_INITIALIZED } from '@angular/common';
 import { BrowserModule } from '@angular/platform-browser';
 import { TranslateService } from '@ngx-translate/core';
 
@@ -15,6 +14,7 @@ import { ToolkitModule, ToolkitConfigurationService } from 'toolkit';
 import { FooterComponent } from './components/footer/footer.component';
 import { HeaderComponent } from './components/header/header.component';
 import { HomeComponent } from './components/pages/home/home.component';
+import { translateFactory } from './util/translateFactory';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
@@ -50,47 +50,6 @@ ddpCfg.doGcpErrorReporting = DDP_ENV.doGcpErrorReporting;
  */
 export const tkCfg = new ToolkitConfigurationService();
 tkCfg.studyGuid = DDP_ENV.studyGuid;
-
-export function translateFactory(
-  translate: TranslateService,
-  injector: Injector,
-  logger: LoggingService,
-  languageService: LanguageService,
-): () => Promise<any> {
-  return () =>
-    new Promise<any>(resolve => {
-      const LOG_SOURCE = 'AppModule';
-      const locationInitialized = injector.get(
-        LOCATION_INITIALIZED,
-        Promise.resolve(null),
-      );
-
-      locationInitialized.then(() => {
-        const locale = languageService.getAppLanguageCode();
-
-        translate.setDefaultLang(locale);
-
-        translate.use(locale).subscribe(
-          () => {
-            logger.logEvent(
-              LOG_SOURCE,
-              `Successfully initialized '${locale}' language as default.`,
-            );
-          },
-          err => {
-            logger.logError(
-              LOG_SOURCE,
-              `Problem with '${locale}' language initialization:`,
-              err,
-            );
-          },
-          () => {
-            resolve(null);
-          },
-        );
-      });
-    });
-}
 
 @NgModule({
   declarations: [AppComponent, FooterComponent, HeaderComponent, HomeComponent],
