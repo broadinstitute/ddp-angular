90d91c08455e430998603f950491fe749931ab3a
PavelYakupov <pyakupov@broadinstitute.org>
2021-05-05 17:48:17 +0300

    DDP-5888 Fixed links in header

--- ddp-workspace/projects/ddp-cgc/src/app/components/header/header.component.ts
+++ ddp-workspace/projects/ddp-cgc/src/app/components/header/header.component.ts
@@ -1,15 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { Route } from '../../constants/route';
+
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
-export class HeaderComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+export class HeaderComponent {
+  Route = Route;
 }
