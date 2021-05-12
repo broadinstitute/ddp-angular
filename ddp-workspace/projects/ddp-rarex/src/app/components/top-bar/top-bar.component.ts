1cd3a1913b20bcfa5bf3d09849dada9925c672c8
Aleksey Kuznetsov <akuznets@broadinstitute.org>
2021-05-12 11:48:17 +0300

    Rare-X | DDP-6067: Display user's first and last name in top bar

--- ddp-workspace/projects/ddp-rarex/src/app/components/top-bar/top-bar.component.ts
+++ ddp-workspace/projects/ddp-rarex/src/app/components/top-bar/top-bar.component.ts
@@ -1,22 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
-import { SessionMementoService } from 'ddp-sdk';
+import { SessionMementoService, UserProfileServiceAgent } from 'ddp-sdk';
+import { filter, map, switchMap, take } from 'rxjs/operators';
 
 import { RoutePaths } from '../../router-resources';
 import { GovernedUserService } from '../../services/governed-user.service';
+
+interface Profile {
+  firstName: string;
+  lastName: string;
+}
 
 @Component({
   selector: 'app-top-bar',
   templateUrl: './top-bar.component.html',
   styleUrls: ['./top-bar.component.scss'],
 })
-export class TopBarComponent {
+export class TopBarComponent implements OnInit {
+  profile: Profile | null = null;
   readonly routes = RoutePaths;
 
   constructor(
     private sessionService: SessionMementoService,
+    private userProfileService: UserProfileServiceAgent,
     private governedUserService: GovernedUserService,
   ) {}
+
+  ngOnInit(): void {
+    this.checkUser();
+  }
 
   get isAuthenticated(): boolean {
     return this.sessionService.isAuthenticatedSession();
@@ -30,5 +42,28 @@ export class TopBarComponent {
     }
 
     return isGoverned ? RoutePaths.ParticipantsList : RoutePaths.Dashboard;
+  }
+
+  private checkUser(): void {
+    this.sessionService.sessionObservable
+      .pipe(
+        filter(session => session !== null),
+        filter(
+          () =>
+            this.sessionService.isAuthenticatedSession() &&
+            !this.sessionService.session.participantGuid,
+        ),
+        switchMap(() => this.userProfileService.profile),
+        map(profile => ({
+          firstName: profile.profile.firstName,
+          lastName: profile.profile.lastName,
+        })),
+        filter(profile => !!profile.firstName && !!profile.lastName),
+        take(1),
+      )
+      .subscribe(profile => {
+        this.profile = profile;
+        console.log(this.profile);
+      });
   }
 }
