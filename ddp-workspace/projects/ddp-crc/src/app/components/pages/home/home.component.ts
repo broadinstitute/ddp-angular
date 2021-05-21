661fd3fc21086418e723479f3c5687ebab787d25
Aleksey Kuznetsov <akuznets@broadinstitute.org>
2021-05-21 13:57:53 +0300

    CRC: Implement home page scrolling to sections

--- ddp-workspace/projects/ddp-crc/src/app/components/pages/home/home.component.ts
+++ ddp-workspace/projects/ddp-crc/src/app/components/pages/home/home.component.ts
@@ -1,4 +1,8 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, OnDestroy, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { ReplaySubject } from 'rxjs';
+
+import { CompositeDisposable } from 'ddp-sdk';
 
 import { Route } from '../../../constants/route';
 
@@ -7,6 +11,55 @@ import { Route } from '../../../constants/route';
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss'],
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit, AfterViewInit, OnDestroy {
   Route = Route;
+  private fragment$ = new ReplaySubject<string | null | undefined>(1);
+  private headerHeight = 100;
+  private subs = new CompositeDisposable();
+
+  constructor(private route: ActivatedRoute) {}
+
+  ngOnInit(): void {
+    const routeFragmentSub = this.route.fragment.subscribe(fragment =>
+      this.fragment$.next(fragment),
+    );
+
+    this.subs.addNew(routeFragmentSub);
+  }
+
+  ngAfterViewInit(): void {
+    const fragmentSub = this.fragment$.subscribe(fragment => {
+      if (fragment === null) {
+        return;
+      }
+
+      if (fragment === undefined) {
+        return this.scrollToTop();
+      }
+
+      this.scrollToAnchor(fragment);
+    });
+
+    this.subs.addNew(fragmentSub);
+  }
+
+  ngOnDestroy(): void {
+    this.subs.removeAll();
+  }
+
+  private scrollToTop(): void {
+    window.scrollTo(0, 0);
+  }
+
+  private scrollToAnchor(anchor: string): void {
+    const el: HTMLElement = document.getElementById(anchor);
+
+    if (!el) {
+      return;
+    }
+
+    const yOffset = el.offsetTop - this.headerHeight;
+
+    window.scrollTo(0, yOffset);
+  }
 }
--- ddp-workspace/projects/ddp-crc/src/app/constants/route.ts
+++ ddp-workspace/projects/ddp-crc/src/app/constants/route.ts
@@ -1,9 +1,5 @@
 export enum Route {
   Home = '',
-  About = 'about',
-  Participation = 'participation',
-  FAQ = 'faq',
-  Team = 'team',
   JoinUs = 'join-us',
   StayInformed = 'stay-informed',
   PrivacyPolicy = 'privacy-policy',
