c607dc9feddbe146144ab9f668927110ee5d9899
Anastasiia_Glushkova <anastasiia.glushkova@clarivate.com>
2021-05-21 19:24:35 +0300

    DDP-5926 failOnEmptyTestSuite false

--- ddp-workspace/projects/ddp-prion/karma.conf.js
+++ ddp-workspace/projects/ddp-prion/karma.conf.js
@@ -39,6 +39,7 @@ module.exports = function (config) {
       }
     },
     singleRun: false,
-    restartOnFileChange: true
+    restartOnFileChange: true,
+    failOnEmptyTestSuite: false,
   });
 };
