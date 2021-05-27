2b3ac4afc28c2021fbae56021df09a34460eef85
PavelYakupov <pyakupov@broadinstitute.org>
2021-05-27 12:05:27 +0300

    sign-in-page-added

--- ddp-workspace/projects/ddp-cgc/src/app/components/login/login.component.spec.ts
+++ ddp-workspace/projects/ddp-cgc/src/app/components/login/login.component.spec.ts
@@ -6,7 +6,7 @@ describe('LoginComponent', () => {
   let component: LoginComponent;
   let fixture: ComponentFixture<LoginComponent>;
 
-  beforeEach(async () => {
+  beforeEach(async() => {
     await TestBed.configureTestingModule({
       declarations: [ LoginComponent ]
     })
