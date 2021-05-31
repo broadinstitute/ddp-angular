a28ed8c97366537947b1430e87d54ff89f5ba452
PavelYakupov <pyakupov@broadinstitute.org>
2021-05-31 18:45:58 +0300

    Fixed linting issues

--- ddp-workspace/projects/ddp-cgc/src/app/components/pages/activity/activity-page.component.spec.ts
+++ ddp-workspace/projects/ddp-cgc/src/app/components/pages/activity/activity-page.component.spec.ts
@@ -6,7 +6,7 @@ describe('ActivityComponent', () => {
   let component: ActivityPageComponent;
   let fixture: ComponentFixture<ActivityPageComponent>;
 
-  beforeEach(async () => {
+  beforeEach(async() => {
     await TestBed.configureTestingModule({
       declarations: [ ActivityPageComponent ]
     })
