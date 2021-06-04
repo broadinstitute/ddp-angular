ba4adaf26a913fe3a0e9e29d2497b0ef4ecd96a6
Anastasiia Glushkova <82094572+aglushko-broad@users.noreply.github.com>
2021-06-04 14:01:19 +0300

    Ddp 6247 pancan bootstrap (#599)
    * DDP-6247 PanCan bootstrap
    
    * DDP-6247 pancan bootstrap
    
    * DDP-6270 remove IrbGuard guard
    
    * DDP-6247 add login
    
    Co-authored-by: Anastasiia_Glushkova <anastasiia.glushkova@clarivate.com>

---
+++ ddp-workspace/projects/ddp-pancan/src/app/components/app-routes.ts
@@ -0,0 +1,10 @@
+export const AppRoutes = {
+    Error: 'error',
+    LoginLanding: 'login-landing',
+    LoginLandingMode: 'login-landing/:mode',
+    LocalAuth: 'auth',
+    Password: 'password',
+    SessionExpired: 'session-expired',
+    PasswordResetDone: 'password-reset-done',
+    CountMeIn: 'count-me-in',
+};
