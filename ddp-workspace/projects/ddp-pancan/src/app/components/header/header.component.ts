9b0e694146ae88dd2c6736515408e0ed53869d69
Anastasiia Glushkova <82094572+aglushko-broad@users.noreply.github.com>
2021-06-08 15:01:55 +0300

    DDP-6303 create base header and add i18n for header and footer for PanCan (#613)
    Co-authored-by: Anastasiia_Glushkova <anastasiia.glushkova@clarivate.com>

--- ddp-workspace/projects/ddp-pancan/src/app/components/header/header.component.ts
+++ ddp-workspace/projects/ddp-pancan/src/app/components/header/header.component.ts
@@ -1,7 +1,43 @@
-import { Component } from '@angular/core';
+import { Component, Inject, HostListener, OnInit } from '@angular/core';
+import { DOCUMENT } from '@angular/common';
+import { Router, NavigationEnd } from '@angular/router';
+import { WindowRef, SessionMementoService } from 'ddp-sdk';
 
 @Component({
-  selector: 'app-header',
-  templateUrl: './header.component.html',
+    selector: 'app-header',
+    templateUrl: './header.component.html',
+    styleUrls: ['./header.component.scss']
 })
-export class HeaderComponent {}
+export class HeaderComponent implements OnInit {
+    public isPanelOpened = false;
+    public isPageScrolled = false;
+
+    constructor(
+        private session: SessionMementoService,
+        private window: WindowRef,
+        private router: Router,
+        @Inject(DOCUMENT) private document: any) { }
+
+    public ngOnInit(): void {
+        this.router.events.subscribe(event => {
+            if (event instanceof NavigationEnd) {
+                this.isPanelOpened = false;
+            }
+        });
+    }
+
+    public openCloseMenu(): void {
+        this.isPanelOpened = !this.isPanelOpened;
+    }
+
+    public get isAuthenticated(): boolean {
+        return this.session.isAuthenticatedSession();
+    }
+
+    @HostListener('window: scroll') public onWindowScroll(): void {
+        const scrolledPixels = this.window.nativeWindow.pageYOffset
+            || this.document.documentElement.scrollTop
+            || this.document.body.scrollTop || 0;
+        this.isPageScrolled = !!scrolledPixels;
+    }
+}
