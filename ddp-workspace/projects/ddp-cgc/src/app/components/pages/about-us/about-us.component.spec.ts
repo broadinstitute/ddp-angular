8982f9de71ec59df76abe5160098a93378e55918
PavelYakupov <83226536+PavelYakupov@users.noreply.github.com>
2021-06-10 21:59:06 +0300

    Study/cgc initial setup (#591)
    Added CGC study

---
+++ ddp-workspace/projects/ddp-cgc/src/app/components/pages/about-us/about-us.component.spec.ts
@@ -0,0 +1,33 @@
+import { Pipe, PipeTransform } from '@angular/core';
+import { AboutUsComponent } from './about-us.component';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+
+@Pipe({name: 'translate'})
+class MockTranslatePipe implements PipeTransform {
+  transform(value: number): number {
+    return value;
+  }
+}
+
+describe('AboutUsComponent', () => {
+  let component: AboutUsComponent;
+  let fixture: ComponentFixture<AboutUsComponent>;
+
+  beforeEach(async() => {
+    await TestBed.configureTestingModule({
+      declarations: [ AboutUsComponent, MockTranslatePipe ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(AboutUsComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ ddp-workspace/projects/ddp-cgc/src/app/components/pages/dashboard/dashboard.component.spec.ts
@@ -0,0 +1,41 @@
+import { of } from 'rxjs';
+import { Router } from '@angular/router';
+import { UserActivityServiceAgent } from 'ddp-sdk';
+import { Pipe, PipeTransform } from '@angular/core';
+import { DashboardComponent } from './dashboard.component';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+
+@Pipe({name: 'translate'})
+class MockTranslatePipe implements PipeTransform {
+  transform(value: number): number {
+    return value;
+  }
+}
+
+describe('DashboardComponent', () => {
+  let component: DashboardComponent;
+  let fixture: ComponentFixture<DashboardComponent>;
+
+  beforeEach(async() => {
+    await TestBed.configureTestingModule({
+      declarations: [ DashboardComponent, MockTranslatePipe ],
+      providers: [
+        { provide: Router, useValue: {} },
+        { provide: 'ddp.config', useValue: {} },
+        { provide: UserActivityServiceAgent, useValue: { getActivities: () => of([]) } },
+      ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(DashboardComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
