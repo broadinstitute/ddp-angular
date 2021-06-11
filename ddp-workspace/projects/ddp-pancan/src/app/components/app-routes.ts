2030092ec5b37f23509b37e8a8ec42a8c3c72ec3
alextanlan <alextanlan@gmail.com>
2021-06-12 00:50:07 +0300

    Add HomePage for Pancan (draft for now)

--- ddp-workspace/projects/ddp-pancan/src/app/components/app-routes.ts
+++ ddp-workspace/projects/ddp-pancan/src/app/components/app-routes.ts
@@ -7,4 +7,6 @@ export const AppRoutes = {
     SessionExpired: 'session-expired',
     PasswordResetDone: 'password-reset-done',
     CountMeIn: 'count-me-in',
+    AboutUs: 'about-us',
+    FAQ: 'faq'
 };
--- ddp-workspace/projects/ddp-pancan/src/app/components/welcome/welcome.component.ts
+++ ddp-workspace/projects/ddp-pancan/src/app/components/welcome/welcome.component.ts
@@ -1,7 +1,17 @@
 import { Component } from '@angular/core';
+import { AppRoutes } from '../app-routes';
 
 @Component({
-  selector: 'app-welcome',
-  templateUrl: './welcome.component.html',
+    selector: 'app-welcome',
+    templateUrl: './welcome.component.html',
+    styleUrls: ['./welcome.component.scss']
 })
-export class WelcomeComponent {}
+export class WelcomeComponent {
+    readonly AppRoutes = AppRoutes;
+    readonly participationSteps = [
+        {key: 'Step1', img: 'Step1.png'},
+        {key: 'Step2', img: 'Step3.png'},
+        {key: 'Step3', img: 'Step4_big.png'},
+        {key: 'Step4', img: 'Step5.png'}
+    ];
+}
