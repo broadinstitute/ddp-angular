0c923dfb881e20b7a798a22181ff4609a25a95a7
Alex Maratkanov <alextanlan@gmail.com>
2021-06-18 14:58:51 +0300

    DDP-6258 FAQ page for PanCan (#652)
    Create a FAQ Page for PanCan study

--- ddp-workspace/projects/ddp-pancan/src/app/app-routing.module.ts
+++ ddp-workspace/projects/ddp-pancan/src/app/app-routing.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { WelcomeComponent } from './components/welcome/welcome.component';
 import { Auth0CodeCallbackComponent, BrowserGuard, IrbGuard } from 'ddp-sdk';
 import {
     ErrorRedesignedComponent,
@@ -11,67 +10,76 @@ import {
     SessionExpiredRedesignedComponent,
     WorkflowStartActivityRedesignedComponent
 } from 'toolkit';
+
 import { AppRoutes } from './components/app-routes';
+import { WelcomeComponent } from './components/welcome/welcome.component';
+import { FaqComponent } from './components/faq/faq.component';
 
 const routes: Routes = [
-  {
-    path: '',
-    component: WelcomeComponent,
-    pathMatch: 'full',
-    canActivate: [IrbGuard]
-  },
-  {
-    path: AppRoutes.LocalAuth,
-    component: Auth0CodeCallbackComponent,
-    canActivate: [IrbGuard]
-  },
-  {
-    path: AppRoutes.LoginLanding,
-    component: LoginLandingRedesignedComponent,
-    canActivate: [IrbGuard]
-  },
-  {
-    path: AppRoutes.LoginLandingMode,
-    component: RedirectToAuth0LoginRedesignedComponent,
-    canActivate: [IrbGuard]
-  },
-  {
-    path: AppRoutes.CountMeIn,
-    component: WorkflowStartActivityRedesignedComponent,
-    canActivate: [
-      IrbGuard,
-      BrowserGuard
-    ]
-  },
-  {
-    path: AppRoutes.Error,
-    component: ErrorRedesignedComponent
-  },
-  {
-    path: AppRoutes.PasswordResetDone,
-    component: RedirectToLoginLandingRedesignedComponent,
-    canActivate: [IrbGuard]
-  },
-  {
-    path: AppRoutes.SessionExpired,
-    component: SessionExpiredRedesignedComponent,
-    canActivate: [
-      IrbGuard,
-      BrowserGuard
-    ]
-  },
-  {
-    path: AppRoutes.Password,
-    component: PasswordRedesignedComponent
-  },
-  {
-    path: '**',
-    redirectTo: ''
-  }
+    {
+        path: '',
+        component: WelcomeComponent,
+        pathMatch: 'full',
+        canActivate: [IrbGuard]
+    },
+    {
+        path: AppRoutes.FAQ,
+        component: FaqComponent,
+        pathMatch: 'full',
+        canActivate: [IrbGuard]
+    },
+    {
+        path: AppRoutes.LocalAuth,
+        component: Auth0CodeCallbackComponent,
+        canActivate: [IrbGuard]
+    },
+    {
+        path: AppRoutes.LoginLanding,
+        component: LoginLandingRedesignedComponent,
+        canActivate: [IrbGuard]
+    },
+    {
+        path: AppRoutes.LoginLandingMode,
+        component: RedirectToAuth0LoginRedesignedComponent,
+        canActivate: [IrbGuard]
+    },
+    {
+        path: AppRoutes.CountMeIn,
+        component: WorkflowStartActivityRedesignedComponent,
+        canActivate: [
+            IrbGuard,
+            BrowserGuard
+        ]
+    },
+    {
+        path: AppRoutes.Error,
+        component: ErrorRedesignedComponent
+    },
+    {
+        path: AppRoutes.PasswordResetDone,
+        component: RedirectToLoginLandingRedesignedComponent,
+        canActivate: [IrbGuard]
+    },
+    {
+        path: AppRoutes.SessionExpired,
+        component: SessionExpiredRedesignedComponent,
+        canActivate: [
+            IrbGuard,
+            BrowserGuard
+        ]
+    },
+    {
+        path: AppRoutes.Password,
+        component: PasswordRedesignedComponent
+    },
+    {
+        path: '**',
+        redirectTo: ''
+    }
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
 })
 export class AppRoutingModule { }
