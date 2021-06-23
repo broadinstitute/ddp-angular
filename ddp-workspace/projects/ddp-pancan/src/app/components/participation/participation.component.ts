eba10a42aa99709c0691ef48a2798fe93f9ba713
alextanlan <alextanlan@gmail.com>
2021-06-23 15:38:26 +0300

    Fix after review

--- ddp-workspace/projects/ddp-pancan/src/app/components/participation/participation.component.ts
+++ ddp-workspace/projects/ddp-pancan/src/app/components/participation/participation.component.ts
@@ -1,21 +1,15 @@
 import { Component } from '@angular/core';
 
-interface StepData {
-    img: string;
-    link?: string;
-}
-
 @Component({
     selector: 'app-participation',
     templateUrl: './participation.component.html',
     styleUrls: ['./participation.component.scss']
 })
 export class ParticipationComponent {
-    readonly stepsData: StepData[] = [
-        { img: 'step1_big.png', link: 'consent.pdf' },
-        { img: 'step2_big.png', link: 'medical_release.pdf' },
-        { img: 'step3_big.png', link: 'surveys.pdf' },
-        { img: 'step4_big.png', link: 'kit-instructions.pdf' },
-        { img: 'step5_big.png' }
+    readonly stepsHref = [
+        'consent.pdf',
+        'medical_release.pdf',
+        'surveys.pdf',
+        'kit-instructions.pdf'
     ];
 }
