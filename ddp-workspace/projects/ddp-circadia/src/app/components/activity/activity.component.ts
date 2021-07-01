80b6434d027a5f63207511b7db5d9c97bcf558e7
Aleksey Kuznetsov <akuznets@broadinstitute.org>
2021-07-01 15:35:39 +0300

    Circadia: revert section index fixes

--- ddp-workspace/projects/ddp-circadia/src/app/components/activity/activity.component.ts
+++ ddp-workspace/projects/ddp-circadia/src/app/components/activity/activity.component.ts
@@ -1,9 +1,7 @@
-import { Component, OnInit } from '@angular/core';
-import { filter } from 'rxjs/operators';
+import { Component } from '@angular/core';
 
 import {
   ActivityRedesignedComponent,
-  ActivityStatusCodes,
   SubmissionManager,
   SubmitAnnouncementService,
 } from 'ddp-sdk';
@@ -14,31 +12,4 @@ import {
   styleUrls: ['./activity.component.scss'],
   providers: [SubmitAnnouncementService, SubmissionManager],
 })
-export class ActivityComponent extends ActivityRedesignedComponent implements OnInit {
-  ngOnInit(): void {
-    super.ngOnInit();
-
-    this.initFixedStepper();
-  }
-
-  private initFixedStepper(): void {
-    const isLoaded$ = this.getIsLoaded$();
-
-    const sub = isLoaded$.pipe(filter(isLoaded => isLoaded)).subscribe(() => {
-      if (this.model.statusCode !== ActivityStatusCodes.COMPLETE) {
-        // @ts-ignore
-        this.shouldSaveLastStep = this.config.usesVerticalStepper.includes(
-          this.model.activityCode,
-        );
-
-        this.currentSectionIndex = this.model.sectionIndex || 0;
-
-        this.visitedSectionIndexes = this.visitedSectionIndexes.map(
-          (_, index) => index <= this.currentSectionIndex,
-        );
-      }
-    });
-
-    this.anchor.addNew(sub);
-  }
-}
+export class ActivityComponent extends ActivityRedesignedComponent {}
