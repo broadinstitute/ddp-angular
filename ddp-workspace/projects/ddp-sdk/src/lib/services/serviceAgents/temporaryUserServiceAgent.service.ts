15bad088903ce2f10f4ab5e5622f6aa80ed9016c
Alex Maratkanov <alextanlan@gmail.com>
2021-07-16 15:30:30 +0300

    Update creation of temp user to include languageCode (#720)

--- ddp-workspace/projects/ddp-sdk/src/lib/services/serviceAgents/temporaryUserServiceAgent.service.ts
+++ ddp-workspace/projects/ddp-sdk/src/lib/services/serviceAgents/temporaryUserServiceAgent.service.ts
@@ -1,23 +1,29 @@
 import { Injectable, Inject } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+
 import { NotAuthenticatedServiceAgent } from './notAuthenticatedServiceAgent.service';
 import { ConfigurationService } from '../configuration.service';
 import { LoggingService } from '../logging.service';
 import { TemporaryUser } from '../../models/temporaryUser';
-import { Observable } from 'rxjs';
-import { map } from 'rxjs/operators';
+import { LanguageService } from '../internationalization/languageService.service';
 
 @Injectable()
 export class TemporaryUserServiceAgent extends NotAuthenticatedServiceAgent<TemporaryUser> {
     constructor(
         @Inject('ddp.config') _configuration: ConfigurationService,
         _http: HttpClient,
-        _logger: LoggingService) {
+        _logger: LoggingService,
+        private langService: LanguageService
+    ) {
         super(_configuration, _http, _logger);
     }
 
     public createTemporaryUser(auth0ClientId: string): Observable<TemporaryUser | null> {
-        return this.postObservable('/temporary-users', JSON.stringify({ auth0ClientId }))
+        const languageCode = this.langService.getCurrentLanguage() || undefined;
+
+        return this.postObservable('/temporary-users', JSON.stringify({ auth0ClientId, languageCode }))
             .pipe(map(data => data && data['body']));
     }
 }
