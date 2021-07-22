e660d0f8023efb46b65be5d268e3358f3f3ef289
PavelYakupov <pyakupov@broadinstitute.org>
2021-07-22 13:36:32 +0300

    DDP-6593 Added new default titles

--- ddp-workspace/projects/ddp-cgc/src/app/util/activities.util.ts
+++ ddp-workspace/projects/ddp-cgc/src/app/util/activities.util.ts
@@ -3,6 +3,11 @@ import { ActivityCode } from '../constants/activity-code';
 import { BlockStableId } from '../constants/block-stable-id';
 
 
+const defaultTitles = {
+  [ActivityCode.ReleaseRequestGenetic]: 'My genetic information provider',
+  [ActivityCode.ReleaseRequestClinical]: 'My clinical information provider',
+};
+
 export interface SimpleActivityQuestionBlock {
   _answer: string;
   stableId: string;
@@ -13,11 +18,11 @@ export const getReleaseRequestTitle = (activityInstance: ActivityInstance, block
     case ActivityCode.ReleaseRequestClinical:
       return blocks.find(
         (block: SimpleActivityQuestionBlock) => block.stableId === BlockStableId.RRC_PROVIDER_NAME
-      )?._answer || activityInstance.activityName;
+      )?._answer || defaultTitles[activityInstance.activityCode];
     case ActivityCode.ReleaseRequestGenetic:
       return blocks.find(
         (block: SimpleActivityQuestionBlock) => block.stableId === BlockStableId.RRG_LAB_NAME
-      )?._answer || activityInstance.activityName;
+      )?._answer || defaultTitles[activityInstance.activityCode];
     default:
       return null;
   }
