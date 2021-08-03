fbfbd629c678c372d97c8b5c0e96a37aabb7c86d
Anastasiia Glushkova <82094572+aglushko-broad@users.noreply.github.com>
2021-08-03 22:39:41 +0300

    DDP-6691 colorectal mailing list signup (#760)
    Co-authored-by: Anastasiia_Glushkova <anastasiia.glushkova@clarivate.com>

--- ddp-workspace/projects/ddp-pancan/src/app/components/faq-section/faq-section.component.ts
+++ ddp-workspace/projects/ddp-pancan/src/app/components/faq-section/faq-section.component.ts
@@ -1,6 +1,8 @@
 import { Component, Input, ViewChild } from '@angular/core';
 import { MatAccordion } from '@angular/material/expansion';
-import { CommunicationService } from 'toolkit';
+import { JoinMailingListComponent } from 'toolkit';
+import { MatDialog } from '@angular/material/dialog';
+import { JOIN_MAILING_LIST_DIALOG_SETTINGS } from '../../utils/join-mailing-list-dialog-confg';
 
 @Component({
     selector: 'app-faq-section',
@@ -11,11 +13,15 @@ export class FaqSectionComponent {
     @Input() questions: string;
     @Input() questionsCount: number; /* don't pass the counter for displaying all questions */
     @Input() darkerBackground: boolean;
+    @Input() isColorectal: boolean;
     @ViewChild(MatAccordion) accordion: MatAccordion; /* Please, do not remove. It is used outside of the component */
 
-    constructor(private communicationService: CommunicationService) {}
+    constructor(private dialog: MatDialog) {}
 
     public openJoinMailingList(): void {
-        this.communicationService.openJoinDialog();
+        this.dialog.open(JoinMailingListComponent, {
+            ...JOIN_MAILING_LIST_DIALOG_SETTINGS,
+            data: { info: this.isColorectal ? ['Colorectal'] : null },
+        });
     }
 }
