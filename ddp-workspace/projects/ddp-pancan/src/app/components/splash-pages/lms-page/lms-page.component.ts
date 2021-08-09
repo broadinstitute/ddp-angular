f5b041a65002cf841e9d14e8f163cb6ccb0b3df2
alextanlan <alextanlan@gmail.com>
2021-08-09 21:39:00 +0300

    Add JoinMailingList functionality for LMS page

--- ddp-workspace/projects/ddp-pancan/src/app/components/splash-pages/lms-page/lms-page.component.ts
+++ ddp-workspace/projects/ddp-pancan/src/app/components/splash-pages/lms-page/lms-page.component.ts
@@ -1,29 +1,32 @@
-import { Component, Inject, OnInit } from '@angular/core';
-import { ToolkitConfigurationService } from 'toolkit';
+import { Component, Inject } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { JoinMailingListComponent, ToolkitConfigurationService } from 'toolkit';
 import { AppRoutes } from '../../app-routes';
-import { FormControl } from '@angular/forms';
+import { JOIN_MAILING_LIST_DIALOG_SETTINGS } from '../../../utils/join-mailing-list-dialog-confg';
 
 @Component({
   selector: 'app-lms-page',
   templateUrl: './lms-page.component.html',
   styleUrls: ['./lms-page.component.scss']
 })
-export class LmsPageComponent implements OnInit {
+export class LmsPageComponent {
     readonly phone: string;
     readonly email: string;
-    emailToNotify: FormControl;
     readonly AppRoutes = AppRoutes;
 
-    constructor(@Inject('toolkit.toolkitConfig') config: ToolkitConfigurationService) {
+    constructor(
+        private dialog: MatDialog,
+        @Inject('toolkit.toolkitConfig') private config: ToolkitConfigurationService) {
         this.phone = config.lmsPagePhone;
         this.email = config.lmsPageEmail;
     }
 
-    ngOnInit(): void {
-        this.emailToNotify = new FormControl('');
-    }
-
     notify(): void {
-        console.log('Notify me');
+        this.dialog.open(JoinMailingListComponent, {
+            ...JOIN_MAILING_LIST_DIALOG_SETTINGS,
+            data: {
+                studyGuid: this.config.lmsStudyGuid
+            },
+        });
     }
 }
