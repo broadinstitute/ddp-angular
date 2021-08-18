da95b96b149ae4514e96f1378461fa948f8ea278
Anastasiia_Glushkova <anastasiia.glushkova@clarivate.com>
2021-08-18 22:22:42 +0300

    DDP-6811 fix wrapping

--- ddp-workspace/projects/toolkit-prion/src/lib/components/workflow-start-activity/prionWorkflowStartActivity.component.ts
+++ ddp-workspace/projects/toolkit-prion/src/lib/components/workflow-start-activity/prionWorkflowStartActivity.component.ts
@@ -5,49 +5,50 @@ import {
     SessionMementoService,
     TemporaryUserServiceAgent,
     WorkflowServiceAgent,
-    WindowRef, SessionStorageService
+    WindowRef,
+    SessionStorageService
 } from 'ddp-sdk';
 import { ToolkitConfigurationService, WorkflowBuilderService, WorkflowStartActivityComponent } from 'toolkit';
 
 @Component({
-  selector: 'prion-workflow-start-activity',
-  template: `
-    <ng-container>
-      <prion-header>
-      </prion-header>
-      <div class="ContainerSurvey-top row">
-            <ddp-activity *ngIf="show"
-                    [studyGuid]="studyGuid"
-                    [activityGuid]="instanceGuid"
-                    (submit)="navigate($event)"
-                    class="ContainerSurvey col-lg-8 col-lg-offset-2 col-md-8 col-md-offset-2 col-sm-10 col-sm-offset-1 col-xs-12">
-            </ddp-activity>
-        </div>
-</ng-container>`
+    selector: 'prion-workflow-start-activity',
+    template: `
+        <ng-container>
+            <prion-header>
+            </prion-header>
+            <div class="ContainerSurvey-top row">
+                <ddp-activity *ngIf="show"
+                              [studyGuid]="studyGuid"
+                              [activityGuid]="instanceGuid"
+                              (submit)="navigate($event)"
+                              class="ContainerSurvey col-lg-8 col-lg-offset-2 col-md-8 col-md-offset-2 col-sm-10 col-sm-offset-1 col-xs-12">
+                </ddp-activity>
+            </div>
+        </ng-container>`
 })
 export class PrionWorkflowStartActivityComponent extends WorkflowStartActivityComponent {
 
-  constructor(
-    private _workflowBuilder: WorkflowBuilderService,
-    private _temporaryUserService: TemporaryUserServiceAgent,
-    private _session: SessionMementoService,
-    private _workflow: WorkflowServiceAgent,
-    private _windowRef: WindowRef,
-    private _cdr: ChangeDetectorRef,
-    private _sessionStorageService: SessionStorageService,
-    @Inject('ddp.config') private _configuration: ConfigurationService,
-    @Inject('toolkit.toolkitConfig') private _toolkitConfiguration: ToolkitConfigurationService) {
-    super(_workflowBuilder,
-        _temporaryUserService,
-        _session,
-        _workflow,
-        _windowRef,
-        _cdr,
-        _sessionStorageService,
-        _configuration,
-        _toolkitConfiguration
-    );
-  }
+    constructor(
+        private _workflowBuilder: WorkflowBuilderService,
+        private _temporaryUserService: TemporaryUserServiceAgent,
+        private _session: SessionMementoService,
+        private _workflow: WorkflowServiceAgent,
+        private _windowRef: WindowRef,
+        private _cdr: ChangeDetectorRef,
+        private _sessionStorageService: SessionStorageService,
+        @Inject('ddp.config') private _configuration: ConfigurationService,
+        @Inject('toolkit.toolkitConfig') private _toolkitConfiguration: ToolkitConfigurationService) {
+        super(_workflowBuilder,
+            _temporaryUserService,
+            _session,
+            _workflow,
+            _windowRef,
+            _cdr,
+            _sessionStorageService,
+            _configuration,
+            _toolkitConfiguration
+        );
+    }
 
 
 }
