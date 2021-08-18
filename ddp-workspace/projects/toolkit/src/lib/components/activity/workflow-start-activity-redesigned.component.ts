d00df26ca4cad571313e3ef1eb5134c9ea0a6256
Anastasiia_Glushkova <anastasiia.glushkova@clarivate.com>
2021-08-18 17:41:16 +0300

    DDP-6811 fix wrapping

--- ddp-workspace/projects/toolkit/src/lib/components/activity/workflow-start-activity-redesigned.component.ts
+++ ddp-workspace/projects/toolkit/src/lib/components/activity/workflow-start-activity-redesigned.component.ts
@@ -8,7 +8,8 @@ import {
     SessionMementoService,
     TemporaryUserServiceAgent,
     WorkflowServiceAgent,
-    WindowRef, SessionStorageService
+    WindowRef,
+    SessionStorageService
 } from 'ddp-sdk';
 
 @Component({
