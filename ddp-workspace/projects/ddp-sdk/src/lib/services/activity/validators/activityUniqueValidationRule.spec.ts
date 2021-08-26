60bc0162bc3183235bc90a181649a3f874448f2b
Anastasiia_Glushkova <anastasiia.glushkova@clarivate.com>
2021-08-26 16:05:49 +0300

    DDP-6343 handle custom value case

--- ddp-workspace/projects/ddp-sdk/src/lib/services/activity/validators/activityUniqueValidationRule.spec.ts
+++ ddp-workspace/projects/ddp-sdk/src/lib/services/activity/validators/activityUniqueValidationRule.spec.ts
@@ -1,5 +1,6 @@
 import { ActivityUniqueValidationRule } from './activityUniqueValidationRule';
-import { ActivityQuestionBlock, ActivityCompositeQuestionBlock, QuestionType } from 'ddp-sdk';
+import { ActivityQuestionBlock, ActivityCompositeQuestionBlock, QuestionType, ActivityPicklistQuestionBlock } from 'ddp-sdk';
+import { PicklistSelectMode } from '../../../models/activity/picklistSelectMode';
 
 let validator: ActivityUniqueValidationRule;
 const MESSAGE = 'Answers should be unique';
@@ -20,50 +21,96 @@ describe('ActivityUniqueValidationRule', () => {
     it('should return true if question is composite but there is only one question', () => {
         const question = {
             questionType: QuestionType.Composite,
-            answer: [[{stableId: 'test', value: [{stableId: 'value'}]}]],
+            answer: [[{ stableId: 'test', value: [{ stableId: 'value' }] }]],
         } as ActivityCompositeQuestionBlock;
         validator = new ActivityUniqueValidationRule(question);
         expect(validator.recalculate()).toBeTrue();
     });
 
-    it('should return true if question is composite and answers are not arrays', () => {
+    it('should return true if question is composite and children question type is not picklist', () => {
         const question = {
+            children: [{ questionType: QuestionType.Text }, { questionType: QuestionType.Text }] as ActivityPicklistQuestionBlock[],
             questionType: QuestionType.Composite,
-            answer: [[{stableId: 'test', value: 'value'}], [{stableId: 'test', value: 'value'}]],
-        } as ActivityCompositeQuestionBlock;
+            answer: [[{ stableId: 'test', value: 'value' }], [{ stableId: 'test', value: 'value' }]],
+        } as unknown as ActivityCompositeQuestionBlock;
         validator = new ActivityUniqueValidationRule(question);
         expect(validator.recalculate()).toBeTrue();
     });
 
-    it('should return true if question is composite and answers are multiple selected arrays', () => {
+    it('should return true if question is composite, children question type is picklist but multiple selection', () => {
         const question = {
+            children: [
+                { questionType: QuestionType.Picklist, selectMode: PicklistSelectMode.MULTIPLE },
+                { questionType: QuestionType.Picklist, selectMode: PicklistSelectMode.MULTIPLE }
+            ] as ActivityPicklistQuestionBlock[],
             questionType: QuestionType.Composite,
             answer: [
-                [{stableId: 'test', value: [{stableId: 'value1'}, {stableId: 'value2'}]}],
-                [{stableId: 'test', value: [{stableId: 'value1'}, {stableId: 'value2'}]}]],
-        } as ActivityCompositeQuestionBlock;
+                [{ stableId: 'test', value: [{ stableId: 'value1' }, { stableId: 'value2' }] }],
+                [{ stableId: 'test', value: [{ stableId: 'value1' }, { stableId: 'value2' }] }]],
+        } as unknown as ActivityCompositeQuestionBlock;
         validator = new ActivityUniqueValidationRule(question);
         expect(validator.recalculate()).toBeTrue();
     });
 
     it('should return true if question is composite and answers are unique', () => {
         const question = {
+            children: [
+                { questionType: QuestionType.Picklist, selectMode: PicklistSelectMode.SINGLE },
+                { questionType: QuestionType.Picklist, selectMode: PicklistSelectMode.SINGLE }
+            ] as ActivityPicklistQuestionBlock[],
             questionType: QuestionType.Composite,
             answer: [
-                [{stableId: 'test', value: [{stableId: 'value1'}]}],
-                [{stableId: 'test', value: [{stableId: 'value2'}]}]],
-        } as ActivityCompositeQuestionBlock;
+                [{ stableId: 'test', value: [{ stableId: 'value1' }] }],
+                [{ stableId: 'test', value: [{ stableId: 'value2' }] }]],
+        } as unknown as ActivityCompositeQuestionBlock;
         validator = new ActivityUniqueValidationRule(question);
         expect(validator.recalculate()).toBeTrue();
     });
 
     it('should return false if question is composite and answers are not unique', () => {
         const question = {
+            children: [
+                { questionType: QuestionType.Picklist, selectMode: PicklistSelectMode.SINGLE },
+                { questionType: QuestionType.Picklist, selectMode: PicklistSelectMode.SINGLE }
+            ] as ActivityPicklistQuestionBlock[],
             questionType: QuestionType.Composite,
             answer: [
-                [{stableId: 'test', value: [{stableId: 'value1'}]}],
-                [{stableId: 'test', value: [{stableId: 'value1'}]}]],
-        } as ActivityCompositeQuestionBlock;
+                [{ stableId: 'test', value: [{ stableId: 'value1' }] }],
+                [{ stableId: 'test', value: [{ stableId: 'value1' }] }]],
+        } as unknown as ActivityCompositeQuestionBlock;
+        validator = new ActivityUniqueValidationRule(question);
+        validator.message = MESSAGE;
+        expect(validator.recalculate()).toBeFalse();
+        expect(validator.result).toBe(MESSAGE);
+    });
+
+    it('should return true if custom answers are unique', () => {
+        const question = {
+            children: [
+                { customValue: 'OTHER', questionType: QuestionType.Picklist, selectMode: PicklistSelectMode.SINGLE },
+                { customValue: 'OTHER', questionType: QuestionType.Picklist, selectMode: PicklistSelectMode.SINGLE }
+            ] as ActivityPicklistQuestionBlock[],
+            questionType: QuestionType.Composite,
+            answer: [
+                [{ stableId: 'test', value: [{ stableId: 'OTHER', detail: 'very custom' }] }],
+                [{ stableId: 'test', value: [{ stableId: 'OTHER', detail: 'very custom 1' }] }]],
+        } as unknown as ActivityCompositeQuestionBlock;
+        validator = new ActivityUniqueValidationRule(question);
+        validator.message = MESSAGE;
+        expect(validator.recalculate()).toBeTrue();
+    });
+
+    it('should return false if custom answers are not unique', () => {
+        const question = {
+            children: [
+                { customValue: 'OTHER', questionType: QuestionType.Picklist, selectMode: PicklistSelectMode.SINGLE },
+                { customValue: 'OTHER', questionType: QuestionType.Picklist, selectMode: PicklistSelectMode.SINGLE }
+            ] as ActivityPicklistQuestionBlock[],
+            questionType: QuestionType.Composite,
+            answer: [
+                [{ stableId: 'test', value: [{ stableId: 'OTHER', detail: 'very custom' }] }],
+                [{ stableId: 'test', value: [{ stableId: 'OTHER', detail: 'very custom' }] }]],
+        } as unknown as ActivityCompositeQuestionBlock;
         validator = new ActivityUniqueValidationRule(question);
         validator.message = MESSAGE;
         expect(validator.recalculate()).toBeFalse();
--- ddp-workspace/projects/ddp-sdk/src/lib/services/activity/validators/activityUniqueValidationRule.ts
+++ ddp-workspace/projects/ddp-sdk/src/lib/services/activity/validators/activityUniqueValidationRule.ts
@@ -1,6 +1,8 @@
 import { ActivityQuestionBlock } from '../../../models/activity/activityQuestionBlock';
 import { ActivityAbstractValidationRule } from './activityAbstractValidationRule';
 import { QuestionType } from '../../../models/activity/questionType';
+import { ActivityCompositeQuestionBlock, ActivityPicklistQuestionBlock } from 'ddp-sdk';
+import { PicklistSelectMode } from '../../../models/activity/picklistSelectMode';
 
 export class ActivityUniqueValidationRule extends ActivityAbstractValidationRule {
     constructor(question: ActivityQuestionBlock<any>) {
@@ -10,15 +12,22 @@ export class ActivityUniqueValidationRule extends ActivityAbstractValidationRule
     public recalculate(): boolean {
         const {answer: answers} = this.question;
         if (this.question.questionType !== QuestionType.Composite || answers.length === 1) {
+            this.result = null;
             return true;
         }
 
-        // compare only single select picklist questions
-        if (answers.find(([{value}]) => !Array.isArray(value) || value.length > 1)) {
+        const [firstChild] = (this.question as ActivityCompositeQuestionBlock).children;
+
+        if (firstChild.questionType !== QuestionType.Picklist
+            || (firstChild as ActivityPicklistQuestionBlock).selectMode !== PicklistSelectMode.SINGLE) {
+            this.result = null;
             return true;
         }
 
-        const answersIds = answers.map(([{value}]) => value[0].stableId);
+        const { customValue } = firstChild as ActivityPicklistQuestionBlock;
+        const answersIds = answers.map(([{value}]) => value[0].stableId === customValue
+            ? value[0].detail
+            : value[0].stableId);
         this.result = this.message;
         return (new Set(answersIds)).size === answersIds.length;
     }
