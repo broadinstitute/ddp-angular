46fe5b0045336beb0bb9c3aa309db19dc7d951a8
Aleksey Kuznetsov <akuznets@broadinstitute.org>
2021-08-30 18:47:58 +0300

    DDP-6874: Add Spanish PDF version

--- ddp-workspace/projects/ddp-rgp/src/app/components/for-your-physician/for-your-physician.component.ts
+++ ddp-workspace/projects/ddp-rgp/src/app/components/for-your-physician/for-your-physician.component.ts
@@ -1,19 +1,40 @@
-import { Component, OnInit, Inject } from '@angular/core';
+import { Component, OnInit, Inject, OnDestroy } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
+import { Subject } from 'rxjs';
+import { takeUntil } from 'rxjs/operators';
+
 import { ToolkitConfigurationService } from 'toolkit';
 
 @Component({
   selector: 'app-for-your-physician',
   templateUrl: './for-your-physician.component.html',
-  styleUrls: ['./for-your-physician.component.scss']
+  styleUrls: ['./for-your-physician.component.scss'],
 })
-export class ForYourPhysicianComponent implements OnInit {
+export class ForYourPhysicianComponent implements OnInit, OnDestroy {
   public phone: string;
   public phoneHref: string;
+  public currentLang: string;
+  private ngUnsub = new Subject();
 
-  constructor(@Inject('toolkit.toolkitConfig') private toolkitConfiguration: ToolkitConfigurationService) { }
+  constructor(
+    private translateService: TranslateService,
+    @Inject('toolkit.toolkitConfig')
+    private toolkitConfiguration: ToolkitConfigurationService,
+  ) {}
 
   public ngOnInit(): void {
     this.phone = this.toolkitConfiguration.phone;
     this.phoneHref = `tel:${this.toolkitConfiguration.phone}`;
+    this.currentLang = this.translateService.currentLang;
+
+    this.translateService.onLangChange
+      .pipe(takeUntil(this.ngUnsub))
+      .subscribe(({ lang }) => {
+        this.currentLang = lang;
+      });
+  }
+
+  public ngOnDestroy(): void {
+    this.ngUnsub.next();
   }
 }
