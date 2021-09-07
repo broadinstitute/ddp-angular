f13ffa948d8102b3c638b85f008c9d9e29c2f4a4
Aleksey Kuznetsov <71830582+aleksey-kuznetsov@users.noreply.github.com>
2021-09-07 13:36:30 +0300

    Update activityDateAnswer.component.ts

--- ddp-workspace/projects/ddp-sdk/src/lib/components/activityForm/answers/activityDateAnswer.component.ts
+++ ddp-workspace/projects/ddp-sdk/src/lib/components/activityForm/answers/activityDateAnswer.component.ts
@@ -33,7 +33,7 @@ export class ActivityDateAnswer {
     constructor(private logger: LoggingService) { }
 
     public handleChange(value: DatePickerValue | null): void {
-        let dateValue: DatePickerValue | null = {
+        let dateValue: DatePickerValue = {
             month: null,
             day: null,
             year: null,
