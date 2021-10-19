ea330353b8ec67acf2617f60fd6f98da3d1ff807
alextanlan <alextanlan@gmail.com>
2021-10-19 17:50:42 +0200

    Fix mailing-list.component.spec.ts
      as to make tests successful

--- ddp-workspace/projects/ddp-dsm-ui/src/app/mailing-list/mailing-list.component.spec.ts
+++ ddp-workspace/projects/ddp-dsm-ui/src/app/mailing-list/mailing-list.component.spec.ts
@@ -6,23 +6,30 @@ import { Auth } from '../services/auth.service';
 import { ComponentService } from '../services/component.service';
 import { RoleService } from '../services/role.service';
 import { MailingListComponent } from './mailing-list.component';
+import { of } from 'rxjs';
 
 describe('Component: MailingListComponent', () => {
   let component: MailingListComponent;
   let fixture: ComponentFixture<MailingListComponent>;
+  let DSMServiceSpy: jasmine.SpyObj<DSMService>;
+  let authSpy: jasmine.SpyObj<Auth>;
 
   beforeEach(async () => {
+    authSpy = jasmine.createSpyObj('authSpy', ['authenticated', 'logout']);
+    DSMServiceSpy = jasmine.createSpyObj('DSMServiceSpy', ['getRealmsAllowed', 'getMailingList']);
+    DSMServiceSpy.getRealmsAllowed.and.returnValue(of([]));
+
     await TestBed.configureTestingModule({
         declarations: [
           MailingListComponent
         ],
         imports: [],
         providers: [
-          { provide: DSMService, useValue: {}},
-          { provide: Auth, useValue: {}},
+          { provide: DSMService, useValue: DSMServiceSpy},
+          { provide: Auth, useValue: authSpy},
           { provide: RoleService, useValue: {}},
           { provide: ComponentService, useValue: {}},
-          { provide: ActivatedRoute, useValue: {}}
+          { provide: ActivatedRoute, useValue: { queryParams: of([]) } }
         ],
       })
       .compileComponents();
