82e8b6b1bd8afccc19fd4b911d7594f78eb8e441
PavelYakupov <pyakupov@broadinstitute.org>
2021-11-01 22:02:20 +0300

    DDP-6210 Added Progress Bar to the activity page.

--- ddp-workspace/projects/ddp-singular/src/app/constants/activity-code.ts
+++ ddp-workspace/projects/ddp-singular/src/app/constants/activity-code.ts
@@ -1,11 +1,12 @@
 export enum ActivityCode {
   Prequal = 'PREQUAL',
   ConsentSelf = 'CONSENT_SELF',
+  ConsentParental = 'CONSENT_PARENTAL',
   AboutHealthy = 'ABOUT_HEALTHY',
   AboutPatient = 'ABOUT_PATIENT',
+  MedicalRecordRelease = 'MEDICAL_RECORD_RELEASE',
   ConsentAssent = 'CONSENT_ASSENT',
   PatientSurvey = 'PATIENT_SURVEY',
   AddParticipant = 'ADD_PARTICIPANT',
-  ConsentParental = 'CONSENT_PARENTAL',
   MedicalRecordFileUpload = 'MEDICAL_RECORD_FILE_UPLOAD',
 }
