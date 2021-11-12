42a016527ae9c61c95fe27fd7c087572dab27540
alextanlan <alextanlan@gmail.com>
2021-11-12 17:17:01 +0100

    Display a sent error message on Error page

--- ddp-workspace/projects/ddp-rarex/src/app/pages/error/error.component.ts
+++ ddp-workspace/projects/ddp-rarex/src/app/pages/error/error.component.ts
@@ -1,8 +1,16 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-error',
   templateUrl: './error.component.html',
   styleUrls: ['./error.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class ErrorComponent {}
+export class ErrorComponent {
+    errorMessage: string;
+
+    constructor(private router: Router) {
+        this.errorMessage = this.router.getCurrentNavigation().extras?.state?.errorMessage;
+    }
+}
