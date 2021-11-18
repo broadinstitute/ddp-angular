5f4ff10577e360c2d264190abc90b7f8708191b1
alextanlan <alextanlan@gmail.com>
2021-11-18 22:31:24 +0100

    Return back committed by mistake

--- ddp-workspace/projects/ddp-dsm-ui/karma.conf.js
+++ ddp-workspace/projects/ddp-dsm-ui/karma.conf.js
@@ -31,7 +31,7 @@ module.exports = function (config) {
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ['ChromeHeadlessNoSandbox'],
+    browsers: ['Chrome','ChromeHeadlessNoSandbox'],
     customLaunchers: {
       ChromeHeadlessNoSandbox: {
         base: 'ChromeHeadless',
