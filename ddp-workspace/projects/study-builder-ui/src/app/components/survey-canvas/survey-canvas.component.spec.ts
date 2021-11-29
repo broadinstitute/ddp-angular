928b9adf3fdc41b281dc2d7ddb88a77a4c285465
Anastasiia_Glushkova <anastasiia.glushkova@clarivate.com>
2021-11-29 15:11:28 +0300

    fix linter for study builder ui

--- ddp-workspace/projects/study-builder-ui/src/app/components/survey-canvas/survey-canvas.component.spec.ts
+++ ddp-workspace/projects/study-builder-ui/src/app/components/survey-canvas/survey-canvas.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { waitForAsync, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { SurveyCanvasComponent } from './survey-canvas.component';
 
@@ -6,7 +6,7 @@ describe('SurveyCanvasComponent', () => {
   let component: SurveyCanvasComponent;
   let fixture: ComponentFixture<SurveyCanvasComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ SurveyCanvasComponent ]
     })
