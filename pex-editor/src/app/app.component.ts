430c6ff251b3c5820b13f72511c31511a5edb103
Anastasiia_Glushkova <anastasiia.glushkova@clarivate.com>
2021-12-14 15:48:37 +0300

    DDP-7226 run detectChanges

--- pex-editor/src/app/app.component.ts
+++ pex-editor/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { ChangeDetectorRef, Component } from '@angular/core';
 import { Error, validate } from 'src/parser-utils/validator';
 
 @Component({
@@ -11,11 +11,14 @@ export class AppComponent {
   code: string = 'user.studies["A"].forms["B"].questions["COUNTRY"].answers.hasOption("US")';
   errors: Error[] = [];
 
+  constructor(private cd: ChangeDetectorRef) {}
+
   editorInit(editor: monaco.editor.IStandaloneCodeEditor) {
     editor.onDidChangeModelContent((e: monaco.editor.IModelContentChangedEvent) => {
       console.log(JSON.stringify(e, null, '\t'));
       console.log(this.code);
       this.errors = validate(this.code);
+      this.cd.detectChanges();
     });
   }
 }
