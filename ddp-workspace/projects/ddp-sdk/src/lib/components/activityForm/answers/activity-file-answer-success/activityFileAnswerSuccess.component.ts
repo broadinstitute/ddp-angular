4a0abcc86e706888ef78206e796dff8ff0131f32
PavelYakupov <pyakupov@broadinstitute.org>
2021-12-16 18:19:33 +0300

    DDP-6203 Added support for display when changing language

--- ddp-workspace/projects/ddp-sdk/src/lib/components/activityForm/answers/activity-file-answer-success/activityFileAnswerSuccess.component.ts
+++ ddp-workspace/projects/ddp-sdk/src/lib/components/activityForm/answers/activity-file-answer-success/activityFileAnswerSuccess.component.ts
@@ -1,10 +1,12 @@
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 import { TranslateService } from '@ngx-translate/core';
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 
 
 @Component({
   selector: 'ddp-activity-file-answer-success',
-  template: '<p *ngIf="showMessage()" class="success-message">{{ SUCCESS_KEY | translate }}</p>',
+  template: '<p *ngIf="showMessage() | async" class="success-message">{{ SUCCESS_KEY | translate }}</p>',
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class ActivityFileAnswerSuccess {
@@ -14,12 +16,13 @@ export class ActivityFileAnswerSuccess {
     private readonly translateService: TranslateService
   ) {}
 
-  showMessage(): boolean {
+  showMessage(): Observable<boolean> {
     return this.hasTranslation(this.SUCCESS_KEY);
   }
 
-  private hasTranslation(key: string): boolean {
-    const translation: string = this.translateService.instant(key);
-    return translation !== key && translation !== '';
+  private hasTranslation(key: string): Observable<boolean> {
+    return this.translateService.get(key).pipe(
+      map((translation: string) => translation !== key && translation !== '')
+    );
   }
 }
