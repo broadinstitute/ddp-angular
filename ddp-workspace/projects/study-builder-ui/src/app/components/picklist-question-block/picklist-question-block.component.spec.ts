d37988cc1e2d5183307fd7ede7bbfe7a1095de97
Alex Maratkanov <alextanlan@gmail.com>
2021-12-23 16:44:18 +0100

    Fix some tests for study-builder-ui (#994)

--- ddp-workspace/projects/study-builder-ui/src/app/components/picklist-question-block/picklist-question-block.component.spec.ts
+++ ddp-workspace/projects/study-builder-ui/src/app/components/picklist-question-block/picklist-question-block.component.spec.ts
@@ -1,25 +1,75 @@
-// import { ComponentFixture, TestBed } from '@angular/core/testing';
-//
-// import { PicklistQuestionBlockComponent } from './picklist-question-block.component';
-//
-// describe('PicklistQuestionBlockComponent', () => {
-//   let component: PicklistQuestionBlockComponent;
-//   let fixture: ComponentFixture<PicklistQuestionBlockComponent>;
-//
-//   beforeEach(async () => {
-//     await TestBed.configureTestingModule({
-//       declarations: [ PicklistQuestionBlockComponent ]
-//     })
-//     .compileComponents();
-//   });
-//
-//   beforeEach(() => {
-//     fixture = TestBed.createComponent(PicklistQuestionBlockComponent);
-//     component = fixture.componentInstance;
-//     fixture.detectChanges();
-//   });
-//
-//   it('should create', () => {
-//     expect(component).toBeTruthy();
-//   });
-// });
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { of } from 'rxjs';
+
+import { PicklistQuestionBlockComponent } from './picklist-question-block.component';
+import { PicklistQuestionDef } from '../../model/core/picklistQuestionDef';
+
+const definitionBlock = {
+    blockType: 'QUESTION' as any,
+    question: {
+        stableId: '',
+        isRestricted: false,
+        isDeprecated: false,
+        promptTemplate: {
+            templateText: '$__template__',
+            templateType: 'HTML',
+            variables: [
+                {
+                    name: '__template__',
+                    translations: [
+                        {
+                            language: 'en',
+                            text: ''
+                        }
+                    ]
+                }
+            ]
+        },
+        validations: [],
+        hideNumber: false,
+        questionType: 'PICKLIST',
+        selectMode: 'SINGLE',
+        renderMode: 'LIST',
+        picklistLabelTemplate: {
+            templateText: '$__template__',
+            templateType: 'HTML',
+            variables: [
+                {
+                    name: '__template__',
+                    translations: [
+                        {
+                            language: 'en',
+                            text: ''
+                        }
+                    ]
+                }
+            ]
+        },
+        groups: [],
+        picklistOptions: []
+    } as PicklistQuestionDef,
+    id: '1'
+};
+
+describe('PicklistQuestionBlockComponent', () => {
+  let component: PicklistQuestionBlockComponent;
+  let fixture: ComponentFixture<PicklistQuestionBlockComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ PicklistQuestionBlockComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(PicklistQuestionBlockComponent);
+    component = fixture.componentInstance;
+    component.definitionBlock$ = of(definitionBlock);
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
--- ddp-workspace/projects/study-builder-ui/src/app/components/text-question-block/text-question-block.component.spec.ts
+++ ddp-workspace/projects/study-builder-ui/src/app/components/text-question-block/text-question-block.component.spec.ts
@@ -1,25 +1,74 @@
-// import { ComponentFixture, TestBed } from '@angular/core/testing';
-//
-// import { TextQuestionBlockComponent } from './text-question-block.component';
-//
-// describe('TextQuestionBlockComponent', () => {
-//   let component: TextQuestionBlockComponent;
-//   let fixture: ComponentFixture<TextQuestionBlockComponent>;
-//
-//   beforeEach(async () => {
-//     await TestBed.configureTestingModule({
-//       declarations: [ TextQuestionBlockComponent ]
-//     })
-//     .compileComponents();
-//   });
-//
-//   beforeEach(() => {
-//     fixture = TestBed.createComponent(TextQuestionBlockComponent);
-//     component = fixture.componentInstance;
-//     fixture.detectChanges();
-//   });
-//
-//   it('should create', () => {
-//     expect(component).toBeTruthy();
-//   });
-// });
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { of } from 'rxjs';
+
+import { TextQuestionBlockComponent } from './text-question-block.component';
+import { TextQuestionDef } from '../../model/core/textQuestionDef';
+
+const definitionBlock = {
+    blockType: 'QUESTION' as any,
+    question: {
+        stableId: '',
+        isRestricted: false,
+        isDeprecated: false,
+        promptTemplate: {
+            templateType: 'HTML',
+            templateText: '$__template__',
+            variables: [
+                {
+                    name: '__template__',
+                    translations: [
+                        {
+                            language: 'en',
+                            text: ''
+                        }
+                    ]
+                }
+            ]
+        },
+        validations: [],
+        hideNumber: false,
+        questionType: 'TEXT',
+        inputType: 'TEXT',
+        suggestionType: 'NONE',
+        placeholderTemplate: {
+            templateType: 'HTML',
+            templateText: '$__template__',
+            variables: [
+                {
+                    name: '__template__',
+                    translations: [
+                        {
+                            language: 'en',
+                            text: ''
+                        }
+                    ]
+                }
+            ]
+        },
+        suggestions: []
+    } as TextQuestionDef,
+    id: '1'
+};
+
+describe('TextQuestionBlockComponent', () => {
+  let component: TextQuestionBlockComponent;
+  let fixture: ComponentFixture<TextQuestionBlockComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ TextQuestionBlockComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(TextQuestionBlockComponent);
+    component = fixture.componentInstance;
+    component.definitionBlock$ = of(definitionBlock);
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
