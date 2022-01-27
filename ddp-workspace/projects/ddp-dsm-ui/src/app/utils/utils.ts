2ee7a870c2d184a108030bd413dc813fc415f15c
pegahtah <ptaheri@broadinstitute.org>
2022-01-27 15:45:43 -0500

    Fixes from dsm pr #192

--- ddp-workspace/projects/ddp-dsm-ui/src/app/utils/utils.ts
+++ ddp-workspace/projects/ddp-dsm-ui/src/app/utils/utils.ts
@@ -820,7 +820,12 @@ export class Utils {
 
   public static getCorrectTextAsAnswerForCSV( questionAnswer: QuestionAnswer, qDef: QuestionDefinition ): string[] {
     const answers = [];
-    for (let answer of questionAnswer.answer) {
+    let probableAnswer = questionAnswer.answer;
+    if (!(questionAnswer.answer instanceof Array)){
+      probableAnswer = new Array();
+      probableAnswer.push(questionAnswer.answer);
+    }
+    for (let answer of probableAnswer) {
       let text = answer;
       let activityAnswers = '';
       const ans = this.getAnswerGroupOrOptionText( answer, qDef );
