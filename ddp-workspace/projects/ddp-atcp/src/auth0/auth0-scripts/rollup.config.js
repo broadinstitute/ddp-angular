2acfe55108947e2859a3ece0100fe939cee27baa
alextanlan <alextanlan@gmail.com>
2022-02-01 19:36:41 +0100

    Wrap ddp-atcp-auth build
     in order to have ddp-atcp-auth.umd.js
     (UMD bundle is required on BE side)

---
+++ ddp-workspace/projects/ddp-atcp/src/auth0/auth0-scripts/rollup.config.js
@@ -0,0 +1,12 @@
+// transform an FESM2015 input file to UMD
+// because it requires in `ddp-study-server\study-builder\tenants\atcp\pages\login.html`
+// in <script> tag
+
+export default {
+    input: 'projects/ddp-atcp/src/auth0/compiled/auth0-scripts/fesm2015/ddp-atcp-auth.mjs',
+    output: {
+        file: 'projects/ddp-atcp/src/auth0/compiled/auth0-scripts/bundles/ddp-atcp-auth.umd.js',
+        format: 'umd',
+        name: 'ddp-atcp-auth'
+    }
+};
