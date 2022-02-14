78088e222cee74ced7da1197279b30d3af6c0659
alextanlan <alextanlan@gmail.com>
2022-02-14 19:20:07 +0100

    Fix import

--- ddp-workspace/projects/ddp-sdk/src/lib/models/activity/answerValue.ts
+++ ddp-workspace/projects/ddp-sdk/src/lib/models/activity/answerValue.ts
@@ -1,7 +1,7 @@
 import { DatePickerValue } from '../datePickerValue';
 import { ActivityPicklistAnswerDto } from './activityPicklistAnswerDto';
 import { ActivityMatrixAnswerDto } from './activityMatrixAnswerDto';
-import { NumericAnswerType } from './activityNumericQuestionBlock';
+import { NumericAnswerType } from './activityDecimalQuestionBlock';
 
 export type AnswerValue =
   | boolean
