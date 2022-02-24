3c945703325167fe574342a732ad42c6920d531f
Jose Manuel Morales Olivera <josemanu@broadinstitute.org>
2022-02-24 13:14:02 -0600

    add REMOTE_AUTOCOMPLETE option

--- ddp-workspace/projects/ddp-sdk/src/lib/components/activityForm/picklist/activityPicklistAnswer.component.ts
+++ ddp-workspace/projects/ddp-sdk/src/lib/components/activityForm/picklist/activityPicklistAnswer.component.ts
@@ -6,39 +6,57 @@ import { PicklistRenderMode } from './../../../models/activity/picklistRenderMod
 
 @Component({
     selector: 'ddp-activity-picklist-answer',
-    template: `
-    <ddp-question-prompt [block]="block"></ddp-question-prompt>
-    <div>
-        <ddp-activity-radiobuttons-picklist-question
-            *ngIf="block.selectMode === SELECT_MODE.SINGLE && block.renderMode === RENDER_MODE.LIST"
-            [block]="block"
-            [readonly]="readonly"
-            (valueChanged)="valueChanged.emit($event)">
-        </ddp-activity-radiobuttons-picklist-question>
-        <ddp-activity-checkboxes-picklist-question
-            *ngIf="block.selectMode === SELECT_MODE.MULTIPLE && block.renderMode === RENDER_MODE.LIST || block.renderMode === RENDER_MODE.CHECKBOX_LIST"
-            [block]="block"
-            [readonly]="readonly"
-            (valueChanged)="valueChanged.emit($event)">
-        </ddp-activity-checkboxes-picklist-question>
-        <ddp-activity-dropdown-picklist-question
-            *ngIf="block.renderMode === RENDER_MODE.DROPDOWN"
-            [block]="block"
-            [readonly]="readonly"
-            (valueChanged)="valueChanged.emit($event)">
-        </ddp-activity-dropdown-picklist-question>
-        <ddp-activity-autocomplete-picklist-question
-            *ngIf="block.renderMode === RENDER_MODE.AUTOCOMPLETE"
-            [block]="block"
-            [readonly]="readonly"
-            (valueChanged)="valueChanged.emit($event)">
-        </ddp-activity-autocomplete-picklist-question>
-    </div>`
+    template: ` <ddp-question-prompt [block]="block"></ddp-question-prompt>
+        <div>
+            <ddp-activity-radiobuttons-picklist-question
+                *ngIf="
+                    block.selectMode === SELECT_MODE.SINGLE &&
+                    block.renderMode === RENDER_MODE.LIST
+                "
+                [block]="block"
+                [readonly]="readonly"
+                (valueChanged)="valueChanged.emit($event)"
+            >
+            </ddp-activity-radiobuttons-picklist-question>
+            <ddp-activity-checkboxes-picklist-question
+                *ngIf="
+                    (block.selectMode === SELECT_MODE.MULTIPLE &&
+                        block.renderMode === RENDER_MODE.LIST) ||
+                    block.renderMode === RENDER_MODE.CHECKBOX_LIST
+                "
+                [block]="block"
+                [readonly]="readonly"
+                (valueChanged)="valueChanged.emit($event)"
+            >
+            </ddp-activity-checkboxes-picklist-question>
+            <ddp-activity-dropdown-picklist-question
+                *ngIf="block.renderMode === RENDER_MODE.DROPDOWN"
+                [block]="block"
+                [readonly]="readonly"
+                (valueChanged)="valueChanged.emit($event)"
+            >
+            </ddp-activity-dropdown-picklist-question>
+            <ddp-activity-autocomplete-picklist-question
+                *ngIf="block.renderMode === RENDER_MODE.AUTOCOMPLETE"
+                [block]="block"
+                [readonly]="readonly"
+                (valueChanged)="valueChanged.emit($event)"
+            >
+            </ddp-activity-autocomplete-picklist-question>
+            <ddp-activity-picklist-remote-auto-complete-options
+                *ngIf="block.renderMode === RENDER_MODE.REMOTE_AUTOCOMPLETE"
+                [block]="block"
+                [readonly]="readonly"
+                (valueChanged)="valueChanged.emit($event)"
+            >
+            </ddp-activity-picklist-remote-auto-complete-options>
+        </div>`,
 })
 export class ActivityPicklistAnswer {
     @Input() block: ActivityPicklistQuestionBlock;
     @Input() readonly: boolean;
-    @Output() valueChanged: EventEmitter<Array<ActivityPicklistAnswerDto>> = new EventEmitter();
+    @Output() valueChanged: EventEmitter<Array<ActivityPicklistAnswerDto>> =
+        new EventEmitter();
 
     public readonly SELECT_MODE = PicklistSelectMode;
     public readonly RENDER_MODE = PicklistRenderMode;
