66b6b884bdbba21e76d7f9bff8ad02eeac79a198
Jose Manuel Morales Olivera <josemanu@broadinstitute.org>
2022-02-24 13:12:48 -0600

    add remote auto complete component

--- ddp-workspace/projects/ddp-sdk/src/lib/components/activityForm/picklist/activity-picklist-remote-auto-complete-options.component.ts
+++ ddp-workspace/projects/ddp-sdk/src/lib/components/activityForm/picklist/activity-picklist-remote-auto-complete-options.component.ts
@@ -1,46 +1,127 @@
-import { Component, Inject, OnInit, SimpleChanges } from "@angular/core";
-import { BehaviorSubject, map, Observable, switchMap } from "rxjs";
-import { ActivityPicklistOption } from "../../../models/activity/activityPicklistOption";
-import { ConfigurationService } from "../../../services/configuration.service";
-import { NGXTranslateService } from "../../../services/internationalization/ngxTranslate.service";
-import { PicklistSortingPolicy } from "../../../services/picklistSortingPolicy.service";
-import { ActivityServiceAgent } from "../../../services/serviceAgents/activityServiceAgent.service";
-import { SessionMementoService } from "../../../services/sessionMemento.service";
-import { BaseActivityPicklistQuestion } from "./baseActivityPicklistQuestion.component";
+import { Component, Inject, OnInit, SimpleChanges } from '@angular/core';
+import { BehaviorSubject, Observable, of, switchMap, tap } from 'rxjs';
+import { ActivityPicklistNormalizedGroup } from '../../../models/activity/activityPicklistNormalizedGroup';
+import { ActivityPicklistOption } from '../../../models/activity/activityPicklistOption';
+import { ConfigurationService } from '../../../services/configuration.service';
+import { NGXTranslateService } from '../../../services/internationalization/ngxTranslate.service';
+import { PicklistSortingPolicy } from '../../../services/picklistSortingPolicy.service';
+import { BaseActivityPicklistQuestion } from './baseActivityPicklistQuestion.component';
 
 @Component({
-    selector: "ddp-activity-picklist-remote-auto-complete-options",
-    templateUrl:
-        "./activity-picklist-remote-auto-complete-options.component.html",
-    styleUrls: [
-        "./activity-picklist-remote-auto-complete-options.component.scss",
+    selector: 'ddp-activity-picklist-remote-auto-complete-options',
+    template: `<mat-form-field
+        class="full-width ddp-autocomplete-field"
+        *ngIf="picklistOptions$ | async as picklistOptions"
+    >
+        <input
+            matInput
+            #autocompleteInput
+            [value]="searchValue$ | async"
+            [placeholder]="block.picklistLabel"
+            [matAutocomplete]="autoCompleteFromSource"
+            (keyup)="onInput($event.target.value)"
+        />
+
+        <mat-autocomplete
+            #autoCompleteFromSource="matAutocomplete"
+            class="autoCompletePanel"
+            [displayWith]="displayAutoComplete"
+        >
+            <mat-option
+                *ngFor="let option of picklistOptions"
+                class="autoCompleteOption"
+                [value]="option"
+                (click)="onValueSelect(option)"
+            >
+                <span
+                    [innerHtml]="
+                        option.optionLabel
+                            | searchHighlight
+                                : autocompleteInput.value
+                                : ignoredSymbolsInQuery
+                    "
+                ></span>
+            </mat-option>
+        </mat-autocomplete>
+    </mat-form-field>`,
+    styles: [
+        `
+            .full-width {
+                width: 100%;
+            }
+
+            .ddp-autocomplete-field ::ng-deep .mat-form-field-infix {
+                width: auto;
+            }
+        `,
     ],
 })
 export class ActivityPicklistRemoteAutoCompleteOptionsComponent
     extends BaseActivityPicklistQuestion
     implements OnInit
 {
-    searchValue$ = new BehaviorSubject("");
-    picklistOptions$: Observable<ActivityPicklistOption>;
+    filteredGroups: ActivityPicklistNormalizedGroup[] = [];
+
+    searchValue$ = new BehaviorSubject('');
+    picklistOptions$: Observable<ActivityPicklistOption[]>;
+
+    readonly ignoredSymbolsInQuery: string[];
+
     constructor(
         translate: NGXTranslateService,
         private sortPolicy: PicklistSortingPolicy,
-        @Inject("ddp.config") public config: ConfigurationService,
-        private activityService: ActivityServiceAgent
+        @Inject('ddp.config') public config: ConfigurationService // private activityService: ActivityServiceAgent
     ) {
         super(translate);
+        this.ignoredSymbolsInQuery =
+            this.config.picklistAutocompleteIgnoredSymbols;
     }
 
     ngOnInit(): void {
-       this.picklistOptions$ = this.searchValue$
-            .pipe(
-                switchMap((value) =>
-                    this.activityService.getPickListOptions(this.block.stableId, value)
-                )
-            );
+        this.picklistOptions$ = this.searchValue$.pipe(
+            switchMap((searchValue) => {
+                this.sortPolicy = this.shouldBeSorted
+                    ? this.sortPolicy
+                    : new PicklistSortingPolicy();
+
+                const sortedOptions = this.sortPolicy.sortPicklistOptions(
+                    this.block.picklistOptions
+                );
+
+                //TO-DO uncomment when there is a valid endpoint
+                // this.activityService.getPickListOptions(
+                //     this.block.stableId,
+                //     searchValue
+                // )
+
+                return of(
+                    sortedOptions.filter((option) =>
+                        option.optionLabel
+                            .toLowerCase()
+                            .includes(searchValue.toLowerCase())
+                    )
+                );
+            })
+        );
     }
 
-    onInput(value) {
+    onInput(value): void {
         this.searchValue$.next(value);
+    }
+
+    displayAutoComplete(option: ActivityPicklistOption | string): string {
+        return typeof option === 'string' ? option : option?.optionLabel || '';
+    }
+
+    onValueSelect(value: ActivityPicklistOption, detail: string | null = null) {
+        this.block.answer = value ? [{ stableId: value.stableId, detail }] : [];
+        console.log([...this.block.answer]);
+        this.valueChanged.emit([...this.block.answer]);
+    }
+
+    private get shouldBeSorted(): boolean {
+        return !this.config.notSortedPicklistAutocompleteStableIds.includes(
+            this.block.stableId
+        );
     }
 }
