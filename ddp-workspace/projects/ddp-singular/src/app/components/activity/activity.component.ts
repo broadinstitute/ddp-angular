617a793c65441338a19144838eebae10bdb70bfa
GiCharkviani <gi.charkviani@gmail.com>
2022-03-02 22:15:15 +0400

    DDP-7548/Parental-Medical-Release-form-fixes

--- ddp-workspace/projects/ddp-singular/src/app/components/activity/activity.component.ts
+++ ddp-workspace/projects/ddp-singular/src/app/components/activity/activity.component.ts
@@ -1,9 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 import { Route } from '../../constants/route';
 import { ActivityCode } from '../../constants/activity-code';
 import { ActivityRedesignedComponent, SubmissionManager, SubmitAnnouncementService } from 'ddp-sdk';
 
 declare const DDP_ENV: Record<string, any>;
+type InputIds = {[key: string]: number};
 
 @Component({
   selector: 'app-activity',
@@ -14,6 +15,26 @@ declare const DDP_ENV: Record<string, any>;
 export class ActivityComponent extends ActivityRedesignedComponent {
   Route = Route;
   isCaptchaResolved = false;
+
+  @HostListener('document:keydown.enter', ['$event']) onEnter(event:KeyboardEvent) {
+      event.preventDefault();
+      this.nextLineInput(event)
+  }
+
+  private nextLineInput(event: KeyboardEvent) {
+      const inputsArray: Element[] = Array.from(document.querySelectorAll('input, mat-select, select')).filter(input => input.id)
+      const inputsIdsObject: InputIds = Object.fromEntries(inputsArray.map((input, i) => [input.id, i]))
+      const pressedInputId = (event.target as HTMLInputElement).id;
+
+      inputsIdsObject[pressedInputId] > -1 ? this.moveToNextInput(inputsArray, inputsIdsObject,pressedInputId) :
+          this.moveToNextInput(inputsArray, inputsIdsObject, pressedInputId, true)
+  }
+
+  private moveToNextInput(inputs: Element[] | HTMLInputElement, inputsIdsObject:InputIds, PII: string, first?:boolean) {
+      !first ?
+          ((inputsIdsObject[PII] !== undefined && inputsIdsObject[PII] > -1) &&  inputs[inputsIdsObject[PII]+1] && inputs[inputsIdsObject[PII]+1].focus()) :
+          inputs[0]?.focus();
+  }
 
   get isPrequal(): boolean {
     return this.model && this.model.activityCode === ActivityCode.Prequal;
