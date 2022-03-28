c2358479ebf3d3b0d47aed62648762da9b14eeaa
simone <simone@broadinstitute.org>
2022-03-28 11:13:41 -0400

    DDP-7322: displaying form for singular

--- ddp-workspace/projects/ddp-dsm-ui/src/app/participant-page/participant-page.component.ts
+++ ddp-workspace/projects/ddp-dsm-ui/src/app/participant-page/participant-page.component.ts
@@ -1170,8 +1170,8 @@ export class ParticipantPageComponent implements OnInit, OnDestroy, AfterViewChe
     return '';
   }
 
-  dynamicFormType(settings: FieldSettings[]): boolean {
-    return settings['TAB_GROUPED'];
+  dynamicFormTypeAndStudyRGP(settings: FieldSettings[]): boolean {
+    return settings['TAB_GROUPED'] && localStorage.getItem(ComponentService.MENU_SELECTED_REALM) === 'RGP';
   }
 
   getDisplayName(displayName: string, columnName: string): string {
