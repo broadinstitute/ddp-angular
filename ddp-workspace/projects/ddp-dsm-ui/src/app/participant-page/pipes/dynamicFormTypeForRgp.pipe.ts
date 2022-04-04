b73076d82740a3033e0327014f20a8afbada5d51
GiCharkviani <gi.charkviani@gmail.com>
2022-04-04 16:58:27 +0400

    not-not-not

--- ddp-workspace/projects/ddp-dsm-ui/src/app/participant-page/pipes/dynamicFormTypeForRgp.pipe.ts
+++ ddp-workspace/projects/ddp-dsm-ui/src/app/participant-page/pipes/dynamicFormTypeForRgp.pipe.ts
@@ -7,6 +7,6 @@ import {ComponentService} from '../../services/component.service';
 
 export class dynamicFormTypeAndStudyRGP implements PipeTransform {
   transform(settings: any): boolean {
-    return settings.hasOwnProperty('TAB_GROUPED') && !!localStorage.getItem(ComponentService.MENU_SELECTED_REALM) === 'RGP';
+    return settings.hasOwnProperty('TAB_GROUPED') && localStorage.getItem(ComponentService.MENU_SELECTED_REALM) === 'RGP';
   }
 }
