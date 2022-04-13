15de7cf409f2d31103854922c4cf101458341478
Jose Manuel Morales Olivera <josemanu@broadinstitute.org>
2022-04-12 22:52:55 -0500

    provide format to file

--- ddp-workspace/projects/ddp-sdk/src/lib/components/activityForm/answers/activityBooleanAnswer.component.ts
+++ ddp-workspace/projects/ddp-sdk/src/lib/components/activityForm/answers/activityBooleanAnswer.component.ts
@@ -4,37 +4,43 @@ import { ActivityBooleanQuestionBlock } from '../../../models/activity/activityB
 @Component({
     selector: 'ddp-activity-boolean-answer',
     template: `
-    <ddp-question-prompt [block]="block"></ddp-question-prompt>
-    <div>
-        <mat-radio-group class="example-radio-group"
-                        [(ngModel)]="block.answer"
-                        [attr.data-ddp-test]="'answer:' + block.stableId"
-                        #booleanInput="ngModel">
-            <mat-radio-button class="primary margin"
-                              [value]="true"
-                              [disabled]="readonly"
-                              [disableRipple]="true"
-                              (change)="saveValue(true)">
+        <ddp-question-prompt [block]="block"></ddp-question-prompt>
+        <mat-radio-group
+            class="example-radio-group"
+            [(ngModel)]="block.answer"
+            [attr.data-ddp-test]="'answer:' + block.stableId"
+            #booleanInput="ngModel"
+        >
+            <mat-radio-button
+                class="primary margin"
+                [value]="true"
+                [disabled]="readonly"
+                [disableRipple]="true"
+                (change)="saveValue(true)"
+            >
                 <div [innerHTML]="block.trueContent"></div>
             </mat-radio-button>
-            <mat-radio-button class="margin"
-                              [value]="false"
-                              [disabled]="readonly"
-                              [disableRipple]="true"
-                              (change)="saveValue(false)">
+            <mat-radio-button
+                class="margin"
+                [value]="false"
+                [disabled]="readonly"
+                [disableRipple]="true"
+                (change)="saveValue(false)"
+            >
                 <div [innerHTML]="block.falseContent"></div>
             </mat-radio-button>
         </mat-radio-group>
-    </div>
     `,
     styles: [
-        `.margin {
-            margin: 15px 30px 15px 0px;
-        }
-        .example-input-field {
-            width: 50%;
-        }`
-    ]
+        `
+            .margin {
+                margin: 15px 30px 15px 0px;
+            }
+            .example-input-field {
+                width: 50%;
+            }
+        `,
+    ],
 })
 export class ActivityBooleanAnswer {
     @Input() block: ActivityBooleanQuestionBlock;
