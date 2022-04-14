80dbb1642e6a199c941e2c6420b3471536ce9a5f
alextanlan <alextanlan@gmail.com>
2022-04-14 16:04:46 +0200

    Add some linter warnings for debug purposes

--- ddp-workspace/projects/ddp-pancan/src/app/components/about-us/about-us.component.ts
+++ ddp-workspace/projects/ddp-pancan/src/app/components/about-us/about-us.component.ts
@@ -3,8 +3,8 @@ import { AppRoutes } from '../app-routes';
 
 @Component({
     selector: 'app-about-us',
-    templateUrl: './about-us.component.html'
+    templateUrl: "./about-us.component.html"
 })
 export class AboutUsComponent {
-    route = AppRoutes.AboutUs;
+    route = AppRoutes.AboutUs
 }
