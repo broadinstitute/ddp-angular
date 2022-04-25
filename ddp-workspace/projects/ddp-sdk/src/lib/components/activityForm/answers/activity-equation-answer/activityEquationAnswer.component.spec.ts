d84edf0a272912cf8dbb89c130e54e714987cc47
alextanlan <alextanlan@gmail.com>
2022-04-25 18:02:32 +0200

    Add tests

--- ddp-workspace/projects/ddp-sdk/src/lib/components/activityForm/answers/activity-equation-answer/activityEquationAnswer.component.spec.ts
+++ ddp-workspace/projects/ddp-sdk/src/lib/components/activityForm/answers/activity-equation-answer/activityEquationAnswer.component.spec.ts
@@ -1,24 +1,59 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
+
+import { QuestionType } from '../../../../models/activity/questionType';
+import { ActivityEquationQuestionBlock } from '../../../../models/activity/activityEquationQuestionBlock';
 import { ActivityEquationAnswerComponent } from './activityEquationAnswer.component';
+import { QuestionPromptComponent } from '../question-prompt/questionPrompt.component';
+import { By } from '@angular/platform-browser';
 
 describe('ActivityEquationAnswerComponent', () => {
-  let component: ActivityEquationAnswerComponent;
-  let fixture: ComponentFixture<ActivityEquationAnswerComponent>;
+    const questionBlock = {
+        answer: null,
+        maximumDecimalPlaces: 2,
+        question: 'Equation question title',
+        questionType: QuestionType.Equation
+    } as ActivityEquationQuestionBlock;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ ActivityEquationAnswerComponent ]
-    })
-    .compileComponents();
-  });
+    let component: ActivityEquationAnswerComponent;
+    let fixture: ComponentFixture<ActivityEquationAnswerComponent>;
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(ActivityEquationAnswerComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+    beforeEach(waitForAsync(() => {
+        TestBed.configureTestingModule({
+                declarations: [
+                    ActivityEquationAnswerComponent,
+                    QuestionPromptComponent
+                ]
+            })
+            .compileComponents();
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+        fixture = TestBed.createComponent(ActivityEquationAnswerComponent);
+        component = fixture.componentInstance;
+        component.block = questionBlock;
+        fixture.detectChanges();
+    }));
+
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
+
+    it('should have valid prompt', () => {
+        const promptElement: HTMLElement = fixture.debugElement.query(By.css('.ddp-question-prompt')).nativeElement;
+        expect(promptElement.innerText).toBe('Equation question title');
+    });
+
+    it('should not have equation value initially', () => {
+        const equationValueElement: HTMLElement = fixture.debugElement.query(By.css('.equation-value')).nativeElement;
+        expect(equationValueElement.innerText).toBe('');
+    });
+
+    it('should have an initial equation value', () => {
+        component.block = {
+           ...questionBlock,
+           answer: {value: 12345, scale: 4}
+        } as ActivityEquationQuestionBlock;
+        fixture.detectChanges();
+
+        const equationValueElement: HTMLElement = fixture.debugElement.query(By.css('.equation-value')).nativeElement;
+        expect(equationValueElement.innerText).toBe('1.23');
+    });
 });
