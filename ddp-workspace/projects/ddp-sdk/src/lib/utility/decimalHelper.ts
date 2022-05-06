51fd148bb29c0825832dde8cb71b3c8bc94068b5
alextanlan <alextanlan@gmail.com>
2022-05-06 21:06:54 +0200

    Improvement of a decimal answer formatting

--- ddp-workspace/projects/ddp-sdk/src/lib/utility/decimalHelper.ts
+++ ddp-workspace/projects/ddp-sdk/src/lib/utility/decimalHelper.ts
@@ -1,12 +1,32 @@
-import { DecimalAnswer } from '../models/activity/decimalAnswer';
 import * as _ from 'underscore';
+import { DecimalAnswer } from '../models/activity/decimalAnswer';
+import { NumericAnswerType } from '../models/activity/numericAnswerType';
 
-export  class DecimalHelper {
-    static mapDecimalAnswerToNumber(answer: DecimalAnswer ): number {
+export class DecimalHelper {
+    static mapDecimalAnswerToNumber(answer: DecimalAnswer): number {
         return answer.value * Math.pow(10, -(answer.scale));
     }
 
     static isDecimalAnswerType(obj: any): boolean {
         return obj && _.isNumber(obj.value) && _.isNumber(obj.scale);
+    }
+
+    static formatDecimalAnswer(answer: NumericAnswerType, decimalPartScale: number, keepDecimalPart: boolean): string {
+        const answerAsNumber = _.isNumber(answer) ? answer : DecimalHelper.mapDecimalAnswerToNumber(answer as DecimalAnswer);
+        let [
+            // eslint-disable-next-line prefer-const
+            integerPart = '0',
+            decimalPart = '0'.repeat(decimalPartScale)
+        ] = String(answerAsNumber).split('.');
+
+        if (decimalPart.length < decimalPartScale) {
+            decimalPart += '0'.repeat(decimalPartScale - decimalPart.length);
+        }
+
+        decimalPart = decimalPartScale ?
+            `.${decimalPart.slice(0, decimalPartScale)}` :
+            (keepDecimalPart ? `.${decimalPart}` : '');
+
+        return integerPart + decimalPart;
     }
 }
