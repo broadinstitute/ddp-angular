afd3816f8fb0d833ee5fc94d0ded90d0ff3d28b2
alextanlan <alextanlan@gmail.com>
2022-05-09 13:35:58 +0200

    Fix a linter warning

--- ddp-workspace/projects/ddp-lms/src/app/app.module.ts
+++ ddp-workspace/projects/ddp-lms/src/app/app.module.ts
@@ -79,20 +79,19 @@ const translateFactory =
 
       locationInitialized.then(() => {
         const locale = languageService.getAppLanguageCode();
-
         translateService.setDefaultLang(locale);
 
-        translateService.use(locale).subscribe(
-          () => {
+        translateService.use(locale).subscribe({
+          next: () => {
             loggingService.logEvent(LOG_SOURCE, `Successfully initialized '${locale}' language as default.`);
           },
-          err => {
+          error: err => {
             loggingService.logError(LOG_SOURCE, `Problem with '${locale}' language initialization:`, err);
           },
-          () => {
+          complete: () => {
             resolve(null);
           },
-        );
+        });
       });
     });
 
