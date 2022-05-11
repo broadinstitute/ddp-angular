e1d69e46514c7139c5b7496f90b7d09275b7e305
Marco Ocana <mocana@broadinstitute.org>
2022-05-11 11:58:49 -0400

    Add support for gtag.js Analytics events. Include some specific events requested by Singular

--- ddp-workspace/projects/ddp-singular/src/app/components/pages/home/home.component.ts
+++ ddp-workspace/projects/ddp-singular/src/app/components/pages/home/home.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { Route } from '../../../constants/route';
+import { AnalyticsEventsService } from 'ddp-sdk';
 
 
 @Component({
@@ -13,4 +14,19 @@ export class HomeComponent {
   readonly BROAD_INSTITUTE_URL = 'https://www.broadinstitute.org';
   readonly BOSTON_HOSPITAL_URL = 'https://www.childrenshospital.org';
   readonly GENOME_MEDICAL_URL = 'https://www.genomemedical.com';
+  readonly CONSENT_PDF_URL = 'https://storage.googleapis.com/singular-dev-assets/consent_self.pdf';
+  readonly SALIVA_KIT_PDF_URL = 'https://storage.googleapis.com/singular-dev-assets/saliva_kit.pdf'
+  readonly SALIVA_KIT_PDF_NAME = 'Preview Kit Instructions';
+  readonly CONSENT_PDF_NAME = 'Preview Consent Form';
+  readonly PRESCREEN_BUTTON_LABEL = 'Sign me up!';
+
+
+  constructor(private analytics: AnalyticsEventsService){};
+
+  public clickPreScreening(): void {
+    this.analytics.emitCustomGtagEvent("sign_me_up_button_link", this.PRESCREEN_BUTTON_LABEL);
+  }
+  public clickDownload(clickText: string, clickUrl: string): void {
+    this.analytics.emitCustomGtagEvent("pdf_download", clickText, clickUrl);
+  }
 }
