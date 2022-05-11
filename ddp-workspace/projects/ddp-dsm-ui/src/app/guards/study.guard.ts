d20519ae8d3cab35cee87bdd02d696896d5d502b
GiCharkviani <gi.charkviani@gmail.com>
2022-05-11 12:27:22 +0200

    linter-fix

--- ddp-workspace/projects/ddp-dsm-ui/src/app/guards/study.guard.ts
+++ ddp-workspace/projects/ddp-dsm-ui/src/app/guards/study.guard.ts
@@ -13,16 +13,16 @@ export class StudyGuard implements CanActivate {
   canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean {
     const selectedRealm = localStorage.getItem(ComponentService.MENU_SELECTED_REALM);
 
-    if(this.allowAccessToStudy(selectedRealm, state)) return true;
+    if(this.allowAccessToStudy(selectedRealm, state)) {return true;}
 
     this.router.navigate([selectedRealm ? selectedRealm : '']);
 
     return false;
   }
 
-  private allowAccessToStudy(selectedRealm: string, state: RouterStateSnapshot) {
+  private allowAccessToStudy(selectedRealm: string, state: RouterStateSnapshot): boolean {
     return localStorage.getItem(Auth.AUTH0_TOKEN_NAME)
       && localStorage.getItem(SessionService.DSM_TOKEN_NAME)
-      && selectedRealm === state.url.slice(1)
+      && selectedRealm === state.url.slice(1);
   }
 }
