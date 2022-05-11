10b0681727f77d2a9d19ed368a945d52c309ce07
Alex Maratkanov <alextanlan@gmail.com>
2022-05-11 17:22:08 +0200

    Fix failed test (#1274)

--- ddp-workspace/projects/ddp-lms/src/app/pages/faq-section/faq-section.component.spec.ts
+++ ddp-workspace/projects/ddp-lms/src/app/pages/faq-section/faq-section.component.spec.ts
@@ -1,6 +1,30 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { MatIconModule } from '@angular/material/icon';
+import { MatExpansionModule } from '@angular/material/expansion';
+import { MatDividerModule } from '@angular/material/divider';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { Observable, of } from 'rxjs';
+import { TranslateLoader, TranslateModule, TranslateService } from '@ngx-translate/core';
 
 import { FaqSectionComponent } from './faq-section.component';
+
+class TranslateLoaderMock implements TranslateLoader {
+  getTranslation(code: string = ''): Observable<object> {
+    const TRANSLATIONS = {
+      en: {
+        App: {
+          FAQ: {
+            Button: {
+              Collapse: 'Collapse',
+              Expand: 'Expand'
+            }
+          }
+        }
+      }
+    };
+    return of(TRANSLATIONS[code]);
+  }
+}
 
 describe('FaqSectionComponent', () => {
   let component: FaqSectionComponent;
@@ -8,9 +32,21 @@ describe('FaqSectionComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ FaqSectionComponent ]
+        imports: [
+          MatIconModule,
+          MatExpansionModule,
+          MatDividerModule,
+          NoopAnimationsModule,
+          TranslateModule.forRoot({
+            loader: {provide: TranslateLoader, useClass: TranslateLoaderMock},
+          }),
+        ],
+        declarations: [ FaqSectionComponent ]
     })
     .compileComponents();
+
+    const translate = TestBed.inject(TranslateService);
+    translate.use('en');
   });
 
   beforeEach(() => {
