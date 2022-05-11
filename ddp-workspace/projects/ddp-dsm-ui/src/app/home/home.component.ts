c904bab95bddbeb6cc214bd2551f9f0b45d01b67
GiCharkviani <gi.charkviani@gmail.com>
2022-05-11 12:54:09 +0200

    small fix

--- ddp-workspace/projects/ddp-dsm-ui/src/app/home/home.component.ts
+++ ddp-workspace/projects/ddp-dsm-ui/src/app/home/home.component.ts
@@ -7,6 +7,7 @@ import {Subject} from 'rxjs';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit, OnDestroy {
   selectedRealm: string;
