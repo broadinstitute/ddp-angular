b2510b3110a5915c60a3e2375e486d8e830fd7e0
grigol-brd <96420397+grigol-brd@users.noreply.github.com>
2022-05-18 13:26:44 +0400

    feat: restyle

--- ddp-workspace/projects/ddp-lms/src/app/pages/for-your-physician/for-your-physician.component.ts
+++ ddp-workspace/projects/ddp-lms/src/app/pages/for-your-physician/for-your-physician.component.ts
@@ -7,5 +7,5 @@ import { Component } from '@angular/core';
 })
 export class ForYourPhysicianComponent {
 
-  FYP = "https://storage.googleapis.com/cmi-study-dev-assets/lms/pdf/For%20Your%20Physician%20PDF%20-%20Linked%20on%20For%20Your%20Physician%20Page.pdf";
+  FYP = 'https://storage.googleapis.com/cmi-study-dev-assets/lms/pdf/For%20Your%20Physician%20PDF%20-%20Linked%20on%20For%20Your%20Physician%20Page.pdf';
 }
