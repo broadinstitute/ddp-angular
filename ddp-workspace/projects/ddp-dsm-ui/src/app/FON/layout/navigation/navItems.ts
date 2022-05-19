48ccd2dcf87374cbb8b96daeecb2c86aca9d156d
alextanlan <alextanlan@gmail.com>
2022-05-19 19:49:15 +0200

    Tweak styles

--- ddp-workspace/projects/ddp-dsm-ui/src/app/FON/layout/navigation/navItems.ts
+++ ddp-workspace/projects/ddp-dsm-ui/src/app/FON/layout/navigation/navItems.ts
@@ -3,7 +3,11 @@ export const topNavItems = [
   {name: 'Patients', route: 'patients', img: ''},
   {name: 'Reporting', route: '', img: ''},
   {name: 'Administration', route: '', img: ''}
-]
+];
 
-export const botNavItems = (userName: string) =>
-  [{name: userName, route: '', img: ''}, {name: 'Sign out', img: '', signOut: true}, {name: 'Help', route: '', img: ''}];
+export const bottomNavItems = (userName: string): any[] =>
+  [
+    {name: userName, route: '', img: ''},
+    {name: 'Sign out', img: '', signOut: true},
+    {name: 'Help', route: '', img: ''}
+  ];
--- ddp-workspace/projects/ddp-dsm-ui/src/app/app-routing.module.ts
+++ ddp-workspace/projects/ddp-dsm-ui/src/app/app-routing.module.ts
@@ -2,19 +2,19 @@ import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
 import {StudyGuard} from './guards/study.guard';
 import {CheckAuthGuard} from './guards/checkAuth.guard';
-import {ParticipantsListComponent} from "./FON/pages/participantsList/participantsList.component";
-import {ActivitiesComponent} from "./FON/pages/activities/activities.component";
-import {FonComponent} from "./FON/fon.component";
-import {HomeComponent} from "./FON/pages/home/home.component";
-import {ActivityComponent} from "./FON/pages/activities/activity/activity.component";
-import {StudyActGuard} from "./guards/studyAct.guard";
+import {ParticipantsListComponent} from './FON/pages/participantsList/participantsList.component';
+import {ActivitiesComponent} from './FON/pages/activities/activities.component';
+import {FonComponent} from './FON/fon.component';
+// import {HomeComponent} from './FON/pages/home/home.component';
+import {ActivityComponent} from './FON/pages/activities/activity/activity.component';
+import {StudyActGuard} from './guards/studyAct.guard';
 
 export const AppRoutes: Routes = [
   {path: '', loadChildren: () => import('./WELCOME/welcome.module').then(m => m.WelcomeModule),
     canLoad: [CheckAuthGuard], pathMatch: 'full'},
 
   {path: 'fon', component: FonComponent, canActivate: [StudyActGuard], children: [
-      {path: '', component: HomeComponent},
+      // {path: '', component: HomeComponent}, // TODO: Do we need it ?
       {path: 'patients', component: ParticipantsListComponent},
       {path: 'patient/:guid', component: ActivitiesComponent, children: [
           {path: ':activity', component: ActivityComponent}
@@ -24,7 +24,7 @@ export const AppRoutes: Routes = [
   {path: ':study', loadChildren: () => import('./ALL-STUDIES/all-studies.module').then(m => m.AllStudiesModule),
     canLoad: [StudyGuard]},
 
-  {path: '**', redirectTo: ''} //@TODO: Delete after "not found" page component is made
+  {path: '**', redirectTo: ''} //@TODO: Delete after 'not found' page component is made
 ];
 
 @NgModule({
