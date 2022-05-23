f3f87e8b7139cd522e4e3a4fe4ed933f234afd13
Guram Makharashvili <gmakhara@Gurams-MacBook-Pro.local>
2022-05-23 15:09:07 +0400

    DDP-8019 | fix test

--- ddp-workspace/projects/ddp-dsm-ui/src/app/tags/cohort-tag/cohort-tag.model.spec.ts
+++ ddp-workspace/projects/ddp-dsm-ui/src/app/tags/cohort-tag/cohort-tag.model.spec.ts
@@ -2,6 +2,6 @@ import { CohortTag } from './cohort-tag.model';
 
 describe('CohortTag', () => {
   it('should create an instance', () => {
-    expect(new CohortTag()).toBeTruthy();
+    expect(new CohortTag('TestTag', 'TestGUID', 999)).toBeTruthy();
   });
 });
