47b35bb2f9898d573519594cd66a41cbf78cfe13
alextanlan <alextanlan@gmail.com>
2022-05-23 17:05:26 +0200

    Fix tests

--- ddp-workspace/projects/ddp-dsm-ui/src/app/tags/cohort-tag/cohort-tag.component.spec.ts
+++ ddp-workspace/projects/ddp-dsm-ui/src/app/tags/cohort-tag/cohort-tag.component.spec.ts
@@ -1,14 +1,33 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatChipsModule } from '@angular/material/chips';
 
 import { CohortTagComponent } from './cohort-tag.component';
+import { ComponentService } from '../../services/component.service';
+import { DSMService } from '../../services/dsm.service';
 
 describe('CohortTagComponent', () => {
   let component: CohortTagComponent;
   let fixture: ComponentFixture<CohortTagComponent>;
+  let componentServiceSpy: jasmine.SpyObj<ComponentService>;
+  let dsmServiceSpy: jasmine.SpyObj<DSMService>;
 
   beforeEach(async () => {
+    componentServiceSpy = jasmine.createSpyObj('ComponentService', ['getRealm']);
+    dsmServiceSpy = jasmine.createSpyObj('DSMService', ['createCohortTag', 'deleteCohortTag']);
+
     await TestBed.configureTestingModule({
-      declarations: [ CohortTagComponent ]
+      declarations: [ CohortTagComponent ],
+      imports: [
+        MatChipsModule,
+        MatFormFieldModule,
+        NoopAnimationsModule
+      ],
+      providers: [
+        { provide: ComponentService, useValue: componentServiceSpy},
+        { provide: DSMService, useValue: componentServiceSpy}
+      ],
     })
     .compileComponents();
   });
@@ -16,6 +35,7 @@ describe('CohortTagComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(CohortTagComponent);
     component = fixture.componentInstance;
+    component.dsm = {};
     fixture.detectChanges();
   });
 
