62fbed395624e8d2258a30ee54cfb8dcf09c27fa
GiCharkviani <gi.charkviani@gmail.com>
2022-05-24 12:59:26 +0200

    more fixes for pagination

--- ddp-workspace/projects/ddp-dsm-ui/src/app/FON/layout/navigation/navigation.component.ts
+++ ddp-workspace/projects/ddp-dsm-ui/src/app/FON/layout/navigation/navigation.component.ts
@@ -1,6 +1,7 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import {ChangeDetectionStrategy, Component, OnInit} from '@angular/core';
 import { bottomNavItems, topNavItems } from './navItems';
 import { Auth } from '../../../services/auth.service';
+import {ActivatedRoute, Params} from '@angular/router';
 
 @Component({
   selector: 'app-navigation',
@@ -9,11 +10,23 @@ import { Auth } from '../../../services/auth.service';
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 
-export class NavigationComponent {
+export class NavigationComponent implements OnInit {
   readonly topNavigation = topNavItems;
   readonly botNavigation = bottomNavItems('Giorgi Charkviani');
 
-  constructor(private auth: Auth) {
+  private readonly LSParams: string = 'pListQueryParams';
+  qParams;
+
+  constructor(private auth: Auth, private activatedRoute: ActivatedRoute) {
+  }
+
+  ngOnInit(): void {
+    this.activatedRoute.queryParams.subscribe((params: Params) => {
+      localStorage.setItem(this.LSParams,
+        JSON.stringify({from: params?.from || 0, to: params?.to || 10}));
+
+      this.qParams = JSON.parse(localStorage.getItem(this.LSParams));
+    });
   }
 
   signOut(allow: boolean): void {
