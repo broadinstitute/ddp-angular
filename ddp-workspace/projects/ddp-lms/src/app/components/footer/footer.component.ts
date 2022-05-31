c8192189e5b5172bc6d0a2e5ceade1becf88d791
GiCharkviani <gi.charkviani@gmail.com>
2022-05-31 12:14:40 +0400

    LMS: footer and header as well

--- ddp-workspace/projects/ddp-lms/src/app/components/footer/footer.component.ts
+++ ddp-workspace/projects/ddp-lms/src/app/components/footer/footer.component.ts
@@ -1,11 +1,20 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 import { Route } from '../../constants/Route';
 
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
+  styleUrls: ['./footer.component.scss']
 })
 export class FooterComponent {
   readonly Route = Route;
+  currentYear: number = new Date().getFullYear();
+
+  scrollTop(): void {
+    window.scroll({
+      top: 0,
+      behavior: 'smooth'
+    });
+  }
 }
--- ddp-workspace/projects/ddp-lms/src/app/components/header/header.component.ts
+++ ddp-workspace/projects/ddp-lms/src/app/components/header/header.component.ts
@@ -1,12 +1,9 @@
 import { Component } from '@angular/core';
 
-import { Route } from '../../constants/Route';
-
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss'],
 })
 export class HeaderComponent {
-  readonly Route = Route;
 }
