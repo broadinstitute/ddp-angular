67464b0070989e6010e36c7717b4428073b65429
grigol-brd <96420397+grigol-brd@users.noreply.github.com>
2022-06-02 22:59:44 +0400

    feat: QA comments fix

--- ddp-workspace/projects/ddp-lms/src/app/pages/scientific-impact/scientific-impact.component.ts
+++ ddp-workspace/projects/ddp-lms/src/app/pages/scientific-impact/scientific-impact.component.ts
@@ -1,8 +1,28 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-scientific-impact',
   templateUrl: './scientific-impact.component.html',
   styleUrls: ['./scientific-impact.component.scss'],
 })
-export class ScientificImpactComponent {}
+export class ScientificImpactComponent {
+  imageArray = [{
+    src: 'assets/images/Angio.png',
+    alt: "test1",
+    text: 'SIPage.IMGSection.Text1',
+    link: "https://www.cbioportal.org/study/summary?id=angs_painter_2020"
+  },
+    {
+      src: "assets/images/meta.png",
+      alt: "test2",
+      text: 'SIPage.IMGSection.Text2',
+      link: "https://www.cbioportal.org/study/summary?id=mpcproject_broad_2021"
+    },
+    {
+      src: "assets/images/breast.png",
+      alt: "test3",
+      text: 'SIPage.IMGSection.Text3',
+      link: "https://www.cbioportal.org/study/summary?id=brca_mbcproject_wagle_2017"
+    }]
+
+}
