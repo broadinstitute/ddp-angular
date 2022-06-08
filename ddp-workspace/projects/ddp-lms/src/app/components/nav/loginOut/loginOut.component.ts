9767fc3fe65aadafdbee9a772542349b507cd218
grigol-brd <gtchkoid@broadinstitute.org>
2022-06-08 16:24:09 +0400

    fixxxxx

--- ddp-workspace/projects/ddp-lms/src/app/components/nav/loginOut/loginOut.component.ts
+++ ddp-workspace/projects/ddp-lms/src/app/components/nav/loginOut/loginOut.component.ts
@@ -21,7 +21,6 @@ import { Route } from '../../../constants/Route';
           {{ 'Header.Links.CountMeIn' | translate }}
         </a>
       </li>
-      
       <li class="button button_primary cmiBtn" [routerLinkActiveOptions]="{exact: true}" routerLinkActive="link-list__item_active" 
       *ngIf="isAuthenticated">
         <a class="link" [routerLink]="Route.Dashboard">
