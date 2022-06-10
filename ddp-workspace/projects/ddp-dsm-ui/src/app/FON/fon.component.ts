10becb3f840fcd65c4c29555331b26ceef025a2d
Jose Manuel Morales Olivera <josemanu@broadinstitute.org>
2022-06-10 02:07:31 -0500

    use material sidenav

--- ddp-workspace/projects/ddp-dsm-ui/src/app/FON/fon.component.ts
+++ ddp-workspace/projects/ddp-dsm-ui/src/app/FON/fon.component.ts
@@ -4,39 +4,21 @@ import {Title} from '@angular/platform-browser';
 @Component({
   selector: 'app-fon',
   template: `
-    <div class="mainHolder">
-      <h1 class="header">Fontan Outcomes Network</h1>
-      <router-outlet></router-outlet>
-
-      <app-navigation></app-navigation>
-    </div>
+    <mat-drawer-container class="main-container">
+      <mat-drawer mode="side" opened>
+        <app-navigation></app-navigation>
+      </mat-drawer>
+      <mat-drawer-content>
+        <h1 class="header">Fontan Outcomes Network</h1>
+        <router-outlet></router-outlet>
+      </mat-drawer-content>
+    </mat-drawer-container>
   `,
   styles: [
     `
-      .mainHolder {
-        display: grid;
-        grid-template-columns: 230px auto;
+      .main-container {
+        width: 100vw;
         height: 100vh;
-        column-gap: 50px;
-        width: 100%;
-        margin: 0;
-        padding: 0;
-        grid-template-areas: '. header' 'sidebarNavigation otherPage';
-      }
-
-      .header {
-        grid-area: header;
-        height: 100px;
-        line-height: 100px;
-        margin: 0;
-      }
-
-      app-navigation {
-        grid-area: sidebarNavigation;
-      }
-
-      router-outlet {
-        grid-area: otherPage;
       }
     `,
   ],
