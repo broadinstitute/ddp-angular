e061d70217b05a1c265b8d30c888c4853b00a2b4
Jose Manuel Morales Olivera <josemanu@broadinstitute.org>
2022-06-15 11:18:33 -0500

    push latest

--- ddp-workspace/projects/ddp-dsm-ui/src/app/add-patients-modal/add-patients-modal.component.ts
+++ ddp-workspace/projects/ddp-dsm-ui/src/app/add-patients-modal/add-patients-modal.component.ts
@@ -1,15 +1,45 @@
 import { Component, OnInit } from '@angular/core';
+import { MatDialogRef } from '@angular/material/dialog';
+import { BehaviorSubject, combineLatest, map, Observable, of } from 'rxjs';
 
 @Component({
   selector: 'app-add-patients-modal',
   templateUrl: './add-patients-modal.component.html',
-  styleUrls: ['./add-patients-modal.component.scss']
+  styleUrls: ['./add-patients-modal.component.scss'],
 })
 export class AddPatientsModalComponent implements OnInit {
+  firstName$ = new BehaviorSubject('');
+  lastName$ = new BehaviorSubject('');
+  dateOfBirth$ = new BehaviorSubject('');
+  email$ = new BehaviorSubject('');
+  consentDate$ = new BehaviorSubject('');
+  assentDate$ = new BehaviorSubject('');
 
-  constructor() { }
+  constructor(public dialogRef: MatDialogRef<AddPatientsModalComponent>) {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  addNow() {
+    console.log({
+      firstName: this.firstName$.value,
+      lastName: this.lastName$.value,
+      dateOfBirth: this.dateOfBirth$.value,
+      email: this.email$.value,
+      consentDate: this.consentDate$.value,
+      assentDate: this.assentDate$.value,
+    });
+
+    this.closeDialog();
   }
 
+  closeDialog() {
+    this.dialogRef.close({
+      firstName: this.firstName$.value,
+      lastName: this.lastName$.value,
+      dateOfBirth: this.dateOfBirth$.value,
+      email: this.email$.value,
+      consentDate: this.consentDate$.value,
+      assentDate: this.assentDate$.value,
+    });
+  }
 }
--- ddp-workspace/projects/ddp-dsm-ui/src/app/patients/patients.component.ts
+++ ddp-workspace/projects/ddp-dsm-ui/src/app/patients/patients.component.ts
@@ -1,20 +1,28 @@
-import { Component, OnInit } from "@angular/core";
-import { MatDialog } from "@angular/material/dialog";
-import { AddPatientsModalComponent } from "../add-patients-modal/add-patients-modal.component";
+import { Component, OnInit } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { AddPatientsModalComponent } from '../add-patients-modal/add-patients-modal.component';
 
 @Component({
-  selector: "app-patients",
-  templateUrl: "./patients.component.html",
-  styleUrls: ["./patients.component.scss"],
+  selector: 'app-patients',
+  templateUrl: './patients.component.html',
+  styleUrls: ['./patients.component.scss'],
 })
 export class PatientsComponent implements OnInit {
+  patientsForm = {};
   constructor(private dialog: MatDialog) {}
 
   ngOnInit(): void {}
 
   openAddPatientsModal() {
-    this.dialog.open(AddPatientsModalComponent, {
-      width: "600px",
+    const dialogRef = this.dialog.open(AddPatientsModalComponent, {
+      width: '600px',
+    });
+
+    dialogRef.afterClosed().subscribe((result) => {
+      console.log(result);
+      this.patientsForm = result;
     });
   }
+
+  addPatient() {}
 }
