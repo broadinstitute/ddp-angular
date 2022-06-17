977b27c2d9b841470e1e3ea57a63e9e28025a327
nsakande <nsakande@broadinstitute.org>
2022-06-17 13:08:02 +0400

    feat: faq page updates

--- ddp-workspace/projects/ddp-osteo/src/app/components/faq/faq.component.ts
+++ ddp-workspace/projects/ddp-osteo/src/app/components/faq/faq.component.ts
@@ -1,56 +1,34 @@
-import { Component, ElementRef, Inject, OnInit, QueryList, ViewChild, ViewChildren } from '@angular/core';
-import { MatExpansionPanel } from '@angular/material/expansion';
-import { ToolkitConfigurationService, HeaderConfigurationService } from 'toolkit';
+import {Component, ElementRef, Inject, OnInit, QueryList, ViewChild, ViewChildren} from '@angular/core';
+import {MatAccordion, MatExpansionPanel} from '@angular/material/expansion';
+import {ToolkitConfigurationService, HeaderConfigurationService} from 'toolkit';
 
 @Component({
-  selector: 'app-faq',
-  templateUrl: './faq.component.html',
-  styleUrls: ['./faq.component.scss']
+    selector: 'app-faq',
+    templateUrl: './faq.component.html',
+    styleUrls: ['./faq.component.scss']
 })
 export class FaqComponent implements OnInit {
-  public infoEmail: string;
-  public phone: string;
-  public infoEmailHref: string;
-  public phoneHref: string;
+    public infoEmail: string;
+    public phone: string;
+    public infoEmailHref: string;
+    public phoneHref: string;
 
-  @ViewChildren(MatExpansionPanel)
-  private expansionPanels: QueryList<MatExpansionPanel>;
 
-  @ViewChildren(MatExpansionPanel, { read: ElementRef })
-  private expansionElements: QueryList<ElementRef<HTMLDivElement>>;
 
-  constructor(
-    private headerConfig: HeaderConfigurationService,
-    @Inject('toolkit.toolkitConfig') private toolkitConfiguration: ToolkitConfigurationService) { }
+    constructor(
+        private headerConfig: HeaderConfigurationService,
+        @Inject('toolkit.toolkitConfig') private toolkitConfiguration: ToolkitConfigurationService) {
+    }
 
-  public ngOnInit(): void {
-    this.infoEmail = this.toolkitConfiguration.infoEmail;
-    this.infoEmailHref = `mailto:${this.toolkitConfiguration.infoEmail}`;
-    this.phone = this.toolkitConfiguration.phone;
-    this.phoneHref = `tel:${this.toolkitConfiguration.phone}`;
-    this.headerConfig.setupDefaultHeader();
-  }
+    public ngOnInit(): void {
+        this.infoEmail = this.toolkitConfiguration.infoEmail;
+        this.infoEmailHref = `mailto:${this.toolkitConfiguration.infoEmail}`;
+        this.phone = this.toolkitConfiguration.phone;
+        this.phoneHref = `tel:${this.toolkitConfiguration.phone}`;
+        this.headerConfig.setupDefaultHeader();
+    }
 
-  public isText(paragraph: unknown): boolean {
-    return typeof paragraph === 'string';
-  }
 
-  public isLinkItem(type: string): boolean {
-    return type === 'link';
-  }
 
-  public expandAndScrollTo(id: string): void {
-    const scrollToElementIndex =
-      this.expansionElements.toArray().findIndex(({nativeElement}) => nativeElement.id === id);
 
-    const element = this.expansionElements.get(scrollToElementIndex);
-    const expansionPanel = this.expansionPanels.get(scrollToElementIndex);
-
-    element.nativeElement.scrollIntoView({
-      behavior: 'smooth',
-      block: 'center'
-    });
-
-    expansionPanel.open();
-  }
 }
