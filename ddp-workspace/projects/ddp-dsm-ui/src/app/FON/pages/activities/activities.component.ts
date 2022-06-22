5b6bc1cc661628be0074eb577433670a74295940
GiCharkviani <gi.charkviani@gmail.com>
2022-06-22 14:37:19 +0400

    Improved UX/UI

--- ddp-workspace/projects/ddp-dsm-ui/src/app/FON/pages/activities/activities.component.ts
+++ ddp-workspace/projects/ddp-dsm-ui/src/app/FON/pages/activities/activities.component.ts
@@ -1,9 +1,14 @@
-import {ChangeDetectionStrategy, Component, OnInit} from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  OnInit,
+} from '@angular/core';
 import {ActivatedRoute, Params, Router} from '@angular/router';
 import {Observable, tap} from 'rxjs';
 import {StoreService} from '../../../STORE/store.service';
 import {MainConstants} from '../../constants/main-constants';
 import {sectionGuids} from './utils/sections_guids';
+import {MatExpansionPanel} from "@angular/material/expansion";
 
 @Component({
   selector: 'app-activities',
@@ -26,7 +31,16 @@ export class ActivitiesComponent implements OnInit {
   ngOnInit(): void {
     this.activatedRoute.params.subscribe((params: Params) => {
       this.patientWithActivities$ = this.storeService.getParticipantActivities(params.guid)
-        .pipe(tap(data => !data && this.storeService.dispatchGetParticipant(params.guid, this.PARENT)));
+        .pipe(
+          tap(data => !data && this.storeService.dispatchGetParticipant(params.guid, this.PARENT)),
+          tap(result => {
+            const openActivityGuid = this.activatedRoute.snapshot.firstChild?.params.activity;
+            if(!openActivityGuid && result) {
+              const activityGuid = result.activities['ENROLLMENT_FORMS'].activities[0].activityGuid;
+              this.router.navigate(['./', activityGuid], {relativeTo: this.activatedRoute});
+            }
+          })
+        );
     });
   }
 
