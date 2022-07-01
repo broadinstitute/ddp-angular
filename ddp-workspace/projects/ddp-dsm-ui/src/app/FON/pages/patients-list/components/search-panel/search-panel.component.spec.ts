9d08380a1c22787fd727103eeaf4a8e96bb0b510
alextanlan <alextanlan@gmail.com>
2022-07-01 14:48:23 +0200

    Add search panel (draft) on Patient list page

---
+++ ddp-workspace/projects/ddp-dsm-ui/src/app/FON/pages/patients-list/components/search-panel/search-panel.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { SearchPanelComponent } from './search-panel.component';
+
+describe('SearchPanelComponent', () => {
+  let component: SearchPanelComponent;
+  let fixture: ComponentFixture<SearchPanelComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ SearchPanelComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(SearchPanelComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
