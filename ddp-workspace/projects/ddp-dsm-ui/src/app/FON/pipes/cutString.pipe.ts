454dced573d627ec4e84ca7a5b297a067bb198bd
GiCharkviani <gi.charkviani@gmail.com>
2022-07-04 14:25:04 +0400

    Updated pipe

--- ddp-workspace/projects/ddp-dsm-ui/src/app/FON/pipes/cutString.pipe.ts
+++ ddp-workspace/projects/ddp-dsm-ui/src/app/FON/pipes/cutString.pipe.ts
@@ -5,7 +5,7 @@ import {Pipe, PipeTransform} from '@angular/core';
 })
 
 export class CutStringPipe implements PipeTransform {
-  transform(value: string, from: number = 17): string {
-    return value.length > from ? value.substring(0, from) + '...' : value;
+  transform(value: string, from: number = 17, trail = '...'): string {
+    return value.length > from ? value.substring(0, from) + trail : value;
   }
 }
