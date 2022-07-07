f62f5703110c28c7358af8b0ffed0a2319358c9a
llobzhanidze <llobjani@broadinstitute.org>
2022-07-07 12:38:45 +0400

    DDP/7520 - DDP/7501 tooltip and mail compare menu fix

--- ddp-workspace/projects/ddp-sdk/src/lib/components/activityForm/answers/question-prompt/questionPrompt.component.ts
+++ ddp-workspace/projects/ddp-sdk/src/lib/components/activityForm/answers/question-prompt/questionPrompt.component.ts
@@ -1,6 +1,11 @@
 import { Component, Input } from '@angular/core';
 import { ActivityQuestionBlock } from '../../../../models/activity/activityQuestionBlock';
 
+
+
+
+
+
 @Component({
     selector: 'ddp-question-prompt',
     template: `
@@ -12,7 +17,7 @@ import { ActivityQuestionBlock } from '../../../../models/activity/activityQuest
                  [innerHTML]="block.additionalInfoHeader"
                  class="ddp-question-prompt__info-header">
            </span>
-           <ddp-tooltip *ngIf="block.tooltip" class="tooltip" [text]="block.tooltip"></ddp-tooltip>
+           <ddp-tooltip *ngIf="block.tooltip" class="tooltip" [text]="block.tooltip" [icon]="icon"></ddp-tooltip>
         </p>
     </div>`,
     styleUrls: ['questionPrompt.component.scss']
@@ -22,6 +27,8 @@ export class QuestionPromptComponent {
      * Content is expected to be an HTML fragment
      */
     @Input() block: ActivityQuestionBlock<any>;
+
+    readonly icon: string = "../../../../../assets/info-circle.svg";
 
     public get displayAsRequired(): boolean {
         return this.block?.isRequired && this.block?.question && (this.block.question.trim().length > 0);
