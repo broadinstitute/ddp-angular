177e5ddcfd530ec886064bd7455abaaac57be887
GiCharkviani <77500504+GiCharkviani@users.noreply.github.com>
2022-07-07 11:56:28 +0400

    fix come ooon (#1495)

--- ddp-workspace/projects/ddp-dsm-ui/src/app/FON/services/http.service.ts
+++ ddp-workspace/projects/ddp-dsm-ui/src/app/FON/services/http.service.ts
@@ -1,15 +1,14 @@
-import {Injectable} from '@angular/core';
+import {Inject, Injectable} from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 import {AddPatientModel} from '../models/addPatient.model';
 import {Observable} from 'rxjs';
-
-declare const DDP_ENV;
+import {ConfigurationService} from 'ddp-sdk';
 
 @Injectable()
 export class HttpService {
-  readonly ADD_PATIENT_URL = `${DDP_ENV.backendUrl}/pepper/v1/user`;
+  readonly ADD_PATIENT_URL = `${this.config.backendUrl}/pepper/v1/user`;
 
-  constructor(private httpClient: HttpClient) {
+  constructor(private httpClient: HttpClient, @Inject('ddp.config') private config: ConfigurationService) {
   }
 
   addPatient(addPatientModel: AddPatientModel): Observable<any> {
