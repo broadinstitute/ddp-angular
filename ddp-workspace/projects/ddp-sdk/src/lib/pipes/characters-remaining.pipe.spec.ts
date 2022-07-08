087996bd7c25c434fdf54d4604f5a5a2dd98d135
gigijanelidze26 <gjanelidze@broadinstitute.org>
2022-07-08 13:45:02 +0400

    update spec file.

--- ddp-workspace/projects/ddp-sdk/src/lib/pipes/characters-remaining.pipe.spec.ts
+++ ddp-workspace/projects/ddp-sdk/src/lib/pipes/characters-remaining.pipe.spec.ts
@@ -1,8 +1,27 @@
 import { CharactersRemainingPipe } from './characters-remaining.pipe';
 
 describe('CharactersRemainingPipe', () => {
-  it('create an instance', () => {
-    const pipe = new CharactersRemainingPipe();
-    expect(pipe).toBeTruthy();
-  });
+    let pipe: CharactersRemainingPipe;
+    const maxLength = 350;
+
+    beforeEach(() => {
+        pipe = new CharactersRemainingPipe();
+    });
+
+    it('create an instance', () => {
+        expect(pipe).toBeTruthy();
+    });
+
+    it('return "350 characters remaining" if value is falsy', () => {
+        const value = null;
+        const expectedValue = '350 characters remaining'
+        expect(pipe.transform(value, maxLength)).toBe(expectedValue);
+    });
+
+    it('value should be "340 characters remaining"  if value has length of 10', () => {
+        const value = '0123456789';
+        const expectedValue = '340 characters remaining';
+
+        expect(pipe.transform(value, maxLength)).toBe(expectedValue);
+    })
 });
