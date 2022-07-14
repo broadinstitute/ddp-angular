d7d6e247a0a1aeadd68690cdf5a55f4a66f855a8
pegahtah <ptaheri@broadinstitute.org>
2022-07-14 13:36:30 -0400

    DDP-8138 this was missed from going into develop

--- ddp-workspace/projects/ddp-dsm-ui/src/app/onc-history-detail/onc-history-detail.model.ts
+++ ddp-workspace/projects/ddp-dsm-ui/src/app/onc-history-detail/onc-history-detail.model.ts
@@ -3,7 +3,6 @@ import { Tissue } from '../tissue/tissue.model';
 export class OncHistoryDetail {
   changed = false;
   selected = false;
-  deleted = false;
 
   changedBy: string;
 
@@ -17,7 +16,7 @@ export class OncHistoryDetail {
               public tissueReceived: string, public gender: string,
               public additionalValuesJson: {}, public tissues: Array<Tissue>,
               public tissueProblemOption: string, public destructionPolicy: string, public unableObtainTissue: boolean,
-              public numberOfRequests) {
+              public numberOfRequests, public deleted: boolean) {
     this.participantId = participantId;
     this.oncHistoryDetailId = oncHistoryDetailId;
     this.medicalRecordId = medicalRecordId;
@@ -47,6 +46,7 @@ export class OncHistoryDetail {
     this.tissueProblemOption = tissueProblemOption;
     this.destructionPolicy = destructionPolicy;
     this.unableObtainTissue = unableObtainTissue;
+    this.deleted = deleted;
   }
 
   static parse(json): OncHistoryDetail {
@@ -73,7 +73,7 @@ export class OncHistoryDetail {
       json.tFaxSent2, json.tFaxSent2By, json.tFaxConfirmed2,
       json.tFaxSent3, json.tFaxSent3By, json.tFaxConfirmed3,
       json.tissueReceived, json.gender, additionalValuesJson, tissues,
-      json.tissueProblemOption, json.destructionPolicy, json.unableObtainTissue, json.numberOfRequests
+      json.tissueProblemOption, json.destructionPolicy, json.unableObtainTissue, json.numberOfRequests, json.deleted
     );
   }
 }
