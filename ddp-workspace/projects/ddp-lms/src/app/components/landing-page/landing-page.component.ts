29dcd5a638c3c2a9d268ca7945496988ff518a7c
GiCharkviani <gi.charkviani@gmail.com>
2022-07-22 00:06:49 +0400

    without-timer

--- ddp-workspace/projects/ddp-lms/src/app/components/landing-page/landing-page.component.ts
+++ ddp-workspace/projects/ddp-lms/src/app/components/landing-page/landing-page.component.ts
@@ -45,9 +45,9 @@ export class LandingPageComponent implements OnInit {
       this.auth0.handleAuthentication(this.handleAuthError.bind(this));
     }
 
-    setTimeout(() => {
+  
       this.load().subscribe();
-    }, 5000);
+
   }
 
   protected handleAuthError(error: any | null): void {
