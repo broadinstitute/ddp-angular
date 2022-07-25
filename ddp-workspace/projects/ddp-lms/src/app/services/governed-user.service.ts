d8b718298fab69791a055f95b221e743dd06bb45
GiCharkviani <77500504+GiCharkviani@users.noreply.github.com>
2022-07-25 21:20:13 +0400

    Ddp it works (#1581)
    * DDP-IT-WORKS
    
    * linter fix

--- ddp-workspace/projects/ddp-lms/src/app/services/governed-user.service.ts
+++ ddp-workspace/projects/ddp-lms/src/app/services/governed-user.service.ts
@@ -30,16 +30,16 @@ export class GovernedUserService {
   public get checkIfGoverned(): Observable<[]> {
     return this.sessionService.sessionObservable.pipe(
       filter((session) => !!session && this.sessionService.isAuthenticatedSession()),
-      mergeMap(() => this.userActivityServiceAgent.getActivities(of(this.config.studyGuid))
-        .pipe(catchError(() => throwError(() => 'PREQUALIFIER_ERROR')))),
-      mergeMap((userActivities) => iif(() =>
-            !userActivities.length || !!userActivities.find(activity => activity.activityCode === this.PREQUAL),
-          this.prequalService.getPrequalifier(this.config.studyGuid),
-          throwError(() => 'PREQUALIFIER_ERROR')
+      mergeMap(() => this.userActivityServiceAgent.getActivities(of(this.config.studyGuid)).pipe(
+          mergeMap((userActivities) => iif(() =>
+                  !!userActivities,
+                this.prequalService.getPrequalifier(this.config.studyGuid),
+                throwError(() => 'NO_PREQUALIFIER')
+              )
+          ),
         )
       ),
-      mergeMap((instanceGuid) =>
-        this.activityService.getActivity(of(this.config.studyGuid), of(instanceGuid.guid))),
+      mergeMap((instanceGuid) => this.activityService.getActivity(of(this.config.studyGuid), of(instanceGuid.guid))),
       pluck('sections'),
       map((sections) => sections[0]),
       pluck('blocks'),
