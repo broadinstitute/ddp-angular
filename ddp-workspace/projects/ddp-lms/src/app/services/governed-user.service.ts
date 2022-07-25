61cb75528206460e5a35f57a459d83696ecddf9c
GiCharkviani <77500504+GiCharkviani@users.noreply.github.com>
2022-07-25 20:21:05 +0400

    REMOVE_ERROR_LOG (#1579)
    * REMOVE_ERROR_LOG
    
    * more more
    
    * linter fix

--- ddp-workspace/projects/ddp-lms/src/app/services/governed-user.service.ts
+++ ddp-workspace/projects/ddp-lms/src/app/services/governed-user.service.ts
@@ -15,6 +15,7 @@ import {PrequalifierService} from './prequalifier.service';
 export class GovernedUserService {
 
   private readonly WHO_ENROLLING = 'WHO_ENROLLING';
+  private readonly PREQUAL = 'PREQUALIFIER';
 
   constructor(
     private router: Router,
@@ -29,23 +30,27 @@ export class GovernedUserService {
   public get checkIfGoverned(): Observable<[]> {
     return this.sessionService.sessionObservable.pipe(
       filter((session) => !!session && this.sessionService.isAuthenticatedSession()),
-      mergeMap(() => this.userActivityServiceAgent.getActivities(of(this.config.studyGuid)).pipe(
-          mergeMap((userActivities) => iif(() =>
-                  !!userActivities,
-                this.prequalService.getPrequalifier(this.config.studyGuid),
-                throwError(() => 'NO_PREQUALIFIER')
-              )
-          ),
+      mergeMap(() => this.userActivityServiceAgent.getActivities(of(this.config.studyGuid))
+        .pipe(catchError(() => throwError(() => 'PREQUALIFIER_ERROR')))
+      ),
+      mergeMap((userActivities) => iif(() =>
+            !!userActivities.find(activity => activity.activityCode === this.PREQUAL),
+          this.prequalService.getPrequalifier(this.config.studyGuid),
+          throwError(() => 'PREQUALIFIER_ERROR')
         )
       ),
-      mergeMap((instanceGuid) => this.activityService.getActivity(of(this.config.studyGuid), of(instanceGuid.guid))),
+      mergeMap((instanceGuid) =>
+        this.activityService.getActivity(of(this.config.studyGuid), of(instanceGuid.guid))),
       pluck('sections'),
       map((sections) => sections[0]),
       pluck('blocks'),
       map((blocks) => blocks.find((block) => (block as ActivityPicklistQuestionBlock).stableId === this.WHO_ENROLLING)),
       pluck('answer'),
       take(1),
-      catchError(() => of(null))
+      catchError(data => {
+        console.error(data, '[ERROR]');
+        return of(null);
+      })
     ) as Observable<[]>;
   }
 }
