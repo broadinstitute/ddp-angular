899d59b8985d16050f01a0199e38c0b309eb7948
gigijanelidze26 <gjanelidze@broadinstitute.org>
2022-07-28 15:59:13 +0400

    DDP-8379 fix email bug

--- ddp-workspace/projects/ddp-osteo/src/app/components/activity-page/activity-page.component.ts
+++ ddp-workspace/projects/ddp-osteo/src/app/components/activity-page/activity-page.component.ts
@@ -1,17 +1,58 @@
-import { Component } from '@angular/core';
-import { ActivityRedesignedComponent } from 'toolkit';
+import { Component, Inject, OnInit } from '@angular/core';
+import {
+    ActivityRedesignedComponent,
+    HeaderConfigurationService,
+    ToolkitConfigurationService,
+    WorkflowBuilderService,
+} from 'toolkit';
+import { ACTUAL_PARTICIPANT_ID_TOKEN } from '../../../../../ddp-lms/src/app/activity-page/localProviders';
+import { Observable } from 'rxjs';
+import { SessionMementoService } from 'ddp-sdk';
+import { ActivatedRoute } from '@angular/router';
+import { first, tap } from 'rxjs/operators';
 
 @Component({
-  selector: 'app-activity-page',
-  template: `
-    <app-activity
-      [studyGuid]="studyGuid"
-      [activityGuid]="instanceGuid"
-      [agreeConsent]="config.agreeConsent"
-      (submit)="navigate($event)"
-      (stickySubtitle)="showStickySubtitle($event)"
-      (activityCode)="activityCodeChanged($event)">
-    </app-activity>
-  `,
+    selector: 'app-activity-page',
+    template: `
+        <app-activity
+            [studyGuid]="studyGuid"
+            [activityGuid]="instanceGuid"
+            [agreeConsent]="config.agreeConsent"
+            (submit)="navigate($event)"
+            (stickySubtitle)="showStickySubtitle($event)"
+            (activityCode)="activityCodeChanged($event)"
+        >
+        </app-activity>
+    `,
 })
-export class ActivityPageComponent extends ActivityRedesignedComponent {}
+export class ActivityPageComponent
+    extends ActivityRedesignedComponent
+    implements OnInit
+{
+    constructor(
+        @Inject(ACTUAL_PARTICIPANT_ID_TOKEN)
+        private readonly participantId$: Observable<string>,
+        private readonly sessionService: SessionMementoService,
+        headerConfig: HeaderConfigurationService,
+        _activatedRoute: ActivatedRoute,
+        _workflowBuilder: WorkflowBuilderService,
+        @Inject('toolkit.toolkitConfig') config: ToolkitConfigurationService
+    ) {
+        super(headerConfig, _activatedRoute, _workflowBuilder, config);
+    }
+    ngOnInit(): void {
+        super.ngOnInit();
+        this.setParticipantGuid();
+    }
+
+    private setParticipantGuid(): void {
+        this.participantId$
+            .pipe(
+                first(),
+                tap((participantGuid: string) =>
+                    this.sessionService.setParticipant(participantGuid)
+                )
+            )
+            .subscribe();
+    }
+}
