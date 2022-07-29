cc0c48974e800c92b03751622380130b56fb11bd
pegahtah <ptaheri@broadinstitute.org>
2022-07-28 22:54:16 -0400

    DDP-8494 adding more checks for the clean file

--- ddp-workspace/projects/ddp-dsm-ui/src/app/file-download/file-download.component.ts
+++ ddp-workspace/projects/ddp-dsm-ui/src/app/file-download/file-download.component.ts
@@ -1,11 +1,11 @@
 import {DatePipe} from '@angular/common';
-import {Component, Input, OnDestroy, Output} from '@angular/core';
+import {Component, Input, OnDestroy} from '@angular/core';
+import {Subscription} from 'rxjs';
 import {File} from '../participant-list/models/file.model';
 import {Participant} from '../participant-list/participant-list.model';
 import {ComponentService} from '../services/component.service';
 import {DSMService} from '../services/dsm.service';
 import {Utils} from '../utils/utils';
-import { Subscription } from 'rxjs';
 
 const fileSaver = require( 'file-saver' );
 
@@ -19,10 +19,11 @@ export class FileDownloadComponent implements OnDestroy {
   @Input() participant: Participant;
   downloadMessage = '';
   downloading = false;
+  QUARANTINE = 'quarantine';
   CLEAN = 'CLEAN';
   subscription: Subscription;
 
-  constructor(private dsmService: DSMService ) {
+  constructor( private dsmService: DSMService ) {
 
   }
 
@@ -34,25 +35,30 @@ export class FileDownloadComponent implements OnDestroy {
 
   downloadParticipantFile( file: File ): void {
     this.downloading = true;
-    if (file.scanResult !== this.CLEAN) {
+    if (!this.isFileClean( file )) {
       this.downloadMessage = 'Error - file has not passed scanning';
+      this.downloading = false;
+      return;
+    }
+    if (file.bucket.includes( this.QUARANTINE )) {
+      this.downloadMessage = 'Error - file is in quarantine and should not be downloaded!';
       this.downloading = false;
       return;
     }
     const realm = localStorage.getItem( ComponentService.MENU_SELECTED_REALM );
     this.subscription = this.dsmService.downloadParticipantFile( file.fileName, file.bucket, file.blobName, realm, file.mimeType )
       .subscribe( {
-        next: data => {
-          this.saveParticipantFile( data, file.mimeType, file.fileName );
-          this.downloading = false;
-          this.downloadMessage = 'File download finished.';
-        },
-       error: err => {
-      this.downloadMessage = err;
-      this.downloading = false;
-    }
-  }
-    );
+          next: data => {
+            this.saveParticipantFile( data, file.mimeType, file.fileName );
+            this.downloading = false;
+            this.downloadMessage = 'File download finished.';
+          },
+          error: err => {
+            this.downloadMessage = err;
+            this.downloading = false;
+          }
+        }
+      );
   }
 
   saveParticipantFile( data: any, type: string, fileName: string ): void {
@@ -62,6 +68,10 @@ export class FileDownloadComponent implements OnDestroy {
 
 
   getNiceDateFormat( uploadedAt: string ): string {
-    return new DatePipe('en-US').transform(uploadedAt, Utils.DATE_STRING_IN_CVS_WITH_TIME);
+    return new DatePipe( 'en-US' ).transform( uploadedAt, Utils.DATE_STRING_IN_CVS_WITH_TIME );
+  }
+
+  public isFileClean( file: File ): boolean {
+    return file.scannedAt && file.bucket && file.scanResult === this.CLEAN;
   }
 }
--- ddp-workspace/projects/ddp-dsm-ui/src/app/participant-list/models/file.model.ts
+++ ddp-workspace/projects/ddp-dsm-ui/src/app/participant-list/models/file.model.ts
@@ -1,11 +1,11 @@
 export class File {
 
   constructor( public bucket: string, public uploadedAt: string, public fileName: string, public mimeType: string,
-               public scanResult: string, public fileSize: number, public blobName: string ) {
+               public scanResult: string, public fileSize: number, public blobName: string, public scannedAt: string ) {
   }
 
 
   static parse( json ): File {
-    return new File( json.bucket, json.uploadedAt, json.fileName, json.mimeType, json.scanResult, json.fileSize, json.blobName );
+    return new File( json.bucket, json.uploadedAt, json.fileName, json.mimeType, json.scanResult, json.fileSize, json.blobName, json.scannedAt );
   }
 }
