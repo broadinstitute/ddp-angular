4b9f2867af6785af1abf1ac1b1b4912d1d099a68
GiCharkviani <gcharkvi@broadinstitute.org>
2022-08-01 13:16:27 +0400

    DSM-DDP-8452-Occasional-Flaky-login

---
+++ ddp-workspace/projects/ddp-dsm-ui/src/app/services/localStorage.service.ts
@@ -0,0 +1,16 @@
+import {Injectable} from "@angular/core";
+import {SessionService} from "./session.service";
+import {Statics} from "../utils/statics";
+import {ComponentService} from "./component.service";
+
+@Injectable({providedIn: 'root'})
+
+export class LocalStorageService {
+
+  public clear(): void {
+    localStorage.removeItem('auth_token');
+    localStorage.removeItem(SessionService.DSM_TOKEN_NAME);
+    localStorage.removeItem(Statics.PERMALINK);
+    localStorage.removeItem(ComponentService.MENU_SELECTED_REALM);
+  }
+}
