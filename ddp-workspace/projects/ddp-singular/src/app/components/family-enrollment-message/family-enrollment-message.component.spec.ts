a8ba1bdc73b3f27a1e6a4ee00c14aa0c31154bc5
alextanlan <alextanlan@gmail.com>
2022-08-18 12:24:54 +0200

    Fix test

--- ddp-workspace/projects/ddp-singular/src/app/components/family-enrollment-message/family-enrollment-message.component.spec.ts
+++ ddp-workspace/projects/ddp-singular/src/app/components/family-enrollment-message/family-enrollment-message.component.spec.ts
@@ -1,6 +1,23 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { FamilyEnrollmentMessageComponent } from './family-enrollment-message.component';
+import { TranslateLoader, TranslateModule, TranslateService } from '@ngx-translate/core';
+import { Observable, of } from 'rxjs';
+import { MatIconModule } from '@angular/material/icon';
+
+class TranslateLoaderMock implements TranslateLoader {
+  getTranslation(code: string = ''): Observable<object> {
+    const TRANSLATIONS = {
+      en: {
+        Title: 'A Title',
+        FamilyEnrollmentMessage: {
+          Content : ['content block 1', 'content block 2']
+        }
+      }
+    };
+    return of(TRANSLATIONS[code]);
+  }
+}
 
 describe('FamilyEnrollmentMessageComponent', () => {
   let component: FamilyEnrollmentMessageComponent;
@@ -8,9 +25,23 @@ describe('FamilyEnrollmentMessageComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ FamilyEnrollmentMessageComponent ]
+      declarations: [
+        FamilyEnrollmentMessageComponent
+      ],
+      imports: [
+        MatIconModule,
+        TranslateModule.forRoot({
+          loader: {provide: TranslateLoader, useClass: TranslateLoaderMock},
+        })
+      ],
+      providers: [
+        TranslateService
+      ]
     })
     .compileComponents();
+
+    const translate = TestBed.inject(TranslateService);
+    translate.use('en');
   });
 
   beforeEach(() => {
