6996c3b7dcd4418177e465efac73e238e4ba7e2c
simone <simone@broadinstitute.org>
2022-09-19 13:36:43 -0400

    DDP-8734 DDP-8759 DDP-8493 DDP-7606

--- ddp-workspace/projects/ddp-dsm-ui/src/app/sequencing-order/sequencing-order.model.ts
+++ ddp-workspace/projects/ddp-dsm-ui/src/app/sequencing-order/sequencing-order.model.ts
@@ -3,17 +3,18 @@ export class SequencingOrder {
 
   constructor( public sampleType: string, public sample: string, public sampleStatus: string, public collectionDate: string,
                public sequencingOrderDate: string, public tissueId: string, public dsmKitRequestId: string,
-               public sequencingRestriction: string, public lastStatus: string, public lastOrderNumber: string ) {
+               public sequencingRestriction: string, public lastStatus: string, public lastOrderNumber: string,
+               public pdoOrderId: string ) {
 
   }
 
   public static parse( json ): SequencingOrder {
     return new SequencingOrder( json.sampleType, json.sample, json.sampleStatus, json.collectionDate, json.sequencingOrderDate,
-      json.tissueId, json.dsmKitRequestId, json.sequencingRestriction, json.lastStatus, json.lastOrderNumber );
+      json.tissueId, json.dsmKitRequestId, json.sequencingRestriction, json.lastStatus, json.lastOrderNumber, json.pdoOrderId );
   }
 
   public static getEmptyInstance(): SequencingOrder{
-    return new SequencingOrder( '', '', '', '', '', '', '', '', '', '' );
+    return new SequencingOrder( ...Array(11).fill('') );
   }
 
 }
