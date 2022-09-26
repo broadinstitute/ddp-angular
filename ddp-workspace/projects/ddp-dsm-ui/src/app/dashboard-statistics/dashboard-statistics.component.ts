3329734d219e85109837ddc76c25410bce579517
GiCharkviani <77500504+GiCharkviani@users.noreply.github.com>
2022-09-26 12:14:04 +0400

    DSM-Dashboard-counts-into-separate-section (#1774)
    * DSM-Dashboard-counts-into-separate-section
    
    * DSM-Dashboard-counts-into-separate-section linter fix

--- ddp-workspace/projects/ddp-dsm-ui/src/app/dashboard-statistics/dashboard-statistics.component.ts
+++ ddp-workspace/projects/ddp-dsm-ui/src/app/dashboard-statistics/dashboard-statistics.component.ts
@@ -1,8 +1,9 @@
-import {Component, OnInit} from '@angular/core';
-import {Observable} from 'rxjs';
+import {Component, ElementRef, OnInit} from '@angular/core';
+import {Observable, tap} from 'rxjs';
 import {DashboardStatisticsService} from '../services/dashboard-statistics.service';
 import {RoleService} from '../services/role.service';
 import {finalize} from 'rxjs/operators';
+import {dashboardType} from '../enums/dashboard.enums';
 
 @Component({
   selector: 'app-dashboard-statistics',
@@ -12,6 +13,7 @@ import {finalize} from 'rxjs/operators';
 
 export class DashboardStatisticsComponent implements OnInit {
   Charts: Observable<any>;
+  Counts: Observable<any>;
   hasRequiredRole;
   loading = true;
 
@@ -21,6 +23,7 @@ export class DashboardStatisticsComponent implements OnInit {
   ngOnInit(): void {
     this.hasRequiredRole = this.roleService.allowedToViewEELData();
     this.Charts = this.dashboardStatisticsService.ChartFactory().pipe(finalize(() => this.loading = false));
+    this.Counts = this.dashboardStatisticsService.Counts;
   }
 
   get getConfiguration(): any {
@@ -28,5 +31,13 @@ export class DashboardStatisticsComponent implements OnInit {
       responsive: true,
       displaylogo: false
     };
+  }
+
+  public scrollToVew(divElement: HTMLDivElement): void {
+    divElement.scrollIntoView({
+      behavior: 'smooth',
+      block: 'start',
+      inline: 'nearest'
+    });
   }
 }
