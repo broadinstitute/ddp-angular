1218eec7c6537c6d21aec40d591e4c2b27ffb991
pegahtah <47120870+pegahtah@users.noreply.github.com>
2022-10-05 13:38:39 -0400

    [DSM] DDP-8880 fix for checking for undefined (#1794)
    DDP-8880 fix for checking for undefined

--- ddp-workspace/projects/ddp-dsm-ui/src/app/participant-page/participant-page.component.ts
+++ ddp-workspace/projects/ddp-dsm-ui/src/app/participant-page/participant-page.component.ts
@@ -1552,11 +1552,11 @@ export class ParticipantPageComponent implements OnInit, OnDestroy, AfterViewChe
     //adult pt
     const prequalActivity = participant.data.activities.find(activity => activity.activityCode === this.PREQUAL);
     if (prequalActivity != null) {
-      const countryQuestion = prequalActivity.questionsAnswers.find(questionAnswer => questionAnswer.stableId === this.SELF_COUNTRY);
+      const countryQuestion = prequalActivity?.questionsAnswers?.find(questionAnswer => questionAnswer.stableId === this.SELF_COUNTRY);
       if (countryQuestion != null && countryQuestion.answer) {
         if (countryQuestion.answer instanceof Array) {
           if (countryQuestion.answer.indexOf(this.SELF_COUNTRY_US) > -1) {
-            const stateQuestion = prequalActivity.questionsAnswers.find(questionAnswer => questionAnswer.stableId === this.SELF_STATE);
+            const stateQuestion = prequalActivity?.questionsAnswers?.find(questionAnswer => questionAnswer.stableId === this.SELF_STATE);
             if (stateQuestion.answer instanceof Array) {
               if (stateQuestion.answer.indexOf(this.SELF_STATE_NY) === -1) {
                 canBeSequencedBasedOnLocation = true;
@@ -1570,11 +1570,11 @@ export class ParticipantPageComponent implements OnInit, OnDestroy, AfterViewChe
     if (!canBeSequencedBasedOnLocation) {
       const addParticipantActivity = participant.data.activities.find(activity => activity.activityCode === this.ADD_PARTICIPANT);
       if (addParticipantActivity != null) {
-        const countryQuestion = prequalActivity.questionsAnswers.find(questionAnswer => questionAnswer.stableId === this.CHILD_COUNTRY);
+        const countryQuestion = prequalActivity?.questionsAnswers?.find(questionAnswer => questionAnswer.stableId === this.CHILD_COUNTRY);
         if (countryQuestion != null && countryQuestion.answer) {
           if (countryQuestion.answer instanceof Array) {
             if (countryQuestion.answer.indexOf(this.SELF_COUNTRY_US) > -1) {
-              const stateQuestion = prequalActivity.questionsAnswers.find(questionAnswer => questionAnswer.stableId === this.CHILD_STATE);
+              const stateQuestion = prequalActivity?.questionsAnswers?.find(questionAnswer => questionAnswer.stableId === this.CHILD_STATE);
               if (stateQuestion.answer instanceof Array) {
                 if (stateQuestion.answer.indexOf(this.SELF_STATE_NY) === -1) {
                   canBeSequencedBasedOnLocation = true;
