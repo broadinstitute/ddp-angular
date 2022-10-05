9b14710d7cede97000812785eb6b98c79ebb29ea
pegahtah <ptaheri@broadinstitute.org>
2022-10-05 14:21:54 -0400

    DDP-8881 linter fix

--- ddp-workspace/projects/ddp-dsm-ui/src/app/tissue/tissue.component.ts
+++ ddp-workspace/projects/ddp-dsm-ui/src/app/tissue/tissue.component.ts
@@ -349,7 +349,7 @@ export class TissueComponent {
     if (!array) {
       return num === 0;
     }
-    let count = array.filter(sm => !sm.deleted);
+    const count = array.filter(sm => !sm.deleted);
     return count.length === num;
   }
 
