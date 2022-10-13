8dbe3d2bf600f78fea185635363854bbbf24a99c
SimoneMaiwald <simone@broadinstitute.org>
2022-10-13 13:27:11 -0400

    DDP-8889: phi label (#1808)
    * DDP-8889: trying to move qr up a bit
    
    * DDP-8889: trying to move qr up a bit

--- ddp-workspace/projects/ddp-dsm-ui/src/app/shipping/shipping.component.ts
+++ ddp-workspace/projects/ddp-dsm-ui/src/app/shipping/shipping.component.ts
@@ -376,7 +376,7 @@ export class ShippingComponent implements OnInit {
             .wrapper {
                 display: flex;
                 align-content: flex-start;
-                justify-content: flex-start;
+                justify-content: center;
                 flex-direction: column;
                 height: 100%;
             }
@@ -392,7 +392,6 @@ export class ShippingComponent implements OnInit {
             }
             .barcode {
                 width: fit-content;
-                align-content: flex-start;
             }
             @page { margin: 0 }
             @media print {
