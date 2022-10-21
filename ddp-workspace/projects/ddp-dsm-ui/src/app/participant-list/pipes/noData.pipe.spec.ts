3f82c7ab3b1190805f6f64af25e07f60ab9b1dfb
GiCharkviani <gcharkvi@broadinstitute.org>
2022-10-21 14:50:35 +0400

    DSm-noData-pipe-fix-and-test linter fix and removed f

--- ddp-workspace/projects/ddp-dsm-ui/src/app/participant-list/pipes/noData.pipe.spec.ts
+++ ddp-workspace/projects/ddp-dsm-ui/src/app/participant-list/pipes/noData.pipe.spec.ts
@@ -1,30 +1,30 @@
-import {NoDataPipe} from "./noData.pipe";
-import {expect} from "@angular/flex-layout/_private-utils/testing";
+import {NoDataPipe} from './noData.pipe';
+import {expect} from '@angular/flex-layout/_private-utils/testing';
 
-fdescribe("noData Pipe", () => {
+describe('noData Pipe', () => {
   const pipe = new NoDataPipe();
 
   const nonBrakingSpace = (): string => {
     const tempDiv = document.createElement('div');
-    tempDiv.innerHTML = '&nbsp;'
+    tempDiv.innerHTML = '&nbsp;';
     return tempDiv.innerText;
   };
 
   /*
     Checking if in case of particular type of values, it returns exactly the same value (In string format)
    */
-  it("Should return value", () => {
-    const diffData = [5, "ddp", "5", 0, true];
-    diffData.forEach(value => expect(pipe.transform(value)).toEqual(String(value)))
-  })
+  it('Should return value', () => {
+    const diffData = [5, 'ddp', '5', 0, true];
+    diffData.forEach(value => expect(pipe.transform(value)).toEqual(String(value)));
+  });
 
   /*
     Checking if in case of falsy value, it returns non-braking space, in order to display
     empty space properly in HTML
    */
-  it("Should return empty space", () => {
-    const falsyValues = ["", false, undefined, null];
+  it('Should return empty space', () => {
+    const falsyValues = ['', false, undefined, null];
     falsyValues.forEach(value => expect(pipe.transform(value)).toEqual(nonBrakingSpace()));
-  })
+  });
 
-})
+});
--- ddp-workspace/projects/ddp-dsm-ui/src/app/participant-list/pipes/noData.pipe.ts
+++ ddp-workspace/projects/ddp-dsm-ui/src/app/participant-list/pipes/noData.pipe.ts
@@ -7,7 +7,7 @@ import {Pipe, PipeTransform} from '@angular/core';
 export class NoDataPipe implements PipeTransform {
   transform(value: any): string {
     const tempElement = document.createElement('div');
-    tempElement.innerHTML = (typeof value === "number" ? String(value) : value) || '&nbsp;';
+    tempElement.innerHTML = (typeof value === 'number' ? String(value) : value) || '&nbsp;';
     return tempElement.innerText;
   }
 }
