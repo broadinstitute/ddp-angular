f4ca53b0d56bda8f80d6335c23adc57cc3671f12
GiCharkviani <gcharkvi@broadinstitute.org>
2022-10-21 14:46:15 +0400

    DSm-noData-pipe-fix-and-test adjusted to 0

--- ddp-workspace/projects/ddp-dsm-ui/src/app/participant-list/pipes/noData.pipe.spec.ts
+++ ddp-workspace/projects/ddp-dsm-ui/src/app/participant-list/pipes/noData.pipe.spec.ts
@@ -11,20 +11,20 @@ fdescribe("noData Pipe", () => {
   };
 
   /*
-    Checking if in case of truthy value, it returns exactly the same value (In string format)
+    Checking if in case of particular type of values, it returns exactly the same value (In string format)
    */
-  it("should return value", () => {
-    const diffData = [5, "ddp", "5", true];
-    diffData.forEach(value => expect(pipe.transform(value)).toEqual(value.toString()))
+  it("Should return value", () => {
+    const diffData = [5, "ddp", "5", 0, true];
+    diffData.forEach(value => expect(pipe.transform(value)).toEqual(String(value)))
   })
 
   /*
     Checking if in case of falsy value, it returns non-braking space, in order to display
     empty space properly in HTML
    */
-  it("should return empty space", () => {
-    const falsyValues = ["", false, 0, undefined, null];
-    falsyValues.forEach(value => expect(pipe.transform(value).toString()).toEqual(nonBrakingSpace()));
+  it("Should return empty space", () => {
+    const falsyValues = ["", false, undefined, null];
+    falsyValues.forEach(value => expect(pipe.transform(value)).toEqual(nonBrakingSpace()));
   })
 
 })
