08cf4408c4c00f76a36b20261703401f354fd52d
GiCharkviani <gcharkvi@broadinstitute.org>
2022-10-21 19:15:46 +0400

    DSm-noData-pipe-fix-and-test adjusted types

--- ddp-workspace/projects/ddp-dsm-ui/src/app/participant-list/pipes/noData.pipe.ts
+++ ddp-workspace/projects/ddp-dsm-ui/src/app/participant-list/pipes/noData.pipe.ts
@@ -1,12 +1,14 @@
 import {Pipe, PipeTransform} from '@angular/core';
 
+
+type expectedValueType = string | number | boolean;
+
 @Pipe({
   name: 'noData'
 })
-
 export class NoDataPipe implements PipeTransform {
-  transform(value: any): string {
+  transform(value: expectedValueType): string {
     const nonBreakingSpace = String.fromCharCode(160);
-    return (value === 0 ? value.toString() : value) || nonBreakingSpace;
+    return value || value === 0 ? value.toString() : nonBreakingSpace;
   }
 }
