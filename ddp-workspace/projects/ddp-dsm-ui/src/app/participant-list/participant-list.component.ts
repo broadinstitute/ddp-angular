dca442e94bd65f38d23398d1a865184eed11b834
pegahtah <47120870+pegahtah@users.noreply.github.com>
2022-11-01 16:33:00 -0400

    [DSM] PEPPER-28 cohort tag duplicate detection (#1841)
    * PEPPER-28 html changes to check for duplicate value and display error
    
    * PEPPER-28 changes to check for bulk cohort

--- ddp-workspace/projects/ddp-dsm-ui/src/app/participant-list/participant-list.component.ts
+++ ddp-workspace/projects/ddp-dsm-ui/src/app/participant-list/participant-list.component.ts
@@ -1743,6 +1743,9 @@ export class ParticipantListComponent implements OnInit {
         if (maybeParticipant) {
           const existingCohortTags = maybeParticipant.data.dsm[CohortTagComponent.COHORT_TAG] as CohortTag[];
           if (existingCohortTags) {
+            if (existingCohortTags.find(tag => tag.cohortTagName === cohortTag.cohortTagName)) {
+              continue;
+            }
             existingCohortTags.push(cohortTag);
           } else {
             maybeParticipant.data.dsm[CohortTagComponent.COHORT_TAG] = [cohortTag];
