1eea5d4b173ac33b98f8aab346f9c8070822451c
aweng98 <bweng@broadinstitute.org>
2022-11-02 18:14:33 -0400

    no test timings

--- playwright-e2e/playwright.config.ts
+++ playwright-e2e/playwright.config.ts
@@ -14,7 +14,8 @@ dotenv.config({ path: path.resolve(__dirname, '.env') });
  */
 const testConfig: PlaywrightTestConfig = {
   globalSetup: require.resolve('./fixtures/global-setup'),
-  // testDir: './tests',
+  testDir: '.',
+  testMatch: '**/*.spec.ts',
   /* Maximum time one test can run for. Test should be short and takes less than 2 minutes to run */
   timeout: 120 * 1000,
   /* For expect() calls */
