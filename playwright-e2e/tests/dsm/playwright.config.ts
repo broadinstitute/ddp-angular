3080633e39ec358ccc6c8842ebb0243eb3968925
simone <simone@broadinstitute.org>
2022-11-17 10:24:09 -0500

    changes to run test locally on dev

--- playwright-e2e/tests/dsm/playwright.config.ts
+++ playwright-e2e/tests/dsm/playwright.config.ts
@@ -1,9 +1,9 @@
 import type { PlaywrightTestConfig } from '@playwright/test';
 import testConfig from 'playwright.config';
-import * as dotenv from "dotenv";
-import path from "path";
+import * as dotenv from 'dotenv';
+import path from 'path';
 
-dotenv.config({path: path.resolve(__dirname, '.env.dsm') })
+dotenv.config({ path: path.resolve(__dirname, '../../config/.env.dev') });
 
 const dsmConfig: PlaywrightTestConfig = {
   ...testConfig,
