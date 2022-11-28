9d08b4e1605c4b1593bcb9d356068d0e63912d3d
pegahtah <ptaheri@broadinstitute.org>
2022-11-28 15:49:19 -0500

    PEPPER-280 sample.model.ts was missed

--- ddp-workspace/projects/ddp-dsm-ui/src/app/participant-list/models/sample.model.ts
+++ ddp-workspace/projects/ddp-dsm-ui/src/app/participant-list/models/sample.model.ts
@@ -13,7 +13,8 @@ export class Sample {
                public upsTrackingStatus: string, public upsReturnStatus: string, public externalOrderStatus: string,
               public externalOrderNumber: string, public externalOrderDate: number,
                public careEvolve: boolean, public uploadReason: string, public collectionDate: string,
-              public sequencingRestriction: string, public dsmKitRequestId: string, public sampleNotes: string
+              public sequencingRestriction: string, public dsmKitRequestId: string, public sampleNotes: string,
+              public bspCollaboratorParticipantId: string
   ) {
     if (this.uploadReason === '' || this.uploadReason === null || this.uploadReason === undefined) {
       this.uploadReason = 'NORMAL';
@@ -55,7 +56,8 @@ export class Sample {
       json.bspCollaboratorSampleId, json.kitTypeName, json.scanDate, json.error, json.receiveDate, json.deactivatedDate,
       json.trackingNumberTo, json.trackingReturnId, json.kitLabel, testResults, json.upsTrackingStatus,
       json.upsReturnStatus, json.externalOrderStatus, json.externalOrderNumber, json.externalOrderDate,
-      json.careEvolve, json.uploadReason, json.collectionDate, json.sequencingRestriction, json.dsmKitRequestId, json.sampleNotes
+      json.careEvolve, json.uploadReason, json.collectionDate, json.sequencingRestriction, json.dsmKitRequestId, json.sampleNotes,
+      json.bspCollaboratorParticipantId
     );
   }
 }
