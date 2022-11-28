8ba8070443a57c6a0e78947910e93c5db264feb2
GiCharkviani <gcharkvi@broadinstitute.org>
2022-11-28 16:50:35 +0400

    DSM-Dashboard-fix subscription added

--- ddp-workspace/projects/ddp-dsm-ui/src/app/dashboard-statistics/dashboard-statistics.component.ts
+++ ddp-workspace/projects/ddp-dsm-ui/src/app/dashboard-statistics/dashboard-statistics.component.ts
@@ -1,11 +1,11 @@
 import {Component, OnDestroy, OnInit} from '@angular/core';
 import {DashboardStatisticsService} from '../services/dashboard-statistics.service';
-import {catchError, finalize, map, switchMap, take, takeUntil} from 'rxjs/operators';
+import {catchError, finalize, map, switchMap, take} from 'rxjs/operators';
 import {ICount} from './interfaces/ICount';
 import {DatePipe} from '@angular/common';
 import {IDateRange} from './interfaces/IDateRange';
 import {StatisticsEnum} from './enums/statistics.enum';
-import {EMPTY, Observable, Subject, tap} from 'rxjs';
+import {EMPTY, Observable, Subject, Subscription, tap} from 'rxjs';
 import {MatTabChangeEvent} from '@angular/material/tabs';
 import {ErrorsService} from '../services/errors.service';
 import {Plotly} from 'angular-plotly.js/lib/plotly.interface';
@@ -34,11 +34,11 @@ export class DashboardStatisticsComponent implements OnInit, OnDestroy {
   private activeTab: StatisticsTypes = 'charts';
   private isDateChanged = false;
 
-  private readonly statisticsSubject: Subject<void> = new Subject<void>();
-  private readonly destroyStatisticsSubject$: Subject<void> = new Subject<void>();
+  private readonly statisticsSubject$: Subject<void> = new Subject<void>();
+  private readonly statisticsSubjectSubscription$: Subscription;
 
   constructor(private dashboardStatisticsService: DashboardStatisticsService, private errorService: ErrorsService) {
-    this.statisticsSubject
+    this.statisticsSubjectSubscription$ = this.statisticsSubject$
       .pipe(
         tap(() => this.loading = true),
         map(() => this.enumeratedActiveTab),
@@ -49,25 +49,23 @@ export class DashboardStatisticsComponent implements OnInit, OnDestroy {
               catchError(this.handleError.bind(this)),
               finalize(() => this.loading = false)
             )
-        ),
-        takeUntil(this.destroyStatisticsSubject$),
+        )
       )
       .subscribe({next: this.initializeData.bind(this)});
   }
 
   ngOnInit(): void {
-    this.statisticsSubject.next();
+    this.statisticsSubject$.next();
   }
 
   ngOnDestroy(): void {
-    this.destroyStatisticsSubject$.next();
-    this.destroyStatisticsSubject$.complete();
+    this.statisticsSubjectSubscription$.unsubscribe();
   }
 
   public getStatisticsFor({tab}: MatTabChangeEvent): void {
     this.activeTab = tab.ariaLabel as StatisticsTypes;
     if(this.allowStatisticsUpdate) {
-      this.statisticsSubject.next();
+      this.statisticsSubject$.next();
       this.isDateChanged = false;
     }
   }
@@ -76,12 +74,12 @@ export class DashboardStatisticsComponent implements OnInit, OnDestroy {
     this.loading = true;
     this.isDateChanged = true;
     this.dateRange = dateRange;
-    this.statisticsSubject.next();
+    this.statisticsSubject$.next();
   }
 
   public retry(): void {
     this.loading = true;
-    this.statisticsSubject.next();
+    this.statisticsSubject$.next();
   }
 
   private get allowStatisticsUpdate(): boolean {
