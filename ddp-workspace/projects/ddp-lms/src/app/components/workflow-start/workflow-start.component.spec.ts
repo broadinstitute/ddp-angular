841a5835736819bb5a3d85029e76f4713b36706e
alextanlan <alextanlan@gmail.com>
2022-11-28 12:49:56 +0100

    Add unit tests

---
+++ ddp-workspace/projects/ddp-lms/src/app/components/workflow-start/workflow-start.component.spec.ts
@@ -0,0 +1,65 @@
+import { ChangeDetectorRef } from '@angular/core';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { of } from 'rxjs';
+import {
+  SessionMementoService,
+  TemporaryUserServiceAgent,
+  WorkflowServiceAgent,
+  WindowRef,
+  SessionStorageService
+} from 'ddp-sdk';
+import { WorkflowStartComponent } from './workflow-start.component';
+import { HeaderConfigurationService, WorkflowBuilderService } from 'toolkit';
+
+describe('WorkflowStartComponent', () => {
+  let fixture: ComponentFixture<WorkflowStartComponent>;
+  let component: WorkflowStartComponent;
+
+  let headerConfigSpy: jasmine.SpyObj<HeaderConfigurationService>;
+  let sessionSpy: jasmine.SpyObj<SessionMementoService>;
+  let temporaryUserServiceSpy: jasmine.SpyObj<TemporaryUserServiceAgent>;
+
+  beforeEach(async () => {
+    headerConfigSpy = jasmine.createSpyObj('headerConfigSpy', ['setupActivityHeader', 'setupDefaultHeader']);
+    sessionSpy = jasmine.createSpyObj('SessionMementoService', ['isTemporarySession', 'isAuthenticatedSession', 'setTemporarySession']);
+    temporaryUserServiceSpy = jasmine.createSpyObj(
+      'TemporaryUserServiceAgent',
+      {createTemporaryUser: of({})}
+    );
+
+    await TestBed.configureTestingModule({
+        providers: [
+          { provide: HeaderConfigurationService, useValue: headerConfigSpy },
+          { provide: WorkflowBuilderService, useValue: {} },
+          { provide: TemporaryUserServiceAgent, useValue: temporaryUserServiceSpy },
+          { provide: SessionMementoService, useValue: sessionSpy },
+          { provide: WorkflowServiceAgent, useValue: {getStart: () => of({})} },
+          { provide: WindowRef, useValue: {} },
+          { provide: ChangeDetectorRef, useValue: {} },
+          { provide: SessionStorageService, useValue: {remove: () => {}} },
+          { provide: 'ddp.config', useValue: {} },
+          { provide: 'toolkit.toolkitConfig', useValue: {} },
+        ],
+        declarations: [WorkflowStartComponent],
+      })
+      .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(WorkflowStartComponent);
+    component = fixture.debugElement.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create component', () => {
+    expect(component).toBeTruthy();
+  });
+
+  it('should set header config for a temporary user', () => {
+    sessionSpy.isTemporarySession.and.returnValue(true);
+    component.ngOnInit();
+
+    expect(headerConfigSpy.showBreadcrumbs).toBeFalse();
+    expect(headerConfigSpy.showMainButtons).toBeTrue();
+  });
+});
