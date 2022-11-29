510bc3348b4ce609df6689f90ddbd78a88c2a25a
alextanlan <alextanlan@gmail.com>
2022-11-29 20:59:20 +0100

    Fix name

--- playwright-e2e/pages/osteo/osteo-page.ts
+++ playwright-e2e/pages/osteo/osteo-page.ts
@@ -8,7 +8,7 @@ export abstract class OsteoPage extends PageBase {
   protected constructor(page: Page) {
     const { OSTEO_BASE_URL } = process.env;
     if (OSTEO_BASE_URL == null) {
-      throw Error(`Invalid LMS base URL: process.env.OSTEO_BASE_URL=${OSTEO_BASE_URL}`);
+      throw Error(`Invalid Osteo base URL: process.env.OSTEO_BASE_URL=${OSTEO_BASE_URL}`);
     }
     super(page, OSTEO_BASE_URL);
   }
