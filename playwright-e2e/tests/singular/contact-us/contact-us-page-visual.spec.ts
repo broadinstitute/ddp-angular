7a66ee44b4b0f5ae96d428772b9de64c012637da
Wilder_Perozo_Sanchez <walejand@broadinstitute.org>
2022-11-29 19:39:26 +0100

    PEPPER-323 fix playwright tests

--- playwright-e2e/tests/singular/contact-us/contact-us-page-visual.spec.ts
+++ playwright-e2e/tests/singular/contact-us/contact-us-page-visual.spec.ts
@@ -1,17 +1,18 @@
-import { expect } from '@playwright/test';
+import {expect, Locator, Page} from '@playwright/test';
 import { test } from 'fixtures/singular-fixture';
 
 /**
  * Functional tests for the Home page
  */
 test.describe('Home page', () => {
+
   test('Our teams @visual @singular', async ({ page, homePage }) => {
     await homePage.gotoURLPath('/about');
     const headerText = page.locator('.our-team h1');
 
-    expect(await headerText.screenshot()).toMatchSnapshot('our-team-text.png');
-    expect(await page.locator('.our-team .cards').screenshot({ mask: [page.locator('img')] })).toMatchSnapshot(
-      'our-team-cards.png'
+    await expect(headerText).toHaveScreenshot('our-team-text.png');
+    await expect(page.locator('.our-team .cards')).toHaveScreenshot(
+      'our-team-cards.png', { mask: [page.locator('img'), page.locator('.header')] }
     );
   });
 
@@ -20,8 +21,7 @@ test.describe('Home page', () => {
     const headerText = page.locator('.advisors h1');
     const cards = page.locator('.advisors .cards');
 
-    expect(await headerText.screenshot()).toMatchSnapshot('our-advisors-text.png');
-
-    expect(await cards.screenshot({ mask: [page.locator('img')] })).toMatchSnapshot('our-scientific-advisors.png');
+    await expect(headerText).toHaveScreenshot('our-advisors-text.png');
+    await expect(cards).toHaveScreenshot('our-scientific-advisors.png', { mask: [page.locator('img'), page.locator('.header')] });
   });
 });
