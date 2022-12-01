b9e5154979c664d75421ed2b3b60ca6395a0ee06
Juan Sebastian Herrera <juanseba@broadinstitute.org>
2022-12-01 17:35:30 -0500

    Self enrollment test automation pancan

--- playwright-e2e/pages/pancan/home/home-page.ts
+++ playwright-e2e/pages/pancan/home/home-page.ts
@@ -10,17 +10,18 @@ export default class HomePage extends PancanPage implements HomePageInterface {
   async waitForReady(): Promise<void> {
     await expect(this.getLogInButton()).toBeVisible();
     await expect(this.getJoinCountMeInButton()).toBeVisible();
+
   }
 
   getLogInButton(): Locator {
-    return this.page.locator('.header button[data-ddp-test="signInButton"]:has-text("Log In")');
+    return this.page.locator('.header .controls button[data-ddp-test="signInButton"]:has-text("Log In")');
   }
 
   /**
    * Returns "Join Count Me In" button
    */
   getJoinCountMeInButton(): Locator {
-    return this.page.locator('a', { hasText: 'Join Count Me In' });
+    return this.page.locator('.info a', { hasText: 'Join Count Me In' });
   }
 
   async join(opts: { waitForNav?: boolean } = {}): Promise<void> {
