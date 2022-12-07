75741c4535394eb2f7da7a8cc5cbc0c2864e659f
Juan Sebastian Herrera <juanseba@broadinstitute.org>
2022-12-07 16:32:54 -0500

    Self enrollment test autm pancan new forms

---
+++ playwright-e2e/pages/pancan/enrollment/survey-about-you.page.ts
@@ -0,0 +1,52 @@
+import { expect, Locator, Page } from "@playwright/test";
+import Question from "lib/component/Question";
+import { PancanPage } from "../pancan-page";
+
+export default class SurveyAboutYouPage extends PancanPage {
+
+  constructor(page: Page) {
+    super(page);
+  }
+  async waitForReady(): Promise<void> {
+    await this.sexAssignedAtBirth().toLocator().waitFor({state:'visible'});
+  }
+  //Question select
+  sexAssignedAtBirth(): Question {
+    return new Question(this.page, { prompt: 'What sex were you assigned at birth?' });
+
+  }
+  //Question select
+  genderIdentity(label: string): Locator {
+    return this.page.locator('.picklist-answer-GENDER_IDENTITY >> mat-checkbox').filter({has: this.page.locator(`text="${label}"`)});
+
+  }
+  //Question selectInput
+  categoriesDecribesYou(label: string): Locator {
+    return this.page.locator('.picklist-answer-RACE >> mat-checkbox').filter({has: this.page.locator(`text="${label}"`)});
+
+  }
+  //Question radio-button
+  tellUsAnythingElse(): Question {
+    return new Question(this.page,{prompt: 'Tell us anything else you would like about yourself or your cancer.'});
+
+  }
+  //Question radio-button
+  howDidYouHearAboutProject(): Question {
+    return new Question(this.page,{prompt: 'How did you hear about the project?'})
+  }
+
+  async checkGenderIdentity(option: string): Promise<void> {
+    const loc = this.genderIdentity(option);
+    await loc.click();
+    await expect(loc).toHaveClass(/checkbox-checked/);
+  }
+
+  async checkCategoriesDecribesYou(option: string) {
+    const loc = this.categoriesDecribesYou(option);
+    await loc.click();
+    await expect(loc).toHaveClass(/checkbox-checked/);
+  }
+
+
+
+}
