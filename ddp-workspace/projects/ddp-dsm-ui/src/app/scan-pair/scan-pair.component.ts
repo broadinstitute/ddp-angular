2531156d43e92932056b9bac992be22676317495
ngigliotosBroad <ngigliot@broadinstitute.org>
2022-12-07 12:43:11 -0500

    Error msg now shows properly.

--- ddp-workspace/projects/ddp-dsm-ui/src/app/scan-pair/scan-pair.component.ts
+++ ddp-workspace/projects/ddp-dsm-ui/src/app/scan-pair/scan-pair.component.ts
@@ -25,9 +25,10 @@ export class ScanPairComponent implements OnInit {
     this.leftInput.nativeElement.focus();
   }
 
-  moveFocus(leftValue: string): void {
+  moveFocus(leftValue: string, rightValue: string): void {
       this.rightInput.nativeElement.focus();
       this.leftLabelAdded.next([leftValue, this.positionScanPair]);
+      this.pairScanned.next([leftValue, rightValue, this.positionScanPair]);
   }
 
   nextPair(leftValue: string, rightValue: string): void {
