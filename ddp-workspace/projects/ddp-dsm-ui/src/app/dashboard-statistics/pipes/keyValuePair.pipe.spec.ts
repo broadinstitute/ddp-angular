0d69745f6ed2bec774329e3e76df37f7fa8df1c7
GiCharkviani <gcharkvi@broadinstitute.org>
2022-12-12 13:16:11 +0400

    DSM-keyValuePariPipe-Unittests linter-fix

--- ddp-workspace/projects/ddp-dsm-ui/src/app/dashboard-statistics/pipes/keyValuePair.pipe.spec.ts
+++ ddp-workspace/projects/ddp-dsm-ui/src/app/dashboard-statistics/pipes/keyValuePair.pipe.spec.ts
@@ -1,13 +1,13 @@
-import {KeyValuePairPipe} from "./KeyValuePair.pipe";
-import {expect} from "@angular/flex-layout/_private-utils/testing";
+import {KeyValuePairPipe} from './KeyValuePair.pipe';
+import {expect} from '@angular/flex-layout/_private-utils/testing';
 
 describe('keyValuePair Pipe', () => {
   const pipe = new KeyValuePairPipe();
   const initialObject = {
     foo: 'bar',
-  }
+  };
 
   it('Should return key-value objects array', () => {
-    expect(pipe.transform(initialObject)).toEqual([{key: 'foo', value: 'bar'}])
-  })
-})
+    expect(pipe.transform(initialObject)).toEqual([{key: 'foo', value: 'bar'}]);
+  });
+});
