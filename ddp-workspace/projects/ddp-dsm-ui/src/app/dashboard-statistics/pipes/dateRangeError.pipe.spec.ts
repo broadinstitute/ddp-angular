174c3a756d5d25ff990d7d1aa354cc5a680502bf
GiCharkviani <gcharkvi@broadinstitute.org>
2022-12-12 12:59:24 +0400

    DSM-dateRangeErrorPipe-UnitTests linter fix

--- ddp-workspace/projects/ddp-dsm-ui/src/app/dashboard-statistics/pipes/dateRangeError.pipe.spec.ts
+++ ddp-workspace/projects/ddp-dsm-ui/src/app/dashboard-statistics/pipes/dateRangeError.pipe.spec.ts
@@ -1,6 +1,6 @@
-import {DateRangeErrorPipe} from "./dateRangeError.pipe";
-import {DateRangeErrorMessages} from "./constants/dateRange-error.messages";
-import {expect} from "@angular/flex-layout/_private-utils/testing";
+import {DateRangeErrorPipe} from './dateRangeError.pipe';
+import {DateRangeErrorMessages} from './constants/dateRange-error.messages';
+import {expect} from '@angular/flex-layout/_private-utils/testing';
 
 describe('dateRangeError Pipe', () => {
   const pipe = new DateRangeErrorPipe();
@@ -10,14 +10,14 @@ describe('dateRangeError Pipe', () => {
     const startDateErrors = Object.entries(startDate);
 
     startDateErrors.forEach(([errorKey, errorMsg]: [string, string]) =>
-      expect(pipe.transform(errorKey, 'startDate')).toEqual(errorMsg))
-  })
+      expect(pipe.transform(errorKey, 'startDate')).toEqual(errorMsg));
+  });
 
   it('Should return endDate error messages accordingly', () => {
     const endDateErrors = Object.entries(endDate);
 
     endDateErrors.forEach(([errorKey, errorMsg]: [string, string]) =>
-      expect(pipe.transform(errorKey, 'endDate')).toEqual(errorMsg))
-  })
+      expect(pipe.transform(errorKey, 'endDate')).toEqual(errorMsg));
+  });
 
-})
+});
