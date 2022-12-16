00dd52fd0f7172cc5c38dcc2175765d06bfbef8d
Marco Ocana <mocana@broadinstitute.org>
2022-12-15 19:16:00 -0500

    Revert "[Playwright] [PEPPER-323] Eslint fix (#1927)"
    This reverts commit 8b7d550949ea69d580f8cf9900fb283f57cbbb10.

--- playwright-e2e/tests/singular/contact-us/contact-us-page-visual.spec.ts
+++ playwright-e2e/tests/singular/contact-us/contact-us-page-visual.spec.ts
@@ -1,18 +1,19 @@
-import { expect } from '@playwright/test';
+import {expect, Locator, Page} from '@playwright/test';
 import { test } from 'fixtures/singular-fixture';
 
 /**
  * Functional tests for the Home page
  */
 test.describe('Home page', () => {
+
   test('Our teams @visual @singular', async ({ page, homePage }) => {
     await homePage.gotoURLPath('/about');
     const headerText = page.locator('.our-team h1');
     await expect(headerText).toHaveText('Our Team');
     await expect(headerText).toHaveScreenshot('our-team-text.png');
-    await expect(page.locator('.our-team .cards')).toHaveScreenshot('our-team-cards.png', {
-      mask: [page.locator('img'), page.locator('.header')]
-    });
+    await expect(page.locator('.our-team .cards')).toHaveScreenshot(
+      'our-team-cards.png', { mask: [page.locator('img'), page.locator('.header')] }
+    );
   });
 
   test('Our scientific advisors @visual @singular', async ({ page, homePage }) => {
