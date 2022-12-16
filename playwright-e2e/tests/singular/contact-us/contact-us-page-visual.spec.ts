41a1c12310568a5300e9a1609e50e70c1e67687f
Marco Ocana <mocana@broadinstitute.org>
2022-12-15 19:16:14 -0500

    Revert "PEPPER-323 Fix playwright tests (#1917)"
    This reverts commit feb25b33596a3623790b2a7e7882d66eb1d4e720.

--- playwright-e2e/tests/singular/contact-us/contact-us-page-visual.spec.ts
+++ playwright-e2e/tests/singular/contact-us/contact-us-page-visual.spec.ts
@@ -1,18 +1,17 @@
-import {expect, Locator, Page} from '@playwright/test';
+import { expect } from '@playwright/test';
 import { test } from 'fixtures/singular-fixture';
 
 /**
  * Functional tests for the Home page
  */
 test.describe('Home page', () => {
-
   test('Our teams @visual @singular', async ({ page, homePage }) => {
     await homePage.gotoURLPath('/about');
     const headerText = page.locator('.our-team h1');
-    await expect(headerText).toHaveText('Our Team');
-    await expect(headerText).toHaveScreenshot('our-team-text.png');
-    await expect(page.locator('.our-team .cards')).toHaveScreenshot(
-      'our-team-cards.png', { mask: [page.locator('img'), page.locator('.header')] }
+
+    expect(await headerText.screenshot()).toMatchSnapshot('our-team-text.png');
+    expect(await page.locator('.our-team .cards').screenshot({ mask: [page.locator('img')] })).toMatchSnapshot(
+      'our-team-cards.png'
     );
   });
 
@@ -21,7 +20,8 @@ test.describe('Home page', () => {
     const headerText = page.locator('.advisors h1');
     const cards = page.locator('.advisors .cards');
 
-    await expect(headerText).toHaveScreenshot('our-advisors-text.png');
-    await expect(cards).toHaveScreenshot('our-scientific-advisors.png', { mask: [page.locator('img'), page.locator('.header')] });
+    expect(await headerText.screenshot()).toMatchSnapshot('our-advisors-text.png');
+
+    expect(await cards.screenshot({ mask: [page.locator('img')] })).toMatchSnapshot('our-scientific-advisors.png');
   });
 });
