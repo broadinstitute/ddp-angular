50b353621f05ae63833d5021127e09eb10b776de
GiCharkviani <gcharkvi@broadinstitute.org>
2022-12-19 13:05:27 +0400

    DSM-PEPPER-117-initialScan-shortID-6-simple

--- ddp-workspace/projects/ddp-dsm-ui/src/app/scan/scan.component.ts
+++ ddp-workspace/projects/ddp-dsm-ui/src/app/scan/scan.component.ts
@@ -29,6 +29,7 @@ export class ScanComponent implements OnInit {
   scanReceived = false;
   initialScan = false;
   scanValues: Array<ScanValueComponent> = [];
+  public showErrorMessage: boolean = false
   private singleScanValues: Array<ScanValue> = [];
 
   constructor(private _changeDetectionRef: ChangeDetectorRef, private dsmService: DSMService, private router: Router,
@@ -59,6 +60,18 @@ export class ScanComponent implements OnInit {
         this.rightPlaceholder = 'Short ID';
       }
     }
+  }
+
+  public get disableButton(): boolean {
+    if(this.initialScan) {
+      if(!this.scanPairsValue.length) {
+        return true;
+      }
+      const invalid = !this.scanPairsValue.every(({rightValue}: ScanPair) => rightValue.length === 6);
+      this.showErrorMessage = invalid;
+      return invalid;
+    }
+    return false;
   }
 
   public scanDone(arg): void { // arg[0] = leftvalue (ddpLabel), arg[1] = rightvalue (kitLabel) and arg[2] = position
