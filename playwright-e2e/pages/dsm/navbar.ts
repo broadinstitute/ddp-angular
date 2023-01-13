37e8c41ebcaad0996d89ce6acbdac48b361f5fc1
GiCharkviani <gcharkvi@broadinstitute.org>
2023-01-13 15:17:39 +0400

    Participant-Page-Playwright removed sample function

--- playwright-e2e/pages/dsm/navbar.ts
+++ playwright-e2e/pages/dsm/navbar.ts
@@ -4,7 +4,3 @@ import Dropdown from 'lib/widget/dropdown';
 export function study(page: Page): Dropdown {
   return new Dropdown(page, 'Study');
 }
-
-export function sample(page: Page): Dropdown {
-  return new Dropdown(page, new RegExp('^Samples$'));
-}
