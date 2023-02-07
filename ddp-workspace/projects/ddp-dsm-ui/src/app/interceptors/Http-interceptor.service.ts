eaa625a90875404979c8b2feb4c7c643e04f9fe0
GiCharkviani <gcharkvi@broadinstitute.org>
2023-02-07 11:49:54 +0400

    DSM-ignore-401-status-for-snackbar remove console

--- ddp-workspace/projects/ddp-dsm-ui/src/app/interceptors/Http-interceptor.service.ts
+++ ddp-workspace/projects/ddp-dsm-ui/src/app/interceptors/Http-interceptor.service.ts
@@ -19,7 +19,6 @@ export class HttpInterceptorService implements HttpInterceptor {
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
     return next.handle(req).pipe(
       catchError((error: any) => {
-        console.log(error, 'ERROR');
         error instanceof HttpErrorResponse &&
         !this.ignoreStatuses.includes(error?.status) &&
         this.errorsService.openSnackbar(error);
