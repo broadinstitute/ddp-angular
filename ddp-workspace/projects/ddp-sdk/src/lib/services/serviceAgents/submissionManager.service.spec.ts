d6c03f3739b193af8b23bb5528cbbb826127c9cf
GiCharkviani <gcharkvi@broadinstitute.org>
2023-02-21 14:32:17 +0400

    da

--- ddp-workspace/projects/ddp-sdk/src/lib/services/serviceAgents/submissionManager.service.spec.ts
+++ ddp-workspace/projects/ddp-sdk/src/lib/services/serviceAgents/submissionManager.service.spec.ts
@@ -117,11 +117,13 @@ describe('SubmissionManagerTest', () => {
         // Let's set the start Time
         const startTime: number = new Date().getTime();
         console.log(startTime, 'START_TIME');
+        console.log(new Date(), 'START_DATE');
 
         // this is what we are actually testing: the subscription to isSubmissionInProgress$
         submissionManager.isAnswerSubmissionInProgress$.subscribe((isInProgress: boolean) => {
             const endTime = new Date().getTime();
             console.log(endTime, 'END_TIME');
+            console.log(new Date(), 'END_DATE');
             const value = {value: isInProgress, delay: (endTime - startTime)};
             console.log('returnedInProgressValue:', value);
             returnedInProgressValues.push(value);
