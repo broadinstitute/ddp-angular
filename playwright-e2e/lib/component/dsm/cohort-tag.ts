8b0b3bd7bb7085588ecf5384a55f76740c75df74
GiCharkviani <gcharkvi@broadinstitute.org>
2023-03-02 19:32:15 +0400

    fix relative import path

--- playwright-e2e/lib/component/dsm/cohort-tag.ts
+++ playwright-e2e/lib/component/dsm/cohort-tag.ts
@@ -1,5 +1,5 @@
 import { expect, Locator, Page } from '@playwright/test';
-import { waitForResponseByURL } from '../../../utils/test-utils';
+import { waitForResponseByURL } from 'utils/test-utils';
 
 export default class CohortTag {
   constructor(private readonly page: Page) {}
