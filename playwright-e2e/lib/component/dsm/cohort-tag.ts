b492eeedcbdf94b39bf4cc71a9345e1617de97cc
GiCharkviani <gcharkvi@broadinstitute.org>
2023-03-02 19:25:18 +0400

    DSM-fixed-failing-playwright-test

--- playwright-e2e/lib/component/dsm/cohort-tag.ts
+++ playwright-e2e/lib/component/dsm/cohort-tag.ts
@@ -4,9 +4,10 @@ import { waitForResponseByURL } from '../../../utils/test-utils';
 export default class CohortTag {
   constructor(private readonly page: Page) {}
 
-  public async add(tagName: string): Promise<void> {
+  public async add(tagName: string, waitForResponse: boolean = true): Promise<void> {
     await this.inputField.fill(tagName);
     await this.inputField.blur();
+    waitForResponse && await this.waitForOKResponse('createCohortTag');
   }
 
   public async remove(tagName: string): Promise<void> {
