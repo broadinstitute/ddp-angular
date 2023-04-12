33e38cc5c8aa0786279a2d97db2b9b1ce06b2e5b
otchet-broad <111771148+otchet-broad@users.noreply.github.com>
2023-04-12 14:02:20 -0400

    [PEPPER-731] Auth0 invalid signature returns a 400 response. (#2085)
    * Auth0 invalid signature returns a 400 response.
    
    * Removes status code examination for Auth0 error checking so that every auth0 error will remove the local dsm token.
    
    * Reorganizes the handleError so the error message is logged and promoted; removes the 500 status check since this will now be covered by the checkForAuthError validation rules.

--- ddp-workspace/projects/ddp-dsm-ui/src/app/services/auth.service.ts
+++ ddp-workspace/projects/ddp-dsm-ui/src/app/services/auth.service.ts
@@ -179,29 +179,25 @@ export class Auth implements OnDestroy {
 
   private checkForAuth0Error(error: any): boolean {
     return (error instanceof HttpErrorResponse &&  error && error.hasOwnProperty('status')
-      && error.hasOwnProperty('ok') && error.url &&
-      (error.status === 401 || error.status === 0)
+      && error.hasOwnProperty('ok') && error.url
       && !error.ok && error.url.includes('ui/auth0'));
   }
   private handleError(error: any): Observable<any> {
     // In a real world app, we might use a remote logging infrastructure
     // We'd also dig deeper into the error to get a better message
     let errMsg: string | null = null;
+
+    errMsg = (error.message) ? error.message :
+      error.status ? `${error.status} - ${error.statusText}` : 'Server error';
+    this.authError.next(errMsg);
+    console.error(errMsg); // log to console instead
+
     if(this.checkForAuth0Error(error)) {
       this.doLogout();
       this.lock.show();
       return;
     }
-    if(error.status === 500) {
-      errMsg = 'Incorrect user name or password.';
-    } else {
-      errMsg = (error.message) ? error.message :
-        error.status ? `${error.status} - ${error.statusText}` : 'Server error';
-    }
 
-    this.authError.next(errMsg);
-
-    console.error(errMsg); // log to console instead
     return throwError(() => new Error(errMsg));
   }
 
