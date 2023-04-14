77664f067154fa52b7b99bb27768092f63e5ff5f
Kiara Westbrooks <kiarawestbrooks@gmail.com>
2023-04-14 13:41:45 -0400

    Update kitUpload-page.ts
    some updates - used help from codegen

--- playwright-e2e/pages/dsm/kitUpload-page.ts
+++ playwright-e2e/pages/dsm/kitUpload-page.ts
@@ -12,16 +12,81 @@ export default class KitUploadPage {
     constructor(private readonly page: Page) {}
 
     public getPageTitle(): Locator {
-        return this.page.locator('h1');
+        return this.page.getByRole('heading', { name: this.PAGE_TITLE});
     }
 
     //looking into using the above enums for this - this was just jotted down
-    public getKitTypeOption(kitType: string): Locator {
-        return this.page.locator(`text='${kitType}'`);
+    public async selectKitTypeOption(kitType: string): Promise<void> {
+        const kitOption = this.page.locator('label').filter({ hasText: `'${kitType}'`});
+        await kitOption.check();
     }
 
-   public getSkipAddressValidationOption(): Locator {
-        return this.page.locator('text=Skip address validation on upload');
+    //This option only appears to some study admins based on permissions - so I have a getter here
+    //so I can use it to verify display of option and also select option later
+   private getSkipAddressValidationOption(): Locator {
+        return this.page.locator('label').filter({ hasText: 'Skip address validation on upload' });
+   }
+
+   //Assert that if the test study admin has the neccessary permissions - they can see the Skip Address Validation option
+   //Assert that if they don't have permissions, they don't see it
+   //Should probably be two different methods - could use a better function name
+   public async verifySkipAddressValidationOptionDisplayed(hasPermissions: boolean): Promise<void> {
+        const validationOption = this.getSkipAddressValidationOption();
+        if (hasPermissions) {
+            await expect(validationOption).toBeVisible();
+        } else {
+            await expect(validationOption).not.toBeVisible();
+        }
+   }
+
+   public async selectSkipAddressValidationOption(): Promise<void> {
+        const validationOption = this.getSkipAddressValidationOption();
+        await validationOption.check();
+   }
+
+   //todo add assertion for display of button
+   private getBrowseButton(): Locator {
+        return this.page.getByText('Browse...');
+   }
+
+   public async selectBrowseButton(): Promise<void> {
+        const browseButton = this.getBrowseButton();
+        await browseButton.click();
+   }
+
+   //todo add assertion for display of button
+   private getUploadButton(): Locator {
+        return this.page.getByRole('button', { name: 'Upload Kits' });
+   }
+
+   public async selectKitUploadButton(): Promise<void> {
+        const kitUploadButton = this.getUploadButton();
+        await kitUploadButton.click();
+   }
+
+   //todo maybe add assertion for the webelements of the popup
+   public getKitUploadConfirmationPopUp(): Locator {
+        return this.page.locator('app-modal div').filter({ hasText: 'Please select the participant for which you want to upload a new one'}).nth(3);
+   }
+
+   public async selectAddNewKitForParticipantOption(guid: string, shortID: string, firstName: string, lastName: string): Promise<void> {
+        const participantKit = this.page.getByText(`Participant already has a kit. '${guid}' '${shortID}' '${firstName}' '${lastName}'`);
+        await participantKit.check();
+   }
+
+   public async closeKitConfirmationPopUp(): Promise<void> {
+        const closeButton = this.page.getByRole('button', { name: 'Close' });
+        await closeButton.click();
+   }
+
+   public async confirmKitUpload(): Promise<void> {
+        const updateKitsButton = this.page.getByRole('button', { name: 'Upload Kit', exact: true});
+        await updateKitsButton.click();
+   }
+
+   public async verifyKitUploadConfirmationMessage(): Promise<void> {
+        const verficationMessage = this.page.getByRole('heading', { name: 'All participants were uploaded.'});
+        await expect(verficationMessage).toBeVisible();
    }
 
    //to-do add screenshot verification for the kit upload instructions
