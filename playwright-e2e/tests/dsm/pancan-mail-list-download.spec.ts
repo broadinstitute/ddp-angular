43ea11617ee1281edd460c893e15c43a3b192e60
aweng98 <bweng@broadinstitute.org>
2023-05-15 13:36:11 -0400

    run successfully

--- playwright-e2e/tests/dsm/pancan-mail-list-download.spec.ts
+++ playwright-e2e/tests/dsm/pancan-mail-list-download.spec.ts
@@ -6,19 +6,24 @@ import { Miscellaneous } from 'lib/component/dsm/navigation/enums/miscellaneousN
 import { Study } from 'lib/component/dsm/navigation/enums/selectStudyNav.enum';
 import { Navigation } from 'lib/component/dsm/navigation/navigation';
 import Modal from 'lib/component/modal';
+import { SortOrder } from 'lib/component/table';
+import MailingListPage, { COLUMN } from 'pages/dsm/mailing-list-page';
 import { WelcomePage } from 'pages/dsm/welcome-page';
+import { assertTableHeaders } from 'utils/assertion-helper';
+import { getDate, getMailingListDownloadedFileDate, mailingListCreatedDate } from 'utils/date-utils';
 import { generateEmailAlias, generateUserName } from 'utils/faker-utils';
 import lodash from 'lodash';
 
 import HomePage from 'pages/pancan/home-page';
+import { MailListCSV, readMailListCSVFile } from 'utils/file-utils';
 
-const RGP_USER_EMAIL = process.env.PANCAN_USER_EMAIL as string;
-const newEmail = generateEmailAlias(RGP_USER_EMAIL);
+const PANCAN_USER_EMAIL = process.env.PANCAN_USER_EMAIL as string;
+const newEmail = generateEmailAlias(PANCAN_USER_EMAIL);
 const firstName = generateUserName('PANCAN');
 const lastName = generateUserName('PANCAN');
 
 test.describe.serial('Join Pancan Mailing List', () => {
-  test('Join Mail List @visual @pancan', async ({ page }) => {
+  test('Join Mail List @dss @visual @pancan', async ({ page }) => {
     const homePage = new HomePage(page);
     await homePage.waitForReady();
     await homePage.fillInStayInformedModal(firstName, lastName, newEmail);
@@ -30,27 +35,27 @@ test.describe.serial('Join Pancan Mailing List', () => {
     expect(await modal.toLocator().screenshot()).toMatchSnapshot('stay-informed-modal.png');
   });
 
-  test('DSM download Mail List @visual @dsm @rgp', async ({ page }) => {
+  test('DSM download Mail List @dsm @pancan', async ({ page }) => {
     await login(page);
 
     const welcomePage = new WelcomePage(page);
-    await welcomePage.selectStudy(Study.RGP);
+    await welcomePage.selectStudy(Study.PANCAN);
 
     const navigation = new Navigation(page);
     const [mailListResponse] = await Promise.all([
-      page.waitForResponse(response => response.url().includes('/ui/mailingList/RGP') && response.status() === 200),
+      page.waitForResponse(response => response.url().includes('/ui/mailingList/PanCan') && response.status() === 200),
       navigation.selectMiscellaneous(Miscellaneous.MAILING_LIST)
     ]);
     const respJson: MailListCSV[] = JSON.parse(await mailListResponse.text());
     expect(respJson.length).toBeGreaterThan(1); // response should contains at least one emails
 
-    const mailingListPage = new MailingListPage(page, APP.RGP);
+    const mailingListPage = new MailingListPage(page, APP.PANCAN);
     await mailingListPage.waitForReady();
 
     // Downloading mailing list
     const download = await mailingListPage.download();
     const actualFileName = download.suggestedFilename();
-    const expectedFileName = `MailingList ${APP.RGP} ${getMailingListDownloadedFileDate()}.csv`;
+    const expectedFileName = `MailingList ${APP.PANCAN} ${getMailingListDownloadedFileDate()}.csv`;
     expect(actualFileName).toBe(expectedFileName);
 
     const file = await download.path();
@@ -74,20 +79,23 @@ test.describe.serial('Join Pancan Mailing List', () => {
 
     // Verify headers
     const table = await mailingListPage.getMailingListTable();
-    const orderedHeaders = [COLUMN.EMAIL, COLUMN.DATE];
+    const orderedHeaders = [COLUMN.FIRST_NAME, COLUMN.LAST_NAME, COLUMN.EMAIL, '', COLUMN.DATE];
     const actualHeaders = await table.getHeaderNames();
     assertTableHeaders(actualHeaders, orderedHeaders);
 
-    // Verify new RGP participant email is found inside table
-    await table.sort(COLUMN.DATE, SortOrder.DESC); // Sorting to get newest record to display first
-    let tCell = await table.findCell(COLUMN.EMAIL, newEmail, COLUMN.EMAIL);
-    await expect(tCell, `Matching email ${newEmail} in Mailing List table`).toBeTruthy();
+    // Verify new Pancan participant email is found inside table.
+    // To handle any delay, retry blocks of code until email is found successfully.
+    await expect(async () => {
+      await table.sort(COLUMN.DATE, SortOrder.DESC); // Sorting to get newest record to display first
+      const cell = await table.findCell(COLUMN.EMAIL, newEmail, COLUMN.EMAIL);
+      await expect(cell, `Matching email ${newEmail} in Mailing List table`).toBeTruthy();
+    }).toPass();
 
     // Verify date signed up is found inside table
-    tCell = await table.findCell(COLUMN.EMAIL, newEmail, COLUMN.DATE, { exactMatch: false });
+    const tCell = await table.findCell(COLUMN.EMAIL, newEmail, COLUMN.DATE, { exactMatch: false });
     await expect(tCell, `Find column ${COLUMN.DATE} in Mailing List table`).toBeTruthy();
 
-    // Retrieve new RGP user Date Signed Up from API response body, compare with what's displayed in table
+    // Retrieve new Pancan user Date Signed Up from API response body, compare with what's displayed in table
     const user: MailListCSV[] = lodash.filter(respJson, row => row.email === newEmail);
     const dateInJson = mailingListCreatedDate(new Date(parseInt(user[0].dateCreated) * 1000));
     const dateInTCell = mailingListCreatedDate(new Date(await tCell!.innerText()));
