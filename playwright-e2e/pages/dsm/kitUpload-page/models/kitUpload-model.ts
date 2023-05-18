5301ecb3ff8fba9cb125948a401020c465eff206
GiCharkviani <gcharkvi@broadinstitute.org>
2023-05-18 18:17:42 +0400

    DSM-PW-PEPPER-777--Kit-Upload_Send-and-Receive-a-Clinical-Saliva-Kit-LMS removed street 2 address

--- playwright-e2e/pages/dsm/kitUpload-page/models/kitUpload-model.ts
+++ playwright-e2e/pages/dsm/kitUpload-page/models/kitUpload-model.ts
@@ -3,7 +3,7 @@ export class KitUploadInfo {
               public readonly firstName: string,
               public readonly lastName: string,
               public street1: string = '75 AMES STREET',
-              public street2: string = 'Broadway',
+              public street2: string = '',
               public city: string = 'CAMBRIDGE',
               public postalCode: string = '02142',
               public state: string = 'MA',
