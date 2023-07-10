00af78a2cca38259f8532cf12e9963978e69543d
aweng98 <bweng@broadinstitute.org>
2023-07-10 16:35:43 -0400

    fix bug in dsm-fixture.ts

--- playwright-e2e/fixtures/dsm-fixture.ts
+++ playwright-e2e/fixtures/dsm-fixture.ts
@@ -1,8 +1,11 @@
 import { Fixtures } from '@playwright/test';
+import { login } from 'authentication/auth-dsm';
 import { fixtureBase as base } from 'fixtures/fixture-base';
 
+// Use this fixture for login in DSM tests
 const fixture = base.extend<Fixtures>({
   page: async ({ page }, use) => {
+    await login(page);
     await use(page);
   }
 });
