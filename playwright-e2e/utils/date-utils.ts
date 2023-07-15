5d3d6e6d711c9a41d73fd6efc1abc308b7492621
aweng98 <bweng@broadinstitute.org>
2023-07-14 23:28:34 -0400

    timezone

--- playwright-e2e/utils/date-utils.ts
+++ playwright-e2e/utils/date-utils.ts
@@ -2,7 +2,8 @@ export function dateFormat(): Intl.DateTimeFormat {
   return new Intl.DateTimeFormat('en-US', {
     month: '2-digit',
     day: '2-digit',
-    year: 'numeric'
+    year: 'numeric',
+    timeZone: 'America/New_York'
   })
 }
 
