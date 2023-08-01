4a1fcd8f978153043bd0f7cab7d971038b3160b5
GiCharkviani <gcharkvi@broadinstitute.org>
2023-08-01 16:42:17 +0400

    PW-DSS-PEPPER-1003-enrollment removed unused

--- playwright-e2e/dss/pages/mbc/mbc-patient-type.ts
+++ playwright-e2e/dss/pages/mbc/mbc-patient-type.ts
@@ -18,11 +18,9 @@ export const MBCPatientsData: any = {
   patient: {
     whoIsSigningUp: 'I have been diagnosed with metastatic breast cancer (also known as advanced or stage IV breast cancer). ' +
       "I'm willing to answer additional questions about myself and my experience with metastatic breast cancer.",
-    researchContentForm: 'Research Consent Form',
   },
   other: {
     whoIsSigningUp: "I haven't been diagnosed with metastatic breast cancer, but I want to stay informed about the" +
       ' Metastatic Breast Cancer Project by joining the email list.',
-    researchContentForm: 'Research Consent Form',
   }
 };
