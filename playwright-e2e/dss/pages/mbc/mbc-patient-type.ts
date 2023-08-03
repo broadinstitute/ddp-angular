bc88bbcbe12d570dfebf509cf666e0dc70dc015e
GiCharkviani <gcharkvi@broadinstitute.org>
2023-08-03 13:41:12 +0400

    PW-DSS-PEPPER-1003-enrollment removed unused things

--- playwright-e2e/dss/pages/mbc/mbc-patient-type.ts
+++ playwright-e2e/dss/pages/mbc/mbc-patient-type.ts
@@ -1,19 +1,3 @@
-import { TestID } from 'dss/pages/patient-type';
-
-export interface PatientData {
-  whoIsSigningUp: string;
-  researchContentForm: string;
-  additionalConsentForm: string;
-  surveyForm: string;
-  testId: Partial<TestID>;
-}
-
-export type TypePatient = 'patient' | 'other';
-
-export type Patient = {
-  [index in TypePatient]: PatientData;
-};
-
 export const MBCPatientsData: any = {
   patient: {
     whoIsSigningUp: 'I have been diagnosed with metastatic breast cancer (also known as advanced or stage IV breast ' +
