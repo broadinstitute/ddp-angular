56622952f3972f69f2d9faf5d6c1375767cdcd32
aweng98 <bweng@broadinstitute.org>
2023-08-22 14:11:52 -0400

    more changes

--- ddp-workspace/projects/ddp-pancan/src/app/components/footer/footer.component.ts
+++ ddp-workspace/projects/ddp-pancan/src/app/components/footer/footer.component.ts
@@ -1,6 +1,9 @@
-import { Component, Input } from '@angular/core';
+import { Component, Inject, Input } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
 import { WindowRef } from 'ddp-sdk';
 import { BaseFooterComponent } from './base-footer/base-footer.component';
+import { JoinMailingListComponent, ToolkitConfigurationService } from 'toolkit';
+import { JOIN_MAILING_LIST_DIALOG_SETTINGS } from '../../utils/join-mailing-list-dialog-confg';
 
 @Component({
     selector: 'app-footer',
@@ -8,14 +11,22 @@ import { BaseFooterComponent } from './base-footer/base-footer.component';
     styleUrls: ['./footer.component.scss']
 })
 export class FooterComponent extends BaseFooterComponent {
+    @Input() isPediHCCTheme: boolean;
     @Input() phone: string;
     @Input() email: string;
 
-    constructor(private _windowRef: WindowRef) {
+    constructor(
+        private _windowRef: WindowRef,
+        private dialog: MatDialog,
+        @Inject('toolkit.toolkitConfig') private toolkitConfiguration: ToolkitConfigurationService) {
         super(_windowRef);
     }
 
-    public get currentYear(): number {
-        return new Date().getFullYear();
+    public openJoinMailingList(): void {
+        const info = 'Pedihcc';
+        this.dialog.open(JoinMailingListComponent, {
+            ...JOIN_MAILING_LIST_DIALOG_SETTINGS,
+            data: { info },
+        });
     }
 }
