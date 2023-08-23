df0dc6d71c82549f0845f053fc8f2a9ba1877b45
GiCharkviani <gcharkvi@broadinstitute.org>
2023-08-23 09:46:00 +0200

    pepper-954 adjusted styles (alex alex alex :D )

--- ddp-workspace/projects/ddp-pancan/src/app/components/auth/auth.component.ts
+++ ddp-workspace/projects/ddp-pancan/src/app/components/auth/auth.component.ts
@@ -1,7 +1,7 @@
 import { Component, Input } from '@angular/core';
 import { SessionMementoService } from 'ddp-sdk';
 import { AppRoutes } from '../app-routes';
-import {ThemePalette} from '@angular/material/core';
+import {ThemePalette} from "@angular/material/core";
 
 @Component({
     selector: 'app-auth',
@@ -14,8 +14,7 @@ import {ThemePalette} from '@angular/material/core';
            [class.dashboard-button]="isAuthenticated"
            [routerLink]="isAuthenticated ? AppRoutes.Dashboard : AppRoutes.CountMeIn"
            queryParamsHandling="merge"
-           [color]="joinButtonColor"
-           [ngClass]="{'pedihccBtnColor': isPediHCCTheme && !isInFooter}">
+           [color]="joinButtonColor">
             <ng-container *ngIf="isAuthenticated; else join">
                 <mat-icon>perm_identity</mat-icon>
                 <span translate>App.Navigation.Dashboard</span>
@@ -25,9 +24,9 @@ import {ThemePalette} from '@angular/material/core';
     styleUrls: ['./auth.component.scss']
 })
 export class AuthComponent {
-    @Input() isColorectalTheme = false;
-    @Input() isPediHCCTheme = false;
-    @Input() isInFooter = false;
+    @Input() isColorectalTheme: boolean = false;
+    @Input() isPediHCCTheme: boolean = false;
+    @Input() isInFooter: boolean = false;
 
     readonly AppRoutes = AppRoutes;
 
@@ -41,9 +40,11 @@ export class AuthComponent {
         let color = 'primary';
 
         if (this.isColorectalTheme)
-            {color = 'colorectal';}
+            color = 'colorectal';
+        if(this.isPediHCCTheme)
+            color = 'pedihcc';
         if(this.isPediHCCTheme && this.isInFooter)
-            {color = 'secondary';}
+            color = 'secondary';
 
         return color as ThemePalette;
     }
