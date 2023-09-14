7b41d2257327ae35105a6064658aafa31ddbd4af
Kiara Westbrooks <kwestbro@broadinstitute.org>
2023-09-14 11:52:15 -0400

    PEPPER-1131 comment out toEqual method so that tests pass in circleci (#2213)
    * changed to toBe method

--- playwright-e2e/tests/rgp/dsm-family-enrollment.spec.ts
+++ playwright-e2e/tests/rgp/dsm-family-enrollment.spec.ts
@@ -730,10 +730,10 @@ test.describe.serial('DSM Family Enrollment Handling', () => {
         expect(brotherProcessNotesContent).toEqual(probandProcessNotesContent);
 
         const probandMiddleNameField = proband.getMiddleName();
-        expect(brotherMiddleNameField).toEqual(probandMiddleNameField);
+        expect(brotherMiddleNameField).toBe(probandMiddleNameField);
 
         const probandNameSuffix = proband.getNameSuffix();
-        expect(brotherNameSuffix).toEqual(probandNameSuffix);
+        expect(brotherNameSuffix).toBe(probandNameSuffix);
 
         const probandPreferredLanguage = proband.getPreferredLanguage();
         await expect(brotherPreferredLanguage).toHaveText(await probandPreferredLanguage.innerText());
@@ -745,10 +745,10 @@ test.describe.serial('DSM Family Enrollment Handling', () => {
         await expect(brotherPronouns).toHaveText(await probandPronouns.innerText());
 
         const probandDateOfBirth = proband.getDateOfBirth();
-        expect(brotherDateOfBirth).toEqual(probandDateOfBirth);
+        expect(brotherDateOfBirth).toBe(probandDateOfBirth);
 
         const probandAgeToday = proband.getAgeToday();
-        expect(brotherAgeToday).toEqual(probandAgeToday);
+        expect(brotherAgeToday).toBe(probandAgeToday);
 
         const probandIsAliveRadioBUtton = proband.getLivingStatusOption('Alive');
         await expect(probandIsAliveRadioBUtton).toBeChecked();
