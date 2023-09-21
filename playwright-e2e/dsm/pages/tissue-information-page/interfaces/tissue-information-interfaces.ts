5abdecfb0a900e3afe97fbec835747fff664209a
GiCharkviani <gcharkvi@broadinstitute.org>
2023-09-21 10:26:32 +0200

    PW-DSM-Tissue-request-flow rename and remove unused

--- playwright-e2e/dsm/pages/tissue-information-page/interfaces/tissue-information-interfaces.ts
+++ playwright-e2e/dsm/pages/tissue-information-page/interfaces/tissue-information-interfaces.ts
@@ -1,5 +1,4 @@
 import {OncHistoryDateInput} from 'dsm/component/tabs/interfaces/onc-history-inputs-types';
-import {TissueTypesEnum} from 'dsm/pages/tissue-information-page/enums/tissue-information-enum';
 import {InputTypeEnum} from 'dsm/component/tabs/enums/onc-history-input-columns-enum';
 
 export interface FillDate {
