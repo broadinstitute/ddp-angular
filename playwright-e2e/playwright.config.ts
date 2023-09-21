f7ff5c4fbdc649edbce031340f5b71c50c930851
aweng98 <bweng@broadinstitute.org>
2023-09-21 15:40:58 -0400

    rm chromium proj

--- playwright-e2e/playwright.config.ts
+++ playwright-e2e/playwright.config.ts
@@ -96,11 +96,6 @@ const testConfig: PlaywrightTestConfig = {
   /* Configure projects for chromium browser */
   projects: [
     {
-      name: 'chromium',
-      testMatch: '**/*.spec.ts',
-      use: {}
-    },
-    {
       // Listing tests: npx playwright test --list --project="dsm"
       // Running tests serially: npx playwright test --project="kit" --workers=1
       name: 'dsm',
