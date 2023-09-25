72d2955ad56016b53376073b6470029c7a9e6625
Kiara Westbrooks <kiarawestbrooks@gmail.com>
2023-09-25 11:33:15 -0400

    moving waitForResponse to new location
    seems to help it pass more consistently

--- playwright-e2e/dsm/pages/tissue/tissue-information-page.ts
+++ playwright-e2e/dsm/pages/tissue/tissue-information-page.ts
@@ -198,9 +198,9 @@ export default class TissueInformationPage {
       const isModalBtnDisabled = await modalBtn.isDisabled();
       if (!isModalBtnDisabled) {
         await modalBtn.click();
-        await waitForResponse(this.page, { uri: 'institutions' });
 
         const successModalBtn = new Button(this.page, { root: this.page.locator('app-modal'), label: 'Ok', exactMatch: true });
+        await waitForResponse(this.page, { uri: 'institutions' });
         await successModalBtn.isVisible() && await successModalBtn.click();
       }
     }
