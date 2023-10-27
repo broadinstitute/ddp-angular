ecd7bb2f6cba4b92cdf848b52636b2f4d4571b61
Kiara Westbrooks <kiarawestbrooks@gmail.com>
2023-10-27 15:14:36 -0400

    circleci fix

--- playwright-e2e/dsm/component/tabs/enums/tab-enum.ts
+++ playwright-e2e/dsm/component/tabs/enums/tab-enum.ts
@@ -2,6 +2,7 @@ export enum TabEnum {
   CONTACT_INFORMATION = 'Contact Information',
   GENOME_STUDY = 'Genome Study',
   INVITAE = 'Invitae',
+  ONC_HISTORY = 'Onc History',
   SAMPLE_INFORMATION = 'Sample Information',
   SEQUENCING_ORDER = 'Sequencing Order',
   SURVEY_DATA = 'Survey Data',
