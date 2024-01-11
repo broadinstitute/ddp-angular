b0488067b9fe0c7a013cd2370e8fcf9879f495ea
aweng98 <bweng@broadinstitute.org>
2024-01-11 09:53:39 -0500

    playwright reading xlsx file

--- playwright-e2e/tests/dsm/participant-list/download-all-data.spec.ts
+++ playwright-e2e/tests/dsm/participant-list/download-all-data.spec.ts
@@ -1,16 +1,19 @@
 import { expect } from '@playwright/test';
 import { StudyEnum } from 'dsm/component/navigation/enums/selectStudyNav-enum';
 import ParticipantListPage from 'dsm/pages/participant-list-page';
+import { FileFormatEnum } from 'dsm/pages/participant-page/enums/download-format-enum';
 import { test } from 'fixtures/dsm-fixture';
 import fs from 'fs';
 import { assertParticipantListDownloadFileName } from 'utils/test-utils';
+import * as XLSX from 'xlsx';
+import path from 'path';
 
 test.describe.parallel('Participant List Download', () => {
   // Chose studies with fewer participants on Dev and Test. Otherwise download all data will take a very long time.
-  const studies = [StudyEnum.BRUGADA, StudyEnum.OSTEO];
+  const studies = [StudyEnum.OSTEO];
 
   for (const study of studies) {
-    test(`Select All in @dsm @${study}`, async ({ page, request }) => {
+    test(`Select All in @dsm @${study}`, async ({ page, request }, testInfo) => {
       test.slow();
 
       const participantListPage = await ParticipantListPage.goto(page, study, request);
@@ -19,14 +22,18 @@ test.describe.parallel('Participant List Download', () => {
       await participantListPage.selectAll();
 
       // Export as human-readable, Excel and include all completion of an activity
-      const download = await participantListPage.downloadParticipant();
+      const download = await participantListPage.downloadParticipant({ fileFormat: FileFormatEnum.XLSX });
       assertParticipantListDownloadFileName(download, study);
 
-      const downloadedFile = await download.path();
-      expect(downloadedFile).toBeTruthy();
-      expect((await fs.promises.stat(downloadedFile as string)).size).toBeGreaterThan(1000);
+      const logDir = testInfo.outputDir;
+      const fileName = download.suggestedFilename();
+      const downloadFile = path.join(logDir, fileName);
 
-      // TODO unzip and check xlsx contents
+      await download.saveAs(downloadFile);
+      // Verify Registration Date format is mm-dd-yyyy in Excel download file
+      const xlsxWorkbook = XLSX.readFile(downloadFile);
+      const firstSheet = xlsxWorkbook.Sheets[xlsxWorkbook.SheetNames[0]];
+      console.log(firstSheet);
     });
   }
 });
