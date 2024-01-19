eb50c6c44c7ed631d493e927f32562a6d108c2cc
aweng98 <bweng@broadinstitute.org>
2024-01-19 13:36:01 -0500

    timezone on circleci

---
+++ .circleci/set-est-timezone.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash -e
+
+rm /etc/timezone
+rm /etc/localtime
+
+TIMEZONE="America/New_York"      
+export TZ="$TIMEZONE"
+echo "$TIMEZONE" > /etc/timezone                     
+cp "/usr/share/zoneinfo/${TIMEZONE}" /etc/localtime 
+dpkg-reconfigure -f noninteractive tzdata
