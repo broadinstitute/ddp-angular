749e80e88675802c0a35dd6ddd260c72c15df352
Alex Weng <35533885+aweng98@users.noreply.github.com>
2024-02-06 17:56:06 -0500

    [DSS] PEPPER-1320 Osteo Home page gallery items do not update automatically when preferred language is changed (#2324)
    PEPPER-1320 Osteo Home page gallery lang translation

--- ddp-workspace/projects/ddp-osteo/src/app/components/gallery/gallery.component.ts
+++ ddp-workspace/projects/ddp-osteo/src/app/components/gallery/gallery.component.ts
@@ -1,27 +1,30 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, OnDestroy } from '@angular/core';
 import { GalleryItem } from '../../models/galleryItem.model';
-import { NGXTranslateService } from 'ddp-sdk';
-import { take } from 'rxjs/operators';
+import { CompositeDisposable, NGXTranslateService } from 'ddp-sdk';
 
 @Component({
   selector: 'app-gallery',
   templateUrl: './gallery.component.html',
   styleUrls: ['./gallery.component.scss']
 })
-export class GalleryComponent implements OnInit {
+export class GalleryComponent implements OnInit, OnDestroy {
   public gallery: Array<GalleryItem>;
   public selectedItem: GalleryItem;
   @Input() private translateSource = 'BehindScenesGallery.Items';
 
+  private anchor = new CompositeDisposable();
+
   constructor(private translate: NGXTranslateService) { }
 
   public ngOnInit(): void {
-    this.translate.getTranslationObject(this.translateSource).pipe(
-      take(1)
-    ).subscribe((gallery: Array<GalleryItem>) => {
-      this.gallery = gallery;
-      this.selectedItem = gallery[0];
-    });
+    if(this.translate) {
+      const translate$ = this.translate.getTranslationObject(this.translateSource)
+        .subscribe((gallery: Array<GalleryItem>) => {
+            this.gallery = gallery;
+            this.selectedItem = gallery[0];
+        });
+      this.anchor.addNew(translate$);
+    }
   }
 
   public isItemSelected(index: number): boolean {
@@ -37,5 +40,9 @@ export class GalleryComponent implements OnInit {
     const item = this.gallery[this.selectedItem.id - 1];
     const length = this.gallery.length;
     this.selectedItem = item !== undefined ? item : this.gallery[length - 1];
+  }
+
+  public ngOnDestroy(): void {
+    this.anchor.removeAll();
   }
 }
