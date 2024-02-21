990a8362ef7e869fb97bac27f309a35a4d53c0fd
Kiara Westbrooks <kwestbro@broadinstitute.org>
2024-02-21 15:31:44 -0500

    Playwright_Fix PW Tests for DSM RC (#2334)
    * fixing up failed playwright tests

--- playwright-e2e/dsm/pages/participant-list-page.ts
+++ playwright-e2e/dsm/pages/participant-list-page.ts
@@ -293,8 +293,11 @@ export default class ParticipantListPage extends DsmPageBase {
           if (participantData === undefined) {
             continue; //Participant does not have a proband tab for some reason, skip them
           }
-          unformattedFirstName = JSON.stringify(value.esData.profile.firstName);
-          firstName = unformattedFirstName.replace(/['"]+/g, ''); //Replace double quotes from JSON.stringify
+          firstName = value.esData.profile?.firstName;
+          if (!firstName) {
+            continue;
+          }
+          unformattedFirstName = JSON.stringify(firstName);
           if (participantData && (firstName === testParticipantFirstName)) {
             foundShortID = JSON.stringify(value.esData.profile.hruid).replace(/['"]+/g, '');
             logInfo(`Found the RGP participant ${foundShortID} to have a proband tab`);
