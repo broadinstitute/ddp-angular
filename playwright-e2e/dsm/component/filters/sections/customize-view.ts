e5b6f1f2ef8308287eb5a2e235dfcb57760c206b
Kiara Westbrooks <kiarawestbrooks@gmail.com>
2024-03-07 06:17:26 -0500

    WIP

--- playwright-e2e/dsm/component/filters/sections/customize-view.ts
+++ playwright-e2e/dsm/component/filters/sections/customize-view.ts
@@ -1,5 +1,8 @@
 import { Locator, Page, expect } from '@playwright/test';
+import { StudyName } from 'dsm/navigation';
 import Checkbox from 'dss/component/checkbox';
+import { CustomizeView as ColumnGroup } from 'dsm/enums';
+import { COLUMN } from 'dsm/pages/mailing-list-page';
 
 export class CustomizeView {
   private activeColumnsGroup = '';
@@ -76,8 +79,9 @@ export class CustomizeView {
     }
   }
 
-  public async isColumnVisible(columns: string[]): Promise<boolean> {
+  public async isColumnVisible(columns: string[] | ColumnGroup[]): Promise<boolean> {
     for (const column of columns) {
+      console.log(`Checking for column group: ${column}`);
       const customizeViewColumn = this.page.getByRole('button', { name: column});
       try {
         await expect(customizeViewColumn.first()).toBeVisible({timeout: 5000});
@@ -86,6 +90,87 @@ export class CustomizeView {
       }
     }
     return true;
+  }
+
+  public async assertStudyColumnsDisplayedFor(opts: { studyName: StudyName }): Promise<void> {
+    const { studyName } = opts;
+    const studyColumnGroups: ColumnGroup[] = [];
+
+    //Put in columns that most studies have
+    if (this.isCMIStudy(studyName)) {
+      //CMI studies usually have all the below columns
+      studyColumnGroups.push(ColumnGroup.PARTICIPANT);
+      studyColumnGroups.push(ColumnGroup.PARTICIPANT_DSM);
+      studyColumnGroups.push(ColumnGroup.MEDICAL_RECORD);
+      studyColumnGroups.push(ColumnGroup.ONC_HISTORY);
+      studyColumnGroups.push(ColumnGroup.TISSUE);
+      studyColumnGroups.push(ColumnGroup.SAMPLE);
+      studyColumnGroups.push(ColumnGroup.COHORT_TAGS);
+    } else {
+      //These columns appear to be in all studies (that have a participant list) in DSM
+      studyColumnGroups.push(ColumnGroup.PARTICIPANT);
+      studyColumnGroups.push(ColumnGroup.PARTICIPANT_DSM);
+      studyColumnGroups.push(ColumnGroup.COHORT_TAGS);
+    }
+
+    //Get all of the column groups set for a study
+    switch (studyName) {
+      case StudyName.OSTEO2:
+        studyColumnGroups.push(ColumnGroup.CLINICAL_ORDERS);
+        studyColumnGroups.push(ColumnGroup.SURVEY_YOUR_CHILDS_OSTEOSARCOMA);
+        studyColumnGroups.push(ColumnGroup.ADDITIONAL_ASSENT_LEARNING_CHILD_DNA_WITH_INVITAE);
+        studyColumnGroups.push(ColumnGroup.BIOLOGICAL_PARENT_FEMALE);
+        studyColumnGroups.push(ColumnGroup.RESEARCH_CONSENT_FORM);
+        studyColumnGroups.push(ColumnGroup.LOVED_ONE_SURVEY);
+        studyColumnGroups.push(ColumnGroup.RESEARCH_CONSENT_ASSENT_FORM);
+        studyColumnGroups.push(ColumnGroup.BIOLOGICAL_PARENT_MALE);
+        studyColumnGroups.push(ColumnGroup.WHAT_WE_LEARNED_FROM_SOMATIC_DNA);
+        studyColumnGroups.push(ColumnGroup.HALF_SIBLING);
+        studyColumnGroups.push(ColumnGroup.ADDITIONAL_CONSENT_LEARNING_DNA_WITH_INVITAE);
+        studyColumnGroups.push(ColumnGroup.PROVIDE_CONTACT_INFORMATION);
+        studyColumnGroups.push(ColumnGroup.CHILD);
+        studyColumnGroups.push(ColumnGroup.MEDICAL_RELEASE_FORM);
+        studyColumnGroups.push(ColumnGroup.ADD_CHILD_PARTICIPANT);
+        studyColumnGroups.push(ColumnGroup.SURVEY_FAMILY_HISTORY_OF_CANCER);
+        studyColumnGroups.push(ColumnGroup.PREQUALIFIER_SURVEY);
+        studyColumnGroups.push(ColumnGroup.SURVEY_YOUR_CHILDS_OR_YOUR_OSTEOSARCOMA);
+        studyColumnGroups.push(ColumnGroup.GRANDPARENT);
+        studyColumnGroups.push(ColumnGroup.ADDITIONAL_CONSENT_ASSENT_LEARNING_ABOUT_CHILD_TUMOR);
+        studyColumnGroups.push(ColumnGroup.SIBLING);
+        studyColumnGroups.push(ColumnGroup.PARENT_SIBLING);
+        studyColumnGroups.push(ColumnGroup.ADDITIONAL_CONSENT_LEARNING_ABOUT_TUMOR);
+        studyColumnGroups.push(ColumnGroup.SURVEY_ABOUT_YOU_OR_YOUR_CHILD);
+        studyColumnGroups.push(ColumnGroup.INVITAE);
+        studyColumnGroups.push(ColumnGroup.PROXY);
+        studyColumnGroups.push(ColumnGroup.INVITATION);
+        studyColumnGroups.push(ColumnGroup.CONTACT_INFORMATION);
+        break;
+      default:
+        throw new Error(`Expected column group names for ${studyName} are not setup/known`);
+        break;
+    }
+    await this.isColumnVisible(studyColumnGroups);
+  }
+
+  private isCMIStudy(studyName: StudyName): boolean {
+    let isCMI;
+    switch (studyName) {
+      case StudyName.ANGIO:
+      case StudyName.BRAIN:
+      case StudyName.ESC:
+      case StudyName.LMS:
+      case StudyName.MBC:
+      case StudyName.OSTEO:
+      case StudyName.OSTEO2:
+      case StudyName.PANCAN:
+      case StudyName.PROSTATE:
+        isCMI = true;
+        break;
+      default:
+        isCMI = false;
+        break;
+    }
+    return isCMI;
   }
 
   private async isChecked(locator: Locator | undefined): Promise<boolean> {
--- playwright-e2e/tests/dsm/osteo/os2-display-verification.spec.ts
+++ playwright-e2e/tests/dsm/osteo/os2-display-verification.spec.ts
@@ -6,7 +6,6 @@ import { test } from 'fixtures/dsm-fixture';
 import { mailingListCreatedDate } from 'utils/date-utils';
 
 test.describe.serial(`OS2 Verify expected display of participant information`, () => {
-  const adultPrequalifier = `I have been diagnosed with osteosarcoma and I'm signing myself up`;
   const savedFilterName = `Playwright PE-CGS filter for OS2 - created on ${mailingListCreatedDate(new Date())}`;
   const participantsWithReturnOfResultsFilter = [
     Label.PARTICIPANT_LIST_CHECKBOX_HEADER,
@@ -14,7 +13,6 @@ test.describe.serial(`OS2 Verify expected display of participant information`, (
     Label.CLINICAL_ORDER_PDO_NUMBER,
     Label.CONSENT_TISSUE,
     Label.SOMATIC_CONSENT_TUMOR,
-    Label.PREQUAL_SELF_DESCRIBE,
     Label.SOMATIC_RESULTS_SURVEY_CREATED
   ];
   let navigation;
@@ -33,7 +31,6 @@ test.describe.serial(`OS2 Verify expected display of participant information`, (
     await customizeViewPanel.selectColumns(CustomizeView.CLINICAL_ORDERS, [Label.CLINICAL_ORDER_PDO_NUMBER]);
     await customizeViewPanel.selectColumns(CustomizeView.RESEARCH_CONSENT_FORM, [Label.CONSENT_TISSUE], { nth: 0 }); //adult's consent
     await customizeViewPanel.selectColumns(CustomizeView.ADDITIONAL_CONSENT_LEARNING_ABOUT_TUMOR, [Label.SOMATIC_CONSENT_TUMOR]);
-    await customizeViewPanel.selectColumns(CustomizeView.PREQUALIFIER_SURVEY, [Label.PREQUAL_SELF_DESCRIBE]);
     await customizeViewPanel.selectColumns(CustomizeView.WHAT_WE_LEARNED_FROM_SOMATIC_DNA, [Label.SOMATIC_RESULTS_SURVEY_CREATED]);
     await customizeViewPanel.close();
 
@@ -41,16 +38,19 @@ test.describe.serial(`OS2 Verify expected display of participant information`, (
     await searchPanel.open();
     await searchPanel.checkboxes(Label.STATUS, { checkboxValues: [DataFilter.ENROLLED]});
     await searchPanel.text(Label.COHORT_TAG_NAME, { textValue: StudyName.OSTEO2, exactMatch: true }); //Exact match needs to be true else nothing is returned
-    await searchPanel.checkboxes(Label.PREQUAL_SELF_DESCRIBE, { checkboxValues: [adultPrequalifier] });
+    await searchPanel.dates(Label.SOMATIC_RESULTS_SURVEY_CREATED, { additionalFilters: [DataFilter.NOT_EMPTY] });
     await searchPanel.search();
 
     await participantListPage.saveCurrentView(savedFilterName); //modify to throw an error if name is not unique
 
     await participantListPage.reloadWithDefaultFilter();
+    const participantListTable = participantListPage.participantListTable;
+    //Check that the default CMI filter is now present after using Reload with Default Filters
+    await participantListTable.assertDisplayedHeaders({ checkDefaultFilterOfStudy: true, studyName: StudyName.OSTEO2 });
 
     const savedFilterSection = participantListPage.savedFilters;
     await savedFilterSection.open(savedFilterName);
-    //await savedFilterSection.delete(savedFilterName); //delete late when adding test that need the saved filter
+    await savedFilterSection.exists(savedFilterName); //check that the filter was saved
   })
 
   test(`OS2: Verify participant list webelements`, async ({ page, request }) => {
@@ -69,6 +69,11 @@ test.describe.serial(`OS2 Verify expected display of participant information`, (
     await savedFilterSection.open(savedFilterName);
     await participantListTable.assertDisplayedHeaders({ customFilter: participantsWithReturnOfResultsFilter });
     await savedFilterSection.delete(savedFilterName);
+
+    //Verify that all Customize View column groups are displayed
+    const customizeViewPanel = participantListPage.filters.customizeViewPanel;
+    await customizeViewPanel.open();
+    await customizeViewPanel.assertStudyColumnsDisplayedFor({ studyName: StudyName.OSTEO2 });
   })
 
   test.skip(`OS2: Verify participant page webelements`, async ({ page, request }) => {
