97cafcd62679d4006331f0b8b8d074802564b46e
Sampath K. Settipalli <sampath@broadinstitute.org>
2024-12-06 18:40:51 -0600

    PEPPER-1561 . Enable google analytics for lms

--- ddp-workspace/projects/ddp-lms/src/app/app.module.ts
+++ ddp-workspace/projects/ddp-lms/src/app/app.module.ts
@@ -202,8 +202,9 @@ export class AppModule {
       if (event instanceof NavigationEnd) {
         if (event.url !== 'undefined' &&
           (event.url.includes('about') || event.url.includes('faq') || event.url.includes('how-to-participate') ||
-            event.url.includes('scientific-impact') || event.url.includes('for-your-physician'))) {
-          console.log('Emitting navigation event: {} from AppMod to TAG: {}', event.url, sdkConfig.projectGAToken);
+            event.url.includes('scientific-impact') || event.url.includes('for-your-physician') ||
+            event.url.includes('count-me-in'))) {
+          //console.log('Emitting navigation event: {} from AppMod to TAG: {}', event.url, sdkConfig.projectGAToken);
           gtag('config', sdkConfig.projectGAToken, {
             page_path: event.url
           });
