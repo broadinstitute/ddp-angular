6a5c3f47461769fe0c9cf4e33d689a46d21f402b
Sampath K. Settipalli <sampath@broadinstitute.org>
2024-12-11 14:50:54 -0600

    PEPPER-1561 . check for non login/active session before invoking GA

--- ddp-workspace/projects/ddp-lms/src/app/app.module.ts
+++ ddp-workspace/projects/ddp-lms/src/app/app.module.ts
@@ -204,7 +204,7 @@ export class AppModule {
     this.renderer = rendererFactory.createRenderer(null, null);
     this.router.events.subscribe(event => {
       if (event instanceof NavigationEnd) {
-        if (event.url !== 'undefined' &&
+        if (localStorage.getItem('session_key') == null && event.url !== 'undefined' &&
           (event.url.includes('about') || event.url.includes('faq') || event.url.includes('how-to-participate') ||
             event.url.includes('scientific-impact') || event.url.includes('for-your-physician') ||
             event.url.includes('count-me-in'))) {
