58dfa67783153a087aae95b151984be4454ced99
Vince Rose <vincent@sambasci.com>
2025-08-11 12:14:27 -0400

    Updated end-enroll component and removed join and login nav items

---
+++ ddp-workspace/projects/ddp-pancan/src/app/components/end-enroll/end-enroll.component.ts
@@ -0,0 +1,60 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { AppRoutes } from '../app-routes';
+import { OnInit, ViewEncapsulation} from '@angular/core';
+import { 
+    AnalyticsEventActions, 
+    AnalyticsEventCategories, 
+    AnalyticsEventsService,
+    BrowserContentService,
+    WindowRef
+} from 'ddp-sdk';
+
+@Component({
+    selector: 'app-end-enroll',
+    templateUrl: './end-enroll.component.html',
+    styleUrls: ['./end-enroll.component.scss'],
+    encapsulation: ViewEncapsulation.None,
+    changeDetection: ChangeDetectionStrategy.OnPush 
+})
+export class EndEnrollComponent implements OnInit{
+    public unsupportedBrowser: boolean;
+    readonly AppRoutes = AppRoutes;
+    private readonly HEADER_HEIGHT: number = 70;
+
+    constructor(
+        private windowRef: WindowRef,
+        private analytics: AnalyticsEventsService,
+        private browserContent: BrowserContentService) { }
+
+    public ngOnInit(): void {
+        this.unsupportedBrowser = this.browserContent.unsupportedBrowser();
+    }
+    public sendCountMeInAnalytics(): void {
+        this.analytics.emitCustomEvent(AnalyticsEventCategories.ClickedCountMeIn, AnalyticsEventActions.FromMainPage);
+    }
+    public get showArrow(): boolean {
+        return this.windowRef.nativeWindow.pageYOffset <= this.HEADER_HEIGHT;
+    }
+
+    public scrollTo(target: HTMLElement): void {
+        if (this.isIE) {
+            this.simpleScrolling(target);
+        } else {
+            this.smoothScrolling(target);
+        }
+    }
+
+    private simpleScrolling(target: HTMLElement): void {
+        this.windowRef.nativeWindow.scrollTo(0, target.offsetTop);
+    }
+
+    private smoothScrolling(target: HTMLElement): void {
+        this.windowRef.nativeWindow.scrollTo({
+            top: target.offsetTop,
+            behavior: 'smooth'
+        });
+    }
+     private get isIE(): boolean {
+        return this.browserContent.unsupportedBrowser();
+    }
+}
