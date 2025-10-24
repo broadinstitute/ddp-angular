061a3afeddfac0aa844d1f749ee404e470e777bd
arko <=>
2025-10-24 16:56:29 +0200

    Refactor header component to hide navigation and ‘Count Me In’ button

--- ddp-workspace/projects/ddp-lms/src/app/components/nav/loginOut/loginOut.component.ts
+++ ddp-workspace/projects/ddp-lms/src/app/components/nav/loginOut/loginOut.component.ts
@@ -16,7 +16,7 @@ import { Route } from '../../../constants/Route';
         <ddp-sign-in-out></ddp-sign-in-out>
       </li>
 
-      <li>
+      <li *ngIf="headerConfig.showCmiButton">
         <a class="button button_primary cmiBtn" *ngIf="!isAuthenticated" [routerLink]="Route.CountMeIn">
           {{ 'Header.Links.CountMeIn' | translate }}
         </a>
