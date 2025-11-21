831ed4feaef8db9c66aeeb6242da3b719b1a08a5
Andrew Zimmer <andrew@broadinstitute.org>
2025-11-21 14:44:43 -0500

    linter

--- ddp-workspace/projects/ddp-osteo/src/app/components/end-enroll/end-enroll.component.ts
+++ ddp-workspace/projects/ddp-osteo/src/app/components/end-enroll/end-enroll.component.ts
@@ -3,7 +3,7 @@ import {WindowRef, AnalyticsEventsService, AnalyticsEventCategories, AnalyticsEv
 import { HeaderConfigurationService, CommunicationService, ToolkitConfigurationService } from 'toolkit';
 
 @Component({
-    selector: 'end-enroll',
+    selector: 'app-end-enroll',
     templateUrl: './end-enroll.component.html',
     styleUrls: ['./end-enroll.component.scss'],
 })
